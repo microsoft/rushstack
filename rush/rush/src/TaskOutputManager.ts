91c9533215808bafc8adf7041c525b1b300a8d17
nickpape-msft <nickpape-msft@users.noreply.github.com>
2016-02-23 15:00:29 -0800

    Introducing TaskOutputManager class, which provides an interface for handling tasks which want to write to the console while running in parallel

---
+++ rush/rush/src/TaskOutputManager.ts
@@ -0,0 +1,81 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+interface ITaskState {
+  completed: boolean;
+  stdout: string[];
+}
+
+export interface ITaskWriter {
+  write: (data: string) => void;
+  writeLine: (data: string) => void;
+}
+
+export default class TaskOutputManager {
+  private _tasks: { [taskName: string]: ITaskState } = {};
+  private _activeTask: string = undefined;
+
+  public registerTask(taskName: string): ITaskWriter {
+    if (taskName in this._tasks) {
+      throw new Error('A task with that name has already been registered');
+    }
+
+    this._tasks[taskName] = {
+      completed: false,
+      stdout: []
+    };
+
+    if (!this._activeTask) {
+      this._activeTask = taskName;
+    }
+
+    console.log(`>>> Task [${taskName}] registered`);
+
+    const write = (data: string) => {
+      this.writeTaskOutput(taskName, data);
+    };
+    return {
+      write: write,
+      writeLine: (data: string) => write(data + '\n')
+    };
+  }
+
+  public writeTaskOutput(taskName: string, data: string) {
+    if (this._activeTask === taskName) {
+      process.stdout.write(data);
+    } else {
+      const taskState = this._tasks[taskName];
+      if (!taskState || taskState.completed) {
+        throw new Error('The task is not registered or has been completed and written.');
+      }
+      taskState.stdout.push(data);
+    }
+  }
+
+  public completeTask(taskName: string) {
+    const taskState = this._tasks[taskName];
+    if (!taskState || taskState.completed) {
+      throw new Error('The task is not registered or has been completed and written.');
+    }
+
+    if (!this._activeTask) {
+      process.stdout.write(taskState.stdout.join(''));
+    } else if (taskName === this._activeTask) {
+      this._tasks[taskName] = this._activeTask = undefined;
+      this._writeAllCompletedTasks();
+    } else {
+      taskState.completed = true;
+    }
+  }
+
+  private _writeAllCompletedTasks() {
+    /* tslint:disable */
+    for (let otherTaskName in this._tasks) {
+    /* tslint:enable */
+      const otherTaskState = this._tasks[otherTaskName];
+      if (otherTaskState && otherTaskState.completed) {
+        process.stdout.write(otherTaskState.stdout.join(''));
+        this._tasks[otherTaskName] = undefined;
+      }
+    }
+  }
+}
