b0a55d3189e67b3899ca862f85029e97625824e4
David Zearing <dzearing@microsoft.com>
2016-03-09 22:57:28 -0800

    Updating typescript build to output lint/typescript parallel task as default.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { TypeScriptTask } from './TypeScriptTask';
 import { TSLintTask } from './TSLintTask';
+import { ITask, parallel } from 'gulp-core-build';
 
-export { ITask } from 'gulp-core-build';
 export const typescript = new TypeScriptTask();
 export const tslint = new TSLintTask();
 
-export default typescript;
+export default parallel(tslint, typescript) as ITask<any>;
