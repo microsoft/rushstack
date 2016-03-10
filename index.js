d48df82e379e95b8f327816699c314b55ac56756
dzearing <dzearing@users.noreply.github.com>
2016-03-10 12:15:42 -0800

    Update web library build.

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 'use strict';
 
 let build = require('gulp-core-build');
-let tslint = require('gulp-core-build-typescript').tslint;
-let typescript = require('gulp-core-build-typescript').typescript;
-let sass = require('gulp-core-build-sass').default;
-let karma = require('gulp-core-build-karma').default;
-let webpack = require('gulp-core-build-webpack').default;
-let serve = require('gulp-core-build-serve').serve;
-let reload = require('gulp-core-build-serve').reload;
+
+let plugins = {
+  build: build,
+  typescript: require('gulp-core-build-typescript'),
+  sass: require('gulp-core-build-sass'),
+  karma: require('gulp-core-build-karma'),
+  webpack: require('gulp-core-build-webpack'),
+  serve: require('gulp-core-build-serve'),
+};
+
+let tasks = {
+  typescript: plugins.typescript.typescript,
+  tslint: plugins.typescript.tslint,
+  text: plugins.typescript.text,
+  sass: plugins.sass.default,
+  karma: plugins.karma.default,
+  webpack: plugins.webpack.default,
+  serve: plugins.serve.serve,
+  reload: plugins.serve.reload
+};
+
+// Shortcuts since node doesn't support destructuring by default yet.
+let task = build.task;
+let parallel = build.parallel;
+let serial = build.serial;
+let watch = build.watch;
 
 // Define task groups.
-let buildTasks = task('build', parallel(tslint, typescript, sass));
-let testTasks = task('test', serial(buildTasks, karma));
-let bundleTasks = task('build', serial(buildTasks, webpack));
+let buildTasks = task('build', parallel(tasks.tslint, tasks.typescript, tasks.text, tasks.sass));
+let testTasks = task('test', serial(buildTasks, tasks.karma));
+let bundleTasks = task('build', serial(buildTasks, tasks.webpack));
 let defaultTasks = task('default', bundleTasks);
 let serveTasks = task('serve',
   serial(
     bundleTasks,
-    serve,
-    watch('src/**/*.{ts,tsx}', serial(parallel(lint, typescript), webpack, reload)),
-    watch('src/**/*.scss', serial(serial(sass, webpack, reload)))
+    tasks.serve,
+    watch('src/**/*.{ts,tsx}', serial(parallel(tasks.tslint, tasks.typescript, tasks.text), tasks.webpack, tasks.reload)),
+    watch('src/**/*.scss', serial(serial(tasks.sass, tasks.webpack, tasks.reload)))
   )
 );
 
 // Export tasks, groups, and initialize.
 module.exports = {
-  tasks: {
-    tslint,
-    typescript,
-    sass,
-    karma,
-    webpack,
-    serve,
-    reload
-  },
+  plugins: plugins,
 
-  taskGroups: {
-    build: buildTasks,
-    test: testTasks,
-    bundle: bundleTasks,
-    serve: serveTasks
-  },
+  tasks: tasks,
+
+  config: (config) => build.config(config),
 
   initialize: (gulp) => build.initialize(gulp)
 };
