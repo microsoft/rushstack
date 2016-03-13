282f5c93cbd98c19ce3db9b34acf569fb4609c5f
dzearing <dzearing@users.noreply.github.com>
2016-03-13 14:56:48 -0700

    Fixing an ITask reference.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { TypeScriptTask } from './TypeScriptTask';
 import { TSLintTask } from './TSLintTask';
 import { TextTask } from './TextTask';
-import { ITask, parallel } from 'gulp-core-build';
+import { IExecutable, parallel } from 'gulp-core-build';
 
 export const typescript = new TypeScriptTask();
 export const tslint = new TSLintTask();
 export const text = new TextTask();
 
-export default parallel(tslint, typescript) as ITask<any>;
+export default parallel(tslint, typescript) as IExecutable;
