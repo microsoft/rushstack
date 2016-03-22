102123ebcf5fb8f1a9b1e78e4926add3ecf9a4e6
dzearing <dzearing@users.noreply.github.com>
2016-03-22 12:36:13 -0700

    Updating dependencies, adding "isEnabled" callback that tasks can optionally specify, adding onTaskStart/End callbacks for logging hooks. Bumping tslint/typescript versions.

--- src/IExecutable.ts
+++ src/IExecutable.ts
@@ -2,6 +2,9 @@ import { IBuildConfig } from './IBuildConfig';
 
 export interface IExecutable {
   execute: (config: IBuildConfig) => Promise<any>;
+
+  name?: string;
+  isEnabled?: () => boolean;
 }
 
 export default IExecutable;
