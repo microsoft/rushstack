45e70bc9bc66cb1dcc4e3b878b7242f1980c54ff
David Zearing <dzearing@microsoft.com>
2016-03-22 18:35:29 -0700

    Adding instrumentation to the mocha plugin.

---
+++ src/InstrumentTask.ts
@@ -0,0 +1,25 @@
+import { GulpTask } from 'gulp-core-build';
+
+export interface IInstrumentTaskConfig {
+  coverageMatch: string[];
+}
+
+export class InstrumentTask extends GulpTask<IInstrumentTaskConfig> {
+  public name = 'Instrument';
+
+  public taskConfig: IInstrumentTaskConfig = {
+    coverageMatch: ['lib/**/*.js', '!lib/**/*.test.js']
+  };
+
+  public executeTask(gulp, completeCallback): any {
+    let istanbul = require('gulp-istanbul');
+
+    return gulp.src(this.taskConfig.coverageMatch)
+      // Covering files
+      .pipe(istanbul())
+      // Force `require` to return covered files
+      .pipe(istanbul.hookRequire())
+      // Write the covered files to a temporary directory
+      .pipe(gulp.dest(this.buildConfig.tempFolder));
+  }
+}
