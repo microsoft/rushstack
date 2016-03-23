e86393c1e20d949225523e935bd3da466936bbda
Ian Clanton-Thuon <ianc@microsoft.com>
2016-03-23 15:19:07 -0700

    Implementation of loader.

--- src/LoadThemedStylesLoader.ts
+++ src/LoadThemedStylesLoader.ts
@@ -1,7 +1,17 @@
 let loaderUtils = require('loader-utils');
 
 export class LoadThemedStylesLoader {
-  public pitch(remainingRequest: string): string {
-    return loaderUtils;
+  public static pitch(remainingRequest: string): string {
+    return [
+      `var content = require(${loaderUtils.stringifyRequest(this, '!!' + remainingRequest)});`,
+      'var loader = require("load-themed-styles");',
+      '',
+      'if(typeof content === "string") content = [[module.id, content]];',
+      '',
+      '// add the styles to the DOM',
+      'for (var i = 0; i < content.length; i++) loader.loadStyles(content[i][1]);',
+      '',
+      'if(content.locals) module.exports = content.locals;'
+    ].join('\n');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 import { LoadThemedStylesLoader } from './LoadThemedStylesLoader';
-
-export const loader = new LoadThemedStylesLoader();
-export default loader;
+export = LoadThemedStylesLoader;
