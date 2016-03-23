0676500a2f6b09be556364002f9c92beabeca77c
dzearing <dzearing@users.noreply.github.com>
2016-03-22 18:35:29 -0700

    Adding instrumentation to the mocha plugin.

--- src/MochaTask.ts
+++ src/MochaTask.ts
@@ -8,13 +8,19 @@ export class MochaTask extends GulpTask<IMochaTaskConfig> {
   public name = 'mocha';
 
   public taskConfig: IMochaTaskConfig = {
-     testMatch: [ 'lib/**/*.test.js']
+    testMatch: ['lib/**/*.test.js']
   };
 
   public executeTask(gulp, completeCallback): any {
+    let istanbul = require('gulp-istanbul');
     let mocha = require('gulp-mocha');
+    let matchIndex = (process.argv.indexOf('--match'));
+    let matchString = (matchIndex === -1) ? '' : process.argv[matchIndex + 1];
 
     return gulp.src(this.taskConfig.testMatch, { read: false })
-      .pipe(mocha());
+      .pipe(mocha({
+        grep: matchString
+      }))
+      .pipe(istanbul.writeReports());
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { serial, IExecutable } from 'gulp-core-build';
 import { MochaTask } from './MochaTask';
+import { InstrumentTask } from './InstrumentTask';
 
+export const instrument = new InstrumentTask();
 export const mocha = new MochaTask();
-export default mocha;
+
+export default serial(instrument, mocha) as IExecutable;
