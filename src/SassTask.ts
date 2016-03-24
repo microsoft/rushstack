694f6779fedbd18327ff3ce2106727ae34234391
dzearing <dzearing@users.noreply.github.com>
2016-03-23 20:07:29 -0700

    Revert "Merge pull request #1 from iclanton/ianc/exporting-scss-directly"
    This reverts commit a8da878e40f51afd436057ed4e975d500d64d301, reversing
    changes made to 5cf744120c345ead5f5cd255450042b53828f3c5.

--- src/SassTask.ts
+++ src/SassTask.ts
@@ -23,7 +23,6 @@ export class SassTask extends GulpTask<ISassTaskConfig> {
   public executeTask(gulp, completeCallback): any {
     let sass = require('gulp-sass');
     let cleancss = require('gulp-clean-css');
-    let rename = require('gulp-rename');
     let texttojs = require('gulp-texttojs');
     let merge = require('merge2');
 
@@ -34,10 +33,6 @@ export class SassTask extends GulpTask<ISassTaskConfig> {
       .pipe(cleancss({
         advanced: false
       }))
-      .pipe(rename({
-        extname: '.scss'
-      }))
-      .pipe(gulp.dest(this.buildConfig.libFolder))
       .pipe(texttojs({
         ext: '.scss.js',
         isExtensionAppended: false,
@@ -53,10 +48,6 @@ export class SassTask extends GulpTask<ISassTaskConfig> {
         .pipe(cleancss({
           advanced: false
         }))
-        .pipe(rename({
-          extname: '.scss'
-        }))
-        .pipe(gulp.dest(this.buildConfig.libAMDFolder))
         .pipe(texttojs({
           ext: '.scss.js',
           isExtensionAppended: false,
