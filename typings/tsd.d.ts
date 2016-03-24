7c7be2fe65e7f56d4fdac86eb7452184b83f817d
iclanton <iclanton@users.noreply.github.com>
2016-03-24 11:58:45 -0700

    Fixing a few issues and including correct type in IWebpackConfig.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="gulp/gulp.d.ts" />
 /// <reference path="orchestrator/orchestrator.d.ts" />
 /// <reference path="q/Q.d.ts" />
+/// <reference path="source-map/source-map.d.ts" />
+/// <reference path="uglify-js/uglify-js.d.ts" />
+/// <reference path="webpack/webpack.d.ts" />
