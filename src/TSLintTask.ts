40915ee02e2e2b39725626fc2162049040ab6c43
David Zearing <dzearing@microsoft.com>
2016-03-25 23:57:34 -0700

    Adding incremental linting support.

--- src/TSLintTask.ts
+++ src/TSLintTask.ts
@@ -20,17 +20,29 @@ export class TSLintTask extends GulpTask<ITSLintTaskConfig> {
 
   public executeTask(gulp, completeCallback): any {
     let lint = require('gulp-tslint');
+    let changed = require('gulp-changed');
 
     if (this.taskConfig.lintConfig) {
       return gulp.src(this.taskConfig.sourceMatch)
+        .pipe(changed(this.buildConfig.libFolder, { extension: '.js' }))
         .pipe(lint({
           configuration: this.taskConfig.lintConfig
         }))
         .pipe(lint.report('full', {
           emitError: false
-        }));
+        }))
+        .pipe(lint.report(touchFile));
     } else {
       completeCallback();
     }
   }
+}
+
+/** We need to touch any files that fail lint so that they're re-evaluated on incremental build. */
+function touchFile(output, file, options) {
+  'use strict';
+
+  let touch = require('touch');
+
+  touch.sync(file.path);
 }
