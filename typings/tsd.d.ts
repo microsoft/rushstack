9d5bee9d86dbbe1d871a44441033e4739bc7e017
dzearing <dzearing@users.noreply.github.com>
2016-03-26 00:01:54 -0700

    Adding missing d.ts files.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="source-map/source-map.d.ts" />
+/// <reference path="uglify-js/uglify-js.d.ts" />
+/// <reference path="webpack/webpack.d.ts" />
