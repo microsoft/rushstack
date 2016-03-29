e8c4f5267d92caa42bf8770296677b6968f9fed0
iclanton <iclanton@users.noreply.github.com>
2016-03-28 17:45:59 -0700

    Allowing for .module.scss files when "useCSSModules" is turned off.

--- src/SassTask.ts
+++ src/SassTask.ts
@@ -2,6 +2,11 @@ import {
   GulpTask
 } from 'gulp-core-build';
 
+const clone = require('gulp-clone');
+const merge = require('merge2');
+
+const scssTsExtName = '.scss.ts';
+
 export interface ISassTaskConfig {
   sassMatch?: string[];
   useCSSModules?: boolean;
@@ -26,86 +31,95 @@ export class SassTask extends GulpTask<ISassTaskConfig> {
   ];
 
   public executeTask(gulp, completeCallback): any {
-    const path = require('path');
-    const sass = require('gulp-sass');
-    const cleancss = require('gulp-clean-css');
-    const texttojs = require('gulp-texttojs');
-    const changed = require('gulp-changed');
-    const postcss = require('gulp-postcss');
     const autoprefixer = require('autoprefixer');
     const cssModules = require('postcss-modules');
-    const clone = require('gulp-clone');
-    const merge = require('merge2');
 
     const postCSSPlugins = [
       autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'ie >= 10'] })
     ];
-    const tasks: NodeJS.ReadWriteStream[] = [];
+    const modulePostCssPlugins = postCSSPlugins.slice(0);
+    modulePostCssPlugins.push(cssModules({
+      getJSON: this.generateModuleStub.bind(this),
+      generateScopedName: this.generateScopedName.bind(this)
+    }));
+
+    const srcPattern = this.taskConfig.sassMatch.slice(0);
+    const moduleSrcPattern = srcPattern.map((value: string) => value.replace('.scss', '.module.scss'));
 
     if (this.taskConfig.useCSSModules) {
-      postCSSPlugins.push(
-        cssModules({
-          getJSON: this.generateModuleStub.bind(this),
-          generateScopedName: this.generateScopedName.bind(this)
-        })
-      );
+      return this.processFiles(gulp, srcPattern, completeCallback, modulePostCssPlugins);
+    } else {
+      moduleSrcPattern.forEach((value: string) => srcPattern.push(`!${value}`));
+
+      return merge(this.processFiles(gulp, srcPattern, completeCallback, postCSSPlugins),
+                   this.processFiles(gulp, moduleSrcPattern, completeCallback, modulePostCssPlugins));
     }
+  }
 
-    const scssTsExtName = '.scss.ts';
+  private processFiles(gulp, srcPattern, completeCallback, postCSSPlugins): NodeJS.ReadWriteStream {
+    const cleancss = require('gulp-clean-css');
+    const changed = require('gulp-changed');
+    const sass = require('gulp-sass');
+    const postcss = require('gulp-postcss');
+    const texttojs = require('gulp-texttojs');
+    const path = require('path');
 
-    let baseTask = gulp.src(this.taskConfig.sassMatch)
+    const tasks: NodeJS.ReadWriteStream[] = [];
+
+    const baseTask = gulp.src(srcPattern)
       .pipe(changed('src', { extension: scssTsExtName }))
       .pipe(sass.sync({
         importer: (url, prev, done) => ({ file: patchSassUrl(url) })
       }).on('error', function(error) {
         sass.logError.call(this, error);
         completeCallback('Errors found in sass file(s).');
-      })).pipe(postcss(postCSSPlugins))
+      }))
+      .pipe(postcss(postCSSPlugins))
       .pipe(cleancss({
         advanced: false
       }));
 
     if (this.taskConfig.dropCssFiles) {
       tasks.push(baseTask.pipe(clone())
-          .pipe(gulp.dest(this.buildConfig.libFolder)));
+        .pipe(gulp.dest(this.buildConfig.libFolder)));
     }
 
     tasks.push(baseTask.pipe(clone())
-    .pipe(texttojs({
-      ext: scssTsExtName,
-      isExtensionAppended: false,
-      template: (file) => {
-        const content = file.contents.toString('utf8');
-        const classNames = _classMaps[file.path];
-        const exportClassNames = classNames ?
-          `export = ${flipDoubleQuotes(JSON.stringify(classNames, null, 2))};` : '';
+      .pipe(texttojs({
+        ext: scssTsExtName,
+        isExtensionAppended: false,
+        template: (file) => {
+          const content = file.contents.toString('utf8');
+          const classNames = _classMaps[file.path];
+          const exportClassNames = classNames ?
+            `export = ${flipDoubleQuotes(JSON.stringify(classNames, null, 2))};` : '';
 
-        let lines = [];
-        if (this.taskConfig.dropCssFiles) {
-          lines = [
-            '/* tslint:disable */',
-            '',
-            exportClassNames,
-            '',
-            `require('${path.basename(file.path, scssTsExtName)}.css');`,
-            ''
-          ];
-        } else if (!!content) {
-          lines = [
-            '/* tslint:disable */',
-            '',
-            'import { loadStyles } from \'load-themed-styles\';',
-            '',
-            exportClassNames,
-            '',
-            `loadStyles(${flipDoubleQuotes(JSON.stringify(content))});`,
-            ''
-          ];
+          let lines = [];
+          if (this.taskConfig.dropCssFiles) {
+            lines = [
+              '/* tslint:disable */',
+              '',
+              exportClassNames,
+              '',
+              `require('${path.basename(file.path, scssTsExtName)}.css');`,
+              ''
+            ];
+          } else if (!!content) {
+            lines = [
+              '/* tslint:disable */',
+              '',
+              'import { loadStyles } from \'load-themed-styles\';',
+              '',
+              exportClassNames,
+              '',
+              `loadStyles(${flipDoubleQuotes(JSON.stringify(content))});`,
+              ''
+            ];
+          }
+
+          return lines.join('\n').replace(/\n\n+/, '\n\n');
         }
-
-        return lines.join('\n').replace(/\n\n+/, '\n\n');
-      }
-    }))
+      }))
       .pipe(gulp.dest('src')));
 
     return merge(tasks);
