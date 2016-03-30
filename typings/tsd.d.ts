3aee734d9fcecb754aca9c53a81026dee0deb508
Ian Clanton-Thuon <ianc@microsoft.com>
2016-03-29 17:44:29 -0700

    Including lodash.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="orchestrator/orchestrator.d.ts" />
 /// <reference path="q/Q.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
