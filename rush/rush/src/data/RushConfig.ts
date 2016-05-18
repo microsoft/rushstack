5e343b2802bf26f514829a0788680b03ba3ba6b5
pgonzal <pgonzal@users.noreply.github.com>
2016-05-17 18:20:41 -0700

    Add projectFolderMinDepth/projectFolderMaxDepth settings to check for projects that don't follow our intended folder hierarchy

--- rush/rush/src/data/RushConfig.ts
+++ rush/rush/src/data/RushConfig.ts
@@ -16,6 +16,8 @@ import Utilities from '../utilities/Utilities';
 export interface IRushConfigJson {
   commonFolder: string;
   npmVersion: string;
+  projectFolderMinDepth?: number;
+  projectFolderMaxDepth?: number;
   projects: IRushConfigProjectJson[];
 };
 
@@ -40,6 +42,9 @@ export default class RushConfig {
   private _rushLinkJsonFilename: string;
   private _npmToolVersion: string;
   private _npmToolFilename: string;
+  private _projectFolderMinDepth: number;
+  private _projectFolderMaxDepth: number;
+
   private _projects: RushConfigProject[];
   private _projectsByName: Map<string, RushConfigProject>;
 
@@ -61,6 +66,18 @@ export default class RushConfig {
 
     this._npmToolVersion = rushConfigJson.npmVersion;
     this._npmToolFilename = path.join(this._commonFolder, 'local-npm', 'node_modules', '.bin', 'npm');
+
+    this._projectFolderMinDepth = rushConfigJson.projectFolderMinDepth !== undefined
+      ? rushConfigJson.projectFolderMinDepth : 2;
+    if (this._projectFolderMinDepth < 1) {
+      throw new Error('Invalid projectFolderMinDepth; the minimum possible value is 1')
+    }
+
+    this._projectFolderMaxDepth = rushConfigJson.projectFolderMaxDepth !== undefined
+      ? rushConfigJson.projectFolderMaxDepth : 2;
+    if (this._projectFolderMaxDepth < this._projectFolderMinDepth) {
+      throw new Error('The projectFolderMaxDepth cannot be smaller than the projectFolderMinDepth')
+    }
 
     this._projects = [];
     this._projectsByName = new Map<string, RushConfigProject>();
@@ -222,6 +239,26 @@ export default class RushConfig {
    */
   public get npmToolFilename(): string {
     return this._npmToolFilename;
+  }
+
+  /**
+   * The minimum allowable folder depth for the projectFolder field in the rush.json file.
+   * This setting provides a way for repository maintainers to discourage nesting of project folders
+   * that makes the directory tree more difficult to navigate.  The default value is 2,
+   * which implements a standard 2-level hierarchy of <categoryFolder>/<projectFolder>/package.json.
+   */
+  public get projectFolderMinDepth(): number {
+    return this._projectFolderMinDepth;
+  }
+
+  /**
+   * The maximum allowable folder depth for the projectFolder field in the rush.json file.
+   * This setting provides a way for repository maintainers to discourage nesting of project folders
+   * that makes the directory tree more difficult to navigate.  The default value is 2,
+   * which implements on a standard convention of <categoryFolder>/<projectFolder>/package.json.
+   */
+  public get projectFolderMaxDepth(): number {
+    return this._projectFolderMaxDepth;
   }
 
   public get projects(): RushConfigProject[] {
