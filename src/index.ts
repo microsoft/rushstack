c4c45fcd477f5d7fceca3642381597b2a7f0aa0a
Nick Pape nickpape <nickpape@microsoft.com>
2016-06-08 13:00:49 -0700

    Renames task

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { TypeScriptTask } from './TypeScriptTask';
 import { TSLintTask } from './TSLintTask';
 import { TextTask } from './TextTask';
-import { TSNpmLintTask } from './TSNpmLintTask';
+import { RemoveTripleSlashReferenceTask } from './RemoveTripleSlashReferenceTask';
 import { IExecutable, parallel, serial } from 'gulp-core-build';
 
 export const typescript: TypeScriptTask = new TypeScriptTask();
 export const tslint: TSLintTask = new TSLintTask();
 export const text: TextTask = new TextTask();
-export const tsNpmLint: TSNpmLintTask = new TSNpmLintTask();
+export const removeTripleSlash: RemoveTripleSlashReferenceTask = new RemoveTripleSlashReferenceTask();
 
 /* tslint:disable:export-name */
-export default parallel(tslint, serial(typescript, tsNpmLint)) as IExecutable;
+export default parallel(tslint, serial(typescript, removeTripleSlash)) as IExecutable;
 /* tslint:enable:export-name */
