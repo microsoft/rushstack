c94240ca258ecb1b094d260605475a38268baa85
Ian Clanton-Thuon <iclantonthuon@outlook.com>
2016-06-27 01:22:34 -0700

    Including tests.

---
+++ src/RawScriptLoader.test.ts
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+import RawScriptLoader = require('./RawScriptLoader');
+
+function wrapResult(result: string): string {
+  return `var exports = {};
+  eval(${result});
+  exports;`;
+}
+
+describe('RawScriptLoader', () => {
+  it('follows the Webpack loader interface', () => {
+    assert.isDefined(RawScriptLoader);
+    assert.isFunction(RawScriptLoader);
+  });
+
+  it('returns a string', () => {
+    assert.isString(RawScriptLoader(''));
+  });
+
+  it('correctly sets exported objects', () => {
+    const testScript: string = 'var x = 123; this.exportedObject = x;';
+    /* tslint:disable:no-eval */
+    const exports: { exportedObject: number } = eval(wrapResult(RawScriptLoader(testScript)));
+    /* tslint:enable:no-eval */
+    assert.equal(exports.exportedObject, 123);
+  });
+});
