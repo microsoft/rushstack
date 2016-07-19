b654d51fcf3e77f2f40cea01fb0f0e82a5bf86da
nickpape-msft <nickpape-msft@users.noreply.github.com>
2016-07-19 13:44:15 -0700

    Fix up Linter conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+/* tslint:disable:export-name */
 'use strict';
 
 import { KarmaTask } from './KarmaTask';
 
-export const karma = new KarmaTask();
+export const karma: KarmaTask = new KarmaTask();
 export default karma;
