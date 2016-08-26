64122b94bb7d95698510f9e8eecd1e0d4cbbf063
iclanton <iclanton@users.noreply.github.com>
2016-08-25 22:06:01 -0700

    Fixing build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const tslint = require('tslint');
-const build = require('node-library-build');
+const build = require('@microsoft/node-library-build');
 build.tslint.setConfig({
   lintConfig: require('./src/defaultTslint.json'),
   rulesDirectory: tslint.getRulesDirectories(tslint.getRulesDirectories([ './node_modules/tslint-microsoft-contrib' ], __dirname))
