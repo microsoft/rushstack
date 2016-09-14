585222ebe6bda5e49fc2a57eba12d3d6c0a29519
iclanton <iclanton@users.noreply.github.com>
2016-09-14 00:16:49 -0700

    Installing node-forge.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -11,3 +11,4 @@
 /// <reference path="express/express.d.ts" />
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="node-forge/node-forge.d.ts" />
