c26c23b96f7b8a1c785648a186877368c903c22c
pgonzal <pgonzal@users.noreply.github.com>
2016-09-21 23:59:13 -0700

    Eliminate error-prone default export for stream-collator

--- libraries/stream-collator/src/index.ts
+++ libraries/stream-collator/src/index.ts
@@ -1,4 +1,4 @@
-export { default as default } from './Interleaver';
+export { default as Interleaver } from './Interleaver';
 export { ITaskWriter as ITaskWriter } from './Interleaver';
 export { default as StreamCollator } from './next/StreamCollator';
 export { default as DualTaskStream } from './next/DualTaskStream';
