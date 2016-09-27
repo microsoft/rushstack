93f1fb7c1f46d37cd1882397159497a089b5b0b4
iclanton <iclanton@users.noreply.github.com>
2016-09-26 18:54:01 -0700

    Implementing trust task.

---
+++ src/TrustCertTask.ts
@@ -0,0 +1,18 @@
+import { GulpTask } from '@microsoft/gulp-core-build';
+import * as gulp from 'gulp';
+
+import { ensureCertificate, ICertificate } from './Certifiates';
+
+export default class TrustCertTask extends GulpTask<{}> {
+  public name: string = 'trust-cert';
+
+  executeTask(gulp: gulp.Gulp, completeCallback: (error?: string) => void): void {
+    const certificate: ICertificate = ensureCertificate(true, this);
+
+    if (certificate.pemCertificate && certificate.pemKey) {
+      completeCallback();
+    } else {
+      completeCallback('Error trusting development certificate.');
+    }
+  }
+}
