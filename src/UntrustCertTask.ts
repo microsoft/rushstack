ecd908e3ac30aae79e24437a35c9402075434736
iclanton <iclanton@users.noreply.github.com>
2016-09-27 01:37:58 -0700

    Documentation updates.

--- src/UntrustCertTask.ts
+++ src/UntrustCertTask.ts
@@ -4,6 +4,13 @@ import * as gulp from 'gulp';
 import CertificateStore from './CertificateStore';
 import { untrustCertificate } from './certificates';
 
+/**
+ * On Windows, this task removes the certificate with the expected serial number from the user's
+ *  root certification authorities list. On macOS, it finds the SHA signature of the certificate
+ *  with the expected serial number and then removes that certificate from the keychain. On
+ *  other platforms, the user must untrust the certificate manually. On all platforms,
+ *  the certificate and private key are deleted from the user's home directory.
+ */
 export default class UntrustCertTask extends GulpTask<{}> {
   public name: string = 'untrust-cert';
 
