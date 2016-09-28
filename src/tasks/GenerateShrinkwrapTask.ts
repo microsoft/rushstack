1ffe2009f111d68c912bc2511d626398bc41e3eb
Cliff Koh <cliffkoh@users.noreply.github.com>
2016-09-27 19:21:21 -0700

    Made some tweaks to generate shrinkwrap (#15)
    * Made some tweaks to generate shrinkwrap
    
    * Remove pullapprove.yml, use Github PR instead

--- src/tasks/GenerateShrinkwrapTask.ts
+++ src/tasks/GenerateShrinkwrapTask.ts
@@ -3,7 +3,6 @@ import gulpType = require('gulp');
 import * as child_process from 'child_process';
 import * as rimraf from 'rimraf';
 import * as os from 'os';
-import * as fs from 'fs';
 import * as path from 'path';
 
 /**
@@ -15,28 +14,20 @@ export class GenerateShrinkwrapTask extends GulpTask<{}> {
   public name: string = 'generate-shrinkwrap';
 
   public executeTask(gulp: gulpType.Gulp): NodeJS.ReadWriteStream {
-    const pathToNodeModules: string = path.join(this.buildConfig.rootPath, 'node_modules');
     const pathToShrinkwrap: string = path.join(this.buildConfig.rootPath, 'npm-shrinkwrap.json');
 
-    if (fs.existsSync(pathToNodeModules) && !this.buildConfig.args.hasOwnProperty('fast')) {
-      this.log(`Deleting node_modules folder: ${pathToNodeModules}/node_modules`);
-      this._dangerouslyDeletePath(pathToNodeModules);
-    }
-
     if (this.fileExists(pathToShrinkwrap)) {
+      this.log(`Remove existing shrinkwrap file.`);
       this._dangerouslyDeletePath(pathToShrinkwrap);
     }
+
+    this.log(`Running npm prune`);
+    child_process.execSync('npm prune');
 
     this.log(`Running npm update`);
     child_process.execSync('npm update');
 
-    if (this.buildConfig.args.hasOwnProperty('fast')) {
-      // In fast mode, we have not deleted the shrinkwrap file...
-      this.log(`Running npm prune`);
-      child_process.execSync('npm prune');
-    }
-
-    this.log(`Running npm shrinkwrap`);
+    this.log(`Running npm shrinkwrap --dev`);
     child_process.execSync('npm shrinkwrap --dev');
 
     return;
