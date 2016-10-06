9accb8b42a85e9d8c9c7cf3e723a7fc569e7c832
David Zearing <dzearing@microsoft.com>
2016-10-06 15:52:01 -0700

    Updates so that dependency updates trickle down correctly.

--- scripts/publish.js
+++ scripts/publish.js
@@ -44,17 +44,12 @@ function findChangesSync() {
     for (let i = 0; i < changeDescription.changes.length; i++) {
       let change = changeDescription.changes[i];
 
-      addChange(allChanges, change.packageName, _allPackages[change.packageName].path, _changeTypes[change.type], change.comments);
+      addChange(allChanges, change);
     }
   });
 
   let packages = Object.keys(allChanges);
   let updatedDeps = {};
-
-  // Update the versions for downstream dependencies within the repo.
-  for (let packageName of packages) {
-    addDependencies(allChanges, packageName, updatedDeps);
-  }
 
   // Update orders so that downstreams are marked to come after upstreams.
   for (let packageName in allChanges) {
@@ -80,17 +75,32 @@ function findChangesSync() {
 }
 
 /** Add a change request to the allChanges dictionary if necessary. */
-function addChange(allChanges, packageName, packagePath, changeType, comments) {
-  comments = comments || [];
+function addChange(allChanges, change) {
+  let packageName = change.packageName;
+  let pkgEntry = _allPackages[packageName];
+  let pkg = pkgEntry.package;
+  let currentChange;
 
   if (!allChanges[packageName]) {
-    allChanges[packageName] = { packageName, packagePath, changeType, comments, order: 0 };
+    currentChange = allChanges[packageName] = {
+      packageName,
+      packagePath: pkgEntry.path,
+      changeType: _changeTypes[change.type],
+      comments: change.comments || [],
+      order: 0,
+      changes: [change]
+    };
   } else {
-    let currentChange = allChanges[packageName];
-
-    currentChange.changeType = Math.max(currentChange.changeType, changeType);
-    currentChange.comments = currentChange.comments.concat(comments);
+    currentChange = allChanges[packageName];
+    currentChange.changeType = Math.max(currentChange.changeType, _changeTypes[change.type]);
+    currentChange.comments = currentChange.comments.concat(change.comments);
+    currentChange.changes.push(change);
   }
+
+  currentChange.newVersion = currentChange.changeType > 0 ? semver.inc(pkg.version, _changeTypes[currentChange.changeType]) : pkg.version;
+  currentChange.newVersionRange = `>=${currentChange.newVersion} <${semver.inc(currentChange.newVersion, 'major')}`;
+
+  updateDownstreamDependencies(allChanges, packageName, currentChange.newVersionRange);
 }
 
 /** Build a downstream dependencies lookup table. */
@@ -109,44 +119,112 @@ function getDownstreamDependencies() {
   return downstreamDeps;
 }
 
-/** Add downstream changes that are implicitly required by version bumps. */
-function addDependencies(allChanges, packageName, updatedDeps) {
-  if (!updatedDeps) {
-    updatedDeps = {};
-  }
+function updateDownstreamDependencies(allChanges, packageName, newVersionRange) {
+  let change = allChanges[packageName];
+  let downstreamNames = _downstreamDeps[packageName];
 
-  if (!updatedDeps[packageName]) {
-    updatedDeps[packageName] = true;
-    let change = allChanges[packageName];
-    let dependencies = _downstreamDeps[packageName];
+  // Iterate through all downstream dependencies for the package.
+  if (downstreamNames) {
+    for (let depName of downstreamNames) {
+      let pkgEntry = _allPackages[depName];
+      let pkg = pkgEntry.package;
+      let requiredVersion = pkgEntry.package.dependencies[packageName];
 
-    for (let i = 0; dependencies && i < dependencies.length; i++) {
-      let depPackage = _allPackages[dependencies[i]];
+      // If the version range has not yet been updated to this version, update it.
+      if (requiredVersion !== newVersionRange) {
+        pkgEntry.package.dependencies[packageName] = newVersionRange;
 
-      addChange(
-        allChanges,
-        depPackage.package.name,
-        depPackage.path,
-        _changeTypes.dependency,
-        [`Updating dependency: ${packageName}`]
-      );
+        // Either it already satisfies the new version, or doesn't. If not, the downstream dep needs to be republished.
+        let changeType = semver.satisfies(change.newVersion, requiredVersion) ? _changeTypes.dependency : _changeTypes.patch;
 
-      addDependencies(allChanges, dependencies[i], updatedDeps);
+        addChange(allChanges, {
+          packageName: pkg.name,
+          type: _changeTypes[changeType],
+          comments: [`Updating ${packageName}: ${newVersionRange} (was ${ requiredVersion })`]
+        });
+      }
     }
   }
 }
 
-/** Print changes to apply. */
-function printChanges(changes) {
-  for (let changeName in changes) {
-    let change = changes[changeName];
+/** Update the package.json for a given change. */
+function updatePackage(change, allChanges) {
+  console.log(os.EOL + `* Applying ${_changeTypes[change.changeType]} update for ${change.packageName} to ${change.newVersion}`);
 
-    if (_allPackages[change.packageName]) {
-      let pkg = _allPackages[change.packageName].package;
-      let newVersion = (change.changeType > _changeTypes.dependency) ? semver.inc(pkg.version, _changeTypes[change.changeType]) : pkg.version;
+  let pkg = _allPackages[change.packageName].package;
 
-      console.log(`${change.packageName} [${_changeTypes[change.changeType]}] ${pkg.version} -> ${newVersion}`);
-      change.comments.forEach(comment => console.log(`|-- ${comment}`))
+  pkg.version = change.newVersion;
+
+  change.changes.forEach(subChange => subChange.comments.forEach(comment => console.log( ` - [${subChange.type}] ${comment}`)));
+
+  if (_shouldCommit) {
+    fs.writeFileSync(change.packagePath, JSON.stringify(pkg, null, 2), 'utf8');
+  }
+}
+
+function execCommand(commandLine, workingPath, isDisabled) {
+  workingPath = workingPath || process.cwd();
+
+  console.log(`Executing: "${commandLine}" from ${workingPath}`);
+
+  if (_shouldCommit && !isDisabled) {
+    execSync(commandLine, {
+      cwd: workingPath
+    });
+  }
+}
+
+function gitAddChanges() {
+  execCommand('git add .');
+}
+
+function gitAddTags(allChanges) {
+  for (let packageName in allChanges) {
+    let change = allChanges[packageName];
+
+    if (change.changeType > _changeTypes.dependency) {
+      let tagName = packageName + '_v' + change.newVersion;
+
+      execCommand(`git tag ${tagName}`);
+    }
+  }
+}
+
+function gitCommit() {
+  execCommand('git commit -m "Applying package updates."');
+}
+
+function gitPush() {
+  execCommand('git push --follow-tags', null, true);
+}
+
+function publishPackage(change) {
+  execCommand(`npm publish`, change.packagePath, true);
+}
+
+function updateChangeLog(change) {
+//  console.log(` - TODO: updating CHANGELOG.md`);
+}
+
+function deleteChangeFiles() {
+  let changesPath = path.join(process.cwd(), 'changes');
+  let changeFiles = [];
+
+  try {
+    changeFiles = fs.readdirSync(changesPath).filter(filename => filename.indexOf('.json') >= 0);
+  } catch (e) { }
+
+  if (changeFiles.length) {
+    console.log(os.EOL + `Deleting ${changeFiles.length} change file(s).`);
+
+    for (let fileName of changeFiles) {
+      let filePath = path.join(changesPath, fileName);
+
+      console.log(` - ${filePath}`);
+
+      if (_shouldCommit) {
+        deleteFile(filePath);
+      }
     }
   }
 }
@@ -167,115 +245,19 @@ function applyChanges(allChanges) {
 
     deleteChangeFiles();
     gitAddChanges(allChanges);
-    gitAddTags(allChanges);
     gitCommit();
+    gitAddTags(allChanges);
     gitPush();
 
     for (let change of orderedChanges) {
-      publishPackage(change);
-    }
-  }
-}
-
-/** Update the package.json for a given change. */
-function updatePackage(change, allChanges) {
-  console.log(os.EOL + `* Applying ${_changeTypes[change.changeType]} update for ${change.packageName} to ${change.newVersion}`);
-
-  let pkg = _allPackages[change.packageName].package;
-
-  for (let depName in pkg.dependencies) {
-    let depChange = allChanges[depName];
-
-    if (depChange) {
-      let requiredVersion = pkg.dependencies[depName];
-      let newVersionRange = `>=${depChange.newVersion} <${semver.inc(depChange.newVersion, 'major')}`;
-
-      console.log(` - updating ${depName}: ${requiredVersion} to ${newVersionRange}`);
-      pkg.dependencies[depName] = newVersionRange;
-
-      if (!semver.satisfies(depChange.newVersion, requiredVersion)) {
-        console.log(`semver not satisfied: ${depChange.packageName} ${depChange.newVersion} ${requiredVersion}`);
-        change.changeType = Math.max(change.changeType, _changeTypes.patch);
-        change.newVersion = semver.inc(pkg.version, _changeTypes[change.changeType]);
-      }
-    }
-  }
-
-  pkg.version = change.newVersion;
-
-  if (_shouldCommit) {
-    fs.writeFileSync(change.packagePath, JSON.stringify(pkg, null, 2), 'utf8');
-  }
-}
-
-function execCommand(commandLine, workingPath) {
-  workingPath = workingPath || process.cwd();
-
-  console.log(`Executing: "${commandLine}" from ${workingPath}`);
-
-  if (_shouldCommit) {
-    execSync(commandLine, {
-      cwd: workingPath
-    });
-  }
-}
-
-function gitAddChanges() {
-  execCommand('git add .');
-}
-
-function gitAddTags(allChanges) {
-  for (let packageName in allChanges) {
-    let change = allChanges[packageName];
-
-    if (change.changeType > _changeTypes.dependency) {
-      let tagName = packageName + ':v' + change.newVersion;
-
-      execCommand(`git tag ${tagName}`);
-    }
-  }
-}
-
-function gitCommit() {
-  execCommand('git commit -m "Applying package updates."');
-}
-
-function gitPush() {
-  execCommand('git push --follow-tags');
-}
-
-function publishPackage(change) {
-  execCommand(`npm publish`, change.packagePath);
-}
-
-function updateChangeLog(change) {
-  console.log(` - TODO: updating CHANGELOG.md`);
-}
-
-function deleteChangeFiles() {
-  if (_shouldCommit) {
-    let changesPath = path.join(process.cwd(), 'changes');
-    let changeFiles = [];
-
-    try {
-      changeFiles = fs.readdirSync(changesPath).filter(filename => filename.indexOf('.json') >= 0);
-    } catch (e) { }
-
-    if (changeFiles.length) {
-      console.log(os.EOL + `Deleting ${changeFiles.length} change file(s).`);
-
-      for (let fileName of changeFiles) {
-        let filePath = path.join(changesPath, fileName);
-
-        console.log(` - ${filePath}`);
-
-        deleteFile(filePath);
+      if (change.changeType > _changeTypes.dependency) {
+        publishPackage(change);
       }
     }
   }
 }
+
 
 let changes = findChangesSync();
 
 applyChanges(changes);
-// printChanges(changes);
