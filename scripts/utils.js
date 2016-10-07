45cd8fff63a94a976276d2a14c25b654b09fbf0d
dzearing <dzearing@users.noreply.github.com>
2016-10-06 17:54:45 -0700

    Adding temporary publish scripts that implement the publish flow. (#4)
    * Adding publish scripts.
    
    * Updating publish script to write the file.
    
    * Improvements to scripts.
    
    * Adding git add.
    
    * Update.
    
    * Updating publish script.
    
    * Removing commitTags
    
    * Updates so that dependency updates trickle down correctly.
    
    * Changes
    
    * npmx update.
    
    * Updating shrinkwrap.
    
    * Adding strict.
    
    * Updating change file.
    
    * Renabling git push and publish. Deleting changelog updating placeholder for now.

---
+++ scripts/utils.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let fs = require('fs');
+let rimraf = require('rimraf');
+
+function fileExists(path) {
+  let exists = false;
+  try {
+    let lstat = fs.lstatSync(path);
+    exists = lstat.isFile();
+  } catch (e) { }
+
+  return exists;
+}
+
+function directoryExists(path) {
+  let exists = false;
+  try {
+    let lstat = fs.lstatSync(path);
+    exists = lstat.isDirectory();
+  } catch (e) { }
+
+  return exists;
+}
+
+function deleteFile(filePath) {
+  if (fileExists(filePath)) {
+    console.log(`Deleting: ${filePath}`);
+    fs.unlinkSync(filePath);
+  }
+  return Promise.resolve();
+}
+
+function deleteDirectory(directoryPath) {
+  return new Promise(done => {
+    if (directoryExists(directoryPath)) {
+      console.log(`Deleting: ${directoryPath}`);
+      rimraf(directoryPath, done);
+    } else {
+      done();
+    }
+  });
+}
+
+module.exports = {
+  fileExists,
+  directoryExists,
+  deleteFile,
+  deleteDirectory
+};
