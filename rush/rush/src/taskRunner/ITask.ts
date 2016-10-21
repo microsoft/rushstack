9c9a0f75a217c1f436dbede62f0abd07f4471242
nickpape-msft <nickpape-msft@users.noreply.github.com>
2016-10-21 22:43:29 +0000

    PR 31143: Rush should build dependencies along the critical path
    Since we have limited parallelization, we should ensure that we are always building projects which are blocking the most other projects (e.g. things along the critical path - which is the limiting factor in build time).. This actually has a positive perf impact.. Builds have improved in speed by over 1 minutes on a few trials on my machine.
    
    Related work items: #168346

--- rush/rush/src/taskRunner/ITask.ts
+++ rush/rush/src/taskRunner/ITask.ts
@@ -22,9 +22,65 @@ export interface ITaskDefinition {
  * The interface used internally by TaskRunner, which tracks the dependencies and execution status
  */
 export interface ITask extends ITaskDefinition {
+  /**
+   * The current execution status of a task. Tasks start in the 'ready' state,
+   * but can be 'blocked' if an upstream task failed. It is 'executing' when
+   * the task is executing. Once execution is complete, it is either 'success' or
+   * 'failure'.
+   */
   status: TaskStatus;
+
+  /**
+   * A set of all dependencies which must be executed before this task is complete.
+   * When dependencies finish execution, they are removed from this list.
+   */
   dependencies: ITask[];
+
+  /**
+   * The inverse of dependencies, lists all projects which are directly dependent on this one.
+   */
   dependents: ITask[];
+
+  /**
+   * This number represents how far away this Task is from the furthest "root" project (i.e.
+   * a project with no dependents). This helps us to calculate the critical path (i.e. the
+   * longest chain of projects which must be executed in order, thereby limiting execution speed
+   * of the entire task tree.
+   *
+   * This number is calculated via a memoized recursive function, and when choosing the next
+   * task to execute, the task with the highest criticalPathLength is chosen.
+   *
+   * Example:
+   *        (0) A
+   *             \
+   *          (1) B     C (0)         (applications)
+   *               \   /|\
+   *                \ / | \
+   *             (2) D  |  X (1)      (utilities)
+   *                    | / \
+   *                    |/   \
+   *                (2) Y     Z (2)   (other utilities)
+   *
+   * All roots (A & C) have a criticalPathLength of 0.
+   * B has a score of 1, since A depends on it.
+   * D has a score of 2, since we look at the longest chain (e.g D->B->A is longer than D->C)
+   * X has a score of 1, since the only package which depends on it is A
+   * Z has a score of 2, since only X depends on it, and X has a score of 1
+   * Y has a score of 2, since the chain Y->X->C is longer than Y->C
+   *
+   * The algorithm is implemented in TaskRunner as _calculateCriticalPaths()
+   */
+  criticalPathLength: number;
+
+  /**
+   * A list of all errors which occurred while executing this task, this is stored in case we need
+   * it later (for example to re-print errors at end of execution).
+   */
   errors: TaskError[];
+
+  /**
+   * The timestamp of the beginning of task execution, in milliseconds since the epoch
+   */
+  startTimestamp: number;
 }
 export default ITask;
