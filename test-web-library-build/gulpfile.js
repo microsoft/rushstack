397a2a30249e6b6fef2b510f9549a8c3d57f3f46
dzearing <dzearing@users.noreply.github.com>
2016-11-01 13:12:49 -0700

    GCB-sass: tslint enable/disable now symmetrical when using css modules. (#15)
    * This removes the broken tslint enable that's causing lint issues downstream.
    
    * Updates.
    
    * PR feedback

--- test-web-library-build/gulpfile.js
+++ test-web-library-build/gulpfile.js
@@ -2,7 +2,9 @@
 
 let build = require('@microsoft/web-library-build');
 
+build.sass.setConfig({ useCSSModules: true });
 build.webpack.setConfig({ configPath: null });
 build.karma.setConfig({ configPath: null });
+
 build.initialize(require('gulp'));
 
