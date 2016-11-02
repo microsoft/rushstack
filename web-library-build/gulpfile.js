8e2a5f23e061452c1371565d641efa4620319379
dzearing <dzearing@users.noreply.github.com>
2016-11-01 19:36:32 -0700

    Fixing the build.

--- web-library-build/gulpfile.js
+++ web-library-build/gulpfile.js
@@ -1,8 +1,11 @@
 'use strict';
 
-let build = require('@microsoft/node-library-build');
+let build = require('@microsoft/gulp-core-build');
+let typescript = require('@microsoft/gulp-core-build-typescript').typescript;
 
-build.typescript.setConfig({ typescript: require('typescript') });
+typescript.setConfig({ typescript: require('typescript') });
+
+build.task('default', typescript);
 
 build.initialize(require('gulp'));
 
