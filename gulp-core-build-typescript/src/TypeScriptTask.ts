2af0ddcecddcf5730e934c1a540e7a85f6c9197e
Nick Pape nickpape <nickpape@microsoft.com>
2016-11-09 11:47:16 -0800

    comment

--- gulp-core-build-typescript/src/TypeScriptTask.ts
+++ gulp-core-build-typescript/src/TypeScriptTask.ts
@@ -201,7 +201,7 @@ export class TypeScriptTask extends GulpTask<ITypeScriptTaskConfig> {
     // tslint:disable-next-line:typedef
     let jsResult = (this.taskConfig.removeCommentsFromJavaScript
       ? tsResult.js.pipe(require('gulp-decomment')({
-        space: !!this.taskConfig.emitSourceMaps
+        space: !!this.taskConfig.emitSourceMaps /* turn comments into spaces to preserve sourcemaps */
       }))
       : tsResult.js);
 
