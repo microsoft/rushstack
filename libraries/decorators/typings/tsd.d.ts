e784a7853dcb36d92a5149dc2f5dc215bb7cc6f7
pgonzal <pgonzal@users.noreply.github.com>
2016-11-22 20:16:02 -0800

    Removing typings files for chai/mocha

--- libraries/decorators/typings/tsd.d.ts
+++ libraries/decorators/typings/tsd.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="@ms/odsp.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
--- libraries/stream-collator/typings/tsd.d.ts
+++ libraries/stream-collator/typings/tsd.d.ts
@@ -1,5 +1,3 @@
 /// <reference path="colors/colors.d.ts" />
 /// <reference path="es6-collections/es6-collections.d.ts" />
 /// <reference path="node/node.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="chai/chai.d.ts" />
