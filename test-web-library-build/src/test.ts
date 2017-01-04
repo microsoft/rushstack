50bbfa7ca08af1b84a91936fb59081912b4cfc7b
iclanton <iclanton@users.noreply.github.com>
2017-01-03 18:44:27 -0800

    Fixing typescript build.

--- test-web-library-build/src/test.ts
+++ test-web-library-build/src/test.ts
@@ -2,9 +2,15 @@ import test from './test.scss';
 import testFunction from './pre-copy-test';
 
 export function log(message: string): void {
-  console.log(test.foo);
+  console.log(message);
 }
 
 export function add(num1: number, num2: number): number {
   return num1 + num2;
 }
+
+export function logClass(): void {
+  console.log(test.foo);
+}
+
+testFunction();
