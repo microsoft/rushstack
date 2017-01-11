c4a0334338801da6c8ef796abf832f36073b1d28
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-01-10 17:29:46 -0800

    Remove sp-tslint-rules dependency

--- api-extractor/gulpfile.js
+++ api-extractor/gulpfile.js
@@ -2,7 +2,4 @@
 
 const build = require('@microsoft/node-library-build');
 
-const tslintCommon = require('@microsoft/sp-tslint-rules');
-tslintCommon.initializeTslintTask(build.tslint);
-
 build.initialize(require('gulp'));
