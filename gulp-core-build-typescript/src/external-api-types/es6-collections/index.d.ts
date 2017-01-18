e2b51d4be033abf6752e794d0c10051450007a2c
dgaeta <dgaeta@users.noreply.github.com>
2017-01-18 14:52:12 -0800

    Added external types definition files

---
+++ gulp-core-build-typescript/src/external-api-types/es6-collections/index.d.ts
@@ -0,0 +1,128 @@
+/// <reference path="es6-collections.d.ts" />
+
+// Type definitions for es6-collections v0.5.1
+// Project: https://github.com/WebReflection/es6-collections/
+// Definitions by: Ron Buckton <http://github.com/rbuckton>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/* *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+/* tslint:disable: interface-name */
+/* tslint:disable: no-any */
+/* tslint:disable: no-shadowed-variable */
+/* tslint:disable: no-var-keyword */
+/* tslint:disable: variable-name */
+/* tslint:disable: member-ordering */
+/* tslint:disable: no-unused-variable */
+/* tslint:disable: quotemark */
+/* tslint:disable: indent */
+/* tslint:disable: max-line-length */
+/* tslint:disable: member-access */
+/* tslint:disable: whitespace */
+
+export interface IteratorResult<T> {
+    done: boolean;
+    value?: T;
+}
+
+export interface Iterator<T> {
+    next(value?: any): IteratorResult<T>;
+    return?(value?: any): IteratorResult<T>;
+    throw?(e?: any): IteratorResult<T>;
+}
+
+export interface ForEachable<T> {
+    forEach(callbackfn: (value: T) => void): void;
+}
+
+export interface Map<K, V> {
+    clear(): void;
+    delete(key: K): boolean;
+    forEach(callbackfn: (value: V, index: K, map: Map<K, V>) => void, thisArg?: any): void;
+    get(key: K): V;
+    has(key: K): boolean;
+    set(key: K, value?: V): Map<K, V>;
+    entries(): Iterator<[K, V]>;
+    keys(): Iterator<K>;
+    values(): Iterator<V>;
+    size: number;
+}
+
+export interface MapConstructor {
+    new <K, V>(): Map<K, V>;
+    new <K, V>(iterable: ForEachable<[K, V]>): Map<K, V>;
+    prototype: Map<any, any>;
+}
+
+// declare var Map: MapConstructor;
+
+export interface Set<T> {
+    add(value: T): Set<T>;
+    clear(): void;
+    delete(value: T): boolean;
+    forEach(callbackfn: (value: T, index: T, set: Set<T>) => void, thisArg?: any): void;
+    has(value: T): boolean;
+    entries(): Iterator<[T, T]>;
+    keys(): Iterator<T>;
+    values(): Iterator<T>;
+    size: number;
+}
+
+export interface SetConstructor {
+    new <T>(): Set<T>;
+    new <T>(iterable: ForEachable<T>): Set<T>;
+    prototype: Set<any>;
+}
+
+// declare var Set: SetConstructor;
+
+export interface WeakMap <K, V> {
+    delete(key: K): boolean;
+	clear(): void;
+    get(key: K): V;
+    has(key: K): boolean;
+    set(key: K, value?: V): WeakMap<K, V>;
+}
+
+export interface WeakMapConstructor {
+    new <K, V>(): WeakMap<K, V>;
+    new <K, V>(iterable: ForEachable<[K, V]>): WeakMap<K, V>;
+    prototype: WeakMap<any, any>;
+}
+
+// declare var WeakMap: WeakMapConstructor;
+
+export interface WeakSet <T> {
+    delete(value: T): boolean;
+	clear(): void;
+    add(value: T): WeakSet<T>;
+    has(value: T): boolean;
+}
+
+export interface WeakSetConstructor {
+    new <T>(): WeakSet<T>;
+    new <T>(iterable: ForEachable<T>): WeakSet<T>;
+    prototype: WeakSet<any>;
+}
+
+// declare var WeakSet: WeakSetConstructor;
+
+// declare module "es6-collections" {
+//     var Map: MapConstructor;
+//     var Set: SetConstructor;
+//     var WeakMap: WeakMapConstructor;
+//     var WeakSet: WeakSetConstructor;
+// }
---
+++ gulp-core-build-typescript/src/external-api-types/whatwg-fetch/index.d.ts
@@ -0,0 +1,113 @@
+/// <reference path="../es6-promise/es6-promise.d.ts" />
+
+// Type definitions for fetch API
+// Project: https://github.com/github/fetch
+// Definitions by: Ryan Graham <https://github.com/ryan-codingintrigue>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/* tslint:disable: interface-name */
+/* tslint:disable: no-any */
+/* tslint:disable: no-shadowed-variable */
+/* tslint:disable: no-var-keyword */
+/* tslint:disable: variable-name */
+/* tslint:disable: member-ordering */
+/* tslint:disable: no-unused-variable */
+/* tslint:disable: quotemark */
+/* tslint:disable: indent */
+/* tslint:disable: max-line-length */
+/* tslint:disable: member-access */
+/* tslint:disable: whitespace */
+
+export class Request extends Body {
+	constructor(input: string|Request, init?:RequestInit);
+	method: string;
+	url: string;
+	headers: Headers;
+	context: RequestContext;
+	referrer: string;
+	mode: RequestMode;
+	redirect: RequestRedirect;
+	credentials: RequestCredentials;
+	cache: RequestCache;
+}
+
+export interface RequestInit {
+	method?: string;
+	headers?: HeaderInit|{ [index: string]: string };
+	body?: BodyInit;
+	mode?: RequestMode;
+	redirect?: RequestRedirect;
+	credentials?: RequestCredentials;
+	cache?: RequestCache;
+}
+
+export type RequestContext =
+	"audio" | "beacon" | "cspreport" | "download" | "embed" |
+	"eventsource" | "favicon" | "fetch" | "font" | "form" | "frame" |
+	"hyperlink" | "iframe" | "image" | "imageset" | "import" |
+	"internal" | "location" | "manifest" | "object" | "ping" | "plugin" |
+	"prefetch" | "script" | "serviceworker" | "sharedworker" |
+	"subresource" | "style" | "track" | "video" | "worker" |
+	"xmlhttprequest" | "xslt";
+export type RequestMode = "same-origin" | "no-cors" | "cors";
+export type RequestRedirect = "follow" | "error" | "manual";
+export type RequestCredentials = "omit" | "same-origin" | "include";
+export type RequestCache =
+	"default" | "no-store" | "reload" | "no-cache" |
+	"force-cache" | "only-if-cached";
+
+export interface HeadersMap {
+	[index: string]: string;
+}
+
+export class Headers {
+	constructor(headers?:Headers|HeadersMap)
+	append(name: string, value: string): void;
+	delete(name: string):void;
+	get(name: string): string;
+	getAll(name: string): Array<string>;
+	has(name: string): boolean;
+	set(name: string, value: string): void;
+	forEach(callback: (value: string, name: string) => void): void;
+}
+
+export class Body {
+	bodyUsed: boolean;
+	arrayBuffer(): Promise<ArrayBuffer>;
+	blob(): Promise<Blob>;
+	formData(): Promise<FormData>;
+	json(): Promise<any>;
+	json<T>(): Promise<T>;
+	text(): Promise<string>;
+}
+
+export class Response extends Body {
+	constructor(body?: BodyInit, init?: ResponseInit);
+	static error(): Response;
+	static redirect(url: string, status: number): Response;
+	type: ResponseType;
+	url: string;
+	status: number;
+	ok: boolean;
+	statusText: string;
+	headers: Headers;
+	clone(): Response;
+}
+
+export type ResponseType = "basic" | "cors" | "default" | "error" | "opaque" | "opaqueredirect";
+
+export interface ResponseInit {
+	status: number;
+	statusText?: string;
+	headers?: HeaderInit;
+}
+
+export type HeaderInit = Headers|Array<string>;
+export type BodyInit = ArrayBuffer|ArrayBufferView|Blob|FormData|string;
+export type RequestInfo = Request|string;
+
+export interface Window {
+	fetch(url: string|Request, init?: RequestInit): Promise<Response>;
+}
+
+// declare var fetch: typeof window.fetch;
