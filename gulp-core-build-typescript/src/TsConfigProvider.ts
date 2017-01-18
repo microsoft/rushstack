367a2d76cd7f5de10e02667e3f9eac6643c82891
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-01-17 17:05:52 -0800

    Add tsConfigProvider to gcb-ts

---
+++ gulp-core-build-typescript/src/TsConfigProvider.ts
@@ -0,0 +1,68 @@
+import * as path from 'path';
+import assign = require('object-assign');
+
+import { SchemaValidator, IBuildConfig } from '@microsoft/gulp-core-build';
+
+/* tslint:disable:no-any */
+export class TsConfigProvider {
+  private static _tsconfig: any;
+  private static _typescript: any = require('typescript');
+
+  public static getConfig(buildConfig: IBuildConfig): any {
+    if (!this._tsconfig) {
+      try {
+        this._tsconfig = SchemaValidator.readCommentedJsonFile<any>(
+          this._getConfigPath(buildConfig)
+        );
+      } catch (e) {
+        /* no-op */
+      }
+
+      if (!this._tsconfig) {
+        this._tsconfig = {
+          compilerOptions: {
+            declaration: true,
+            experimentalDecorators: true,
+            jsx: 'react',
+            moduleResolution: 'node',
+            sourceMap: true,
+            target: 'es5',
+            noUnusedParameters: true,
+            noUnusedLocals: true
+          }
+        };
+      }
+      assign(this._tsconfig.compilerOptions, {
+        rootDir: buildConfig.rootPath,
+        module: 'commonjs',
+        typescript: this.getTypescriptCompiler()
+      });
+    }
+
+    return this._tsconfig;
+  }
+
+  /**
+   * Override the version of the typescript compiler used
+   */
+  public static setTypescriptCompiler(typescript: any): void {
+    if (this._typescript) {
+      throw new Error('The version of the typescript compiler should only be set once.');
+    }
+    if (this._tsconfig) {
+      throw new Error('Set the version of the typescript compiler before tasks call getConfig()');
+    }
+    this._typescript = typescript;
+  }
+
+  public static getTypescriptCompiler(): any {
+    if (!this._typescript) {
+      return require('typescript');
+    }
+    return this._typescript;
+  }
+
+  private static _getConfigPath(buildConfig: IBuildConfig): string {
+    return path.resolve(path.join(buildConfig.rootPath, 'tsconfig.json'));
+  }
+}
