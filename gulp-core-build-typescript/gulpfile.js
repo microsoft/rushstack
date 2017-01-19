e95b7e7a2984b89f9dea9c686d3d792f253e1de2
iclanton <iclanton@users.noreply.github.com>
2017-01-18 18:45:12 -0800

    Including API extractor task in gcb-TS.

--- gulp-core-build-typescript/gulpfile.js
+++ gulp-core-build-typescript/gulpfile.js
@@ -2,6 +2,7 @@
 
 const tslint = require('tslint');
 const build = require('@microsoft/node-library-build');
+const path = require('path');
 
 build.tslint.setConfig({
   lintConfig: require('./src/defaultTslint.json'),
@@ -11,5 +12,22 @@ build.tslint.setConfig({
 build.typescript.setConfig({
   typescript: require('typescript')
 });
+
+build.task('default', build.serial(build.defaultTasks, build.subTask('runApiExtractor', (gulp, buildConfig, done) => {
+  const apiExtractor = require('@microsoft/api-extractor');
+
+  const files = ['external-api-types/es6-collections/index.d.ts',
+                 'external-api-types/es6-collections/index.d.ts',
+                 'external-api-types/es6-promise/index.d.ts'];
+
+  files.forEach((filePath) => {
+    const rootDir = path.dirname(filePath);
+    const outputApiJsonFilePath = path.join(buildConfig.distFolder,
+                                            'external-api-json',
+                                            `${path.basename(rootDir)}.json`);
+    const entryPointFile = path.join(buildConfig.rootDir, filePath);
+    apiExtractor.ExternalApiHelper.generateApiJson(rootDir, entryPointFile, outputApiJsonFilePath);
+  });
+})));
 
 build.initialize(require('gulp'));
