04d8c8118239edcfe6830487776102bb1b9dad2e
Nick Pape nickpape <nickpape@microsoft.com>
2017-01-19 16:23:19 -0800

    Improve documentation and remove a few private things from the public API

--- common/reviews/api/gulp-core-build.api.ts
+++ common/reviews/api/gulp-core-build.api.ts
@@ -1,56 +1,29 @@
 // (undocumented)
 export function addSuppression(str: string): void;
 
-// (undocumented)
-class CleanTask extends GulpTask<ICleanConfig> {
-  // (undocumented)
+class CleanTask extends GulpTask<void> {
+  constructor();
   public executeTask(gulp: gulp.Gulp,
       completeCallback: (result?: Object) => void): void;
-  // (undocumented)
-  public name: string;
-  // (undocumented)
-  public taskConfig: ICleanConfig;
 }
 
 class CopyTask extends GulpTask<ICopyConfig> {
-  // (undocumented)
+  constructor();
   public executeTask(gulp: gulp.Gulp,
       completeCallback: (result?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
-  // (undocumented)
+  // @internal
   public loadSchema(): Object;
-  // (undocumented)
-  public taskConfig: ICopyConfig;
 }
 
 // (undocumented)
 export function coverageData(coverage: number, threshold: number, filePath: string): void;
 
 // (undocumented)
-export function endTaskSrc(taskName: string, startHrtime: [number, number], fileCount: number): void;
-
-// (undocumented)
-export function error(...args: Array<string | Chalk.ChalkChain>): void;
-
-// (undocumented)
-export function fileError(taskName: string, filePath: string, line: number, column: number, errorCode: string, message: string): void;
-
-// (undocumented)
-export function fileLog(write: (text: string) => void, taskName: string, filePath: string, line: number, column: number, errorCode: string, message: string): void;
-
-// (undocumented)
-export function fileWarning(taskName: string, filePath: string, line: number, column: number, errorCode: string,  message: string): void;
-
-// (undocumented)
 export function functionalTestRun(name: string, result: TestResultState, duration: number): void;
 
-// (undocumented)
-export function generateGulpError(error: Object): Object;
-
-class GenerateShrinkwrapTask extends GulpTask<{}> {
-  // (undocumented)
-  public executeTask(gulp: gulpType.Gulp): NodeJS.ReadWriteStream;
-  // (undocumented)
-  public name: string;
+class GenerateShrinkwrapTask extends GulpTask<void> {
+  constructor();
+  public executeTask(gulp: gulpType.Gulp, completeCallback: (result?: Object) => void): NodeJS.ReadWriteStream;
 }
 
 export function getConfig(): IBuildConfig;
@@ -59,60 +32,34 @@ export function getConfig(): IBuildConfig;
 export function getErrors(): string[];
 
 // (undocumented)
-export function getStart(): [number, number];
-
-// (undocumented)
 export function getWarnings(): string[];
 
-// (undocumented)
-export function getWatchMode(): boolean;
-
-// (undocumented)
 class GulpTask<TASK_CONFIG> implements IExecutable {
   protected _getConfigFilePath(): string;
-  protected _readCommentedJsonFile(filename: string): TASK_CONFIG;
-  // (undocumented)
   public buildConfig: IBuildConfig;
-  // (undocumented)
   public cleanMatch: string[];
-  // (undocumented)
   public copyFile(localSourcePath: string, localDestPath?: string): void;
-  // (undocumented)
   public execute(config: IBuildConfig): Promise<void>;
-  // (undocumented)
   public abstract executeTask(gulp: gulp.Gulp | GulpProxy, completeCallback?: (result?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
-  // (undocumented)
   public fileError(filePath: string, line: number, column: number, errorCode: string, message: string): void;
-  // (undocumented)
   public fileExists(localPath: string): boolean;
-  // (undocumented)
-  public fileWarning(filePath: string, line: number, column: number, errorCode: string, message: string): void;
-  // (undocumented)
+  public fileWarning(filePath: string, line: number, column: number, warningCode: string, message: string): void;
   public getCleanMatch(buildConfig: IBuildConfig, taskConfig: TASK_CONFIG = this.taskConfig): string[];
-  // (undocumented)
   public isEnabled(buildConfig: IBuildConfig): boolean;
-  public loadSchema(): Object;
-  // (undocumented)
+  protected loadSchema(): Object;
   public log(message: string): void;
-  // (undocumented)
   public logError(message: string): void;
-  // (undocumented)
   public logVerbose(message: string): void;
-  // (undocumented)
   public logWarning(message: string): void;
   public mergeConfig(taskConfig: TASK_CONFIG): void;
-  // (undocumented)
   public name: string;
-  // (undocumented)
   public onRegister(): void;
   // (undocumented)
   public readJSONSync(localPath: string): Object;
   public replaceConfig(taskConfig: TASK_CONFIG): void;
-  // (undocumented)
   public resolvePath(localPath: string): string;
   public readonly schema: Object;
   public setConfig(taskConfig: TASK_CONFIG): void;
-  // (undocumented)
   public taskConfig: TASK_CONFIG;
 }
 
@@ -147,11 +94,6 @@ interface IBuildConfig {
   verbose?: boolean;
 }
 
-// (undocumented)
-interface ICleanConfig {
-}
-
-// (undocumented)
 interface ICopyConfig {
   copyTo: {
     // (undocumented)
@@ -186,29 +128,11 @@ interface ISchemaValidatorResult {
   name?: string;
 }
 
-// (undocumented)
-export function log(...args: Array<string | Chalk.ChalkChain>): void;
-
-// (undocumented)
-export function logEndSubtask(name: string, startTime: [number, number], errorObject?: Error): void;
-
-// (undocumented)
-export function logStartSubtask(name: string): void;
-
-// (undocumented)
-export function logSummary(value: string): void;
-
-// (undocumented)
-export function markTaskCreationTime(): void;
-
 export function mergeConfig(config: IBuildConfig): void;
 
 export function parallel(...tasks: Array<IExecutable[] | IExecutable>): IExecutable;
 
 export function replaceConfig(config: IBuildConfig): void;
-
-// (undocumented)
-export function reset(): void;
 
 class SchemaValidator {
   // (undocumented)
@@ -222,12 +146,6 @@ class SchemaValidator {
 export function serial(...tasks: Array<IExecutable[] | IExecutable>): IExecutable;
 
 export function setConfig(config: IBuildConfig): void;
-
-// (undocumented)
-export function setExitCode(exitCode: number): void;
-
-// (undocumented)
-export function setWatchMode(): void;
 
 export function subTask(taskName: string, fn: ICustomGulpTask): IExecutable;
 
@@ -245,22 +163,11 @@ enum TestResultState {
   Skipped
 }
 
-class ValidateShrinkwrapTask extends GulpTask<{}> {
-  // (undocumented)
+class ValidateShrinkwrapTask extends GulpTask<void> {
+  constructor();
   public executeTask(gulp: gulpType.Gulp): NodeJS.ReadWriteStream;
-  // (undocumented)
-  public name: string;
 }
 
-// (undocumented)
-export function verbose(...args: Array<string | Chalk.ChalkChain>): void;
-
-// (undocumented)
-export function warn(...args: Array<string | Chalk.ChalkChain>): void;
-
 export function watch(watchMatch: string | string[], task: IExecutable): IExecutable;
-
-// (undocumented)
-export function writeError(e: any): void;
 
 // WARNING: Unsupported export: clean
--- gulp-core-build/src/tasks/GulpTask.ts
+++ gulp-core-build/src/tasks/GulpTask.ts
@@ -18,16 +18,33 @@ const eos = require('end-of-stream');
 import { args } from '../State';
 import { SchemaValidator } from '../jsonUtilities/SchemaValidator';
 
+/**
+ * The base GulpTask class, should be extended by any classes which represent build tasks.
+ * It provides convenient mechanisms for reading configuration files, validating their schema,
+ * etc. It also provides convenient utility and logging functions.
+ */
 export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
+  /** The name of the task. The configuration file with this name will be loaded and applied to the task. */
   public name: string;
+  /** The global build configuration object. Will be the same for all task instances. */
   public buildConfig: IBuildConfig;
+  /** The configuration for this task instance. */
   public taskConfig: TASK_CONFIG;
+  /**
+   * An overridable array of file patterns which will be utilized by the CleanTask to
+   * determine which files to delete. Unless overridden, the getCleanMatch() function
+   * will return this value.
+   */
   public cleanMatch: string[];
 
+  /**
+   * The memoized schema for this task. Should not be utilized by child classes, use schema property instead.
+   */
   private _schema: Object;
 
   /**
-   * A JSON Schema object which will be used to validate this task's configuration file
+   * A JSON Schema object which will be used to validate this task's configuration file.
+   * @returns a z-schema schema definition
    */
   public get schema(): Object {
     return this._schema ?
@@ -37,14 +54,17 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
 
   /**
    * Override this function to provide a schema which will be used to validate
-   * the task's configuration file.
+   * the task's configuration file. This function is called once per task instance.
+   * @returns a z-schema schema definition
    */
-  public loadSchema(): Object {
+  protected loadSchema(): Object {
     return undefined;
   };
 
   /**
    * Shallow merges config settings into the task config.
+   * Note this will override configuration options for those which are objects.
+   * @param taskConfig - configuration settings which should be applied
    */
   public setConfig(taskConfig: TASK_CONFIG): void {
     /* tslint:disable:typedef */
@@ -56,6 +76,8 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
 
   /**
    * Deep merges config settings into task config.
+   * Do not use this function if the configuration contains complex objects that cannot be merged.
+   * @param taskConfig - configuration settings which should be applied
    */
   public mergeConfig(taskConfig: TASK_CONFIG): void {
     /* tslint:disable:typedef */
@@ -66,12 +88,17 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
   }
 
   /**
-   * Replaces task config settings with new settings.
+   * Replaces all of the task config settings with new settings.
+   * @param taskConfig - the new task configuration
    */
   public replaceConfig(taskConfig: TASK_CONFIG): void {
     this.taskConfig = taskConfig;
   }
 
+  /**
+   * This function is called when the task is initially registered into gulp-core-build as a task or subtask. It reads
+   * the configuration file, validates it against the schema, then applies it to the task instance's configuration.
+   */
   public onRegister(): void {
     const configFilename: string = this._getConfigFilePath();
     const schema: Object = this.schema;
@@ -83,40 +110,98 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
     }
   }
 
+  /**
+   * Overridable function which returns true if this task should be executed, or false if it should be skipped.
+   * @param buildConfig - the build configuration which should be used when determining if the task is enabled
+   * @returns true if the build is not redundant
+   */
   public isEnabled(buildConfig: IBuildConfig): boolean {
     return (!buildConfig || !buildConfig.isRedundantBuild);
   }
 
+  /**
+   * When the task is executed by the build system, this function is called once. Note that this function
+   * must either return a Promise, a Stream, or call the completeCallback() parameter.
+   * @param gulp - an instance of the gulp library
+   * @param completeCallback - a callback which should be called if the function is non-value returning
+   * @returns a Promise, a Stream or undefined if completeCallback() is called
+   */
   public abstract executeTask(gulp: gulp.Gulp | GulpProxy, completeCallback?: (result?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
 
+  /**
+   * Logs a message to standard output.
+   * @param message - the message to log to standard output.
+   */
   public log(message: string): void {
     log(`[${gutil.colors.cyan(this.name)}] ${message}`);
   }
 
+  /**
+   * Logs a message to standard output if the verbose flag is specified.
+   * @param message - the message to log when in verbose mode
+   */
   public logVerbose(message: string): void {
     verbose(`[${gutil.colors.cyan(this.name)}] ${message}`);
   }
 
+  /**
+   * Logs a warning. It will be logged to standard error and cause the build to fail
+   * if buildConfig.shouldWarningsFailBuild is true, otherwise it will be logged to standard output.
+   * @param message - the warning description
+   */
   public logWarning(message: string): void {
     warn(`[${gutil.colors.cyan(this.name)}] ${message}`);
   }
 
+  /**
+   * Logs an error to standard error and causes the build to fail.
+   * @param message - the error description
+   */
   public logError(message: string): void {
     error(`[${gutil.colors.cyan(this.name)}] ${message}`);
   }
 
+  /**
+   * Logs an error regarding a specific file to standard error and causes the build to fail.
+   * @param filePath - the path to the file which encountered an issue
+   * @param line - the line in the file which had an issue
+   * @param column - the column in the file which had an issue
+   * @param errorCode - the custom error code representing this error
+   * @param message - a description of the error
+   */
   public fileError(filePath: string, line: number, column: number, errorCode: string, message: string): void {
     fileError(this.name, filePath, line, column, errorCode, message);
   }
 
-  public fileWarning(filePath: string, line: number, column: number, errorCode: string, message: string): void {
-    fileWarning(this.name, filePath, line, column, errorCode, message);
+  /**
+   * Logs a warning regarding a specific file.
+   * @param filePath - the path to the file which encountered an issue
+   * @param line - the line in the file which had an issue
+   * @param column - the column in the file which had an issue
+   * @param warningCode - the custom warning code representing this warning
+   * @param message - a description of the warning
+   */
+  public fileWarning(filePath: string, line: number, column: number, warningCode: string, message: string): void {
+    fileWarning(this.name, filePath, line, column, warningCode, message);
   }
 
+  /**
+   * An overridable function which returns a list of glob patterns representing files that should be deleted
+   * by the CleanTask.
+   * @param buildConfig - the current build configuration
+   * @param taskConfig - a task instance's configuration
+   */
   public getCleanMatch(buildConfig: IBuildConfig, taskConfig: TASK_CONFIG = this.taskConfig): string[] {
     return this.cleanMatch;
   }
 
+  /**
+   * This function is once to execute the task. It calls executeTask() and handles the return
+   * value from that function. It also provides some utilities such as logging how long each
+   * task takes to execute.
+   * @param config - the buildConfig which is applied to the task instance before execution\
+   * @returns a Promise which is completed when the task is finished executing
+   */
   public execute(config: IBuildConfig): Promise<void> {
     this.buildConfig = config;
 
@@ -193,6 +278,11 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
       });
   }
 
+  /**
+   * Resolves a path relative to the buildConfig.rootPath.
+   * @param localPath - a relative or absolute path
+   * @returns If localPath is relative, returns an absolute path relative to the rootPath. Otherwise, returns localPath.
+   */
   public resolvePath(localPath: string): string {
     /* tslint:disable:typedef */
     const path = require('path');
@@ -203,6 +293,11 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
     return path.resolve(path.join(this.buildConfig.rootPath, localPath));
   }
 
+  /**
+   * Synchronously detect if a file exists.
+   * @param localPath - the path to the file [resolved using resolvePath()]
+   * @returns true if the file exists, false otherwise
+   */
   public fileExists(localPath: string): boolean {
     /* tslint:disable:typedef */
     const fs = require('fs');
@@ -217,6 +312,11 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
     return doesExist;
   }
 
+  /**
+   * Copy a file from one location to another.
+   * @param localSourcePath - path to the source file
+   * @param localDestPath - path to the destination file
+   */
   public copyFile(localSourcePath: string, localDestPath?: string): void {
     /* tslint:disable:typedef */
     const path = require('path');
@@ -255,28 +355,23 @@ export abstract class GulpTask<TASK_CONFIG> implements IExecutable {
   }
 
   /**
-   * Loads a JSON file, supports reading JSON files with comments
+   * Helper function which loads a custom configuration file from disk and validates it against the schema
+   * @param filePath - the path to the custom configuration file
+   * @param schema - the z-schema schema object used to validate the configuration file
+   * @returns If the configuration file is valid, returns the configuration as an object.
    */
-  protected _readCommentedJsonFile(filename: string): TASK_CONFIG {
-    return SchemaValidator.readCommentedJsonFile(filename) as TASK_CONFIG;
-  }
-
-  /**
-   * Helper function which loads a custom config file from disk, runs the SchemaValidator on it,
-   * and then apply it to the callback defined on the IConfigurableTask
-   */
-  private _readConfigFile(filename: string, schema?: Object): TASK_CONFIG {
-    if (!fs.existsSync(filename)) {
+  private _readConfigFile(filePath: string, schema?: Object): TASK_CONFIG {
+    if (!fs.existsSync(filePath)) {
       return undefined;
     } else {
       if (args['verbose']) { // tslint:disable-line:no-string-literal
-        console.log(`Found config file: ${path.basename(filename)}`);
+        console.log(`Found config file: ${path.basename(filePath)}`);
       }
 
-      const rawData: TASK_CONFIG =  this._readCommentedJsonFile(filename);
+      const rawData: TASK_CONFIG = SchemaValidator.readCommentedJsonFile<TASK_CONFIG>(filePath);
 
       if (schema) {
-        SchemaValidator.validate(rawData, schema, filename);
+        SchemaValidator.validate(rawData, schema, filePath);
       }
 
       return rawData;
