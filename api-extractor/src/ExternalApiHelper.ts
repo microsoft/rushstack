56b81423d3a3e3c3001e7eaeb41e5e413b8667c5
dgaeta <gaeta.d@gmail.com>
2017-01-23 18:06:09 -0800

    Moved logic from gulpfile to ExternalApiHelper

--- api-extractor/src/ExternalApiHelper.ts
+++ api-extractor/src/ExternalApiHelper.ts
@@ -1,17 +1,33 @@
 import * as ts from 'typescript';
 import * as os from 'os';
+import * as path from 'path';
+import * as fs from 'fs';
 import Analyzer from './Analyzer';
 import ApiJsonGenerator from './generators/ApiJsonGenerator';
 
 export default class ExternalApiHelper {
 
-  public static generateApiJson(rootDir: string, entryPointFile: string, outputApiJsonFilePath: string): void {
+  public static generateApiJson(rootDir: string, libFolder: string, externalPackageFilePath: string): void {
     const analyzer: Analyzer = new Analyzer(
         (message: string, fileName: string, lineNumber: number): void => {
           console.log(`TypeScript error: ${message}` + os.EOL
             + `  ${fileName}#${lineNumber}`);
         }
       );
+
+    let outputPath: string = path.join(rootDir, libFolder);
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath);
+    }
+
+    outputPath = path.join(outputPath, 'external-api-json');
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath);
+    }
+
+    const externalPackageRootDir: string = path.dirname(externalPackageFilePath);
+    const outputApiJsonFilePath: string = path.join(outputPath, `${path.basename(externalPackageRootDir)}.api.json`);
+    const entryPointFile: string = path.join(rootDir, externalPackageFilePath);
 
     analyzer.analyze({
       compilerOptions: {
