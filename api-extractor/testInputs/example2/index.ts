0c807c95fefbb9af775aefd65b72b2a25381d6ae
dgaeta <gaeta.d@gmail.com>
2017-01-25 12:40:06 -0800

    Added: remarks to package doc, .d.ts warning for no package doc, cleaned up comments and type casting

--- api-extractor/testInputs/example2/index.ts
+++ api-extractor/testInputs/example2/index.ts
@@ -1,5 +1,7 @@
 /**
  * Here is some documentation for example2.
+ * @remarks These are additional remarks that may be too long for the summary.
+ * They should appear in the remarks of the json generated file for this package.
  */
 declare const packageDescription: void;
 
