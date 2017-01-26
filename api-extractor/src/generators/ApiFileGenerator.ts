9e58df61e8eca8b1e8a694059652d1306758178a
dgaeta <dgaeta@users.noreply.github.com>
2017-01-25 20:11:27 -0800

    Enabled ApiExtractor for ApiExtractor

--- api-extractor/src/generators/ApiFileGenerator.ts
+++ api-extractor/src/generators/ApiFileGenerator.ts
@@ -40,8 +40,8 @@ export default class ApiFileGenerator extends ApiItemVisitor {
 
   /**
    * Generates the report and writes it to disk.
-   * @param reportFilename   The output filename
-   * @param analyzer         An Analyzer object representing the input project.
+   * @param reportFilename - The output filename
+   * @param analyzer       - An Analyzer object representing the input project.
    */
   public writeApiFile(reportFilename: string, analyzer: Analyzer): void {
     const fileContent: string = this.generateApiFileContent(analyzer);
