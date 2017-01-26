5555b7beabc8385d95a4ed070d9d86f4e3ba43af
dgaeta <dgaeta@users.noreply.github.com>
2017-01-26 14:07:05 -0800

    Renamed Analyzer to Extractor

--- api-extractor/src/index.ts
+++ api-extractor/src/index.ts
@@ -5,7 +5,7 @@
  */
 declare const packageDescription: void;
 
-export { default as Analyzer, IApiAnalyzerOptions, ApiErrorHandler } from './Analyzer';
+export { default as Extractor, IExtractorOptions, IApiAnalyzerOptions, ApiErrorHandler } from './Extractor';
 export { default as ApiFileGenerator  } from './generators/ApiFileGenerator';
 export { default as ApiJsonGenerator  } from './generators/ApiJsonGenerator';
 export { default as ExternalApiHelper } from './ExternalApiHelper';
