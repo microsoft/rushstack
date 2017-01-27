dfa37c42690354a128e3e812496ede6d44dd6c5d
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-01-27 15:25:24 -0800

    Karma: automatically create the tests.js file with a user-specified RegExp pattern (#70)
    * Automatically create the tests.js file in the tempFolder with a pattern from taskConfig
    
    * Automatically create the tests.js file in the temp folder with a user-specified RegExp
    
    * Remove unneeded code
    
    * Changefile
    
    * Remove commented out code
    
    * Add a try catch block around the RegExp constructor

--- test-web-library-build/gulpfile.js
+++ test-web-library-build/gulpfile.js
@@ -5,7 +5,6 @@ let build = require('@microsoft/web-library-build');
 
 build.sass.setConfig({ useCSSModules: true });
 build.webpack.setConfig({ configPath: null });
-build.karma.setConfig({ configPath: null });
 
 build.setConfig({
   libAMDFolder: path.join(build.getConfig().packageFolder, 'lib-amd')
