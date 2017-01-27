9f0bd91c3d6e7766899c863ca32c949598b38917
OneDrive Build <odbuild@microsoft.com>
2017-01-27 20:04:15 +0000

    Applying package updates.

---
+++ test-web-library-build/src/test.scss.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+import { loadStyles } from '@microsoft/load-themed-styles';
+
+const styles = {
+  foo: 'foo_3d5b8190',
+};
+
+export default styles;
+
+loadStyles([{"rawString":"body{background:red}.foo_3d5b8190{border:1px solid red}"}]);
+/* tslint:enable */
