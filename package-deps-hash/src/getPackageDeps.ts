f37c78a00b5e9cb20eb2ab41457b4d171509566f
Nick Pape <nickpape-msft@users.noreply.github.com>
2017-01-27 18:42:06 -0800

    Run packagedeps hash synchronously, the async version has an issue where it doesn't capture the entire output

--- package-deps-hash/src/getPackageDeps.ts
+++ package-deps-hash/src/getPackageDeps.ts
@@ -11,63 +11,61 @@ export function getPackageDeps(packagePath: string = process.cwd(), excludedPath
   }
 
   return new Promise((complete) => {
-    child_process.exec(
+    const stdout: string = child_process.execSync(
       `git ls-tree HEAD -r && echo ${PROCESS_OUTPUT_DELIMITER} && git status -s -u .`,
-      { cwd: packagePath },
-      (error: Error, stdout: string) => {
-        const changes: IPackageDeps = {
-          files: {}
-        };
-        const processOutputBlocks: string[] = stdout.split(PROCESS_OUTPUT_DELIMITER);
+      { cwd: packagePath }).toString();
 
-        // Note: The output of git ls-tree uses \n newlines regardless of OS.
-        processOutputBlocks[0].split('\n').forEach(line => {
-          if (line) {
-            const parts: string[] = line.substr(line.indexOf('blob ') + 5).split('\t');
-            if (!excludedHashes[parts[1]]) {
-              changes.files[parts[1]] = parts[0];
+    const changes: IPackageDeps = {
+      files: {}
+    };
+    const processOutputBlocks: string[] = stdout.split(PROCESS_OUTPUT_DELIMITER);
+
+    // Note: The output of git ls-tree uses \n newlines regardless of OS.
+    processOutputBlocks[0].split('\n').forEach(line => {
+      if (line) {
+        const parts: string[] = line.substr(line.indexOf('blob ') + 5).split('\t');
+        if (!excludedHashes[parts[1]]) {
+          changes.files[parts[1]] = parts[0];
+        }
+      }
+    });
+
+    if (processOutputBlocks[1]) {
+      const filesToHash: string[] = [];
+
+      // Note: The output of git hash-object uses \n newlines regardless of OS.
+      processOutputBlocks[1]
+        .trim()
+        .split('\n')
+        .forEach(line => {
+          const parts: string[] = line.trim().split(' ');
+
+          if (parts.length === 2) {
+            if (parts[0] === 'D') {
+              delete changes.files[parts[1]];
+            } else {
+              if (!excludedHashes[parts[1]]) {
+                filesToHash.push(parts[1]);
+              }
             }
           }
         });
 
-        if (processOutputBlocks[1]) {
-          const filesToHash: string[] = [];
+      if (filesToHash.length) {
+        const hashStdout: string = child_process.execSync(
+          'git hash-object ' + filesToHash.join(' '),
+          { cwd: packagePath }).toString();
 
-          // Note: The output of git hash-object uses \n newlines regardless of OS.
-          processOutputBlocks[1]
-            .trim()
-            .split('\n')
-            .forEach(line => {
-              const parts: string[] = line.trim().split(' ');
+          const hashes: string[] = hashStdout.split('\n');
 
-              if (parts.length === 2) {
-                if (parts[0] === 'D') {
-                  delete changes.files[parts[1]];
-                } else {
-                  if (!excludedHashes[parts[1]]) {
-                    filesToHash.push(parts[1]);
-                  }
-                }
-              }
-            });
+          filesToHash.forEach((filename, i) => changes.files[filename] = hashes[i]);
 
-          if (filesToHash.length) {
-            child_process.exec(
-              'git hash-object ' + filesToHash.join(' '),
-              { cwd: packagePath },
-              (hashError: Error, hashStdout: string) => {
-                const hashes: string[] = hashStdout.split('\n');
-
-                filesToHash.forEach((filename, i) => changes.files[filename] = hashes[i]);
-
-                complete(changes);
-              });
-          } else {
-            complete(changes);
-          }
-        } else {
           complete(changes);
-        }
-      });
+      } else {
+        complete(changes);
+      }
+    } else {
+      complete(changes);
+    }
   });
 }
