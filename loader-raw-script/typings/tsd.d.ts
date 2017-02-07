b3e6b8d7835f559cf0d4e297833489798fc31a47
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2017-02-06 21:21:18 -0800

    Fixing build of loader-raw-script

--- loader-raw-script/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
