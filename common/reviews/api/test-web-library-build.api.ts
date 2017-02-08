c447d490a544f8bbcb63ffb1d1f58a4e76f85bb5
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-02-07 17:05:33 -0800

    API Files

--- common/reviews/api/test-web-library-build.api.ts
+++ common/reviews/api/test-web-library-build.api.ts
@@ -1,7 +1,10 @@
+// (undocumented)
 export function add(num1: number, num2: number): number;
 
+// (undocumented)
 export function log(message: string): void;
 
+// (undocumented)
 export function logClass(): void;
 
 // (No packageDescription for this package)
