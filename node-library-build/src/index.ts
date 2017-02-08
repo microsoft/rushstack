b258fa56be63177889a429d9007dc103bdaea04c
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-02-07 16:46:16 -0800

    Also treat tslint as warnings

--- node-library-build/src/index.ts
+++ node-library-build/src/index.ts
@@ -7,13 +7,18 @@ export * from '@microsoft/gulp-core-build-typescript';
 export * from '@microsoft/gulp-core-build-mocha';
 
 // Define default task groups.
-const buildSubtask: IExecutable = serial(parallel(tslint, typescript), apiExtractor);
 
 const PRODUCTION = process.argv.indexOf('--production') !== -1 || process.argv.indexOf('--ship') !== -1;
 setConfig({
+  production: PRODUCTION,
   shouldWarningsFailBuild: PRODUCTION
 });
 
+tslint.mergeConfig({
+  displayAsWarning: true
+});
+
+const buildSubtask: IExecutable = serial(parallel(tslint, typescript), apiExtractor);
 export const buildTasks: IExecutable = task('build', buildSubtask);
 export const testTasks: IExecutable = task('test', serial(buildSubtask, mocha));
 export const defaultTasks: IExecutable = task('default', serial(buildSubtask, instrument, mocha));
