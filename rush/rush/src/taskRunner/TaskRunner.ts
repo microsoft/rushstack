70469278daeb54ffe453dd5570acecd695be3db8
nickpape-msft <nickpape-msft@users.noreply.github.com>
2017-02-10 21:04:12 +0000

    Merged PR 42073: Revert "Update Rush TaskRunner endCondition"
    Revert "Update Rush TaskRunner endCondition"

--- rush/rush/src/taskRunner/TaskRunner.ts
+++ rush/rush/src/taskRunner/TaskRunner.ts
@@ -152,8 +152,13 @@ export default class TaskRunner {
    * It calls the complete callback when all tasks are completed, or rejects if any task fails.
    */
   private _startAvailableTasks(complete: () => void, reject: (err?: Object) => void): void {
-    if (this._checkForEndCondition()) {
-      return;
+    if (!this._areAnyTasksReadyOrExecuting()) {
+      this._printTaskStatus();
+      if (this._hasAnyFailures) {
+        reject();
+      } else {
+        complete();
+      }
     }
 
     let ctask: ITask;
@@ -200,20 +205,6 @@ export default class TaskRunner {
           this._startAvailableTasks(complete, reject);
         }
       );
-    }
-
-    this._checkForEndCondition();
-  }
-
-  private _checkForEndCondition(): boolean {
-    if (!this._areAnyTasksReadyOrExecuting()) {
-      this._printTaskStatus();
-      if (this._hasAnyFailures) {
-        reject();
-      } else {
-        complete();
-      }
-      return true;
     }
   }
 
