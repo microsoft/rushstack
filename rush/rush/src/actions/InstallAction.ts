e1edc4c4eca0655a89e835ba2722a36b0f26e665
Nick Pape <nickpape@users.noreply.github.com>
2017-02-13 14:56:06 -0800

    Fix copyright

--- rush/rush/src/actions/InstallAction.ts
+++ rush/rush/src/actions/InstallAction.ts
@@ -1,6 +1,5 @@
-/**
- * @Copyright (c) Microsoft Corporation.  All rights reserved.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.		 +/**
+// See LICENSE in the project root for license information.
 
 import * as colors from 'colors';
 import * as fsx from 'fs-extra';
