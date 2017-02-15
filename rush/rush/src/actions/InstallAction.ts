microsoft/rushstack

commit a2d53d47861ad0e038a688572edb784a1254edd6

Ensure that the temp_modules are up to date before an install (#102)
* Ensure that the temp_modules are up to date before an install

* Small fix

* Fix copyright

* Changefile & comments

* Fixup spacing to be less sketchy

* PR Feedback

* Update error message

rush/rush/src/actions/InstallAction.ts
@@ -7,19 +7,30 @@ import * as glob from 'glob';
 import globEscape = require('glob-escape');
 import * as os from 'os';
 import * as path from 'path';
+import * as _ from 'lodash';
+
 import { CommandLineAction, CommandLineFlagParameter } from '@microsoft/ts-command-line';
 import {
   JsonFile,
   RushConfiguration,
+  RushConfigurationProject,
   Utilities,
   Stopwatch,
-  AsyncRecycle
+  AsyncRecycle,
+  IPackageJson
 } from '@microsoft/rush-lib';
 
 import RushCommandLineParser from './RushCommandLineParser';
-import GitPolicy from './GitPolicy';
+import GitPolicy from '../utilities/GitPolicy';
+import { TempModuleGenerator } from '../utilities/TempModuleGenerator';
 
 const MAX_INSTALL_ATTEMPTS: number = 5;
+
+interface ITempModuleInformation {
+  packageJson: IPackageJson;
+  existsInProjectConfiguration: boolean;
+  filename: string;
+}
 
 export default class InstallAction extends CommandLineAction {
   private _parser: RushCommandLineParser;
@@ -27,6 +38,8 @@ export default class InstallAction extends CommandLineAction {
   private _cleanInstall: CommandLineFlagParameter;
   private _cleanInstallFull: CommandLineFlagParameter;
   private _bypassPolicy: CommandLineFlagParameter;
+
+  private _tempModulesFiles: string[] = [];
 
   public static ensureLocalNpmTool(rushConfiguration: RushConfiguration, cleanInstall: boolean): void {
     // Example: "C:\Users\YourName\.rush"
@@ -132,12 +145,87 @@ export default class InstallAction extends CommandLineAction {
 
     console.log('Starting "rush install"' + os.EOL);
 
+    // Example: "C:\MyRepo\common\temp_modules\rush-example-project\package.json"
+    const normalizedPath: string = Utilities.getAllReplaced(this._rushConfiguration.tempModulesFolder, '\\', '/');
+    const globPattern: string = `${globEscape(normalizedPath)}/rush-*/package.json`;
+    this._tempModulesFiles = glob.sync(globPattern, { nodir: true });
+
+    this._checkThatTempModulesMatch();
+
     InstallAction.ensureLocalNpmTool(this._rushConfiguration, this._cleanInstallFull.value);
     this._installCommonModules();
 
     stopwatch.stop();
     console.log(colors.green(`The common NPM packages are up to date. (${stopwatch.toString()})`));
     console.log(os.EOL + 'Next you should probably run: "rush link"');
+  }
+
+  private _checkThatTempModulesMatch(): void {
+    // read all temp_modules
+    const tempModules: { [packageName: string]: ITempModuleInformation } = {};
+
+    this._tempModulesFiles.forEach((filename) => {
+      const contents: IPackageJson = require(filename);
+      tempModules[contents.name] = {
+        packageJson: contents,
+        existsInProjectConfiguration: false,
+        filename: filename
+      };
+    });
+
+    // 1st ensure that every temp_module exists in the configuration
+    Object.keys(tempModules).forEach((tempModuleName: string) => {
+      const tempModule: ITempModuleInformation = tempModules[tempModuleName];
+
+      let foundMatchingProject: boolean;
+      this._rushConfiguration.projects.forEach((project: RushConfigurationProject) => {
+        if (project.tempProjectName === tempModuleName) {
+          foundMatchingProject = true;
+        }
+      });
+
+      if (!foundMatchingProject) {
+        throw new Error(`The file "${tempModule.filename}" exists in the temp_modules folder ` +
+          `but we could not find a matching project for it. This file may need to be deleted.` +
+          `\n\nDid you forget to run 'rush generate'?`);
+      }
+    });
+
+    const expectedTempModules: Map<string, IPackageJson> = new TempModuleGenerator(this._rushConfiguration).tempModules;
+
+    // 2nd ensure that each config project has a temp_module which matches the expected value
+    this._rushConfiguration.projects.forEach((project: RushConfigurationProject) => {
+      //   if no temp_module, throw
+
+      const tempModule: ITempModuleInformation
+        = tempModules[project.tempProjectName];
+      if (!tempModule) {
+        throw new Error(`The project ${project.packageName} is missing a corresponding ` +
+          `file in the temp_modules folder.` +
+          `\n\nDid you forget to run 'rush generate'?`);
+      }
+
+      // Generate an expected temp_modules package.json & compare
+      const expectedTempModule: IPackageJson = expectedTempModules.get(project.packageName);
+
+      if (!_.isEqual(expectedTempModule, tempModule.packageJson)) {
+        // wordwrap attempts to remove any leading spaces, however, we are attempting to serialize
+        // some JSON information into the error, and therefore we want to maintain proper spacing.
+        // the workaround is to wrap our spaces in the non-breaking character
+
+        const errorMsg: string = `The project ${project.packageName}'s temp_module is outdated`;
+        const rerunGenerate: string = '\nDid you forget to run rush generate?';
+
+        console.log(colors.red(`${errorMsg}:\n`));
+        console.log(colors.red(`EXPECTED:\n${JSON.stringify(expectedTempModule, undefined, 2)}\n`));
+        console.log(colors.red(`ACTUAL: \n${JSON.stringify(tempModule.packageJson, undefined, 2)}`));
+
+        throw new Error(errorMsg + '\n' + rerunGenerate);
+      }
+    });
+
+    console.log('ALL DONE!');
+    process.exit(1);
   }
 
   private _installCommonModules(): void {
@@ -196,10 +284,8 @@ export default class InstallAction extends CommandLineAction {
       // or deleted it entirely, then isFileTimestampCurrent() will cause us to redo "npm install".
       packageJsonFilenames.push(commonNodeModulesFolder);
 
-      // Example: "C:\MyRepo\common\temp_modules\rush-example-project\package.json"
-      const normalizedPath: string = Utilities.getAllReplaced(this._rushConfiguration.tempModulesFolder, '\\', '/');
-      const globPattern: string = `${globEscape(normalizedPath)}/rush-*/package.json`;
-      packageJsonFilenames.push(...glob.sync(globPattern, { nodir: true }));
+      // Make sure we look at all the temp_modules/*/package.json files as well
+      packageJsonFilenames.push(...this._tempModulesFiles);
 
       if (!Utilities.isFileTimestampCurrent(commonNodeModulesMarkerFilename, packageJsonFilenames)) {
         needToInstall = true;

rush/rush-lib/src/data/test/VersionMismatchFinder.test.ts
@@ -28,7 +28,6 @@ describe('VersionMismatchFinder', () => {
       }
     ] as any as RushConfigurationProject[]; // tslint:disable-line:no-any
     const mismatchFinder: VersionMismatchFinder = new VersionMismatchFinder(projects);
-    console.log(mismatchFinder.getMismatches());
     assert.isNumber(mismatchFinder.numberOfMismatches);
     assert.equal(mismatchFinder.numberOfMismatches, 0);
     assert.equal(mismatchFinder.getMismatches().length, 0);

rush/rush/src/actions/ChangeAction.ts
@@ -24,7 +24,7 @@ import {
 } from '@microsoft/rush-lib';
 
 import RushCommandLineParser from './RushCommandLineParser';
-import PublishUtilities from './PublishUtilities';
+import PublishUtilities from '../utilities/PublishUtilities';
 
 const BUMP_OPTIONS: { [type: string]: string } = {
   'major': 'major - for breaking changes (ex: renaming a file)',

rush/rush/src/actions/GenerateAction.ts
@@ -6,7 +6,6 @@ import * as glob from 'glob';
 import globEscape = require('glob-escape');
 import * as os from 'os';
 import * as path from 'path';
-import * as semver from 'semver';
 import * as fsx from 'fs-extra';
 import { CommandLineAction, CommandLineFlagParameter } from '@microsoft/ts-command-line';
 import {
@@ -21,7 +20,8 @@ import {
 
 import InstallAction from './InstallAction';
 import RushCommandLineParser from './RushCommandLineParser';
-import PackageReviewChecker from './PackageReviewChecker';
+import PackageReviewChecker from '../utilities/PackageReviewChecker';
+import { TempModuleGenerator } from '../utilities/TempModuleGenerator';
 
 export default class GenerateAction extends CommandLineAction {
   private _parser: RushCommandLineParser;
@@ -85,6 +85,9 @@ export default class GenerateAction extends CommandLineAction {
     sortedRushProjects.sort(
       (a: RushConfigurationProject, b: RushConfigurationProject) => a.tempProjectName.localeCompare(b.tempProjectName)
     );
+
+    const tempModules: Map<string, IPackageJson> = new TempModuleGenerator(rushConfiguration).tempModules;
+
     for (const rushProject of sortedRushProjects) {
       const packageJson: PackageJson = rushProject.packageJson;
 
@@ -97,69 +100,7 @@ export default class GenerateAction extends CommandLineAction {
 
       const tempPackageJsonFilename: string = path.join(tempProjectFolder, 'package.json');
 
-      const tempPackageJson: IPackageJson = {
-        name: tempProjectName,
-        version: '0.0.0',
-        private: true,
-        dependencies: {}
-      };
-
-      // If there are any optional dependencies, copy them over directly
-      if (packageJson.optionalDependencies) {
-        tempPackageJson.optionalDependencies = packageJson.optionalDependencies;
-      }
-
-      // Collect pairs of (packageName, packageVersion) to be added as temp package dependencies
-      const pairs: { packageName: string, packageVersion: string }[] = [];
-
-      // If there are devDependencies, we need to merge them with the regular
-      // dependencies.  If the same library appears in both places, then the
-      // regular dependency takes precedence over the devDependency.
-      // It also takes precedence over a duplicate in optionalDependencies,
-      // but NPM will take care of that for us.  (Frankly any kind of duplicate
-      // should be an error, but NPM is pretty lax about this.)
-      if (packageJson.devDependencies) {
-        for (const packageName of Object.keys(packageJson.devDependencies)) {
-          pairs.push({ packageName: packageName, packageVersion: packageJson.devDependencies[packageName] });
-        }
-      }
-
-      if (packageJson.dependencies) {
-        for (const packageName of Object.keys(packageJson.dependencies)) {
-          pairs.push({ packageName: packageName, packageVersion: packageJson.dependencies[packageName] });
-        }
-      }
-
-      for (const pair of pairs) {
-        // Is there a locally built Rush project that could satisfy this dependency?
-        // If so, then we will symlink to the project folder rather than to common/node_modules.
-        // In this case, we don't want "npm install" to process this package, but we do need
-        // to record this decision for "rush link" later, so we add it to a special 'rushDependencies' field.
-        const localProject: RushConfigurationProject = rushConfiguration.getProjectByName(pair.packageName);
-        if (localProject) {
-
-          // Don't locally link if it's listed in the cyclicDependencyProjects
-          if (!rushProject.cyclicDependencyProjects.has(pair.packageName)) {
-
-            // Also, don't locally link if the SemVer doesn't match
-            const localProjectVersion: string = localProject.packageJson.version;
-            if (semver.satisfies(localProjectVersion, pair.packageVersion)) {
-
-              // We will locally link this package
-              if (!tempPackageJson.rushDependencies) {
-                tempPackageJson.rushDependencies = {};
-              }
-              tempPackageJson.rushDependencies[pair.packageName] = pair.packageVersion;
-              continue;
-            }
-          }
-        }
-
-        // We will NOT locally link this package; add it as a regular dependency.
-        tempPackageJson.dependencies[pair.packageName] = pair.packageVersion;
-      }
-
-      JsonFile.saveJsonFile(tempPackageJson, tempPackageJsonFilename);
+      JsonFile.saveJsonFile(tempModules.get(rushProject.packageName), tempPackageJsonFilename);
     }
 
     console.log('Writing common/package.json');

rush/rush/src/actions/LinkAction.ts
@@ -20,7 +20,7 @@ import {
   Stopwatch
 } from '@microsoft/rush-lib';
 
-import PackageLookup from './PackageLookup';
+import PackageLookup from '../utilities/PackageLookup';
 import RushCommandLineParser from './RushCommandLineParser';
 
 export default class LinkAction extends CommandLineAction {

rush/rush/src/actions/PublishAction.ts
@@ -22,8 +22,8 @@ import {
 import RushCommandLineParser from './RushCommandLineParser';
 import PublishUtilities, {
   IChangeInfoHash
-} from './PublishUtilities';
-import ChangelogGenerator from './ChangelogGenerator';
+} from '../utilities/PublishUtilities';
+import ChangelogGenerator from '../utilities/ChangelogGenerator';
 
 export default class PublishAction extends CommandLineAction {
   private _addCommitDetails: CommandLineFlagParameter;

commit 656413affdb78abd169aacf8c7c22019f8165a72

The office-ui-fabric-react Travis builds were broken because the evergreen bot is bumping package.json without running "rush generate", which causes _checkThatTempModulesMatch() to fail.  This is a regression caused by PR #102.  Technically the bot *should* run "rush generate" after bumping the version, but insisting on that would be unreasonable because it would break the scenario.
I'm temporarily reverting this feature until we can sort it out.

rush/rush/src/actions/InstallAction.ts
@@ -150,7 +150,8 @@ export default class InstallAction extends CommandLineAction {
     const globPattern: string = `${globEscape(normalizedPath)}/rush-*/package.json`;
     this._tempModulesFiles = glob.sync(globPattern, { nodir: true });
 
-    this._checkThatTempModulesMatch();
+    // TEMPORARILY DISABLED DUE TO REGRESSION (VSO 313164)
+    // this._checkThatTempModulesMatch();
 
     InstallAction.ensureLocalNpmTool(this._rushConfiguration, this._cleanInstallFull.value);
     this._installCommonModules();