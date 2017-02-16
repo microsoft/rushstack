669a2456855b9af5c7dbed0a8b0644c3911a0af1
Nick Pape <nickpape@users.noreply.github.com>
2017-02-16 15:50:46 -0800

    Lint errors

--- gulp-core-build/src/tasks/GulpTask.ts
+++ gulp-core-build/src/tasks/GulpTask.ts
@@ -2,7 +2,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-import { getConfig } from '../index';
 import { GulpProxy } from '../GulpProxy';
 import { IExecutable } from '../IExecutable';
 import { IBuildConfig } from '../IBuildConfig';
