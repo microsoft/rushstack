752fdf4c8edac9a6bb905568c260699a52c29ad3
Nick Pape <nickpape@users.noreply.github.com>
2017-02-24 12:24:54 -0800

    tslint

--- rush/rush/src/actions/RebuildAction.ts
+++ rush/rush/src/actions/RebuildAction.ts
@@ -123,7 +123,8 @@ export default class RebuildAction extends CommandLineAction {
     this._rushLinkJson = JsonFile.loadJsonFile(this._rushConfiguration.rushLinkJsonFilename);
 
     if (this._quietParameter.value) {
-      console.log(colors.yellow(`DEPRECATED: The --quiet parameter will be removed soon, because builds are now quiet by default`));
+      console.log(colors.yellow(`DEPRECATED: The --quiet parameter will be removed soon, ` +
+        `because builds are now quiet by default`));
     }
 
     console.log(`Starting "rush ${this.options.actionVerb}"` + os.EOL);
