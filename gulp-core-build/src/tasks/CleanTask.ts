f358a414af665e8ba59028e0ce0c675830ad29fa
Nick Pape <nickpape@users.noreply.github.com>
2017-03-16 17:50:06 -0700

    Improve the clean task to work around the issue with del

--- gulp-core-build/src/tasks/CleanTask.ts
+++ gulp-core-build/src/tasks/CleanTask.ts
@@ -1,8 +1,8 @@
 import { GulpTask } from './GulpTask';
 import gulp = require('gulp');
-import globby = require('globby');
 import * as path from 'path';
-import * as globEscape from 'glob-escape';
+
+import { FileDeletionUtility } from '../utilities/FileDeletionUtility';
 
 /**
  * The clean task is a special task which iterates through all registered
@@ -25,10 +25,8 @@ export class CleanTask extends GulpTask<void> {
     gulp: gulp.Gulp,
     completeCallback: (result?: Object) => void
   ): void {
-    /* tslint:disable:typedef */
-    const del = require('del');
-    /* tslint:disable:typedef */
 
+    // tslint:disable:typedef
     const { distFolder, libFolder, libAMDFolder, tempFolder } = this.buildConfig;
     let cleanPaths = [
       distFolder,
@@ -62,61 +60,11 @@ export class CleanTask extends GulpTask<void> {
       }
     }
 
-    // Appears to be a known issue with `del` whereby
-    // if you ask to delete both a folder, and something in the folder,
-    // it randomly chooses which one to delete first, which can cause
-    // the function to fail sporadically. The fix for this is simple:
-    // we need to remove any cleanPaths which exist under a folder we
-    // are attempting to delete
-    const fileMatches: string[] = globby.sync(cleanPaths);
-
-    // First we sort the list of files. We know that if something is a file,
-    // if matched, the parent folder should appear earlier in the list
-    fileMatches.sort();
-
-    if (fileMatches.length > 0) {
-      // We need to determine which paths exist under other paths, and remove them from the
-      // list of files to delete
-      const filesToDelete: string[] = [];
-
-      // current working directory
-      let curDir = undefined;
-
-      for (let i: number = 0; i < fileMatches.length; i++) {
-        const curFile: string = fileMatches[i];
-        if (this.isParentDirectory(curDir, curFile)) {
-          continue;
-        } else {
-          filesToDelete.push(globEscape(curFile));
-          curDir = curFile;
-        }
-      }
-
-      del(filesToDelete)
-        .then(() => completeCallback())
-        .catch((error) => completeCallback(error));
-    } else {
+    try {
+      FileDeletionUtility.deletePatterns(cleanPaths);
       completeCallback();
+    } catch (e) {
+      completeCallback(e);
     }
-  }
-
-  private isParentDirectory(directory: string, filepath: string): boolean {
-    if (!directory || !filepath) {
-      return false;
-    }
-
-    const directoryParts: string[] = path.resolve(directory).split(path.sep);
-    const fileParts: string[] = path.resolve(filepath).split(path.sep);
-
-    if (directoryParts.length >= fileParts.length) {
-      return false;
-    }
-
-    for (let i: number = 0; i < directoryParts.length; i++) {
-      if (directoryParts[i] !== fileParts[i]) {
-        return false;
-      }
-    }
-    return true;
   }
 }
