c2ef35f7d716499d34744c1748c58b87c9258048
qz2017 <qz2017@users.noreply.github.com>
2017-03-29 14:41:49 -0700

    Detect published version for packages before publishing.

---
+++ rush/rush-lib/src/utilities/Npm.ts
@@ -0,0 +1,28 @@
+import Utilities from './Utilities';
+
+export default class Npm {
+  public static publishedVersions(
+    packageName: string,
+    cwd: string,
+    env: { [key: string]: string }
+  ): string[] {
+    const versions: string[] = [];
+    try {
+      const packageTime: string = Utilities.executeCommandAndCaptureOutput('npm',
+        `view ${packageName} time --json`.split(' '),
+        cwd,
+        env);
+      Object.keys(JSON.parse(packageTime)).forEach(v => {
+        versions.push(v);
+      });
+    } catch (error) {
+      if (error.message.indexOf('npm ERR! 404') >= 0) {
+        console.log(`Package ${packageName} does not exist in the registry.`);
+      } else {
+        console.log(`Failed to get npm information about ${packageName}.`);
+        throw error;
+      }
+    }
+    return versions;
+  }
+}
