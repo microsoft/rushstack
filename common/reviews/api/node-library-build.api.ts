6b23dff0b6defdbe3a2260f95e22beab0c12ff89
dgaeta <dgaeta@users.noreply.github.com>
2017-04-12 16:33:33 -0700

    Updating *api.ts files after rush rebuild

--- common/reviews/api/node-library-build.api.ts
+++ common/reviews/api/node-library-build.api.ts
@@ -1,52 +1,24 @@
 export declare function addSuppression(str: string): void;
 
-// WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-// WARNING: Unable to resolve external type reference for "IApiExtractorTaskConfig"
 class ApiExtractorTask extends GulpTask<IApiExtractorTaskConfig> {
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulp.Gulp"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   // (undocumented)
   executeTask(gulp: gulp.Gulp, completeCallback: (error?: string) => void): NodeJS.ReadWriteStream;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   // (undocumented)
   loadSchema(): Object;
   // (undocumented)
   name: string;
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IApiExtractorTaskConfig"
   // (undocumented)
   taskConfig: IApiExtractorTaskConfig;
 }
 
 class CleanTask extends GulpTask<void> {
   constructor();
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulp.Gulp"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   executeTask(gulp: gulp.Gulp, completeCallback: (result?: Object) => void): void;
 }
 
-// WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-// WARNING: Unable to resolve external type reference for "ICopyConfig"
 class CopyTask extends GulpTask<ICopyConfig> {
   constructor();
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulp.Gulp"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("es6-promise.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Promise"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   executeTask(gulp: gulp.Gulp, completeCallback: (result?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   // @internal
   loadSchema(): Object;
 }
@@ -65,12 +37,6 @@ export declare function functionalTestRun(name: string, result: TestResultState,
 
 class GenerateShrinkwrapTask extends GulpTask<void> {
   constructor();
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulpType.Gulp"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   executeTask(gulp: gulpType.Gulp, completeCallback: (result?: Object) => void): NodeJS.ReadWriteStream;
 }
 
@@ -82,41 +48,17 @@ export declare function getWarnings(): string[];
 
 class GulpTask<TASK_CONFIG> implements IExecutable {
   protected _getConfigFilePath(): string;
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
   buildConfig: IBuildConfig;
   cleanMatch: string[];
   copyFile(localSourcePath: string, localDestPath?: string): void;
   enabled: boolean;
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
-  // WARNING: Unable to find a documentation file ("es6-promise.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Promise"
   execute(config: IBuildConfig): Promise<void>;
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulp.Gulp"
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "GulpProxy"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("es6-promise.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Promise"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   abstract executeTask(gulp: gulp.Gulp | GulpProxy, completeCallback?: (result?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
   fileError(filePath: string, line: number, column: number, errorCode: string, message: string): void;
   fileExists(localPath: string): boolean;
   fileWarning(filePath: string, line: number, column: number, warningCode: string, message: string): void;
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
   getCleanMatch(buildConfig: IBuildConfig, taskConfig?: TASK_CONFIG): string[];
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
   isEnabled(buildConfig: IBuildConfig): boolean;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   protected loadSchema(): Object;
   log(message: string): void;
   logError(message: string): void;
@@ -125,13 +67,9 @@ class GulpTask<TASK_CONFIG> implements IExecutable {
   mergeConfig(taskConfig: TASK_CONFIG): void;
   name: string;
   onRegister(): void;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   readJSONSync(localPath: string): Object;
   replaceConfig(taskConfig: TASK_CONFIG): void;
   resolvePath(localPath: string): string;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   schema: Object;
   setConfig(taskConfig: TASK_CONFIG): void;
   taskConfig: TASK_CONFIG;
@@ -174,20 +112,6 @@ interface ICopyConfig {
 }
 
 interface ICustomGulpTask {
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulp.Gulp"
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "GulpProxy"
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("es6-promise.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Promise"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   // (undocumented)
   (gulp: gulp.Gulp | GulpProxy, buildConfig: IBuildConfig, done: (failure?: Object) => void): Promise<Object> | NodeJS.ReadWriteStream | void;
 }
@@ -226,10 +150,6 @@ class SchemaValidator {
   static readAndValidateJson < TResult >(dataFilePath: string, schemaFilePath: string): TResult;
   // (undocumented)
   static readCommentedJsonFile < TResult >(filename: string): TResult;
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   static validate(data: Object, schema: Object, dataFilePath?: string): void;
 }
 
@@ -254,60 +174,30 @@ enum TestResultState {
 
 // (undocumented)
 class TypeScriptConfiguration {
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ITsConfigFile"
-  // WARNING: Unable to find a documentation file ("gulp-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ts.Settings"
   static getGulpTypescriptOptions(buildConfig: IBuildConfig): ITsConfigFile<ts.Settings>;
   static getTypescriptCompiler(): any;
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ITsConfigFile"
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "typescript.CompilerOptions"
   // (undocumented)
   static getTypescriptOptions(buildConfig: IBuildConfig): ITsConfigFile<typescript.CompilerOptions>;
   static setTypescriptCompiler(typescript: any): void;
 }
 
-// WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-// WARNING: Unable to resolve external type reference for "ITypeScriptTaskConfig"
 // (undocumented)
 class TypeScriptTask extends GulpTask<ITypeScriptTaskConfig> {
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulpType.Gulp"
   // (undocumented)
   executeTask(gulp: gulpType.Gulp, completeCallback: (result?: string) => void): void;
-  // WARNING: Unable to find a documentation file ("gulp-core-build.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "IBuildConfig"
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ITypeScriptTaskConfig"
   // (undocumented)
   getCleanMatch(buildConfig: IBuildConfig, taskConfig?: ITypeScriptTaskConfig): string[];
-  // WARNING: Unable to find a documentation file ("typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "Object"
   // (undocumented)
   loadSchema(): Object;
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ITypeScriptTaskConfig"
   mergeConfig(config: ITypeScriptTaskConfig): void;
   // (undocumented)
   name: string;
-  // WARNING: Unable to find a documentation file ("gulp-core-build-typescript.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "ITypeScriptTaskConfig"
   // (undocumented)
   taskConfig: ITypeScriptTaskConfig;
 }
 
 class ValidateShrinkwrapTask extends GulpTask<void> {
   constructor();
-  // WARNING: Unable to find a documentation file ("gulp.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "gulpType.Gulp"
-  // WARNING: Unable to find a documentation file ("node.api.json")for the referenced package
-  // WARNING: Unable to resolve external type reference for "NodeJS.ReadWriteStream"
   executeTask(gulp: gulpType.Gulp): NodeJS.ReadWriteStream;
 }
 
