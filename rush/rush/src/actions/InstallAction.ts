e2c3674249f78e7adf59c1e6cfb952e9429df97a
pgonzal <pgonzal@users.noreply.github.com>
2017-04-14 21:27:30 -0700

    Implemented the new "rush install"/"rush generate" design from the PR discussion

--- rush/rush/src/actions/InstallAction.ts
+++ rush/rush/src/actions/InstallAction.ts
@@ -7,13 +7,11 @@ import * as glob from 'glob';
 import globEscape = require('glob-escape');
 import * as os from 'os';
 import * as path from 'path';
-import * as _ from 'lodash';
 
 import { CommandLineAction, CommandLineFlagParameter } from '@microsoft/ts-command-line';
 import {
   JsonFile,
   RushConfiguration,
-  RushConfigurationProject,
   Utilities,
   Stopwatch,
   AsyncRecycle,
@@ -24,6 +22,7 @@ import RushCommandLineParser from './RushCommandLineParser';
 import GitPolicy from '../utilities/GitPolicy';
 import { TempModuleGenerator } from '../utilities/TempModuleGenerator';
 import LinkAction from './LinkAction';
+import ShrinkwrapFile from '../utilities/ShrinkwrapFile';
 
 const MAX_INSTALL_ATTEMPTS: number = 5;
 
@@ -41,9 +40,99 @@ export default class InstallAction extends CommandLineAction {
   private _bypassPolicy: CommandLineFlagParameter;
   private _noLinkParameter: CommandLineFlagParameter;
 
-  private _tempModulesFiles: string[] = [];
+  constructor(parser: RushCommandLineParser) {
+    super({
+      actionVerb: 'install',
+      summary: 'Install NPM packages as specified by the configuration files in the Rush "common" folder',
+      documentation: 'Use this command after pulling new changes from git into your working folder.'
+      + ' It will download and install the appropriate NPM packages needed to build your projects.'
+      + ' The complete sequence is as follows:  1. If not already installed, install the'
+      + ' version of the NPM tool that is specified in the rush.json configuration file.  2. Create the'
+      + ' common/npm-local symlink, which points to the folder from #1.  3. If necessary, run'
+      + ' "npm prune" in the Rush common folder.  4. If necessary, run "npm install" in the'
+      + ' Rush common folder.'
+    });
+    this._parser = parser;
+  }
 
-  public static ensureLocalNpmTool(rushConfiguration: RushConfiguration, cleanInstall: boolean): void {
+  protected onDefineParameters(): void {
+    this._cleanInstall = this.defineFlagParameter({
+      parameterLongName: '--clean',
+      parameterShortName: '-c',
+      description: 'Delete any previously installed files before installing;'
+      + ' this takes longer but will resolve data corruption that is often'
+      + ' encountered with the NPM tool'
+    });
+    this._cleanInstallFull = this.defineFlagParameter({
+      parameterLongName: '--full-clean',
+      parameterShortName: '-C',
+      description: 'Like "--clean", but also deletes and reinstalls the NPM tool itself'
+    });
+    this._bypassPolicy = this.defineFlagParameter({
+      parameterLongName: '--bypass-policy',
+      description: 'Overrides "gitPolicy" enforcement (use honorably!)'
+    });
+    this._noLinkParameter = this.defineFlagParameter({
+      parameterLongName: '--no-link',
+      description: 'Do not automatically run the "rush link" action after "rush install"'
+    });
+  }
+
+  protected onExecute(): void {
+    this._rushConfiguration = RushConfiguration.loadFromDefaultLocation();
+
+    if (!this._bypassPolicy.value) {
+      if (!GitPolicy.check(this._rushConfiguration)) {
+        process.exit(1);
+        return;
+      }
+    }
+
+    const stopwatch: Stopwatch = Stopwatch.start();
+
+    console.log('Starting "rush install"' + os.EOL);
+
+    InstallHelpers.ensureLocalNpmTool(this._rushConfiguration, this._cleanInstallFull.value);
+
+    const shrinkwrapFile: ShrinkwrapFile | undefined
+      = ShrinkwrapFile.loadFromFile(this._rushConfiguration.shrinkwrapFilename);
+
+    if (!shrinkwrapFile) {
+      console.log('');
+      console.log(colors.red('Unable to proceed:  The NPM shrinkwrap file is missing.'));
+      console.log('');
+      console.log('You need to run "rush generate" first.');
+      process.exit(1);
+      return;
+    }
+
+    const tempModuleGenerator: TempModuleGenerator = new TempModuleGenerator(this._rushConfiguration);
+    if (!tempModuleGenerator.regenerateAndValidateShrinkwrap(shrinkwrapFile)) {
+      console.log('');
+      console.log(colors.red('Unable to proceed:  A required dependency was not found in the common folder.'));
+      console.log('');
+      console.log('You need to run "rush generate" to update the common folder.');
+      process.exit(1);
+      return;
+    }
+
+    InstallHelpers.installCommonModules(this._rushConfiguration,
+      this._cleanInstall.value || this._cleanInstallFull.value);
+
+    stopwatch.stop();
+    console.log(colors.green(`The common NPM packages are up to date. (${stopwatch.toString()})`));
+
+    if (!this._noLinkParameter.value) {
+      const linkAction: LinkAction = new LinkAction(this._parser);
+      linkAction.execute();
+    } else {
+      console.log(os.EOL + 'Next you should probably run: "rush link"');
+    }
+  }
+}
+
+export class InstallHelpers {
+  public static ensureLocalNpmTool(rushConfiguration: RushConfiguration, forceReinstall: boolean): void {
     // Example: "C:\Users\YourName\.rush"
     const rushHomeFolder: string = path.join(rushConfiguration.homeFolder, '.rush');
 
@@ -59,7 +148,7 @@ export default class InstallAction extends CommandLineAction {
 
     // NOTE: We don't care about the timestamp for last-install.flag, because nobody will change
     // the package.json for this case
-    if (cleanInstall || !fsx.existsSync(npmToolFlagFile)) {
+    if (forceReinstall || !fsx.existsSync(npmToolFlagFile)) {
       console.log(colors.bold('Installing NPM version ' + rushConfiguration.npmToolVersion) + os.EOL);
 
       if (fsx.existsSync(npmToolFolder)) {
@@ -99,170 +188,25 @@ export default class InstallAction extends CommandLineAction {
     fsx.symlinkSync(npmToolFolder, localNpmToolFolder, 'junction');
   }
 
-  constructor(parser: RushCommandLineParser) {
-    super({
-      actionVerb: 'install',
-      summary: 'Install NPM packages as specified by the configuration files in the Rush "common" folder',
-      documentation: 'Use this command after pulling new changes from git into your working folder.'
-      + ' It will download and install the appropriate NPM packages needed to build your projects.'
-      + ' The complete sequence is as follows:  1. If not already installed, install the'
-      + ' version of the NPM tool that is specified in the rush.json configuration file.  2. Create the'
-      + ' common/npm-local symlink, which points to the folder from #1.  3. If necessary, run'
-      + ' "npm prune" in the Rush common folder.  4. If necessary, run "npm install" in the'
-      + ' Rush common folder.'
-    });
-    this._parser = parser;
-  }
-
-  protected onDefineParameters(): void {
-    this._cleanInstall = this.defineFlagParameter({
-      parameterLongName: '--clean',
-      parameterShortName: '-c',
-      description: 'Delete any previously installed files before installing;'
-      + ' this takes longer but will resolve data corruption that is often'
-      + ' encountered with the NPM tool'
-    });
-    this._cleanInstallFull = this.defineFlagParameter({
-      parameterLongName: '--full-clean',
-      parameterShortName: '-C',
-      description: 'Like "--clean", but also deletes and reinstalls the NPM tool itself'
-    });
-    this._bypassPolicy = this.defineFlagParameter({
-      parameterLongName: '--bypass-policy',
-      description: 'Overrides gitPolicy enforcement (use honorably!)'
-    });
-    this._noLinkParameter = this.defineFlagParameter({
-      parameterLongName: '--no-link',
-      description: 'Do not automatically run the Link action after completing Generate action'
-    });
-  }
-
-  protected onExecute(): void {
-    this._rushConfiguration = RushConfiguration.loadFromDefaultLocation();
-
-    if (!this._bypassPolicy.value) {
-      if (!GitPolicy.check(this._rushConfiguration)) {
-        process.exit(1);
-        return;
-      }
-    }
-
-    const stopwatch: Stopwatch = Stopwatch.start();
-
-    console.log('Starting "rush install"' + os.EOL);
-
-    // Example: "C:\MyRepo\common\temp_modules\rush-example-project\package.json"
-    const normalizedPath: string = Utilities.getAllReplaced(this._rushConfiguration.tempModulesFolder, '\\', '/');
-    const globPattern: string = `${globEscape(normalizedPath)}/rush-*/package.json`;
-    this._tempModulesFiles = glob.sync(globPattern, { nodir: true });
-
-    // Ensure that the common/package.json exists
-    const commonPackageJson: string = path.join(this._rushConfiguration.commonFolder, 'package.json');
-    if (!fsx.existsSync(commonPackageJson)) {
-      throw new Error(`"${commonPackageJson}" was not found.${os.EOL}Did you forget to run "rush generate" ?`);
-    }
-
-    // TEMPORARILY DISABLED DUE TO REGRESSION (VSO 313164)
-    // this._checkThatTempModulesMatch();
-
-    InstallAction.ensureLocalNpmTool(this._rushConfiguration, this._cleanInstallFull.value);
-    this._installCommonModules();
-
-    stopwatch.stop();
-    console.log(colors.green(`The common NPM packages are up to date. (${stopwatch.toString()})`));
-
-    if (!this._noLinkParameter.value) {
-      const linkAction: LinkAction = new LinkAction(this._parser);
-      linkAction.execute();
-    } else {
-      console.log(os.EOL + 'Next you should probably run: "rush link"');
-    }
-  }
-
-  private _checkThatTempModulesMatch(): void {
-    // read all temp_modules
-    const tempModules: { [packageName: string]: ITempModuleInformation } = {};
-
-    this._tempModulesFiles.forEach((filename) => {
-      const contents: IPackageJson = require(filename);
-      tempModules[contents.name] = {
-        packageJson: contents,
-        existsInProjectConfiguration: false,
-        filename: filename
-      };
-    });
-
-    // 1st ensure that every temp_module exists in the configuration
-    Object.keys(tempModules).forEach((tempModuleName: string) => {
-      const tempModule: ITempModuleInformation = tempModules[tempModuleName];
-
-      let foundMatchingProject: boolean;
-      this._rushConfiguration.projects.forEach((project: RushConfigurationProject) => {
-        if (project.tempProjectName === tempModuleName) {
-          foundMatchingProject = true;
-        }
-      });
-
-      if (!foundMatchingProject) {
-        throw new Error(`The file "${tempModule.filename}" exists in the temp_modules folder ` +
-          `but we could not find a matching project for it. This file may need to be deleted.` +
-          `\n\nDid you forget to run 'rush generate'?`);
-      }
-    });
-
-    const expectedTempModules: Map<string, IPackageJson> = new TempModuleGenerator(this._rushConfiguration).tempModules;
-
-    // 2nd ensure that each config project has a temp_module which matches the expected value
-    this._rushConfiguration.projects.forEach((project: RushConfigurationProject) => {
-      //   if no temp_module, throw
-
-      const tempModule: ITempModuleInformation
-        = tempModules[project.tempProjectName];
-      if (!tempModule) {
-        throw new Error(`The project ${project.packageName} is missing a corresponding ` +
-          `file in the temp_modules folder.` +
-          `\n\nDid you forget to run 'rush generate'?`);
-      }
-
-      // Generate an expected temp_modules package.json & compare
-      const expectedTempModule: IPackageJson = expectedTempModules.get(project.packageName);
-
-      if (!_.isEqual(expectedTempModule, tempModule.packageJson)) {
-        // wordwrap attempts to remove any leading spaces, however, we are attempting to serialize
-        // some JSON information into the error, and therefore we want to maintain proper spacing.
-        // the workaround is to wrap our spaces in the non-breaking character
-
-        const errorMsg: string = `The project ${project.packageName}'s temp_module is outdated`;
-        const rerunGenerate: string = '\nDid you forget to run rush generate?';
-
-        console.log(colors.red(`${errorMsg}:\n`));
-        console.log(colors.red(`EXPECTED:\n${JSON.stringify(expectedTempModule, undefined, 2)}\n`));
-        console.log(colors.red(`ACTUAL: \n${JSON.stringify(tempModule.packageJson, undefined, 2)}`));
-
-        throw new Error(errorMsg + '\n' + rerunGenerate);
-      }
-    });
-  }
-
-  private _installCommonModules(): void {
+  public static installCommonModules(rushConfiguration: RushConfiguration, cleanInstall: boolean): void {
     // Example: "C:\MyRepo\common\npm-local\node_modules\.bin\npm"
-    const npmToolFilename: string = this._rushConfiguration.npmToolFilename;
+    const npmToolFilename: string = rushConfiguration.npmToolFilename;
     if (!fsx.existsSync(npmToolFilename)) {
       // This is a sanity check.  It should never happen if the above logic worked correctly.
       throw new Error('Failed to create "' + npmToolFilename + '"');
     }
 
-    console.log(os.EOL + colors.bold('Checking modules in ' + this._rushConfiguration.commonFolder) + os.EOL);
+    console.log(os.EOL + colors.bold('Checking modules in ' + rushConfiguration.commonFolder) + os.EOL);
 
     // Example: "C:\MyRepo\common\last-install.flag"
     const commonNodeModulesMarkerFilename: string =
-      path.join(this._rushConfiguration.commonFolder, 'last-install.flag');
-    const commonNodeModulesFolder: string = path.join(this._rushConfiguration.commonFolder, 'node_modules');
+      path.join(rushConfiguration.commonFolder, 'last-install.flag');
+    const commonNodeModulesFolder: string = path.join(rushConfiguration.commonFolder, 'node_modules');
 
     let needToInstall: boolean = false;
     let skipPrune: boolean = false;
 
-    if (this._cleanInstall.value || this._cleanInstallFull.value) {
+    if (cleanInstall) {
       if (fsx.existsSync(commonNodeModulesMarkerFilename)) {
         // If we are cleaning the node_modules folder, then also delete the flag file
         // to force a reinstall
@@ -276,10 +220,10 @@ export default class InstallAction extends CommandLineAction {
         Utilities.createFolderWithRetry(commonNodeModulesFolder);
       }
 
-      if (this._rushConfiguration.cacheFolder) {
-        const cacheCleanArgs: string[] = ['cache', 'clean', this._rushConfiguration.cacheFolder];
+      if (rushConfiguration.cacheFolder) {
+        const cacheCleanArgs: string[] = ['cache', 'clean', rushConfiguration.cacheFolder];
         console.log(os.EOL + `Running "npm ${cacheCleanArgs.join(' ')}"`);
-        Utilities.executeCommand(npmToolFilename, cacheCleanArgs, this._rushConfiguration.commonFolder);
+        Utilities.executeCommand(npmToolFilename, cacheCleanArgs, rushConfiguration.commonFolder);
       } else {
         // Ideally we should clean the global cache here.  However, the global NPM cache
         // is (inexplicably) not threadsafe, so if there are any concurrent "npm install"
@@ -293,19 +237,12 @@ export default class InstallAction extends CommandLineAction {
       // Compare the timestamps last-install.flag, npm-shrinkwrap, and package.json to see if our install is outdated
       const potentiallyChangedFiles: string[] = [];
 
-      // Example: "C:\MyRepo\common\package.json"
-      potentiallyChangedFiles.push(path.join(this._rushConfiguration.commonFolder, 'package.json'));
-
-      // Also consider the timestamp on the node_modules folder; if someone tampered with it
+      // Consider the timestamp on the node_modules folder; if someone tampered with it
       // or deleted it entirely, then isFileTimestampCurrent() will cause us to redo "npm install".
       potentiallyChangedFiles.push(commonNodeModulesFolder);
 
-      // Additionally, someone could have run generate without changing a package.json, in this case
-      // only the shrinkwrap would have changed, so if the shrinkwrap is newer we will want to install as well
-      potentiallyChangedFiles.push(path.join(this._rushConfiguration.commonFolder, 'npm-shrinkwrap.json'));
-
-      // Make sure we look at all the temp_modules/*/package.json files as well
-      potentiallyChangedFiles.push(...this._tempModulesFiles);
+      // Additionally, if they pulled an updated shrinkwrap file from Git, we need to install as well
+      potentiallyChangedFiles.push(rushConfiguration.shrinkwrapFilename);
 
       if (!Utilities.isFileTimestampCurrent(commonNodeModulesMarkerFilename, potentiallyChangedFiles)) {
         needToInstall = true;
@@ -324,7 +261,7 @@ export default class InstallAction extends CommandLineAction {
           console.log('Deleting the "node_modules" folder because the previous Rush install' +
             ' did not complete successfully.');
 
-          AsyncRecycle.recycleDirectory(this._rushConfiguration, commonNodeModulesFolder);
+          AsyncRecycle.recycleDirectory(rushConfiguration, commonNodeModulesFolder);
         }
 
         skipPrune = true;
@@ -334,9 +271,9 @@ export default class InstallAction extends CommandLineAction {
       }
 
       if (!skipPrune) {
-        console.log(`Running "npm prune" in ${this._rushConfiguration.commonFolder}`);
+        console.log(`Running "npm prune" in ${rushConfiguration.commonFolder}`);
         Utilities.executeCommandWithRetry(npmToolFilename, ['prune'], MAX_INSTALL_ATTEMPTS,
-          this._rushConfiguration.commonFolder);
+          rushConfiguration.commonFolder);
 
         // Delete the temp projects because NPM will not notice when they are changed.
         // We can recognize them because their names start with "rush-"
@@ -353,25 +290,25 @@ export default class InstallAction extends CommandLineAction {
       }
 
       const npmInstallArgs: string[] = ['install'];
-      if (this._rushConfiguration.cacheFolder) {
-        npmInstallArgs.push('--cache', this._rushConfiguration.cacheFolder);
+      if (rushConfiguration.cacheFolder) {
+        npmInstallArgs.push('--cache', rushConfiguration.cacheFolder);
       }
 
-      if (this._rushConfiguration.tmpFolder) {
-        npmInstallArgs.push('--tmp', this._rushConfiguration.tmpFolder);
+      if (rushConfiguration.tmpFolder) {
+        npmInstallArgs.push('--tmp', rushConfiguration.tmpFolder);
       }
 
       // Next, run "npm install" in the common folder
-      console.log(os.EOL + `Running "npm ${npmInstallArgs.join(' ')}" in ${this._rushConfiguration.commonFolder}`);
+      console.log(os.EOL + `Running "npm ${npmInstallArgs.join(' ')}" in ${rushConfiguration.commonFolder}`);
       Utilities.executeCommandWithRetry(npmToolFilename,
         npmInstallArgs,
         MAX_INSTALL_ATTEMPTS,
-        this._rushConfiguration.commonFolder);
+        rushConfiguration.commonFolder);
 
       // Create the marker file to indicate a successful install
       fsx.createFileSync(commonNodeModulesMarkerFilename);
       console.log('');
     }
-
   }
+
 }
