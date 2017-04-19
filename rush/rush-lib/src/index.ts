a0d305c33a1cc381f72f321a4eb91c0a0b07c50e
pgonzal <pgonzal@users.noreply.github.com>
2017-04-19 03:41:11 -0700

    Enable api-extractor.json for rush-lib

--- rush/rush-lib/src/index.ts
+++ rush/rush-lib/src/index.ts
@@ -2,7 +2,6 @@
 // See LICENSE in the project root for license information.
 
 export {
-  IRushConfigurationJson,
   IRushLinkJson,
   default as RushConfiguration
 } from './data/RushConfiguration';
--- rush/rush-lib/src/utilities/Utilities.ts
+++ rush/rush-lib/src/utilities/Utilities.ts
@@ -272,9 +272,9 @@ export default class Utilities {
   /**
    * Returns the same thing as targetString.replace(searchValue, replaceValue), except that
    * all matches are replaced, rather than just the first match.
-   * @param targetString  The string to be modified
-   * @param searchValue   The value to search for
-   * @param replaceValue  The replacement text
+   * @param targetString  - The string to be modified
+   * @param searchValue   - The value to search for
+   * @param replaceValue  - The replacement text
    */
   public static getAllReplaced(targetString: string, searchValue: string, replaceValue: string): string {
     return targetString.split(searchValue).join(replaceValue);
