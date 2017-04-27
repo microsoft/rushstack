f0f067b129b4bef3786247f07e327a5cae15ee6d
dgaeta <dgaeta@users.noreply.github.com>
2017-04-27 13:46:20 -0700

    Added test case

--- api-extractor/testInputs/example4/src/NamespaceExport.ts
+++ api-extractor/testInputs/example4/src/NamespaceExport.ts
@@ -12,27 +12,55 @@ export namespace NamespaceExport {
   export const accessLogo: string = '\uED69';
   export const accounts: string = '\uE910';
 
+  /**
+   * Testing a structured type in a namespace.
+   */
   export class RealNumber {
+    /**
+     * Testing a private property, this should
+     * not appear in the JSON nor *api.ts file.
+     */
     private _value: number; 
 
+    /**
+     * Testing a public property.
+     */
     public isReal: boolean;
     
+    /**
+     * Testing a getter.
+     */
     public get getNumber(): number {
       return this._value;
     }
 
+    /**
+     * Testing a setter.
+     */
     public set setNumber(value: number) {
       this._value = value;
     }
 
+    /**
+     * Testing a method.
+     */
     public squared(): number {
       return this._value * 2;
     }
   }
 
+  /**
+   * This is a test for interfaces in a namespace. 
+   */
   export interface Number {
     real: RealNumber;
   }
 
+  /**
+   * This is a test for a function in a namespace.
+   * @param value - this is a description for the param. 
+   */
+  export function aFunction(value: number): void {
+  }
 }
  
