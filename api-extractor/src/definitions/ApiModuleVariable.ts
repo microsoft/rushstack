a05e254ec122e55505caef7608348b4d65afc26d
dgaeta <dgaeta@users.noreply.github.com>
2017-05-02 14:59:07 -0700

    Adding documentation

--- api-extractor/src/definitions/ApiModuleVariable.ts
+++ api-extractor/src/definitions/ApiModuleVariable.ts
@@ -3,8 +3,10 @@ import { ApiItemKind, IApiItemOptions } from './ApiItem';
 import ApiMember from './ApiMember';
 
 /**
- * This class is part of the ApiItem abstract syntax tree. It represents fields of
- * an ApiNamespace.
+ * This class is part of the ApiItem abstract syntax tree. It represents variables
+ * that are exported by an ApiNamespace (or conceivably an ApiPackage in the future).
+ * The variables have a name, a type, and an initializer. The ApiNamespace implementation
+ * currently requires them to use a primitive type and be declared as "const".
  */
 class ApiModuleVariable extends ApiMember {
   public type: string;
