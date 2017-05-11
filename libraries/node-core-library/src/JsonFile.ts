bba410cabc1036aa4cc38583e4421b06d48a6c3b
pgonzal <pgonzal@users.noreply.github.com>
2017-05-10 19:21:34 -0700

    Enabled api-extractor.json and built an empty class

--- libraries/node-core-library/src/JsonFile.ts
+++ libraries/node-core-library/src/JsonFile.ts
@@ -1,13 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import * as fsx from 'fs-extra';
-import * as os from 'os';
-import * as jju from 'jju';
-import Validator = require('z-schema');
-
 /**
  * Utilities for reading/writing JSON files.
  */
-export default class JsonFile {
+export class JsonFile {
 }
--- libraries/node-core-library/src/index.ts
+++ libraries/node-core-library/src/index.ts
@@ -6,4 +6,4 @@
  */
 declare const packageDescription: void;
 
-export { JsonFile } from './src/JsonFile';
+export { JsonFile } from './JsonFile';
