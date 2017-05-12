40dce56a83c880eb4bd78a9afa5e4f66edf80565
pgonzal <pgonzal@users.noreply.github.com>
2017-05-12 16:19:24 -0700

    Fix a bug where "rush link --force" wasn't honored

--- apps/rush/src/actions/LinkAction.ts
+++ apps/rush/src/actions/LinkAction.ts
@@ -34,6 +34,6 @@ export default class LinkAction extends CommandLineAction {
     this._rushConfiguration = this._rushConfiguration = RushConfiguration.loadFromDefaultLocation();
 
     const linkManager: LinkManager = new LinkManager(this._rushConfiguration);
-    this._parser.catchSyncErrors(linkManager.createSymlinksForProjects(false));
+    this._parser.catchSyncErrors(linkManager.createSymlinksForProjects(this._force.value));
   }
 }
