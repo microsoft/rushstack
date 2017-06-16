9727826eb704572bed38e5e1c9f467aaa1fffbdb
pgonzal <pgonzal@users.noreply.github.com>
2017-06-15 20:16:55 -0700

    Implement caching

--- libraries/api-extractor/src/PackageJsonLookup.ts
+++ libraries/api-extractor/src/PackageJsonLookup.ts
@@ -16,6 +16,14 @@ interface IPackageJson {
  * and retrieving the name of the package.  The results are cached.
  */
 export default class PackageJsonLookup {
+  private _tryFindPackagePathUpwardsCache: Map<string, string>;
+  private _readPackageNameCache: Map<string, string>;
+
+  constructor() {
+    this._tryFindPackagePathUpwardsCache = new Map<string, string>();
+    this._readPackageNameCache = new Map<string, string>();
+  }
+
   /**
    * Finds the path to the package folder of a given currentPath, by probing
    * upwards from the currentPath until a package.json file is found.
@@ -25,21 +33,25 @@ export default class PackageJsonLookup {
    * @returns a relative path to the package folder
    */
   public tryFindPackagePathUpwards(currentPath: string): string {
-    let packageFolder: string = '';
-
-    // no-constant-condition
-    while (true) {
-      const folder: string = path.dirname(currentPath);
-      if (!folder || folder === currentPath) {
-        return undefined;
-      }
-      currentPath = folder;
-      if (fsx.existsSync(path.join(currentPath, 'package.json'))) {
-        packageFolder = path.normalize(currentPath);
-        break;
-      }
+    // Since Map cannot store an undefined value, we use the weird "null" object to
+    // represent "undefined".
+    let result: string = this._tryFindPackagePathUpwardsCache.get(currentPath);
+    if (result !== undefined) {
+      return result === null ? undefined : result; // tslint:disable-line: no-null-keyword
     }
-    return packageFolder;
+
+    const parentFolder: string = path.dirname(currentPath);
+    if (!parentFolder || parentFolder === currentPath) {
+      result = undefined;
+    } else if (fsx.existsSync(path.join(parentFolder, 'package.json'))) {
+      result = path.normalize(parentFolder);
+    } else {
+      result = this.tryFindPackagePathUpwards(parentFolder);
+    }
+
+     // tslint:disable-next-line: no-null-keyword
+    this._tryFindPackagePathUpwardsCache.set(currentPath, result === undefined ? null : result);
+    return result;
   }
 
   /**
@@ -50,7 +62,15 @@ export default class PackageJsonLookup {
    * @returns the name of the package (E.g. @microsoft/api-extractor)
    */
   public readPackageName(packageJsonPath: string): string {
+    let result: string = this._readPackageNameCache.get(packageJsonPath);
+    if (result !== undefined) {
+      return result;
+    }
+
     const packageJson: IPackageJson = JsonFile.loadJsonFile(path.join(packageJsonPath, 'package.json')) as IPackageJson;
-    return packageJson.name;
+    result = packageJson.name;
+
+    this._readPackageNameCache.set(packageJsonPath, result);
+    return result;
   }
 }
