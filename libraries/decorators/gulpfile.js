a6232647ddef85a80a7394027bfa0dfc73a32800
pgonzal <pgonzal@users.noreply.github.com>
2017-06-22 19:37:58 -0700

    Add decorators, stream-collator, and ts-command-line to rush.json and convert to use web-library-build / node-library-build

--- libraries/decorators/gulpfile.js
+++ libraries/decorators/gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
-let build = require('@ms/sp-build-internal-web');
+let path = require('path');
+let build = require('@microsoft/web-library-build');
+
+build.webpack.setConfig({ configPath: null });
 
 build.initialize(require('gulp'));
-build.rig.configureOnPremBuild("../../tools/vsts-deploy/app-min/on-prem");
--- libraries/stream-collator/gulpfile.js
+++ libraries/stream-collator/gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const build = require('@microsoft/sp-build-node');
+const build = require('@microsoft/node-library-build');
 
 build.initialize(require('gulp'));
