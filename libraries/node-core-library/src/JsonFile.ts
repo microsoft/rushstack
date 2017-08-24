a8169ec5d510a6703db02eb9449890a42a9c76a4
pgonzal <pgonzal@users.noreply.github.com>
2017-08-23 17:49:33 -0700

    Delete placeholder file

--- libraries/node-core-library/src/JsonFile.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-// See LICENSE in the project root for license information.
-
-/**
- * Utilities for reading/writing JSON files.
- * @alpha
- */
-export class JsonFile {
-}
