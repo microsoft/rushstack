f5a3c4dcb1e6c664b874e73ef3ff408850d64fbe
pgonzal <pgonzal@users.noreply.github.com>
2017-10-06 16:56:12 -0700

    Set up basic command line for parsing a *.d.ts file

--- libraries/api-extractor/src/cli/SimpleAction.ts
+++ libraries/api-extractor/src/cli/SimpleAction.ts
@@ -1,14 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import * as fsx from 'fs-extra';
+
 import {
-  CommandLineAction
+  CommandLineAction,
+  CommandLineStringParameter
 } from '@microsoft/ts-command-line';
 
 import { ApiExtractorCommandLine } from './ApiExtractorCommandLine';
 
 export class SimpleAction extends CommandLineAction {
   private _parser: ApiExtractorCommandLine;
+  private _dtsFileParameter: CommandLineStringParameter;
 
   constructor(parser: ApiExtractorCommandLine) {
     super({
@@ -20,10 +24,24 @@ export class SimpleAction extends CommandLineAction {
   }
 
   protected onDefineParameters(): void { // override
-
+    this._dtsFileParameter = this.defineStringParameter({
+      parameterLongName: '--dtsFile',
+      parameterShortName: '-f',
+      key: 'FILE',
+      description: `Uses the specified *.d.ts file as the entry point`
+    });
   }
 
   protected onExecute(): void { // override
-    console.log('SIMPLE');
+    if (!this._dtsFileParameter.value) {
+      throw new Error('The -f parameter is required');
+    }
+
+    const dtsFilename: string = this._dtsFileParameter.value;
+    if (!fsx.existsSync(dtsFilename)) {
+      throw new Error('Input file not found: ' + dtsFilename);
+    }
+
+
   }
 }
