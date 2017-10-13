d2f18a0baa8cb7d790dc50c286b79f6f6afdde13
Srikanth Deshpande <srdeshpa@users.noreply.github.com>
2017-10-13 15:08:58 -0700

    fixing a build error

--- common/reviews/api/load-themed-styles.api.ts
+++ common/reviews/api/load-themed-styles.api.ts
@@ -1,3 +1,4 @@
+// @public
 enum ClearStyleOptions {
   // (undocumented)
   all = 3,
@@ -7,23 +8,28 @@ enum ClearStyleOptions {
   onlyThemable = 1
 }
 
+// @public
 export function clearStyles(option: ClearStyleOptions = ClearStyleOptions.all): void;
 
+// @public
 export function configureLoadStyles(loadStylesFn: ((processedStyles: string, rawStyles?: string | ThemableArray) => void) | undefined): void;
 
+// @public
 export function configureRunMode(mode: Mode): void;
 
+// @public
 export function detokenize(styles: string | undefined): string | undefined;
 
+// @public
 export function flush(): void;
 
-// (undocumented)
+// @public (undocumented)
 interface ITheme {
   // (undocumented)
   [ key: string ]: string;
 }
 
-// (undocumented)
+// @public (undocumented)
 interface IThemingInstruction {
   // (undocumented)
   defaultValue?: string;
@@ -33,10 +39,13 @@ interface IThemingInstruction {
   theme?: string;
 }
 
+// @public
 export function loadStyles(styles: string | ThemableArray, loadAsync: boolean = false): void;
 
+// @public
 export function loadTheme(theme: ITheme | undefined): void;
 
+// @public
 enum Mode {
   // (undocumented)
   async,
@@ -44,6 +53,7 @@ enum Mode {
   sync
 }
 
+// @public
 export function splitStyles(styles: string): ThemableArray;
 
 // WARNING: Unsupported export: ThemableArray
