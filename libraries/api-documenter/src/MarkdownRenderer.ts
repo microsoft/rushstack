1ce0c6cb8f993271c01ad8ed2cd1b1c57ade81fe
pgonzal <pgonzal@users.noreply.github.com>
2017-10-13 11:08:08 -0700

    Add unit tests, fix a couple edge cases

--- libraries/api-documenter/src/MarkdownRenderer.ts
+++ libraries/api-documenter/src/MarkdownRenderer.ts
@@ -116,11 +116,12 @@ export class MarkdownRenderer {
   private static _getEscapedText(text: string): string {
     const textWithBackslashes: string = text
       .replace(/\\/g, '\\\\')  // first replace the escape character
-      .replace(/[*#[\]_|]/g, (x) => '\\' + x); // then escape any special characters
-    return textWithBackslashes
+      .replace(/[*#[\]_|`~]/g, (x) => '\\' + x) // then escape any special characters
+      .replace(/---/g, '\\-\\-\\-') // hyphens only if it's 3 or more
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
+    return textWithBackslashes
   }
 
   /**
