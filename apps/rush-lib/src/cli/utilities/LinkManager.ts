540904f793a029aeb2e99e8c0d99d4d7d8665289
Nick Pape <nickpape@users.noreply.github.com>
2017-11-16 16:15:44 -0800

    Fixup some missing typings

--- apps/rush-lib/src/cli/utilities/LinkManager.ts
+++ apps/rush-lib/src/cli/utilities/LinkManager.ts
@@ -22,9 +22,9 @@ export default abstract class LinkManager {
   protected _rushConfiguration: RushConfiguration;
 
   public static getLinkManager(rushConfiguration: RushConfiguration): LinkManager {
-    return rushConfiguration.packageManager === 'pnpm'
+    return new (rushConfiguration.packageManager === 'pnpm'
       ? require('./pnpm/PnpmLinkManager').default
-      : require('./npm/NpmLinkManager').default;
+      : require('./npm/NpmLinkManager').default)(rushConfiguration);
   }
 
   protected static _createSymlink(linkTarget: string, linkSource: string, symlinkKind: SymlinkKind): void {
