18b3e7ba64a1c26fcff5b9399f8692faa6004543
pgonzal <pgonzal@users.noreply.github.com>
2017-12-19 17:57:01 -0800

    Ran API Extrator

---
+++ build-tests/api-extractor-test-01/etc/api-extractor-test-01.api.ts
@@ -0,0 +1,22 @@
+// @public
+class AmbientConsumer {
+  // (undocumented)
+  builtinDefinition1(): Map<string, string>;
+  // (undocumented)
+  builtinDefinition2(): Promise<void>;
+  // (undocumented)
+  definitelyTyped(): jest.Context;
+  // WARNING: The type "IAmbientInterfaceExample" needs to be exported by the package (e.g. added to index.ts)
+  // (undocumented)
+  localTypings(): IAmbientInterfaceExample;
+}
+
+// @public
+interface ISimpleInterface {
+}
+
+// @public
+class ReexportedClass {
+}
+
+// (No packageDescription for this package)
