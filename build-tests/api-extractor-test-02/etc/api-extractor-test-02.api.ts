f8bb63ee6e9496d8c01cd4e1a7a5541bb38f235e
pgonzal <pgonzal@users.noreply.github.com>
2018-01-16 18:30:05 -0800

    Updated unit test output

--- build-tests/api-extractor-test-02/etc/api-extractor-test-02.api.ts
+++ build-tests/api-extractor-test-02/etc/api-extractor-test-02.api.ts
@@ -1,4 +1,20 @@
 // @public
+interface GenericInterface<T> {
+  // (undocumented)
+  member: T;
+}
+
+// @public
+class ImportedModuleAsBaseClass extends semver3.SemVer {
+}
+
+// @public
+export function importedModuleAsGenericParameter(): GenericInterface<semver2.SemVer> | undefined;
+
+// @public
+export function importedModuleAsReturnType(): semver1.SemVer | undefined;
+
+// @public
 class SubclassWithImport extends RenamedReexportedClass, implements ISimpleInterface {
   // (undocumented)
   test(): void;
