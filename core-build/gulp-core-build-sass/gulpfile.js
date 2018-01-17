21ed3640d23d5b18336782c7969ff8dc52117fc7
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-01-17 13:04:30 -0800

    Removing the workarounds in GCB-TS and GCB-SASS.

--- core-build/gulp-core-build-sass/gulpfile.js
+++ core-build/gulp-core-build-sass/gulpfile.js
@@ -2,15 +2,4 @@
 
 let build = require('@microsoft/node-library-build');
 
-// Temporarily fix rules that aren't compatible with the old version of TSLint
-build.tslint.setConfig({
-  lintConfig: {
-    rules: {
-      'no-duplicate-case': false,
-      'valid-typeof': false,
-      'typeof-compare': true
-    }
-  }
-});
-
 build.initialize(require('gulp'));
