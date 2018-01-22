49a31bb67ddccf21a7e66700bea0f63cd520c64d
Nicholas Pape <nickpape-msft@users.noreply.github.com>
2018-01-22 12:48:39 -0800

    Treat warnings as errors in production mode when building node-library-build & web-library-build (#499)
    * Errors as warnings in web-library-build and node-library-build
    
    * Changefiles

--- core-build/node-library-build/gulpfile.js
+++ core-build/node-library-build/gulpfile.js
@@ -5,6 +5,10 @@ let apiExtractor = require('@microsoft/gulp-core-build-typescript').apiExtractor
 let typescript = require('@microsoft/gulp-core-build-typescript').typescript;
 let mocha = require('@microsoft/gulp-core-build-mocha');
 
+build.setConfig({
+  shouldWarningsFailBuild: build.getConfig().production
+});
+
 build.task('default', build.serial(typescript, mocha, apiExtractor));
 
 build.initialize(require('gulp'));
