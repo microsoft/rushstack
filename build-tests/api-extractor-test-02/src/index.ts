854947e86d7405a1940ac70329659dcc06b3c4c2
pgonzal <pgonzal@users.noreply.github.com>
2018-02-11 00:29:23 -0800

    Updated unit test to repro issue where triple-slash type reference directives are not included in the rollup
    For example:
    
    /// <reference types="some-library" />

--- build-tests/api-extractor-test-02/src/index.ts
+++ build-tests/api-extractor-test-02/src/index.ts
@@ -13,3 +13,11 @@
 export { SubclassWithImport } from './SubclassWithImport';
 
 export * from './TypeFromImportedModule';
+
+import { AmbientConsumer } from 'api-extractor-test-01';
+
+// Test that the ambient types are accessible even though api-extractor-02 doesn't
+// import Jest
+const x = new AmbientConsumer();
+const y = x.definitelyTyped();
+const z = y.config;
