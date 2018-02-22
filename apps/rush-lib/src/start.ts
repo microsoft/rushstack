f47ef4cc3858c7d4663369547c1eed8ebc66807a
Nicholas Pape <nickpape-msft@users.noreply.github.com>
2018-02-22 12:33:53 -0800

    Rush should warn about unsupported versions of the NodeJS runtime (#539)
    * Add notice to Rush for unsupported NodeJS runtimes:
    
    * Changefile
    
    * Update engines
    
    * Fix warnings
    
    * Fix tslint warnings

--- apps/rush-lib/src/start.ts
+++ apps/rush-lib/src/start.ts
@@ -1,6 +1,37 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import * as colors from 'colors';
+import * as semver from 'semver';
+
+const nodeVersion: string = process.versions.node;
+
+// tslint:disable-next-line
+
+// We are on an ancient version of NodeJS that is known not to work with Rush
+if (semver.satisfies(nodeVersion, '<= 6.4.0')) {
+  console.error(colors.red(`Your version of Node.js (${nodeVersion}) is very old and incompatible with Rush.`
+    + ` Please upgrade to the latest Long-Term Support (LTS) version.`));
+  process.exit(1);
+}
+
+// We are on a much newer release than we have tested and support
+// tslint:disable-next-line
+else if (semver.satisfies(nodeVersion, '>=9.0.0')) {
+  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) has not been tested with this release of Rush.`
+    + ` The Rush team will not accept issue reports for it.`
+    + ` Please consider upgrading Rush or downgrading Node.js.`));
+}
+
+// We are not on an LTS release
+// tslint:disable-next-line
+else if (!semver.satisfies(nodeVersion, '^6.9.0')
+      && !semver.satisfies(nodeVersion, '^8.9.0')) {
+  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) is not a Long-Term Support (LTS) release.`
+    + ` These versions frequently contain bugs, and the Rush team will not accept issue reports for them.`
+    + ` Please consider installing a stable release.`));
+}
+
 import Rush from './Rush';
 
 Rush.launch(Rush.version, false);
