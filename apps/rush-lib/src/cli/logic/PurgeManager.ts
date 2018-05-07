ff1d15c3cdda1681c11115716807c0a068d274c3
pgonzal <pgonzal@users.noreply.github.com>
2018-05-06 17:47:30 -0700

    PR feedback: prevent "rush purge" from purging itself

--- apps/rush-lib/src/cli/logic/PurgeManager.ts
+++ apps/rush-lib/src/cli/logic/PurgeManager.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import * as colors from 'colors';
 import * as path from 'path';
 
 import { AsyncRecycler } from '../../utilities/AsyncRecycler';
@@ -27,6 +28,10 @@ export class PurgeManager {
     this._rushUserFolderRecycler = new AsyncRecycler(rushUserAsyncRecyclerPath);
   }
 
+  /**
+   * Performs the AsyncRecycler.deleteAll() operation.  This should be called before
+   * the PurgeManager instance is disposed.
+   */
   public deleteAll(): void {
     this._commonTempFolderRecycler.deleteAll();
     this._rushUserFolderRecycler.deleteAll();
@@ -42,17 +47,50 @@ export class PurgeManager {
   public purgeNormal(): void {
     // Delete everything under common\temp except for the recycler folder itself
     console.log('Purging ' + this._rushConfiguration.commonTempFolder);
+
     this._commonTempFolderRecycler.moveAllItemsInFolder(this._rushConfiguration.commonTempFolder,
-      [RushConstants.rushRecyclerFolderName]);
+      this._getMembersToExclude(this._rushConfiguration.commonTempFolder));
   }
 
-  /** */
+  /**
+   * In addition to performing the purgeNormal() operation, this method also cleans the
+   * .rush folder in the user's home directory.
+   */
   public purgeUnsafe(): void {
     this.purgeNormal();
 
     // Also delete everything under ~/.rush except for the recycler folder itself
     console.log('Purging ' + this._rushConfiguration.rushUserFolder);
     this._rushUserFolderRecycler.moveAllItemsInFolder(this._rushConfiguration.rushUserFolder,
-      [RushConstants.rushRecyclerFolderName]);
+      this._getMembersToExclude(this._rushConfiguration.rushUserFolder));
+  }
+
+  private _getMembersToExclude(folderToRecycle: string): string[] {
+    // Don't recycle the recycler
+    const membersToExclude: string[] = [RushConstants.rushRecyclerFolderName];
+
+    // If the current process is running inside one of the folders, don't recycle that either
+    // Example: "/home/user/.rush/rush-1.2.3/lib/example.js"
+    const currentFolderPath: string = path.resolve(__dirname);
+
+    // Example:
+    // folderToRecycle = "/home/user/.rush"
+    // relative =  "rush-1.2.3/lib/example.js"
+    const relative: string = path.relative(folderToRecycle, currentFolderPath);
+
+    // (The result can be an absolute path if the two folders are on different drive letters)
+    if (!path.isAbsolute(relative)) {
+      // Get the first path segment:
+      const firstPart: string = relative.split(/[\\\/]/)[0];
+      if (firstPart.length > 0 && firstPart !== '..') {
+        membersToExclude.push(firstPart);
+
+        // Warn that we won't dispose this folder
+        console.log(colors.yellow('The active process\'s folder will not be deleted: '
+          + path.join(folderToRecycle, firstPart)));
+      }
+    }
+
+    return membersToExclude;
   }
 }
