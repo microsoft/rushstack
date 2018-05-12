cd4c7b38096d7b4f1cefd96752d1be3cbbebbc23
qz <qz2017@users.noreply.github.com>
2018-05-11 17:58:51 -0700

    Add a message when a package exists before publishing.

--- apps/rush-lib/src/cli/actions/PublishAction.ts
+++ apps/rush-lib/src/cli/actions/PublishAction.ts
@@ -253,8 +253,14 @@ export class PublishAction extends BaseRushAction {
         for (const change of orderedChanges) {
           if (change.changeType && change.changeType > ChangeType.dependency) {
             const project: RushConfigurationProject | undefined = allPackages.get(change.packageName);
-            if (project && !this._packageExists(project)) {
-              this._npmPublish(change.packageName, project.projectFolder);
+            if (project) {
+              if (!this._packageExists(project)) {
+                this._npmPublish(change.packageName, project.projectFolder);
+              } else {
+                console.log(`Skip ${change.packageName}. Package exists.`);
+              }
+            } else {
+              console.log(`Skip ${change.packageName}. Failed to find its project.`);
             }
           }
         }
@@ -326,7 +332,7 @@ export class PublishAction extends BaseRushAction {
         const registryUrl: string = this._registryUrl.value;
         env['npm_config_registry'] = registryUrl; // tslint:disable-line:no-string-literal
         registry = registryUrl.substring(registryUrl.indexOf('//'));
-        args.push(`--registry ${registryUrl}`);
+        args.push(`--registry`, `${registryUrl}`);
       }
 
       if (this._npmAuthToken.value) {
