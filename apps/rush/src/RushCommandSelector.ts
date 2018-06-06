2bb243d1dc308b1196767e10a772d577ce54c712
pgonzal <pgonzal@users.noreply.github.com>
2018-06-06 16:37:04 -0700

    PR feedback

--- apps/rush/src/RushCommandSelector.ts
+++ apps/rush/src/RushCommandSelector.ts
@@ -3,10 +3,17 @@
 
 import * as colors from 'colors';
 import * as path from 'path';
-import * as myRushLib from '@microsoft/rush-lib';
+import * as rushLib from '@microsoft/rush-lib';
 
 type CommandName = 'rush' | 'rushx' | undefined;
 
+/**
+ * Both "rush" and "rushx" share the same src/start.ts entry point.  This makes it
+ * a little easier for them to share all the same startup checks and version selector
+ * logic.  RushCommandSelector looks at argv to determine whether we're doing "rush"
+ * or "rushx" behavior, and then invokes the appropriate entry point in the selected
+ * @microsoft/rush-lib.
+ */
 export class RushCommandSelector {
   public static failIfNotInvokedAsRush(version: string): void {
     if (RushCommandSelector._getCommandName() === 'rushx') {
@@ -16,8 +23,8 @@ export class RushCommandSelector {
   }
 
   // tslint:disable-next-line:no-any
-  public static execute(launcherVersion: string, isManaged: boolean, rushLib: any): void {
-    const Rush: typeof myRushLib.Rush = rushLib.Rush;
+  public static execute(launcherVersion: string, isManaged: boolean, selectedRushLib: any): void {
+    const Rush: typeof rushLib.Rush = selectedRushLib.Rush;
 
     if (!Rush) {
       // This should be impossible unless we somehow loaded an unexpected version
