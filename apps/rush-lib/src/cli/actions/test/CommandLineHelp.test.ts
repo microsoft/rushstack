1152d8f1e4b1cab0a88401f8ac0369d814cd6fe4
pgonzal <pgonzal@users.noreply.github.com>
2018-06-07 17:04:57 -0700

    Update unit tests

--- apps/rush-lib/src/cli/actions/test/CommandLineHelp.test.ts
+++ apps/rush-lib/src/cli/actions/test/CommandLineHelp.test.ts
@@ -2,21 +2,34 @@
 // See LICENSE in the project root for license information.
 
 import * as colors from 'colors';
+import * as path from 'path';
 import { RushCommandLineParser } from '../RushCommandLineParser';
 
 describe('CommandLineHelp', () => {
-  const parser: RushCommandLineParser = new RushCommandLineParser();
 
-  it('prints the global help', () => {
-    const helpText: string = colors.stripColors(parser.renderHelpText());
-    expect(helpText).toMatchSnapshot();
-  });
+  const oldCwd: string = process.cwd();
+  const localCwd: string = path.join(__dirname, 'repo');
 
-  for (const action of parser.actions) {
-    it(`prints the help for "rush ${action.actionName}"`, () => {
-      const helpText: string = colors.stripColors(action.renderHelpText());
+  try {
+    process.chdir(localCwd);
+
+    const parser: RushCommandLineParser = new RushCommandLineParser();
+
+    it('prints the global help', () => {
+      const helpText: string = colors.stripColors(parser.renderHelpText());
       expect(helpText).toMatchSnapshot();
     });
+
+    for (const action of parser.actions) {
+      it(`prints the help for "rush ${action.actionName}"`, () => {
+        const helpText: string = colors.stripColors(action.renderHelpText());
+        expect(helpText).toMatchSnapshot();
+      });
+    }
+
+  } finally {
+    process.chdir(oldCwd);
   }
 
+  expect(true).toBeTruthy();
 });
