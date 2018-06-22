fd6c8a86c4723fdfe51575c1866c54fbce84e0a6
pgonzal <pgonzal@users.noreply.github.com>
2018-06-21 19:02:14 -0700

    Add missing copyright banners

--- libraries/rushell/src/TextRange.ts
+++ libraries/rushell/src/TextRange.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 /**
  * Text coordinates represented as a line number and column number.
  *
