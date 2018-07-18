deb3b6c8d5e14ea565defe2c45b5efe5188e006a
pgonzal <pgonzal@users.noreply.github.com>
2018-07-18 11:48:39 -0700

    Update all tests to use Jest API

--- libraries/node-core-library/src/test/ProtectableMap.test.ts
+++ libraries/node-core-library/src/test/ProtectableMap.test.ts
@@ -1,10 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-/// <reference types='mocha' />
-
 import { ProtectableMap } from '../ProtectableMap';
-import { assert } from 'chai';
 
 class ExampleApi {
   public clearedCount: number = 0;
@@ -57,13 +54,13 @@ describe('ProtectableMap', () => {
     exampleApi.studentAgesByName.set('CHARLIE', 22);
     exampleApi.studentAgesByName.delete('CHARLIE');
 
-    assert.equal(exampleApi.clearedCount, 1);
-    assert.equal(exampleApi.setCount, 4);
-    assert.equal(exampleApi.deletedCount, 1);
+    expect(exampleApi.clearedCount).toEqual(1);
+    expect(exampleApi.setCount).toEqual(4);
+    expect(exampleApi.deletedCount).toEqual(1);
 
-    assert.equal(exampleApi.studentAgesByName.get('ALICE'), 23);
-    assert.equal(exampleApi.studentAgesByName.get('BOB'), 0); // clamped by onSet()
-    assert.equal(exampleApi.studentAgesByName.has('CHARLIE'), false);
+    expect(exampleApi.studentAgesByName.get('ALICE')).toEqual(23);
+    expect(exampleApi.studentAgesByName.get('BOB')).toEqual(0); // clamped by onSet()
+    expect(exampleApi.studentAgesByName.has('CHARLIE')).toEqual(false);
   });
 
   it('Unprotected operations', () => {
@@ -72,15 +69,15 @@ describe('ProtectableMap', () => {
     exampleApi.doUnprotectedOperations();
 
     // Interacting directly with the ProtectableMap bypasses the hooks
-    assert.equal(exampleApi.clearedCount, 0);
-    assert.equal(exampleApi.studentAgesByName.get('Dave'), -123);
+    expect(exampleApi.clearedCount).toEqual(0);
+    expect(exampleApi.studentAgesByName.get('Dave')).toEqual(-123);
   });
 
   it('Error case', () => {
     const exampleApi: ExampleApi = new ExampleApi();
-    assert.throw(() => {
+    expect(() => {
       exampleApi.studentAgesByName.set('Jane', 23);
-    }, 'The key must be all upper case: Jane');
+    }).toThrowError('The key must be all upper case: Jane');
   });
 
 });
--- libraries/node-core-library/src/test/Text.test.ts
+++ libraries/node-core-library/src/test/Text.test.ts
@@ -1,30 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-/// <reference types='mocha' />
-
 import { Text } from '../Text';
-import { assert } from 'chai';
 
 describe('Text', () => {
   it('Text.padEnd()', () => {
-    assert.equal(Text.padEnd('', 5),        '     ');
-    assert.equal(Text.padEnd('123', 5),     '123  ');
-    assert.equal(Text.padEnd('12345', 5),   '12345');
-    assert.equal(Text.padEnd('123456', 5),  '123456');
+    expect(Text.padEnd('', 5)).toEqual(      '     ');
+    expect(Text.padEnd('123', 5)).toEqual(   '123  ');
+    expect(Text.padEnd('12345', 5)).toEqual( '12345');
+    expect(Text.padEnd('123456', 5)).toEqual('123456');
   });
   it('Text.truncateWithEllipsis()', () => {
-    assert.throws(() => { Text.truncateWithEllipsis('123', -1); });
-    assert.equal(Text.truncateWithEllipsis('123', 0),     '');
+    expect(() => {
+      Text.truncateWithEllipsis('123', -1);
+    }).toThrow();
+    expect(Text.truncateWithEllipsis('123', 0)).toEqual(   '');
 
-    assert.equal(Text.truncateWithEllipsis('', 2),        '');
-    assert.equal(Text.truncateWithEllipsis('1', 2),       '1');
-    assert.equal(Text.truncateWithEllipsis('12', 2),      '12');
-    assert.equal(Text.truncateWithEllipsis('123', 2),     '12');
+    expect(Text.truncateWithEllipsis('', 2)).toEqual(      '');
+    expect(Text.truncateWithEllipsis('1', 2)).toEqual(     '1');
+    expect(Text.truncateWithEllipsis('12', 2)).toEqual(    '12');
+    expect(Text.truncateWithEllipsis('123', 2)).toEqual(   '12');
 
-    assert.equal(Text.truncateWithEllipsis('123', 5),     '123');
-    assert.equal(Text.truncateWithEllipsis('1234', 5),    '1234');
-    assert.equal(Text.truncateWithEllipsis('12345', 5),   '12345');
-    assert.equal(Text.truncateWithEllipsis('123456', 5),  '12...');
+    expect(Text.truncateWithEllipsis('123', 5)).toEqual(   '123');
+    expect(Text.truncateWithEllipsis('1234', 5)).toEqual(  '1234');
+    expect(Text.truncateWithEllipsis('12345', 5)).toEqual( '12345');
+    expect(Text.truncateWithEllipsis('123456', 5)).toEqual('12...');
   });
 });
