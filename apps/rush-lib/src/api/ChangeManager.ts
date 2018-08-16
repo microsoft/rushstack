2f15254046c9d2c749b632c760ff4689f355b230
Nick Pape <nickpape-msft@users.noreply.github.com>
2018-08-16 16:37:41 -0700

    add some comments

--- apps/rush-lib/src/api/ChangeManager.ts
+++ apps/rush-lib/src/api/ChangeManager.ts
@@ -5,8 +5,15 @@ import { IChangeFile } from './ChangeManagement';
 
 /**
  * @public
+ * A class that helps with programatically interacting with Rush's change files.
  */
 export class ChangeManager {
+  /**
+   * Creates a change file that has a 'none' type.
+   * @param rushConfiguration The rush configuration we are working with
+   * @param projectName The name of the project for which to create a change file
+   * @param emailAddress The email address which should be associated with this change
+   */
   public static createEmptyChangeFiles(
     rushConfiguration: RushConfiguration,
     projectName: string,
