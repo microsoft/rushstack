916da80b0b96c304653e055007c1c0a95f3547e0
pgonzal <pgonzal@users.noreply.github.com>
2018-08-22 10:59:13 -0700

    Update JJU typings

--- libraries/node-core-library/typings/jju/jju.d.ts
+++ libraries/node-core-library/typings/jju/jju.d.ts
@@ -2,12 +2,24 @@
 // Project: https://www.npmjs.com/package/jju
 // Definitions by: pgonzal
 
-interface IJjuOptions {
+interface IJjuParseOptions {
   reserved_keys? : 'ignore' | 'throw' | 'replace';
-  mode?: 'json';
+  null_prototype?: boolean;
   reviver?: (key: any, value: any) => any;
+  mode?: 'json';
+}
+
+interface IJjuStringifyOptions {
+  ascii?: boolean;
+  indent?: string | number | boolean;
+  quote_keys?: boolean;
+  sort_keys?: boolean;
+  replacer?: (key: string, value: any) => any;
+  no_trailing_comma?: boolean;
+  mode?: string | 'json';
 }
 
 declare module 'jju' {
-  export function parse(text: string, options?: IJjuOptions): any;
+  export function parse(text: string, options?: IJjuParseOptions): any;
+  export function stringify(value: any, options?: IJjuStringifyOptions): string;
 }
