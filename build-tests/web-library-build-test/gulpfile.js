7662b591c2ae0dd7e1f017339f8c77bfa7dee051
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-08-28 17:42:57 -0700

    Fixing WBT test.

--- build-tests/web-library-build-test/gulpfile.js
+++ build-tests/web-library-build-test/gulpfile.js
@@ -6,11 +6,6 @@ let build = require('@microsoft/web-library-build');
 build.sass.setConfig({ useCSSModules: true });
 build.webpack.setConfig({ configPath: null });
 
-build.setConfig({
-  libAMDFolder: path.join(build.getConfig().packageFolder, 'lib-amd'),
-  libES6Folder: path.join(build.getConfig().packageFolder, 'lib-es6')
-});
-
 build.preCopy.cleanMatch = ['src/preCopyTest.ts'];
 
 build.initialize(require('gulp'));
