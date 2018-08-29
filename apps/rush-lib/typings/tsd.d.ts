178d8e52df7b4a8ff01a105104fdff50f8fd5602
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-08-28 23:06:44 -0700

    Fixing some typigns issues.

---
+++ apps/rush-lib/typings/tsd.d.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/// <reference path="git-repo-info/git-repo-info.d.ts" />
+/// <reference path="jju/jju.d.ts" />
+/// <reference path="rimraf/rimraf.d.ts" />
+/// <reference path="builtins/builtins.d.ts" />
+/// <reference path="glob-escape/glob-escape.d.ts" />
+/// <reference path="inquirer/inquirer.d.ts" />
+/// <reference path="npm-package-arg/npm-package-arg.d.ts" />
+/// <reference path="read-package-tree/read-package-tree.d.ts" />
+/// <reference path="strict-uri-encode/strict-uri-encode.d.ts" />
+/// <reference path="wordwrap/wordwrap.d.ts" />
