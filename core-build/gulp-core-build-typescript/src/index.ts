36ea0d96b5c070c8306bb0cf4970c5b39b0df87e
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-09-10 13:10:06 -0700

    Removing the RemoveTripleSlash task.

--- core-build/gulp-core-build-typescript/src/index.ts
+++ core-build/gulp-core-build-typescript/src/index.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { RemoveTripleSlashReferenceTask } from './RemoveTripleSlashReferenceTask';
-
 import {
   TscCmdTask,
   ITscCmdTaskConfig
@@ -19,8 +17,6 @@ export {
   TslintCmdTask,
   ITslintCmdTaskConfig
 };
-
-export const removeTripleSlash: RemoveTripleSlashReferenceTask = new RemoveTripleSlashReferenceTask();
 
 export const tscCmd: TscCmdTask = new TscCmdTask();
 export const tslintCmd: TslintCmdTask = new TslintCmdTask();
