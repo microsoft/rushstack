179dc7f103a461fe8bb9ec4eb66729ab973b4fb4
pgonzal <pgonzal@users.noreply.github.com>
2018-10-25 00:29:52 -0700

    Add Sort API

---
+++ libraries/node-core-library/src/Sort.ts
@@ -0,0 +1,127 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/**
+ * Operations for sorting collections.
+ *
+ * @public
+ */
+export class Sort {
+  /**
+   * Compares `x` and `y` using the JavaScript `>` and `<` operators.  This function is suitable for usage as
+   * the callback for `array.sort()`.
+   * @returns -1 if `x` is smaller than `y`, 1 if `x` is greater than `y`, or 0 if the values are equal.
+   *
+   * @example
+   *
+   * ```ts
+   * let array: number[] = [3, 6, 2];
+   * array.sort(Sort.compareByValue);  // [2, 3, 6]
+   * ```
+   */
+  // tslint:disable-next-line:no-any
+  public static compareByValue(x: any, y: any): number {
+    if (x === y) {
+      return 0;
+    }
+    if (x === undefined) {
+      return -1;
+    }
+    if (x === null) {
+      return -1;
+    }
+    if (x < y) {
+      return -1;
+    }
+    if (x > y) {
+      return 1;
+    }
+    return 0;
+  }
+
+  /**
+   * Sorts the array according to a key which is obtained from the array elements.
+   *
+   * @example
+   *
+   * ```ts
+   * let array: string[] = [ 'ccc', 'bb', 'a' ];
+   * Sort.sortBy(array, x => x.length);  // [ 'a', 'bb', 'ccc' ]
+   * ```
+   */
+  // tslint:disable-next-line:no-any
+  public static sortBy<T>(array: T[], keySelector: (element: T) => any, comparer: (x: any, y: any) => number
+    = Sort.compareByValue): void {
+    array.sort((x, y) => comparer(keySelector(x), keySelector(y)));
+  }
+
+  /**
+   * Returns true if the array is already sorted.
+   */
+  // tslint:disable-next-line:no-any
+  public static isSorted<T>(array: T[], comparer: (x: any, y: any) => number = Sort.compareByValue): boolean {
+    let previous: T | undefined = undefined;
+    for (const element of array) {
+      if (comparer(previous, element) > 0) {
+        return false;
+      }
+      previous = element;
+    }
+    return true;
+  }
+
+  /**
+   * Returns true if the array is already sorted by the specified key.
+   *
+   * @example
+   *
+   * ```ts
+   * let array: string[] = [ 'a', 'bb', 'ccc' ];
+   * Sort.isSortedBy(array, x => x.length); // true
+   * ```
+   */
+  // tslint:disable-next-line:no-any
+  public static isSortedBy<T>(array: T[], keySelector: (element: T) => any, comparer: (x: any, y: any) => number
+    = Sort.compareByValue): boolean {
+
+    let previousKey: T | undefined = undefined;
+    for (const element of array) {
+      const key: T = keySelector(element);
+      if (comparer(previousKey, key) > 0) {
+        return false;
+      }
+      previousKey = key;
+    }
+    return true;
+  }
+
+  /**
+   * Sorts the entries in a Map object according to the keys.
+   *
+   * @example
+   *
+   * ```ts
+   * let map: Map<string, number> = new Map<string, number>();
+   * map.set('zebra', 1);
+   * map.set('goose', 2);
+   * map.set('aardvark', 3);
+   * Sort.sortMapKeys(map);
+   * console.log(JSON.stringify(Array.from(map.keys()))); // ["aardvark","goose","zebra"]
+   * ```
+   */
+  // tslint:disable-next-line:no-any
+  public static sortMapKeys<K, V>(map: Map<K, V>, keyComparer: (x: K, y: K) => number = Sort.compareByValue): void {
+    const pairs: [K, V][] = Array.from(map.entries());
+
+    // Sorting a map is expensive, so first check whether it's already sorted.
+    if (Sort.isSortedBy(pairs, x => x[0], keyComparer)) {
+      return;
+    }
+
+    Sort.sortBy(pairs, x => x[0], keyComparer);
+    map.clear();
+    for (const pair of pairs) {
+      map.set(pair[0], pair[1]);
+    }
+  }
+}
