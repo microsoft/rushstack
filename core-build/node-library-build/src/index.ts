12ce0f8c05f919f21071e7301c48e12f8e142b8b
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-10-25 23:44:24 -0700

    Fixing TSLint issues in node-library-build.

--- core-build/node-library-build/src/index.ts
+++ core-build/node-library-build/src/index.ts
@@ -10,8 +10,7 @@ import {
   serial,
   parallel,
   IExecutable,
-  setConfig,
-  IBuildConfig
+  setConfig
 } from '@microsoft/gulp-core-build';
 import { tscCmd, tslintCmd, apiExtractor } from '@microsoft/gulp-core-build-typescript';
 import { instrument, mocha } from '@microsoft/gulp-core-build-mocha';
@@ -27,13 +26,18 @@ preCopy.name = 'pre-copy';
 export const postCopy: CopyTask = new CopyTask();
 postCopy.name = 'post-copy';
 
-const PRODUCTION = process.argv.indexOf('--production') !== -1 || process.argv.indexOf('--ship') !== -1;
+const PRODUCTION: boolean = process.argv.indexOf('--production') !== -1 || process.argv.indexOf('--ship') !== -1;
 setConfig({
   production: PRODUCTION,
   shouldWarningsFailBuild: PRODUCTION
 });
 
-const buildSubtask: IExecutable = serial(preCopy, parallel(tslintCmd, tscCmd, copyStaticAssets), apiExtractor, postCopy);
+const buildSubtask: IExecutable = serial(
+  preCopy,
+  parallel(tslintCmd, tscCmd, copyStaticAssets),
+  apiExtractor,
+  postCopy
+);
 export const buildTasks: IExecutable = task('build', buildSubtask);
 export const testTasks: IExecutable = task('test', serial(buildSubtask, mocha, jest));
 export const defaultTasks: IExecutable = task('default', serial(buildSubtask, instrument, mocha, jest));
