8fce858c5bdc97d15c88c00d31116c43965d2fb0
pgonzal <pgonzal@users.noreply.github.com>
2018-11-26 21:57:03 -0800

    Add PackageJsonLookup.loadOwnPackageJson() API to node-core-library
    (cherry picked from commit 924dc88bef57bd5131b7e593029827b3f462498c)

--- common/reviews/api/node-core-library.api.ts
+++ common/reviews/api/node-core-library.api.ts
@@ -350,6 +350,7 @@ enum NewlineKind {
 class PackageJsonLookup {
   constructor(parameters?: IPackageJsonLookupParameters);
   clearCache(): void;
+  static loadOwnPackageJson(dirnameOfCaller: string, pathToPackageJsonFolder: string): IPackageJson;
   loadPackageJson(jsonFilename: string): IPackageJson;
   tryGetPackageFolderFor(fileOrFolderPath: string): string | undefined;
   tryGetPackageJsonFilePathFor(fileOrFolderPath: string): string | undefined;
