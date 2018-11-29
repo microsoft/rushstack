5f9a7b7e30fd9c903b1858616026c16223c326fc
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2018-11-28 16:52:51 -0800

    Bumping cyclic dependencies and tying up a few loose ends.

--- core-build/gulp-core-build-typescript/gulpfile.js
+++ core-build/gulp-core-build-typescript/gulpfile.js
@@ -2,18 +2,5 @@
 
 const build = require('@microsoft/node-library-build');
 
-// Remove these when node-library-build is published and bumped
-build.tscCmd.mergeConfig({
-  allowBuiltinCompiler: true
-});
-
-build.tslintCmd.mergeConfig({
-  allowBuiltinCompiler: true
-});
-
-build.apiExtractor.mergeConfig({
-  allowBuiltinCompiler: true
-});
-
 build.mocha.enabled = false;
 build.initialize(require('gulp'));
