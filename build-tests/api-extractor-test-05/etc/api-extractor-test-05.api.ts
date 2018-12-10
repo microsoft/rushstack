ca28d117c784c12647aa7e1c427e193a1c8875ca
pgonzal <pgonzal@users.noreply.github.com>
2018-12-09 22:01:58 -0700

    Add a unit test with "extends" and "inherits" heritage types

--- build-tests/api-extractor-test-05/etc/api-extractor-test-05.api.ts
+++ build-tests/api-extractor-test-05/etc/api-extractor-test-05.api.ts
@@ -1,5 +1,9 @@
 // @public
-declare class DocClass1 {
+declare class DocBaseClass {
+}
+
+// @public
+declare class DocClass1 extends DocBaseClass implements IDocInterface1, IDocInterface2 {
     // @deprecated (undocumented)
     deprecatedExample(): void;
     exampleFunction(a: string, b: string): string;
@@ -19,6 +23,17 @@ declare enum DocEnum {
     One = 1,
     Two = 2,
     Zero = 0
+}
+
+// @public (undocumented)
+interface IDocInterface1 {
+    regularProperty: SystemEvent;
+}
+
+// @public (undocumented)
+interface IDocInterface2 extends IDocInterface1 {
+    // @deprecated (undocumented)
+    deprecatedExample(): void;
 }
 
 // @public
---
+++ build-tests/api-extractor-test-05/etc/markdown/api-extractor-test-05.idocinterface1.regularproperty.md
@@ -0,0 +1,11 @@
+[Home](./index) &gt; [api-extractor-test-05](./api-extractor-test-05.md) &gt; [IDocInterface1](./api-extractor-test-05.idocinterface1.md) &gt; [regularProperty](./api-extractor-test-05.idocinterface1.regularproperty.md)
+
+## IDocInterface1.regularProperty property
+
+Does something
+
+<b>Signature:</b>
+
+```typescript
+regularProperty: SystemEvent;
+```
