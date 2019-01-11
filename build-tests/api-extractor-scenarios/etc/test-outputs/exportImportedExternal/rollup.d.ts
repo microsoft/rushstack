287d5364862e74a7bdf27305b071e006a79c3f59
pgonzal <pgonzal@users.noreply.github.com>
2019-01-10 16:34:14 -0800

    Add "exportStar3" and "exportImportedExternal" scenarios

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/exportImportedExternal/rollup.d.ts
@@ -0,0 +1,10 @@
+import { Lib2Interface as DoubleRenamedLib2Class } from 'api-extractor-lib2-test';
+export { DoubleRenamedLib2Class };
+import { Lib1Class } from 'api-extractor-lib1-test';
+export { Lib1Class };
+import { Lib1Interface } from 'api-extractor-lib1-test';
+export { Lib1Interface };
+import { Lib2Class as RenamedLib2Class } from 'api-extractor-lib2-test';
+export { RenamedLib2Class };
+
+export { }
--- build-tests/api-extractor-scenarios/src/exportStar3/index.ts
+++ build-tests/api-extractor-scenarios/src/exportStar3/index.ts
@@ -1,6 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-export * from './reexportStar';
-
-export * from 'api-extractor-lib2-test';
+export { A, Lib1Class } from './reexportStar';
