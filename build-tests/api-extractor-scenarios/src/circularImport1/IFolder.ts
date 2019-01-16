1aa8357993b6f890f4fd6919a274ae452e2bc351
pgonzal <pgonzal@users.noreply.github.com>
2019-01-16 11:31:35 -0800

    Add a test case for circular imports

---
+++ build-tests/api-extractor-scenarios/src/circularImport1/IFolder.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import { IFile } from "./IFile";
+
+export interface IFolder {
+  containingFolder: IFolder | undefined;
+  files: IFile[];
+}
