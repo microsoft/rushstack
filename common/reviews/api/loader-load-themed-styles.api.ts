0ea3a34ee846ca6e6e08256b9df07a7bd67ce081
pgonzal <pgonzal@users.noreply.github.com>
2019-01-18 19:26:14 -0800

    Regenerate .api.ts files

--- common/reviews/api/loader-load-themed-styles.api.ts
+++ common/reviews/api/loader-load-themed-styles.api.ts
@@ -1,6 +1,2 @@
-// WARNING: Definition with no declarations: prototype
-// WARNING: Unsupported export: _loadedThemedStylesPath
-// WARNING: Unsupported export: loadedThemedStylesPath
-// WARNING: Unsupported export: resetLoadedThemedStylesPath
-// WARNING: Unsupported export: pitch
-// (No @packagedocumentation comment for this package)
+
+// (No @packageDocumentation comment for this package)
