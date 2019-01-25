f5e596b8f609b77bc873429e000df5225aee4ab2
Scott Hysom <ScottHysom@users.noreply.github.com>
2019-01-25 11:54:46 -0800

    Add standard rebuild action unit test

--- apps/rush-lib/src/cli/test/RushCommandLineParser.test.ts
+++ apps/rush-lib/src/cli/test/RushCommandLineParser.test.ts
@@ -8,6 +8,7 @@ import { resolve } from 'path';
 import { chdir } from 'process';
 import { ChildProcessModuleMock, ISpawnMockConfig } from 'child_process';
 import { FileSystem } from '@microsoft/node-core-library';
+import { Interleaver } from '@microsoft/stream-collator';
 import { RushCommandLineParser } from '../RushCommandLineParser';
 
 /**
@@ -29,51 +30,113 @@ const SPAWN_ARG_OPTIONS: number = 2;
 
 describe('RushCommandLineParser', () => {
   describe('execute', () => {
-    describe(`'build' action`, () => {
-      it(`executes the package's 'build' script`, () => {
-        // Point to the test repo folder
-        chdir(resolve(__dirname, 'buildActionsRepo'));
+    afterEach(() => {
+      // Reset Interleaver so we can re-register a task with the same name for the tests.
+      //
+      // Sadly, Interleaver retains a static list of writer loggers which must have unique names. The names are the
+      // names of the packages. Our unit tests use test repos with generic 'a', 'b' package names, so anything after
+      // the first test that runs an instance of RushCommandLineParser throws an exception complaining of duplicate
+      // writer names.
+      Interleaver.reset();
 
-        // The `build` task is hard-coded to be incremental. So delete the `package-deps.json` files in
-        // the test repo to override so the test actually runs.
-        FileSystem.deleteFile(resolve(__dirname, 'buildActionsRepo/a/package-deps.json'));
-        FileSystem.deleteFile(resolve(__dirname, 'buildActionsRepo/b/package-deps.json'));
+      jest.clearAllMocks();
+    });
 
-        // Create a Rush CLI instance. This instance is heavy-weight and relies on setting process.exit
-        // to exit and clear the Rush file lock. So running multiple `it` test blocks over the same test
-        // repo will fail due to contention over the same lock.
-        const parser: RushCommandLineParser = new RushCommandLineParser();
+    describe('in basic repo', () => {
+      describe(`'build' action`, () => {
+        it(`executes the package's 'build' script`, () => {
+          // Point to the test repo folder
+          chdir(resolve(__dirname, 'buildActionRepo'));
 
-        // Mock the command
-        process.argv = ['pretend-this-is-node.exe', 'pretend-this-is-rush', 'build'];
-        const spawnMock: jest.Mock = setSpawnMock();
+          // The `build` task is hard-coded to be incremental. So delete the `package-deps.json` files in
+          // the test repo to override so the test actually runs.
+          FileSystem.deleteFile(resolve(__dirname, 'buildActionRepo/a/package-deps.json'));
+          FileSystem.deleteFile(resolve(__dirname, 'buildActionRepo/b/package-deps.json'));
 
-        expect.assertions(8);
-        return expect(parser.execute()).resolves.toEqual(true)
-          .then(() => {
-            // There should be 1 build per package
-            const packageCount: number = spawnMock.mock.calls.length;
-            expect(packageCount).toEqual(2);
+          // Create a Rush CLI instance. This instance is heavy-weight and relies on setting process.exit
+          // to exit and clear the Rush file lock. So running multiple `it` or `describe` test blocks over the same test
+          // repo will fail due to contention over the same lock.
+          const parser: RushCommandLineParser = new RushCommandLineParser();
 
-            // Use regex for task name in case spaces were prepended or appended to spawned command
-            const expectedBuildTaskRegexp: RegExp = /fake_build_task_but_works_with_mock/;
+          // Mock the command
+          process.argv = ['pretend-this-is-node.exe', 'pretend-this-is-rush', 'build'];
+          const spawnMock: jest.Mock = setSpawnMock();
 
-            // tslint:disable-next-line: no-any
-            const firstSpawn: any[] = spawnMock.mock.calls[0];
-            expect(firstSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
-              expect.stringMatching(expectedBuildTaskRegexp)
-            ]));
-            expect(firstSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
-            expect(firstSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'buildActionsRepo/a'));
+          expect.assertions(8);
+          return expect(parser.execute()).resolves.toEqual(true)
+            .then(() => {
+              // There should be 1 build per package
+              const packageCount: number = spawnMock.mock.calls.length;
+              expect(packageCount).toEqual(2);
 
-            // tslint:disable-next-line: no-any
-            const secondSpawn: any[] = spawnMock.mock.calls[1];
-            expect(secondSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
-              expect.stringMatching(expectedBuildTaskRegexp)
-            ]));
-            expect(secondSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
-            expect(secondSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'buildActionsRepo/b'));
-          });
+              // Use regex for task name in case spaces were prepended or appended to spawned command
+              const expectedBuildTaskRegexp: RegExp = /fake_build_task_but_works_with_mock/;
+
+              // tslint:disable-next-line: no-any
+              const firstSpawn: any[] = spawnMock.mock.calls[0];
+              expect(firstSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
+                expect.stringMatching(expectedBuildTaskRegexp)
+              ]));
+              expect(firstSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
+              expect(firstSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'buildActionRepo/a'));
+
+              // tslint:disable-next-line: no-any
+              const secondSpawn: any[] = spawnMock.mock.calls[1];
+              expect(secondSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
+                expect.stringMatching(expectedBuildTaskRegexp)
+              ]));
+              expect(secondSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
+              expect(secondSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'buildActionRepo/b'));
+            });
+        });
+      });
+
+      describe(`'rebuild' action`, () => {
+        it(`executes the package's 'build' script`, () => {
+          // Point to the test repo folder
+          chdir(resolve(__dirname, 'rebuildActionRepo'));
+
+          // The `build` task is hard-coded to be incremental. So delete the `package-deps.json` files in
+          // the test repo to override so the test actually runs.
+          FileSystem.deleteFile(resolve(__dirname, 'rebuildActionRepo/a/package-deps.json'));
+          FileSystem.deleteFile(resolve(__dirname, 'rebuildActionRepo/b/package-deps.json'));
+
+          // Create a Rush CLI instance. This instance is heavy-weight and relies on setting process.exit
+          // to exit and clear the Rush file lock. So running multiple `it` or `describe` test blocks over the same test
+          // repo will fail due to contention over the same lock.
+          const parser: RushCommandLineParser = new RushCommandLineParser();
+
+          // Mock the command
+          process.argv = ['pretend-this-is-node.exe', 'pretend-this-is-rush', 'rebuild'];
+          const spawnMock: jest.Mock = setSpawnMock();
+
+          expect.assertions(8);
+          return expect(parser.execute()).resolves.toEqual(true)
+            .then(() => {
+              // There should be 1 build per package
+              const packageCount: number = spawnMock.mock.calls.length;
+              expect(packageCount).toEqual(2);
+
+              // Use regex for task name in case spaces were prepended or appended to spawned command
+              const expectedBuildTaskRegexp: RegExp = /fake_build_task_but_works_with_mock/;
+
+              // tslint:disable-next-line: no-any
+              const firstSpawn: any[] = spawnMock.mock.calls[0];
+              expect(firstSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
+                expect.stringMatching(expectedBuildTaskRegexp)
+              ]));
+              expect(firstSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
+              expect(firstSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'rebuildActionRepo/a'));
+
+              // tslint:disable-next-line: no-any
+              const secondSpawn: any[] = spawnMock.mock.calls[1];
+              expect(secondSpawn[SPAWN_ARG_ARGS]).toEqual(expect.arrayContaining([
+                expect.stringMatching(expectedBuildTaskRegexp)
+              ]));
+              expect(secondSpawn[SPAWN_ARG_OPTIONS]).toEqual(expect.any(Object));
+              expect(secondSpawn[SPAWN_ARG_OPTIONS].cwd).toEqual(resolve(__dirname, 'rebuildActionRepo/b'));
+            });
+        });
       });
     });
   });
