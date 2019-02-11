79991cba0410254b77260858a458c62a02b97db1
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-02-11 13:52:35 -0800

    Add a test case illustrating issue #1095

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/ambientNameConflict/rollup.d.ts
@@ -0,0 +1,14 @@
+
+/**
+ * @public
+ */
+export declare function ambientNameConflict(p1: Promise<void>, p2: Promise<void>): void;
+
+/**
+ * @public
+ */
+declare class Promise<T> {
+    notTheRealPromise(arg: T): void;
+}
+
+export { }
