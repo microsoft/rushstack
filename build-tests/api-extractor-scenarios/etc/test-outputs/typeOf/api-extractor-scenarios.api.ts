2cda09e5e03996b98ff4baafc747f12320aa422f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-02-11 00:55:10 -0800

    Add a test case for "typeof" references

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/typeOf/api-extractor-scenarios.api.ts
@@ -0,0 +1,8 @@
+// @public
+declare function f(): typeof Lib1Class | undefined;
+
+// @public
+declare function g(): typeof ForgottenExport | undefined;
+
+
+// (No @packageDocumentation comment for this package)
---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/typeOf/rollup.d.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Reference Lib1Class via "typeof"
+ * @public
+ */
+export declare function f(): typeof Lib1Class | undefined;
+
+/**
+ * Reference IForgottenExport via "typeof"
+ * @public
+ */
+export declare function g(): typeof ForgottenExport | undefined;
+
+export { }
