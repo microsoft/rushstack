971fa54bb556caeb5bd78af108ae76bfb9eb0f79
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-03-19 22:40:40 -0700

    Fix incorrect type

--- apps/api-extractor/src/generators/StringWriter.ts
+++ apps/api-extractor/src/generators/StringWriter.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { StringBuilder } from '@microsoft/node-core-library';
+import { StringBuilder } from '@microsoft/tsdoc';
 
 // A small helper used by the generators
 export class StringWriter {
