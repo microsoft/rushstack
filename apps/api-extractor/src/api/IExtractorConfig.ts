7e55d2c4cf38e7e948dbe7eb7785819d43f4ec10
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-03-31 13:56:26 -0700

    Update IExtractorConfig to use the new schema

--- apps/api-extractor/src/api/IExtractorConfig.ts
+++ apps/api-extractor/src/api/IExtractorConfig.ts
@@ -2,273 +2,207 @@
 // See LICENSE in the project root for license information.
 
 /**
- * With this configuration, API Extractor configures the compiler based on settings that
- * it finds in the project's tsconfig.json file.
+ * Determines how the TypeScript compiler engine will be invoked by API Extractor.
+ *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
  *
  * @public
  */
-export interface IExtractorTsconfigCompilerConfig {
-  configType: 'tsconfig';
+export interface IExtractorCompilerConfig {
 
   /**
-   * The root folder for the project.
+   * The root folder for the project.  This folder typically contains the tsconfig.json and package.json
+   * config files.
+   *
    * @remarks
-   * This folder typically contains the tsconfig.json and package.json config files.
+   *
+   * The `rootFolder` path is resolved relative to the folder containing api-extractor.json.
+   *
+   * The default value for `rootFolder` is the token `<lookup>`, which means the folder is determined by traversing
+   * parent folders, starting from the folder containing api-extractor.json, and stopping at the first folder
+   * that contains a tsconfig.json file.  If a tsconfig.json file cannot be found in this way, then an error
+   * will be reported.
    */
   rootFolder: string;
 
   /**
-   * Override the tsconfig.json file contents.
+   * Provides already parsed tsconfig.json contents.
    *
    * @remarks
-   * Provides already parsed tsconfig.json contents conforming to the TypeScript tsconfig schema:
+   * The value must conform to the TypeScript tsconfig schema:
+   *
    * http://json.schemastore.org/tsconfig
    *
-   * If omitted, then by default the tsconfig.json file will be loaded from the root folder.
+   * If omitted, then the tsconfig.json file will instead be read from the rootFolder.
    */
   overrideTsconfig?: { };
-}
 
-/**
- * With this configuration, API Extractor is configured using an already prepared compiler state
- * that is provided programmatically at runtime.  This can potentially enable faster builds,
- * by reusing the same compiler invocation for tsc, tslint, and API Extractor.
- *
- * If configType='runtime' is specified, then IExtractorRuntimeOptions.compilerProgram must be
- * provided.
- *
- * @public
- */
-export interface IExtractorRuntimeCompilerConfig {
-  configType: 'runtime';
-}
-
-/**
- * Describes a specific project that will be analyzed.  In principle, multiple individual
- * projects can be processed while reusing a common compiler state.
- *
- * @public
- */
-export interface IExtractorProjectConfig {
   /**
-   * Specifies the TypeScript *.d.ts file that will be treated as the entry point
-   * for compilation.  Typically this corresponds to the "typings" or "types" field
-   * from package.json, but secondary entry points are also possible.
+   * This option causes the compiler to be invoked with the --skipLibCheck option.
    *
    * @remarks
-   * The file extension must not be *.ts.  API Extractor does NOT process TypeScript
-   * source code, but instead the output of the compiler.  This is needed for compatibility
-   * with preprocessors and also custom tooling that produces TypeScript-compatible outputs
-   * without using the real compiler.  It also speeds up the analysis by avoiding the
-   * need to parse implementation code.
+   * This option is not recommended and may cause API Extractor to produce incomplete or incorrect declarations,
+   * but it may be required when dependencies contain declarations that are incompatible with the TypeScript engine
+   * that API Extractor uses for its analysis.  Where possible, the underlying issue should be fixed rather than
+   * relying on skipLibCheck.
    */
-  entryPointSourceFile: string;
-}
-
-/**
- * These policies determine how API Extractor validates various best practices for API design.
- *
- * @public
- */
-export interface IExtractorPoliciesConfig {
-  /**
-   * Controls how API Extractor treats the TypeScript namespace keyword:
-   *
-   * conservative - (the default) namespaces may only be used to represent tables of constants
-   *
-   * permissive - arbitrary nesting of namespaces is allowed
-   */
-  namespaceSupport?: 'conservative' | 'permissive';
-}
-
-/**
- * Configuration values used for the {@link IExtractorValidationRulesConfig} block.
- * @public
- */
-export const enum ExtractorValidationRulePolicy {
-  /**
-   * Violations of the rule will be reported as build errors.
-   */
-  error = 'error',
-  /**
-   * Violations of the rule are silently ignored.
-   */
-  allow = 'allow'
-}
-
-/**
- * Configuration for various validation checks that ensure good API design
- *
- * @public
- */
-export interface IExtractorValidationRulesConfig {
-  /**
-   * This rule checks for top-level API items that are missing a release tag such as \@beta or \@internal.
-   * If "allow" is chosen, then missing release tags will be assumed to be \@public.
-   * The default policy is "error".
-   */
-  missingReleaseTags?: ExtractorValidationRulePolicy;
+  skipLibCheck?: boolean;
 }
 
 /**
  * Configures how the API review files (*.api.md) will be generated.
  *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
+ *
  * @public
  */
-export interface IExtractorApiReviewFileConfig {
+export interface IExtractorApiReportConfig {
   /**
-   * Whether to generate review files at all.  The default is true.
+   * Whether to generate an API report.
    */
   enabled: boolean;
 
   /**
-   * The file path of the folder containing API review file, relative to
-   * the project folder.  This is part of an API review workflow:  During a build,
-   * the API Extractor will output an API file, e.g. "my-project/temp/my-project.api.md".
-   * It will then compare this file against the last reviewed file,
-   * e.g. "../api-review/my-project.api.md" (assuming that apiReviewFolder is "../api-review").
-   * If the files are different, the build will fail with an error message that instructs
-   * the developer to update the approved file, and then commit it to Git.  When they
-   * create a Pull Request, a branch policy will look for changes under "api-review/*"
-   * and require signoff from the appropriate reviewers.
+   * The filename for the API report files.  It will be combined with `trackedFolder` or `tempFolder` to produce
+   * a full output filename.
    *
-   * The default value is "./etc".
-   *
-   * Example: "config" (for a standalone project)
-   * Example: "../../common/api-review"  (for a Git repository with Rush)
+   * @remarks
+   * The file extension should be ".api.md", and the string should not contain a path separator such as `\` or `/`.
    */
-  apiReviewFolder?: string;
+  reportFileName?: string;
 
   /**
-   * The *.api.md report is saved into this folder.  During a production build
-   * (i.e. when IExtractorRuntimeOptions.productionBuild=true) the temporary file will
-   * be compared with the file in apiReviewFolder; if there are differences, and error
-   * will be reported.  During a non-production build, the temporary file will be
-   * automatically copied to the apiReviewFolder.
+   * Specifies the folder where the API report file is written.  The file name portion is determined by
+   * the "reportFileName" setting.
    *
-   * The default value is "./temp".
+   * @remarks
+   * The API report file is normally tracked by Git.  Changes to it can be used to trigger a branch policy,
+   * e.g. for an API review.
+   *
+   * The path is resolved relative to the `rootFolder` location.
+   */
+  reportFolder?: string;
+
+  /**
+   * Specifies the folder where the temporary report file is written.  The file name portion is determined by
+   * the "reportFileName" setting.
+   *
+   * @remarks
+   * After the temporary file is written to disk, it is compared with the file in the `reportFolder`.
+   * If they are different, a production build will fail.
+   *
+   * The path is resolved relative to the `rootFolder` location.
    */
   tempFolder?: string;
 }
 
 /**
- * Configures how the API JSON files (*.api.json) will be generated.
+ * Configures how the doc model file (*.api.json) will be generated.
+ *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
  *
  * @public
  */
-export interface IExtractorApiJsonFileConfig {
+export interface IExtractorDocModelConfig {
   /**
-   * Whether to generate API JSON files at all.  The default is true.
+   * Whether to generate doc model file.
    */
   enabled: boolean;
 
   /**
-   * Specifies where the *.api.json file should be written.
+   * The output path for the doc model file.
    *
-   * The default value is "./dist"
+   * @remarks
+   * The file extension should be ".api.json".
+   * The path is resolved relative to the `rootFolder` location.
    */
-  outputFolder?: string;
+  apiJsonFilePath?: string;
 }
 
 /**
- * Configures how the *.d.ts rollup files will be generated.
+ * Configures how the .d.ts rollup file will be generated.
  *
  * @remarks
- * API Extractor can generate a consolidated *.d.ts file that contains all
- * the exported typings for the package entry point.  It can also trim
- * \@alpha, \@beta, and \@internal definitions according to the release type.
+ * This is part of the {@link IExtractorConfig} structure.
  *
- * @beta
+ * @public
  */
 export interface IExtractorDtsRollupConfig {
   /**
-   * Whether to generate rollup *.d.ts files.  The default is false.
+   * Whether to generate the .d.ts rollup file.
    */
   enabled: boolean;
 
   /**
-   * If "trimming" is false (the default), then a single *.d.ts rollup file will be generated in the
-   * "publishFolder".  If "trimming" is true, then three separate *.d.ts rollups will be
-   * generated in "publishFolderForInternal", "publishFolderForBeta", and "publishFolderForPublic".
+   * Specifies the output path for a .d.ts rollup file to be generated without any trimming.
    *
    * @remarks
-   * In either case, "mainDtsRollupPath" indicates the relative file path.
+   * This file will include all declarations that are exported by the main entry point.
+   *
+   * If the path is an empty string, then this file will not be written.
+   *
+   * The path is resolved relative to the `rootFolder` location.
    */
-  trimming?: boolean;
+  untrimmedFilePath?: string;
 
   /**
-   * This setting is only used if "trimming" is false.
-   * It indicates the folder where "npm publish" will be run.  The default value is "./dist".
-   */
-  publishFolder?: string;
-
-  /**
-   * This setting is only used if "trimming" is true.
-   * It indicates the folder where "npm publish" will be run for an internal release.
-   * The default value is "./dist/internal".
+   * Specifies the output path for a .d.ts rollup file to be generated with trimming for a "beta" release.
    *
    * @remarks
-   * An internal release will contain all definitions that are reachable from the entry point.
+   * This file will include only declarations that are marked as `@public` or `@beta`.
+   *
+   * If the path is an empty string, then this file will not be written.
+   *
+   * The path is resolved relative to the `rootFolder` location.
    */
-  publishFolderForInternal?: string;
+  betaTrimmedFilePath?: string;
 
   /**
-   * This setting is only used if "trimming" is true.
-   * It indicates the folder where "npm publish" will be run for a beta release.
-   * The default value is "./dist/beta".
+   * Specifies the output path for a .d.ts rollup file to be generated with trimming for a "public" release.
    *
    * @remarks
-   * A beta release will contain all definitions that are reachable from the entry point,
-   * except definitions marked as \@alpha or \@internal.
-   */
-  publishFolderForBeta?: string;
-
-  /**
-   * This setting is only used if "trimming" is true.
-   * It indicates the folder where "npm publish" will be run for a public release.
-   * The default value is "./dist/public".
+   * This file will include only declarations that are marked as `@public`.
    *
-   * @remarks
-   * A public release will contain all definitions that are reachable from the entry point,
-   * except definitions marked as \@beta, \@alpha, or \@internal.
-   */
-  publishFolderForPublic?: string;
-
-  /**
-   * Specifies the relative path for the *.d.ts rollup file to be generated for the
-   * package's main entry point.  The default value is an empty string, which causes
-   * the path to be automatically inferred from the "typings" field of the project's
-   * package.json file.
+   * If the path is an empty string, then this file will not be written.
    *
-   * @remarks
-   * If specified, the value must be a relative path that can be combined with one of
-   * the publish folder settings.
+   * The path is resolved relative to the `rootFolder` location.
    */
-  mainDtsRollupPath?: string;
+  publicTrimmedFilePath?: string;
 }
 
 /**
- * Configures how the tsdoc metadata file will be generated.
+ * Configures how the tsdoc-metadata.json file will be generated.
  *
- * @beta
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
+ *
+ * @public
  */
 export interface IExtractorTsdocMetadataConfig {
   /**
-   * Whether to generate the TSDoc metadata file. The default is false.
+   * Whether to generate the tsdoc-metadata.json file.
    */
   enabled: boolean;
 
   /**
-   * Specifies where the TSDoc metadata file should be written. The default value is
-   * an empty string, which causes the path to be automatically inferred from the
-   * "tsdocMetadata", "typings" or "main" fields of the project's package.json.
-   * If none of these fields are set, it defaults to "tsdoc-metadata.json".
+   * Specifies where the TSDoc metadata file should be written.
+   *
+   * @remarks
+   * The default value is `<lookup>`, which causes the path to be automatically inferred from the `tsdocMetadata`,
+   * `typings` or `main` fields of the project's package.json.  If none of these fields are set, the lookup
+   * falls back to `./tsdoc-metadata.json`.
    */
-  tsdocMetadataPath?: string;
+  tsdocMetadataFilePath?: string;
 }
 
 /**
  * Used with {@link IExtractorMessageReportingRuleConfig.logLevel}.
+ *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
  *
  * @public
  */
@@ -299,6 +233,9 @@ export const enum ExtractorMessageLogLevel {
 /**
  * Configures reporting for a given message identifier.
  *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
+ *
  * @public
  */
 export interface IExtractorMessageReportingRuleConfig {
@@ -322,6 +259,9 @@ export interface IExtractorMessageReportingRuleConfig {
  * Specifies a table of reporting rules for different message identifiers, and also the default rule used for
  * identifiers that do not appear in the table.
  *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
+ *
  * @public
  */
 export interface IExtractorMessageReportingTableConfig {
@@ -335,6 +275,9 @@ export interface IExtractorMessageReportingTableConfig {
 
 /**
  * Configures how API Extractor reports error and warning messages produced during analysis.
+ *
+ * @remarks
+ * This is part of the {@link IExtractorConfig} structure.
  *
  * @public
  */
@@ -370,36 +313,30 @@ export interface IExtractorConfig {
   extends?: string;
 
   /**
-   * Determines how the TypeScript compiler will be invoked.
-   * The compiler.configType selects the type of configuration;
-   * Different options are available according to the configuration type.
+   * Specifies the .d.ts file to be used as the starting point for analysis.  API Extractor
+   * analyzes the symbols exported by this module.
+   *
+   * @remarks
+   *
+   * The file extension must be ".d.ts" and not ".ts".
+   * The path is resolved relative to the "rootFolder" location.
    */
-  compiler: IExtractorTsconfigCompilerConfig | IExtractorRuntimeCompilerConfig;
+  mainEntryPointFile: string;
 
   /**
-   * {@inheritDoc IExtractorPoliciesConfig}
+   * {@inheritDoc IExtractorCompilerConfig}
    */
-  policies?: IExtractorPoliciesConfig;
+  compiler?: IExtractorCompilerConfig;
 
   /**
-   * {@inheritDoc IExtractorValidationRulesConfig}
+   * {@inheritDoc IExtractorApiReportConfig}
    */
-  validationRules?: IExtractorValidationRulesConfig;
+  apiReport?: IExtractorApiReportConfig;
 
   /**
-   * {@inheritDoc IExtractorProjectConfig}
+   * {@inheritDoc IExtractorDocModelConfig}
    */
-  project: IExtractorProjectConfig;
-
-  /**
-   * {@inheritDoc IExtractorApiReviewFileConfig}
-   */
-  apiReviewFile?: IExtractorApiReviewFileConfig;
-
-  /**
-   * {@inheritDoc IExtractorApiJsonFileConfig}
-   */
-  apiJsonFile?: IExtractorApiJsonFileConfig;
+  docModel?: IExtractorDocModelConfig;
 
   /**
    * {@inheritDoc IExtractorDtsRollupConfig}
@@ -417,18 +354,6 @@ export interface IExtractorConfig {
    * {@inheritDoc IExtractorMessagesConfig}
    */
   messages?: IExtractorMessagesConfig;
-
-  /**
-   * This option causes the typechecker to be invoked with the --skipLibCheck option. This option is not
-   * recommended and may cause API Extractor to produce incomplete or incorrect declarations, but it
-   * may be required when dependencies contain declarations that are incompatible with the TypeScript engine
-   * that API Extractor uses for its analysis. If this option is used, it is strongly recommended that broken
-   * dependencies be fixed or upgraded.
-   *
-   * @remarks
-   * This option only applies when compiler.config.configType is set to "tsconfig"
-   */
-  skipLibCheck?: boolean;
 
   /**
    * Set to true when invoking API Extractor's test harness.
