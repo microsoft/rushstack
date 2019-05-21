0c071f4ef6bc122dbdaf3972c10541235563b1b6
Kevin Grandon <kevingrandon@yahoo.com>
2019-05-21 15:34:25 -0700

    Maybe this is a better test?

--- apps/rush-lib/src/logic/npm/test/NpmLinkManager.test.ts
+++ apps/rush-lib/src/logic/npm/test/NpmLinkManager.test.ts
@@ -3,18 +3,46 @@
 
 import * as path from 'path';
 
+import { InstallManager, IInstallManagerOptions } from '../../InstallManager';
+import { PurgeManager } from '../../PurgeManager';
 import { RushConfiguration } from '../../../api/RushConfiguration';
-import { NpmLinkManager } from '../NpmLinkManager';
+import { RushGlobalFolder } from '../../../api/RushGlobalFolder';
 
 describe('_linkProjects', () => {
   const rushFilename: string = path.resolve(__dirname, 'repo', 'rush.json');
-  const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(rushFilename);
+  const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(
+    rushFilename
+  );
 
-  const testNpmLinkManager: NpmLinkManager = new NpmLinkManager(rushConfiguration);
+  const rushGlobalFolder: RushGlobalFolder = new RushGlobalFolder();
+
+  const purgeManager: PurgeManager = new PurgeManager(
+    rushConfiguration,
+    rushGlobalFolder
+  );
+
+  const installManagerOptions: IInstallManagerOptions = {
+    debug: true,
+    allowShrinkwrapUpdates: true,
+    bypassPolicy: false,
+    noLink: false,
+    fullUpgrade: false,
+    recheckShrinkwrap: false,
+    networkConcurrency: undefined,
+    collectLogFile: false
+  };
+
+  const testInstallManager: InstallManager = new InstallManager(
+    rushConfiguration,
+    rushGlobalFolder,
+    purgeManager,
+    installManagerOptions
+  );
 
   it('links projects', () => {
-    return testNpmLinkManager.createSymlinksForProjects(false).then(() => {
+    return testInstallManager.doInstall().then(() => {
       // HALP: Not sure how to get this into an error state first to test the new logic
+      console.log('Installation done?');
     });
-  });
+  }, 60000);
 });
