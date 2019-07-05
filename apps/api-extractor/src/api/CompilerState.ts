045c0fb362a15be6a5014a386ef1f3fd1149eb41
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-07-04 21:24:31 -0700

    Fix an issue where API Extractor would accidentally analyze a .ts file if found in the same folder as the corresponding .d.ts file (GitHub #1310)

--- apps/api-extractor/src/api/CompilerState.ts
+++ apps/api-extractor/src/api/CompilerState.ts
@@ -132,14 +132,71 @@ export class CompilerState {
   private static _createCompilerHost(commandLine: ts.ParsedCommandLine,
     options: IExtractorInvokeOptions | undefined):  ts.CompilerHost {
 
-    // Create a default CompilerHost that we can override
+    // Create a default CompilerHost that we will override
     const compilerHost: ts.CompilerHost = ts.createCompilerHost(commandLine.options);
+
+    // Save a copy of the original members.  Note that "compilerHost" cannot be the copy, because
+    // createCompilerHost() captures that instance in a closure that is used by the members.
+    const defaultCompilerHost: ts.CompilerHost = { ...compilerHost };
 
     if (options && options.typescriptCompilerFolder) {
       // Prevent a closure parameter
       const typescriptCompilerLibFolder: string = path.join(options.typescriptCompilerFolder, 'lib');
       compilerHost.getDefaultLibLocation = () => typescriptCompilerLibFolder;
     }
+
+    // Used by compilerHost.fileExists()
+    // .d.ts file path --> whether the file exists
+    const dtsExistsCache: Map<string, boolean> = new Map<string, boolean>();
+
+    // Used by compilerHost.fileExists()
+    // Example: "c:/folder/file.part.ts"
+    const fileExtensionRegExp: RegExp = /^(.+)(\.[a-z0-9_]+)$/i;
+
+    compilerHost.fileExists = (fileName: string): boolean => {
+      // In certain deprecated setups, the compiler may write its output files (.js and .d.ts)
+      // in the same folder as the corresponding input file (.ts or .tsx).  When following imports,
+      // API Extractor wants to analyze the .d.ts file; however recent versions of the compiler engine
+      // will instead choose the .ts file.  To work around this, we hook fileExists() to hide the
+      // existence of those files.
+
+      // Is "fileName" a .d.ts file?  The double extension ".d.ts" needs to be matched specially.
+      if (!ExtractorConfig.hasDtsFileExtension(fileName)) {
+        // It's not a .d.ts file.  Is the file extension a potential source file?
+        const match: RegExpExecArray | null = fileExtensionRegExp.exec(fileName);
+        if (match) {
+          // Example: "c:/folder/file.part"
+          const pathWithoutExtension: string = match[1];
+          // Example: ".ts"
+          const fileExtension: string = match[2];
+
+          switch (fileExtension.toLocaleLowerCase()) {
+            case '.ts':
+            case '.tsx':
+            case '.js':
+            case '.jsx':
+              // Yes, this is a possible source file.  Is there a corresponding .d.ts file in the same folder?
+              const dtsFileName: string = `${pathWithoutExtension}.d.ts`;
+
+              let dtsFileExists: boolean | undefined = dtsExistsCache.get(dtsFileName);
+              if (dtsFileExists === undefined) {
+                dtsFileExists = defaultCompilerHost.fileExists!(dtsFileName);
+                dtsExistsCache.set(dtsFileName, dtsFileExists);
+              }
+
+              if (dtsFileExists) {
+                // fileName is a potential source file and a corresponding .d.ts file exists.
+                // Thus, API Extractor should ignore this file (so the .d.ts file will get analyzed instead).
+                return false;
+              }
+              break;
+          }
+        }
+      }
+
+      // Fall through to the default implementation
+      return defaultCompilerHost.fileExists!(fileName);
+    };
 
     return compilerHost;
   }
