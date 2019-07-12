7c37074303f4e7ccc32337140a0ba9b42d9e7f99
Mike Harder <mharder@microsoft.com>
2019-07-12 15:49:52 -0700

    Remove version warnings for Node 12
    - Do warn for Node 6 which is End-of-Life

--- apps/rush/src/start.ts
+++ apps/rush/src/start.ts
@@ -19,18 +19,18 @@ if (semver.satisfies(nodeVersion, '<= 6.4.0')) {
 
 // We are on a much newer release than we have tested and support
 // tslint:disable-next-line
-else if (semver.satisfies(nodeVersion, '>=11.0.0')) {
+else if (semver.satisfies(nodeVersion, '>=13.0.0')) {
   console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) has not been tested with this release of Rush.`
     + ` The Rush team will not accept issue reports for it.`
     + ` Please consider upgrading Rush or downgrading Node.js.`));
 }
 
-// We are not on an LTS release
+// We are not on an LTS or Current release
 // tslint:disable-next-line
-else if (!semver.satisfies(nodeVersion, '^6.9.0')
-      && !semver.satisfies(nodeVersion, '^8.9.0')
-      && !semver.satisfies(nodeVersion, '^10.13.0')) {
-  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) is not a Long-Term Support (LTS) release.`
+else if (!semver.satisfies(nodeVersion, '^8.9.0')
+      && !semver.satisfies(nodeVersion, '^10.13.0')
+      && !semver.satisfies(nodeVersion, '^12.0.0')) {
+  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) is not a Long-Term Support (LTS) or Current release.`
     + ` These versions frequently contain bugs, and the Rush team will not accept issue reports for them.`
     + ` Please consider installing a stable release.`));
 }
