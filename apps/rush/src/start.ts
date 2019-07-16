12496ab7455a430817e1c02bc58e7757de98fcff
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2019-07-15 20:27:21 -0700

    Update version warning messages.

--- apps/rush/src/start.ts
+++ apps/rush/src/start.ts
@@ -8,31 +8,38 @@ import * as semver from 'semver';
 
 const nodeVersion: string = process.versions.node;
 
-// tslint:disable-next-line
+interface IExtendedNodeProcess extends NodeJS.Process {
+  release: {
+    lts?: string;
+  };
+}
 
 // We are on an ancient version of NodeJS that is known not to work with Rush
-if (semver.satisfies(nodeVersion, '<= 6.4.0')) {
-  console.error(colors.red(`Your version of Node.js (${nodeVersion}) is very old and incompatible with Rush.`
-    + ` Please upgrade to the latest Long-Term Support (LTS) version.`));
+if (semver.satisfies(nodeVersion, '< 8.9.0')) {
+  console.error(colors.red(
+    `Your version of Node.js (${nodeVersion}) is very old and incompatible with Rush. ` +
+    `Please upgrade to the latest Long-Term Support (LTS) version.`
+  ));
   process.exit(1);
 }
 
 // We are on a much newer release than we have tested and support
 // tslint:disable-next-line
-else if (semver.satisfies(nodeVersion, '>=11.0.0')) {
-  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) has not been tested with this release of Rush.`
-    + ` The Rush team will not accept issue reports for it.`
-    + ` Please consider upgrading Rush or downgrading Node.js.`));
+else if (semver.satisfies(nodeVersion, '>= 11.0.0')) {
+  console.warn(colors.yellow(
+    `Your version of Node.js (${nodeVersion}) has not been tested with this release ` +
+    `of Rush. Please consider installing a newer version of the "@microsoft/rush" ` +
+    `package, or else downgrading Node.js.`
+  ));
 }
 
 // We are not on an LTS release
 // tslint:disable-next-line
-else if (!semver.satisfies(nodeVersion, '^6.9.0')
-      && !semver.satisfies(nodeVersion, '^8.9.0')
-      && !semver.satisfies(nodeVersion, '^10.13.0')) {
-  console.warn(colors.yellow(`Your version of Node.js (${nodeVersion}) is not a Long-Term Support (LTS) release.`
-    + ` These versions frequently contain bugs, and the Rush team will not accept issue reports for them.`
-    + ` Please consider installing a stable release.`));
+else if ((process as IExtendedNodeProcess).release.lts) {
+  console.warn(colors.yellow(
+    `Your version of Node.js (${nodeVersion}) is not a Long-Term Support (LTS) release. ` +
+    `These versions frequently have bugs. Please consider installing a stable release.`
+  ));
 }
 
 import {
