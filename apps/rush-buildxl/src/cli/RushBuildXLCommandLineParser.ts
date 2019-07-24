36e8f0cbbe4fc0508f73b2bbfb91f62f563fc494
Nayan Shah <nayanshah@users.noreply.github.com>
2019-07-23 23:43:31 -0700

    Add command to cleanup generated BuildXL modules

--- apps/rush-buildxl/src/cli/RushBuildXLCommandLineParser.ts
+++ apps/rush-buildxl/src/cli/RushBuildXLCommandLineParser.ts
@@ -3,6 +3,7 @@
 
 import { CommandLineParser } from '@microsoft/ts-command-line';
 import { Terminal } from '@microsoft/node-core-library';
+import { CleanAction } from './actions/CleanAction';
 import { GenerateAction } from './actions/GenerateAction';
 
 export class RushBuildXLCommandLineParser extends CommandLineParser {
@@ -16,6 +17,7 @@ export class RushBuildXLCommandLineParser extends CommandLineParser {
 
     this._terminal = terminal;
 
+    this.addAction(new CleanAction(this._terminal));
     this.addAction(new GenerateAction(this._terminal));
   }
 
---
+++ apps/rush-buildxl/src/cli/actions/CleanAction.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import {
+  CommandLineAction
+} from '@microsoft/ts-command-line';
+
+import { Terminal, FileSystem } from '@microsoft/node-core-library';
+import { RushConfiguration } from '@microsoft/rush-lib';
+
+export class CleanAction extends CommandLineAction {
+  private _terminal: Terminal;
+
+  constructor(terminal: Terminal) {
+    super({
+      actionName: 'clean',
+      summary: 'Cleans up generated BuildXL configuration for the current Rush repository.',
+      documentation: 'Cleans up generated BuildXL configuration for the current Rush repository.'
+    });
+
+    this._terminal = terminal;
+  }
+
+  public onDefineParameters(): void {/***/}
+
+  protected async onExecute(): Promise<void> {
+    const rushConfig: RushConfiguration = RushConfiguration.loadFromDefaultLocation();
+
+    FileSystem.deleteFolder(`${rushConfig.commonTempFolder}/bxl`);
+
+    this._terminal.writeLine(`Successfully cleaned BuildXL configuration.`);
+  }
+}
