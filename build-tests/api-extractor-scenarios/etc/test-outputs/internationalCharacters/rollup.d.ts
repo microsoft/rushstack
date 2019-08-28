baff9d9bd33fc25739f65c3f593599b2652a4b97
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-08-28 16:33:31 -0700

    Add a unit test for international characters in API names

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/internationalCharacters/rollup.d.ts
@@ -0,0 +1,11 @@
+
+/** @public */
+declare class "ClassΞ" {
+    memberΔ(paramΩ: string): "ClassΞ";
+    'invalid chars'<T = "ClassΞ">(): void;
+    'validChars'(): void;
+}
+export { "ClassΞ" as ClassΞ }
+export { "ClassΞ" as ClassΣ }
+
+export { }
