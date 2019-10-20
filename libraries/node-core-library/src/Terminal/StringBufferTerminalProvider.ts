d99f77ca30634168991da0a9cddebd7f8a31c5e4
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-10-20 10:48:27 -0700

    Fix a regression in StringBufferTerminalProvider.ts

--- libraries/node-core-library/src/Terminal/StringBufferTerminalProvider.ts
+++ libraries/node-core-library/src/Terminal/StringBufferTerminalProvider.ts
@@ -94,10 +94,10 @@ export class StringBufferTerminalProvider implements ITerminalProvider {
     return this._normalizeOutput(this._warningBuffer.toString());
   }
 
-  private _normalizeOutput(s: string): string { // tslint:disable-line:export-name
+  private _normalizeOutput(s: string): string {
     return Text.convertToLf(s)
-    .replace(/\u801b/g, '[x]')
-    .replace(/\n/g, '[-n-]')
-    .replace(/\r/g, '[-r-]');
+      .replace(/\u001b/g, '[x]') // eslint-disable-line no-control-regex
+      .replace(/\n/g, '[-n-]')
+      .replace(/\r/g, '[-r-]');
   }
 }
