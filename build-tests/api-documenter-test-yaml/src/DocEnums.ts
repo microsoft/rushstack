d5e216b3840b8a4276667043f768b2f7032af859
paperbd <paperbd@users.noreply.github.com>
2019-11-22 14:19:28 -0500

    Splitting up api-documenter-test into two packages, for yaml and markdown.

---
+++ build-tests/api-documenter-test-yaml/src/DocEnums.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/**
+ * Docs for DocEnum
+ * @public
+ * {@docCategory SystemEvent}
+ */
+export enum DocEnum {
+  /**
+   * These are some docs for Zero
+   */
+  Zero,
+
+  /**
+   * These are some docs for One
+   */
+  One = 1,
+
+  /**
+   * These are some docs for Two
+   */
+  Two = DocEnum.One + 1
+}
