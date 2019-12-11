1ea26630f53e8dc51f4393dccfd0a8a31212e191
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2019-12-10 14:22:21 -1000

    LastInstallFlag: throw errors when certain error conditions are met

--- apps/rush-lib/src/api/LastInstallFlag.ts
+++ apps/rush-lib/src/api/LastInstallFlag.ts
@@ -4,6 +4,43 @@ import { FileSystem, JsonFile, JsonObject } from '@microsoft/node-core-library';
 
 export const LAST_INSTALL_FLAG_FILE_NAME: string = 'last-install.flag';
 
+interface IPropertyErrorDeps {
+      /**
+       * Original content property must match this value in order to check for
+       * inequality
+       */
+      [prop: string]: string;
+}
+interface IPropertyErrors {
+  /**
+   * The properties to check and force an abort on inequality
+   */
+  [key: string]: {
+    /**
+     * Properties to check in the original content before checking for inequality.
+     * All deps must match.  If no deps are defined, then inequality will be checked
+     * unconditionally
+     */
+    deps?: IPropertyErrorDeps;
+    /**
+     * In case an inequality is found, and all deps match, then throw this error
+     */
+    error: string;
+  }
+}
+
+const PROPERTY_ERRORS: IPropertyErrors = {
+  storePath: {
+    deps: {
+      packageManager: "pnpm"
+    },
+    error: "Current PNPM store path does not match the last one used.  This may cause inconsistency in your builds.\n\n" +
+      "If you wish to install with the currently configured path, please run \"rush update --purge\"\n\n" +
+      "Old: <%= oldValue %>\n" +
+      "New: <%= newValue %>"
+  }
+}
+
 /**
  * A helper class for managing last-install flags, which are persistent and
  * indicate that something installed in the folder was sucessfully completed.
@@ -28,16 +65,33 @@ export class LastInstallFlag {
   /**
    * Returns true if the file exists and the contents match the current state
    */
-  public isValid(): boolean {
+  public isValid(abortOnInvalid: boolean = false): boolean {
     if (!FileSystem.exists(this._path)) {
       return false;
     }
+    let contents: JsonObject;
     try {
-      const contents: JsonObject = JsonFile.load(this._path);
-      return _.isEqual(contents, this._state);
-    } catch (error) {
+      contents = JsonFile.load(this._path);
+    } catch (err) {
+      return false;
+    } 
+    if (!_.isEqual(contents, this._state)) {
+      if (abortOnInvalid) {
+        for (const errorKey of Object.keys(PROPERTY_ERRORS)) {
+          const errorDeps: IPropertyErrorDeps = PROPERTY_ERRORS[errorKey].deps || {};
+          const depKeys: string[] = Object.keys(errorDeps);
+          const depsMatch: boolean = depKeys.length === 0 || depKeys.every((depProp) => errorDeps[depProp] === contents[depProp])
+          if (!_.isEqual(contents[errorKey], this._state[errorKey]) && depsMatch) {
+            throw _.template(PROPERTY_ERRORS[errorKey].error)({
+              oldValue: contents[errorKey],
+              newValue: this._state[errorKey]
+            });
+          }
+        }
+      }
       return false;
     }
+    return true;
   }
 
   /**
