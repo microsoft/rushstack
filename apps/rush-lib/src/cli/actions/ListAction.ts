87443cc4276e278c47fabdd05206e0d1ffb185be
Claudiazhaoya <zhas@microsoft.com>
2020-01-02 15:43:29 -0600

    add --json to rush list

--- apps/rush-lib/src/cli/actions/ListAction.ts
+++ apps/rush-lib/src/cli/actions/ListAction.ts
@@ -1,57 +1,77 @@
-import { BaseRushAction } from './BaseRushAction';
-import { RushCommandLineParser } from '../RushCommandLineParser';
-import { CommandLineFlagParameter } from '@microsoft/ts-command-line';
-import { RushConfigurationProject } from '../../api/RushConfigurationProject';
-import * as Table from 'cli-table';
+import { BaseRushAction } from "./BaseRushAction";
+import { RushCommandLineParser } from "../RushCommandLineParser";
+import { CommandLineFlagParameter } from "@microsoft/ts-command-line";
+import { RushConfigurationProject } from "../../api/RushConfigurationProject";
+import * as Table from "cli-table";
 
 export class ListAction extends BaseRushAction {
   private _version: CommandLineFlagParameter;
   private _path: CommandLineFlagParameter;
   private _fullPath: CommandLineFlagParameter;
+  private _jsonFlag: CommandLineFlagParameter;
 
   public constructor(parser: RushCommandLineParser) {
     super({
-      actionName: 'list',
-      summary: 'List package information for all projects in the repo',
+      actionName: "list",
+      summary: "List package information for all projects in the repo",
       documentation:
-        'List package names, and optionally version (--version) and ' +
-        'path (--path) or full path (--full-path), for projects in the ' +
-        'current rush config.',
+        "List package names, and optionally version (--version) and " +
+        "path (--path) or full path (--full-path), for projects in the " +
+        "current rush config.",
       parser
     });
   }
 
   protected onDefineParameters(): void {
     this._version = this.defineFlagParameter({
-      parameterLongName: '--version',
-      parameterShortName: '-v',
-      description: 'If this flag is specified, the project version will be ' +
-        'displayed in a column along with the package name.'
+      parameterLongName: "--version",
+      parameterShortName: "-v",
+      description:
+        "If this flag is specified, the project version will be " +
+        "displayed in a column along with the package name."
     });
 
     this._path = this.defineFlagParameter({
-      parameterLongName: '--path',
-      parameterShortName: '-p',
-      description: 'If this flag is specified, the project path will be ' +
-        'displayed in a column along with the package name.'
+      parameterLongName: "--path",
+      parameterShortName: "-p",
+      description:
+        "If this flag is specified, the project path will be " +
+        "displayed in a column along with the package name."
     });
 
     this._fullPath = this.defineFlagParameter({
-      parameterLongName: '--full-path',
-      parameterShortName: '-f',
-      description: 'If this flag is specified, the project full path will ' +
-        'be displayed in a column along with the package name.'
+      parameterLongName: "--full-path",
+      parameterShortName: "-f",
+      description:
+        "If this flag is specified, the project full path will " +
+        "be displayed in a column along with the package name."
+    });
+
+    this._jsonFlag = this.defineFlagParameter({
+      parameterLongName: "--json",
+      description: "If specified,  a json file of result will be generated."
     });
   }
 
   protected run(): Promise<void> {
     return Promise.resolve().then(() => {
-      const allPackages: Map<string, RushConfigurationProject> = this.rushConfiguration.projectsByName;
+      const allPackages: Map<string, RushConfigurationProject> = this
+        .rushConfiguration.projectsByName;
       if (this._version.value || this._path.value || this._fullPath.value) {
         this._printListTable(allPackages);
+      } else if (this._jsonFlag.value) {
+        this._serializeToJson(allPackages);
       } else {
         this._printList(allPackages);
       }
+    });
+  }
+
+  private _serializeToJson(
+    allPackages: Map<string, RushConfigurationProject>
+  ): void {
+    allPackages.forEach((_config: RushConfigurationProject, name: string) => {
+      console.log(JSON.stringify(name));
     });
   }
 
@@ -61,16 +81,18 @@ export class ListAction extends BaseRushAction {
     });
   }
 
-  private _printListTable(allPackages: Map<string, RushConfigurationProject>): void {
-    const tableHeader: string[] = ['Project'];
+  private _printListTable(
+    allPackages: Map<string, RushConfigurationProject>
+  ): void {
+    const tableHeader: string[] = ["Project"];
     if (this._version.value) {
-      tableHeader.push('Version');
+      tableHeader.push("Version");
     }
     if (this._path.value) {
-      tableHeader.push('Path');
+      tableHeader.push("Path");
     }
     if (this._fullPath.value) {
-      tableHeader.push('Full Path');
+      tableHeader.push("Full Path");
     }
     const table: Table = new Table({
       head: tableHeader
