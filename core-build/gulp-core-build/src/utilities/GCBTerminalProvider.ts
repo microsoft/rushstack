e253464f1b8e43988699c1454d59cab046ed5e09
Joshua Wedekind <halfnibble@users.noreply.github.com>
2020-01-17 17:24:19 -0800

    Move local import to separate line.

--- core-build/gulp-core-build/src/utilities/GCBTerminalProvider.ts
+++ core-build/gulp-core-build/src/utilities/GCBTerminalProvider.ts
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { GulpTask } from '../tasks/GulpTask';
 import {
   TerminalProviderSeverity,
   ConsoleTerminalProvider
 } from '@microsoft/node-core-library';
+
+import { GulpTask } from '../tasks/GulpTask';
 
 /**
  * @public
