fa94d394b19f6f1d6f56005f5aa676ac9af7b3cb
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2020-02-02 21:18:45 -0800

    Move file extension regexes to Constants.

--- webpack/localization-plugin/src/WebpackConfigurationUpdater.ts
+++ webpack/localization-plugin/src/WebpackConfigurationUpdater.ts
@@ -76,11 +76,11 @@ export class WebpackConfigurationUpdater {
       options.configuration,
       [
         {
-          test: /\.loc\.json$/i,
+          test: Constants.LOC_JSON_REGEX,
           loader: loader
         },
         {
-          test: /\.resx$/i,
+          test: Constants.RESX_REGEX,
           use: [
             require.resolve('json-loader'),
             {
@@ -134,7 +134,7 @@ export class WebpackConfigurationUpdater {
           test: {
             and: [
               (filePath: string) => options.locFiles.has(filePath),
-              /\.loc\.json$/i
+              Constants.LOC_JSON_REGEX
             ]
           },
           loader: loader,
@@ -144,7 +144,7 @@ export class WebpackConfigurationUpdater {
           test: {
             and: [
               (filePath: string) => options.locFiles.has(filePath),
-              /\.resx$/i
+              Constants.RESX_REGEX
             ]
           },
           use: [
@@ -172,12 +172,7 @@ export class WebpackConfigurationUpdater {
             and: [
               (filePath: string) => !options.locFiles.has(filePath),
               (filePath: string) => !options.filesToIgnore.has(filePath),
-              {
-                or: [
-                  /\.loc\.json$/i,
-                  /\.resx$/i
-                ]
-              }
+              Constants.RESX_OR_LOC_JSON_REGEX
             ]
           },
           loader: path.resolve(__dirname, 'loaders', 'MissingLocDataWarningLoader.js')
