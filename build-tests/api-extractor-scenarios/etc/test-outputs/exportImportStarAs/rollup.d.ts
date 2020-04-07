c88df794ca275d59ab5860ff622c377a1f92dc94
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-04-07 14:00:20 -0700

    Convert "api-extractor-test-no-report" to be a scenario test

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/exportImportStarAs/rollup.d.ts
@@ -0,0 +1,26 @@
+
+/**
+ * Returns the sum of adding b to a
+ * @param a - first number
+ * @param b - second number
+ * @returns Sum of adding b to a
+ */
+declare const add: (a: number, b: number) => number;
+
+declare namespace calculator {
+  export {
+    add,
+    subtract,
+  }
+}
+export { calculator }
+
+/**
+ * Returns the sum of subtracting b from a
+ * @param a - first number
+ * @param b - second number
+ * @returns Sum of subtract b from a
+ */
+declare const subtract: (a: number, b: number) => number;
+
+export { }
