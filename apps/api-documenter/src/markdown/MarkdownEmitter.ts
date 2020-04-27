7fa8d3f2251e3b4dd0f5a9f3d17fa952525e1dc1
Feiyang1 <plane1113@gmail.com>
2020-04-27 13:48:38 -0700

    debug settup

--- apps/api-documenter/src/markdown/MarkdownEmitter.ts
+++ apps/api-documenter/src/markdown/MarkdownEmitter.ts
@@ -114,9 +114,12 @@ export class MarkdownEmitter {
           writer.write('`');
         }
         if (context.insideTable) {
-          const code: string = this.getTableEscapedText(docCodeSpan.code);
+          const code: string = this.getTableEscapedText(docCodeSpan.code) + 'fei';
           const parts: string[] = code.split(/\r?\n/g);
           writer.write(parts.join('</code><br/><code>'));
+          if (docCodeSpan.code === 'FirebaseApp') {
+            console.log(JSON.stringify(docNode));
+          }
         } else {
           writer.write(docCodeSpan.code);
         }
