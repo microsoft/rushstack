7811bb3c762571c70c8a35752aed00632d296444
Javier <javier.garcia@meteologica.com>
2020-05-25 16:37:49 +0200

    Support for import() type

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/dynamicImportType/rollup.d.ts
@@ -0,0 +1,22 @@
+import * as Lib1 from 'api-extractor-lib1-test';
+import { Lib1Class } from 'api-extractor-lib3-test';
+import { Lib1Interface } from 'api-extractor-lib1-test';
+import { Lib2Class } from 'api-extractor-lib2-test';
+import { Lib2Interface } from 'api-extractor-lib2-test';
+
+export declare class Item {
+    options: Options;
+    lib1: Lib1Interface;
+    lib2: Lib2Interface;
+    lib3: Lib1Class;
+    reExport: Lib2Class;
+}
+export { Lib1 }
+export { Lib2Interface }
+
+declare interface Options {
+    name: string;
+    color: 'red' | 'blue';
+}
+
+export { }
