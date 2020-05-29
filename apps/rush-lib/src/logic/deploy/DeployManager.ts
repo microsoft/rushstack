77cd0630ef96753042f0027141e2ac7bb754cac4
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-05-28 19:23:56 -0700

    Add deploySymlink() from prototype

--- apps/rush-lib/src/logic/deploy/DeployManager.ts
+++ apps/rush-lib/src/logic/deploy/DeployManager.ts
@@ -14,7 +14,7 @@ import {
   IPackageJson,
 } from "@rushstack/node-core-library";
 import { RushConfiguration } from '../../api/RushConfiguration';
-import { SymlinkAnalyzer } from './SymlinkAnalyzer';
+import { SymlinkAnalyzer, ILinkInfo } from './SymlinkAnalyzer';
 import { RushConfigurationProject } from "../../api/RushConfigurationProject";
 
 interface IDeployScenarioProjectJson {
@@ -29,6 +29,9 @@ interface IDeployScenarioJson {
 
 export class DeployManager {
   private readonly _rushConfiguration: RushConfiguration;
+
+  private _targetRootFolder: string;
+  private _sourceRootFolder: string;
 
   private readonly _packageJsonLookup: PackageJsonLookup;
   private readonly _symlinkAnalyzer: SymlinkAnalyzer;
@@ -135,22 +138,19 @@ export class DeployManager {
     this._collectFoldersRecursive(path.join(project.projectFolder, 'package.json'));
   }
 
-  private _remapPathForDeployFolder(absolutePathInSourceFolder: string,
-    sourceRootFolder: string, targetRootFolder: string): string {
+  private _remapPathForDeployFolder(absolutePathInSourceFolder: string): string {
 
-    if (!Path.isUnderOrEqual(absolutePathInSourceFolder, sourceRootFolder)) {
-      throw new Error("Source path is not under " + sourceRootFolder + "\n" + absolutePathInSourceFolder);
+    if (!Path.isUnderOrEqual(absolutePathInSourceFolder, this._sourceRootFolder)) {
+      throw new Error("Source path is not under " + this._sourceRootFolder + "\n" + absolutePathInSourceFolder);
     }
-    const relativePath: string = path.relative(sourceRootFolder, absolutePathInSourceFolder);
-    const absolutePathInTargetFolder: string = path.join(targetRootFolder, relativePath);
+    const relativePath: string = path.relative(this._sourceRootFolder, absolutePathInSourceFolder);
+    const absolutePathInTargetFolder: string = path.join(this._targetRootFolder, relativePath);
     return absolutePathInTargetFolder;
   }
 
-  private _deployFolder(sourceFolderPath: string,
-    sourceRootFolder: string, targetRootFolder: string): void {
+  private _deployFolder(sourceFolderPath: string): void {
 
-    const targetFolderPath: string = this._remapPathForDeployFolder(sourceFolderPath,
-      sourceRootFolder, targetRootFolder);
+    const targetFolderPath: string = this._remapPathForDeployFolder(sourceFolderPath);
 
     // When copying a package folder, we always ignore the node_modules folder; it will be added indirectly
     // only if needed
@@ -175,8 +175,76 @@ export class DeployManager {
     });
   }
 
+  private _deploySymlink(originalLinkInfo: ILinkInfo): boolean {
+    const linkInfo: ILinkInfo = {
+      kind: originalLinkInfo.kind,
+      linkPath: this._remapPathForDeployFolder(originalLinkInfo.linkPath),
+      targetPath: this._remapPathForDeployFolder(originalLinkInfo.targetPath),
+    };
+
+    // Has the link target been created yet?  If not, we should try again later
+    if (!FileSystem.exists(linkInfo.targetPath)) {
+      return false;
+    }
+
+    const newLinkFolder: string = path.dirname(linkInfo.linkPath);
+    FileSystem.ensureFolder(newLinkFolder);
+
+    // Link to the relative path for symlinks
+    const relativeTargetPath: string = path.relative(FileSystem.getRealPath(newLinkFolder), linkInfo.targetPath);
+
+    // NOTE: This logic is based on NpmLinkManager._createSymlink()
+    if (process.platform === "win32") {
+      if (linkInfo.kind === "folderLink") {
+        // For directories, we use a Windows "junction".  On Unix, this produces a regular symlink.
+        FileSystem.createSymbolicLinkJunction({
+          linkTargetPath: relativeTargetPath,
+          newLinkPath: linkInfo.linkPath,
+        });
+      } else {
+        // For files, we use a Windows "hard link", because creating a symbolic link requires
+        // administrator permission.
+
+        // NOTE: We cannot use the relative path for hard links
+        FileSystem.createHardLink({
+          linkTargetPath: relativeTargetPath,
+          newLinkPath: linkInfo.linkPath,
+        });
+      }
+    } else {
+      // However hard links seem to cause build failures on Mac, so for all other operating systems
+      // we use symbolic links for this case.
+      if (linkInfo.kind === "folderLink") {
+        FileSystem.createSymbolicLinkFolder({
+          linkTargetPath: relativeTargetPath,
+          newLinkPath: linkInfo.linkPath,
+        });
+      } else {
+        FileSystem.createSymbolicLinkFile({
+          linkTargetPath: relativeTargetPath,
+          newLinkPath: linkInfo.linkPath,
+        });
+      }
+    }
+
+    return true;
+  }
+
   public deploy(scenarioName: string, overwriteExisting: boolean, targetFolderParameter: string | undefined): void {
     this._loadConfigFile(scenarioName);
+
+    if (targetFolderParameter) {
+      this._targetRootFolder = path.resolve(targetFolderParameter);
+      if (!FileSystem.exists(this._targetRootFolder)) {
+        throw new Error('The specified target folder does not exist: ' + JSON.stringify(targetFolderParameter));
+      }
+    } else {
+      this._targetRootFolder = path.join(this._rushConfiguration.commonFolder, 'deploy');
+    }
+    this._sourceRootFolder = this._rushConfiguration.rushJsonFolder;
+
+    console.log("Deploying to target folder: " + this._targetRootFolder);
+
 
     for (const includedProject of this._deployScenarioJson.includedProjects) {
       this._collectProject(includedProject);
@@ -184,37 +252,33 @@ export class DeployManager {
 
     Sort.sortSet(this._foldersToCopy);
 
-    let targetRootFolder: string;
-    if (targetFolderParameter) {
-      targetRootFolder = path.resolve(targetFolderParameter);
-      if (!FileSystem.exists(targetRootFolder)) {
-        throw new Error('The specified target folder does not exist: ' + JSON.stringify(targetFolderParameter));
-      }
-    } else {
-      targetRootFolder = path.join(this._rushConfiguration.commonFolder, 'deploy');
-    }
-
-    console.log("Deploying to target folder: " + targetRootFolder);
-
-    FileSystem.ensureFolder(targetRootFolder);
+    FileSystem.ensureFolder(this._targetRootFolder);
 
     // Is the target folder empty?
-    if (FileSystem.readFolder(targetRootFolder).length > 0) {
+    if (FileSystem.readFolder(this._targetRootFolder).length > 0) {
       if (overwriteExisting) {
         console.log('Deleting folder contents because "--overwrite" was specified...');
-        FileSystem.ensureEmptyFolder(targetRootFolder);
+        FileSystem.ensureEmptyFolder(this._targetRootFolder);
       } else {
         throw new Error('The deploy target folder is not empty. You can specify "--overwrite"'
           + ' to recursively delete all folder contents.');
       }
     }
 
-    const sourceRootFolder: string = this._rushConfiguration.rushJsonFolder;
-
     console.log("Copying folders");
     for (const folderToCopy of this._foldersToCopy) {
-      this._deployFolder(folderToCopy, sourceRootFolder, targetRootFolder);
+      this._deployFolder(folderToCopy);
     }
 
+    console.log("Copying symlinks");
+    const linksToCopy: ILinkInfo[] = this._symlinkAnalyzer.reportSymlinks();
+
+    for (const linkToCopy of linksToCopy) {
+      if (!this._deploySymlink(linkToCopy)) {
+        throw new Error("Target does not exist: " + JSON.stringify(linkToCopy, undefined, 2));
+      }
+    }
+
+    console.log("SUCCESS");
   }
 }
