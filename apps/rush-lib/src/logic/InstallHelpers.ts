95c83c37d05e5dcc9455d88ed837bfb5c3ee66e1
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-06-08 16:00:51 -0700

    Convert ensureLocalPackageManager() to use async/await

--- apps/rush-lib/src/logic/InstallHelpers.ts
+++ apps/rush-lib/src/logic/InstallHelpers.ts
@@ -21,7 +21,7 @@ export class InstallHelpers {
    * If the "(p)npm-local" symlink hasn't been set up yet, this creates it, installing the
    * specified (P)npm version in the user's home directory if needed.
    */
-  public static ensureLocalPackageManager(rushConfiguration: RushConfiguration,
+  public static async ensureLocalPackageManager(rushConfiguration: RushConfiguration,
     rushGlobalFolder: RushGlobalFolder, maxInstallAttempts: number): Promise<void> {
 
     // Example: "C:\Users\YourName\.rush"
@@ -44,62 +44,63 @@ export class InstallHelpers {
     });
 
     console.log(`Trying to acquire lock for ${packageManagerAndVersion}`);
-    return LockFile.acquire(rushUserFolder, packageManagerAndVersion).then((lock: LockFile) => {
-      console.log(`Acquired lock for ${packageManagerAndVersion}`);
 
-      if (!packageManagerMarker.isValid() || lock.dirtyWhenAcquired) {
-        console.log(colors.bold(`Installing ${packageManager} version ${packageManagerVersion}${os.EOL}`));
+    const lock: LockFile = await LockFile.acquire(rushUserFolder, packageManagerAndVersion);
 
-        // note that this will remove the last-install flag from the directory
-        Utilities.installPackageInDirectory({
-          directory: packageManagerToolFolder,
-          packageName: packageManager,
-          version: rushConfiguration.packageManagerToolVersion,
-          tempPackageTitle: `${packageManager}-local-install`,
-          maxInstallAttempts: maxInstallAttempts,
-          // This is using a local configuration to install a package in a shared global location.
-          // Generally that's a bad practice, but in this case if we can successfully install
-          // the package at all, we can reasonably assume it's good for all the repositories.
-          // In particular, we'll assume that two different NPM registries cannot have two
-          // different implementations of the same version of the same package.
-          // This was needed for: https://github.com/microsoft/rushstack/issues/691
-          commonRushConfigFolder: rushConfiguration.commonRushConfigFolder
-        });
+    console.log(`Acquired lock for ${packageManagerAndVersion}`);
 
-        console.log(`Successfully installed ${packageManager} version ${packageManagerVersion}`);
-      } else {
-        console.log(`Found ${packageManager} version ${packageManagerVersion} in ${packageManagerToolFolder}`);
-      }
+    if (!packageManagerMarker.isValid() || lock.dirtyWhenAcquired) {
+      console.log(colors.bold(`Installing ${packageManager} version ${packageManagerVersion}${os.EOL}`));
 
-      packageManagerMarker.create();
-
-      // Example: "C:\MyRepo\common\temp"
-      FileSystem.ensureFolder(rushConfiguration.commonTempFolder);
-
-      // Example: "C:\MyRepo\common\temp\pnpm-local"
-      const localPackageManagerToolFolder: string =
-        path.join(rushConfiguration.commonTempFolder, `${packageManager}-local`);
-
-      console.log(os.EOL + 'Symlinking "' + localPackageManagerToolFolder + '"');
-      console.log('  --> "' + packageManagerToolFolder + '"');
-
-      // We cannot use FileSystem.exists() to test the existence of a symlink, because it will
-      // return false for broken symlinks.  There is no way to test without catching an exception.
-      try {
-        FileSystem.deleteFolder(localPackageManagerToolFolder);
-      } catch (error) {
-        if (error.code !== 'ENOENT') {
-          throw error;
-        }
-      }
-
-      FileSystem.createSymbolicLinkJunction({
-        linkTargetPath: packageManagerToolFolder,
-        newLinkPath: localPackageManagerToolFolder
+      // note that this will remove the last-install flag from the directory
+      Utilities.installPackageInDirectory({
+        directory: packageManagerToolFolder,
+        packageName: packageManager,
+        version: rushConfiguration.packageManagerToolVersion,
+        tempPackageTitle: `${packageManager}-local-install`,
+        maxInstallAttempts: maxInstallAttempts,
+        // This is using a local configuration to install a package in a shared global location.
+        // Generally that's a bad practice, but in this case if we can successfully install
+        // the package at all, we can reasonably assume it's good for all the repositories.
+        // In particular, we'll assume that two different NPM registries cannot have two
+        // different implementations of the same version of the same package.
+        // This was needed for: https://github.com/microsoft/rushstack/issues/691
+        commonRushConfigFolder: rushConfiguration.commonRushConfigFolder
       });
 
-      lock.release();
+      console.log(`Successfully installed ${packageManager} version ${packageManagerVersion}`);
+    } else {
+      console.log(`Found ${packageManager} version ${packageManagerVersion} in ${packageManagerToolFolder}`);
+    }
+
+    packageManagerMarker.create();
+
+    // Example: "C:\MyRepo\common\temp"
+    FileSystem.ensureFolder(rushConfiguration.commonTempFolder);
+
+    // Example: "C:\MyRepo\common\temp\pnpm-local"
+    const localPackageManagerToolFolder: string =
+      path.join(rushConfiguration.commonTempFolder, `${packageManager}-local`);
+
+    console.log(os.EOL + 'Symlinking "' + localPackageManagerToolFolder + '"');
+    console.log('  --> "' + packageManagerToolFolder + '"');
+
+    // We cannot use FileSystem.exists() to test the existence of a symlink, because it will
+    // return false for broken symlinks.  There is no way to test without catching an exception.
+    try {
+      FileSystem.deleteFolder(localPackageManagerToolFolder);
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+
+    FileSystem.createSymbolicLinkJunction({
+      linkTargetPath: packageManagerToolFolder,
+      newLinkPath: localPackageManagerToolFolder
     });
+
+    lock.release();
   }
 
 }
