f7ad5d1bf966659813f7e33be2032eb4baf3db37
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-06-09 00:09:48 -0700

    Add the Prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// Documentation for this file: https://prettier.io/docs/en/configuration.html
+module.exports = {
+  // We use a larger print width because Prettier's word-wrapping seems to be tuned
+  // for plain JavaScript without type annotations
+  printWidth: 110,
+  // Microsoft style quotes
+  singleQuote: true,
+  // Preserve existing newlines
+  endOfLine: 'auto',
+};
