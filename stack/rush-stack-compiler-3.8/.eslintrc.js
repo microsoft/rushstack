e5e685350f1933b7a1d9bf2a378ed2cb12768b13
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-06-16 16:16:29 -0700

    Update patch-eslint6.js to patch-eslint-resolver.js

--- stack/rush-stack-compiler-3.8/.eslintrc.js
+++ stack/rush-stack-compiler-3.8/.eslintrc.js
@@ -1,5 +1,5 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch-eslint6');
+require('@rushstack/eslint-config/patch-eslint-resolver');
 
 module.exports = {
   extends: ['@rushstack/eslint-config'],
