63e40c94827faf19688674bc2422713fbd4622e4
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-06-16 08:11:51 -0700

    Fix up some "@typescript-eslint/ban-types" issues coming from "extendDefaults=true"

--- stack/eslint-config/index.js
+++ stack/eslint-config/index.js
@@ -59,25 +59,41 @@ module.exports = {
         '@typescript-eslint/ban-types': [
           'error',
           {
+            extendDefaults: false, // (the complete list is in this file)
             types: {
               String: {
-                message: "Use 'string' instead",
+                message: 'Use "string" instead',
                 fixWith: 'string'
               },
               Boolean: {
-                message: "Use 'boolean' instead",
+                message: 'Use "boolean" instead',
                 fixWith: 'boolean'
               },
               Number: {
-                message: "Use 'number' instead",
+                message: 'Use "number" instead',
                 fixWith: 'number'
               },
               Object: {
-                message: "Use 'object' instead, or else define a proper TypeScript type:"
+                message: 'Use "object" instead, or else define a proper TypeScript type:'
               },
               Symbol: {
-                message: "Use 'symbol' instead",
+                message: 'Use "symbol" instead',
                 fixWith: 'symbol'
+              },
+              Function: {
+                message: [
+                  'The "Function" type accepts any function-like value.',
+                  'It provides no type safety when calling the function, which can be a common source of bugs.',
+                  'It also accepts things like class declarations, which will throw at runtime as they will not be called with "new".',
+                  'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.'
+                ].join('\n')
+              },
+              '{}': {
+                message: [
+                  '"{}" actually means "any non-nullish value".',
+                  '- If you want a type meaning "any object", you probably want "Record<string, unknown>" instead.',
+                  '- If you want a type meaning "any value", you probably want "unknown" instead.'
+                ].join('\n')
               }
             }
           }
