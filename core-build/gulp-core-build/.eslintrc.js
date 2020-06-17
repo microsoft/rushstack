f42abf7718da8ca33642567f4412c5f06db1cc8f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-06-16 20:39:24 -0700

    Update .eslintrc to use the new path

--- core-build/gulp-core-build/.eslintrc.js
+++ core-build/gulp-core-build/.eslintrc.js
@@ -1,5 +1,5 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch-eslint-resolver');
+require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
   extends: ['@rushstack/eslint-config'],
