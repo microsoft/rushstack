46a7d3db4a4bb8fad206e3faa057751d54171e68
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2020-07-02 21:26:17 -0700

    PR feedback

---
+++ apps/rush-lib/src/logic/pnpm/PnpmfileShim.ts
@@ -0,0 +1,83 @@
+// THIS FILE IS PROVIDED BY RUSH. ANY MODIFICATIONS WILL BE OVERWRITTEN.
+import { existsSync } from 'fs';
+
+interface ILockfile {}
+
+interface IPackageJson {
+  dependencies?: { [dependencyName: string]: string };
+  devDependencies?: { [dependencyName: string]: string };
+  optionalDependencies?: { [dependencyName: string]: string };
+}
+
+interface IPnpmfile {
+  hooks?: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    afterAllResolved?: (lockfile: ILockfile, context: any) => ILockfile;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    readPackage?: (pkg: IPackageJson, context: any) => IPackageJson;
+  };
+}
+
+// We will require semver from this path on disk, since this is the version of semver shipping with Rush
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const semver: any = require('__semverPath');
+// Only require the client pnpmfile if it exists
+const clientPnpmfile: IPnpmfile | undefined = existsSync('clientPnpmfile.js')
+  ? require('./clientPnpmfile')
+  : undefined;
+const allPreferredVersions: { [dependencyName: string]: string } = JSON.parse('__allPreferredVersions');
+const allowedAlternativeVersions: { [dependencyName: string]: string[] } = JSON.parse(
+  '__allowedAlternativeVersions'
+);
+
+// Set the preferred versions on the dependency map. If the version on the map is an allowedAlternativeVersion
+// then skip it. Otherwise, check to ensure that the common version is a subset of the specified version. If
+// it is, then replace the specified version with the preferredVersion
+function setPreferredVersions(dependencies?: { [dependencyName: string]: string }): void {
+  for (const name of Object.keys(dependencies || {})) {
+    if (allPreferredVersions.hasOwnProperty(name)) {
+      const preferredVersion: string = allPreferredVersions[name];
+      const version: string = dependencies![name];
+      if (allowedAlternativeVersions.hasOwnProperty(name)) {
+        const allowedAlternatives: string[] | undefined = allowedAlternativeVersions[name];
+        if (allowedAlternatives && allowedAlternatives.indexOf(version) > -1) {
+          continue;
+        }
+      }
+      let isValidRange: boolean = false;
+      try {
+        isValidRange = !!semver.validRange(preferredVersion) && !!semver.validRange(version);
+      } catch {
+        // Swallow invalid range errors
+      }
+      if (isValidRange && semver.subset(preferredVersion, version)) {
+        dependencies![name] = preferredVersion;
+      }
+    }
+  }
+}
+
+const pnpmfileShim: IPnpmfile = {
+  hooks: {
+    // Call the original pnpmfile (if it exists)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    afterAllResolved: (lockfile: ILockfile, context: any) => {
+      return clientPnpmfile && clientPnpmfile.hooks && clientPnpmfile.hooks.afterAllResolved
+        ? clientPnpmfile.hooks.afterAllResolved(lockfile, context)
+        : lockfile;
+    },
+
+    // Set the preferred versions in the package, then call the original pnpmfile (if it exists)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    readPackage: (pkg: IPackageJson, context: any) => {
+      setPreferredVersions(pkg.dependencies);
+      setPreferredVersions(pkg.devDependencies);
+      setPreferredVersions(pkg.optionalDependencies);
+      return clientPnpmfile && clientPnpmfile.hooks && clientPnpmfile.hooks.readPackage
+        ? clientPnpmfile.hooks.readPackage(pkg, context)
+        : pkg;
+    }
+  }
+};
+
+export = pnpmfileShim;
