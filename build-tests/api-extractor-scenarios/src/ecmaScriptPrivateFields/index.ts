b981e073e424fefc8a4cd7a9ac4528d8eb4e795c
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-02 21:32:29 -0700

    Add support for ECMAScript private fields

---
+++ build-tests/api-extractor-scenarios/src/ecmaScriptPrivateFields/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/** @public */
+export class Example {
+  private _typeScriptPrivate: string;
+  #ecmaScriptField: string;
+}
