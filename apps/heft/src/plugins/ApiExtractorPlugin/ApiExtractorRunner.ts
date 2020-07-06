4e3d4e71a6aae8613e74ab024997d1d7739fdeb8
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2020-07-06 16:54:00 -0700

    Clean up logigng in TS And AE

--- apps/heft/src/plugins/ApiExtractorPlugin/ApiExtractorRunner.ts
+++ apps/heft/src/plugins/ApiExtractorPlugin/ApiExtractorRunner.ts
@@ -2,7 +2,7 @@
 // See LICENSE in the project root for license information.
 
 import * as path from 'path';
-import { Colors, IColorableSequence, ITerminalProvider, Terminal } from '@rushstack/node-core-library';
+import { Colors, IColorableSequence, ITerminalProvider, Terminal, Path } from '@rushstack/node-core-library';
 import { ApiExtractor as TApiExtractor } from '@microsoft/rush-stack-compiler-3.7';
 
 import { SubprocessRunnerBase } from '../../utilities/subprocess/SubprocessRunnerBase';
@@ -49,17 +49,15 @@ export class ApiExtractorRunner extends SubprocessRunnerBase<IApiExtractorRunner
           case apiExtractor.ExtractorLogLevel.Error: {
             let logMessage: (IColorableSequence | string)[];
             if (message.sourceFilePath) {
-              const buildFolderRelativeFilePath: string = path.relative(
-                this._configuration.buildFolder,
-                message.sourceFilePath
-              );
+              const filePathForLog: string = Path.isUnderOrEqual(
+                message.sourceFilePath,
+                this._configuration.buildFolder
+              )
+                ? path.relative(this._configuration.buildFolder, message.sourceFilePath)
+                : message.sourceFilePath;
               logMessage = [
-                Colors.red(
-                  `${buildFolderRelativeFilePath}(${message.sourceFileLine},${message.sourceFileColumn})`
-                ),
-                Colors.white(' - '),
-                Colors.gray(`(${message.category})`),
-                ' ',
+                Colors.red(`ERROR: ${filePathForLog}(${message.sourceFileLine},${message.sourceFileColumn})`),
+                ` - (${message.category}) `,
                 Colors.red(message.text)
               ];
             } else {
@@ -73,17 +71,17 @@ export class ApiExtractorRunner extends SubprocessRunnerBase<IApiExtractorRunner
           case apiExtractor.ExtractorLogLevel.Warning: {
             let logMessage: (IColorableSequence | string)[];
             if (message.sourceFilePath) {
-              const buildFolderRelativeFilePath: string = path.relative(
-                this._configuration.buildFolder,
-                message.sourceFilePath
-              );
+              const filePathForLog: string = Path.isUnderOrEqual(
+                message.sourceFilePath,
+                this._configuration.buildFolder
+              )
+                ? path.relative(this._configuration.buildFolder, message.sourceFilePath)
+                : message.sourceFilePath;
               logMessage = [
                 Colors.yellow(
-                  `${buildFolderRelativeFilePath}(${message.sourceFileLine},${message.sourceFileColumn})`
+                  `WARNING: ${filePathForLog}(${message.sourceFileLine},${message.sourceFileColumn})`
                 ),
-                Colors.white(' - '),
-                Colors.gray(`(${message.category})`),
-                ' ',
+                ` - (${message.category}) `,
                 Colors.yellow(message.text)
               ];
             } else {
@@ -99,9 +97,13 @@ export class ApiExtractorRunner extends SubprocessRunnerBase<IApiExtractorRunner
             break;
           }
 
-          case apiExtractor.ExtractorLogLevel.None:
           case apiExtractor.ExtractorLogLevel.Info: {
             this._terminal.writeLine(message.text);
+            break;
+          }
+
+          case apiExtractor.ExtractorLogLevel.None: {
+            // Ignore messages with ExtractorLogLevel.None
             break;
           }
 
@@ -123,13 +125,11 @@ export class ApiExtractorRunner extends SubprocessRunnerBase<IApiExtractorRunner
     const { errorCount, warningCount } = apiExtractorResult;
     if (errorCount > 0) {
       this._terminal.writeErrorLine(
-        `Encountered ${errorCount} API Extractor error${errorCount > 1 ? 's' : ''}`
+        `API Extractor completed with ${errorCount} error${errorCount > 1 ? 's' : ''}`
       );
-    }
-
-    if (warningCount > 0) {
+    } else if (warningCount > 0) {
       this._terminal.writeErrorLine(
-        `Encountered ${warningCount} API Extractor warning${warningCount > 1 ? 's' : ''}`
+        `API Extractor completed with ${warningCount} warning${warningCount > 1 ? 's' : ''}`
       );
     }
 
