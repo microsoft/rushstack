040c33bc788362bd13fa660c323c68a8609ca18c
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2020-07-06 19:04:41 -0700

    Add a test to heft-test-01

---
+++ build-tests/heft-test-01/src/test/ExampleTest.test.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+describe('Example Test', () => {
+  it('Correctly tests stuff', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Correctly handles snapshots', () => {
+    expect({ a: 1, b: 2, c: 3 }).toMatchSnapshot();
+  });
+});
