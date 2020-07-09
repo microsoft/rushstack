acbdf9a54c545fe50f43a1da19979b0b47d780b2
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-08 21:08:26 -0700

    Add a unit test that repros issue #2014

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/exportImportedExternalDefault/rollup.d.ts
@@ -0,0 +1,10 @@
+import { default } from 'api-extractor-lib2-test';
+import { Lib2Class } from 'api-extractor-lib2-test';
+
+/** @public */
+export declare class Child extends default {
+}
+export default default;
+export { Lib2Class }
+
+export { }
