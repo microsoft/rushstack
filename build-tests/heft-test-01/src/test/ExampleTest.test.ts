64ee507ca39fc0d238aeed79e6574160a89d91a9
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2020-07-14 17:54:28 -0700

    Update example test.

--- build-tests/heft-test-01/src/test/ExampleTest.test.ts
+++ build-tests/heft-test-01/src/test/ExampleTest.test.ts
@@ -1,6 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+interface IInterface {
+  element: string;
+}
+
 describe('Example Test', () => {
   it('Correctly tests stuff', () => {
     expect(true).toBeTruthy();
@@ -8,5 +12,12 @@ describe('Example Test', () => {
 
   it('Correctly handles snapshots', () => {
     expect({ a: 1, b: 2, c: 3 }).toMatchSnapshot();
+  });
+
+  it('Correctly handles TypeScript constructs', () => {
+    const interfaceInstance: IInterface = {
+      element: 'a'
+    };
+    expect(interfaceInstance).toBeTruthy();
   });
 });
