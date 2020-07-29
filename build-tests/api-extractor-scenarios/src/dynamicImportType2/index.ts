fa05af415ef90acc9eb5a4ab23457147229e0074
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-29 00:12:34 -0700

    Add dynamicImportType2 test that illustrates how dotted namespaces are handled

--- build-tests/api-extractor-scenarios/src/dynamicImportType2/index.ts
+++ build-tests/api-extractor-scenarios/src/dynamicImportType2/index.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-export { Lib2Class } from 'api-extractor-lib2-test';
+/** @public */
+export interface IExample {
+  dottedImportType: import('api-extractor-lib1-test').Lib1Namespace.Inner.X;
+}
