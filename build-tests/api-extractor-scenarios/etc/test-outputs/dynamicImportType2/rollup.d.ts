3c7fe5e22cb6e4fcd3ceb669ae57981d7fba56a7
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-29 00:12:34 -0700

    Add dynamicImportType2 test that illustrates how dotted namespaces are handled

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/dynamicImportType2/rollup.d.ts
@@ -0,0 +1,8 @@
+import * as Lib1Namespace_Inner_X from 'api-extractor-lib1-test';
+
+/** @public */
+export declare interface IExample {
+    dottedImportType: Lib1Namespace_Inner_X.Lib1Namespace.Inner.X;
+}
+
+export { }
