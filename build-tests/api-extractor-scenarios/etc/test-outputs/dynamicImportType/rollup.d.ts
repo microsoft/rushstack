f53532ec43912a8223eb974d891abcf0e00f6331
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-28 23:49:27 -0700

    Eliminate warning

--- build-tests/api-extractor-scenarios/etc/test-outputs/dynamicImportType/rollup.d.ts
+++ build-tests/api-extractor-scenarios/etc/test-outputs/dynamicImportType/rollup.d.ts
@@ -4,6 +4,7 @@ import { Lib1Interface } from 'api-extractor-lib1-test';
 import { Lib2Class } from 'api-extractor-lib2-test';
 import { Lib2Interface } from 'api-extractor-lib2-test';
 
+/** @public */
 export declare class Item {
     options: Options;
     lib1: Lib1Interface;
