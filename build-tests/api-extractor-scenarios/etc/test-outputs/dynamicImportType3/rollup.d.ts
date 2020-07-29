373c945348353c9578294816335151e851d80e5c
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-29 00:18:59 -0700

    Add a simplified repro for https://github.com/microsoft/rushstack/issues/1754

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/dynamicImportType3/rollup.d.ts
@@ -0,0 +1,9 @@
+import { Lib1GenericType } from 'api-extractor-lib1-test';
+import { Lib1Interface } from 'api-extractor-lib1-test';
+
+/** @public */
+export declare interface IExample {
+    generic: Lib1GenericType;
+}
+
+export { }
