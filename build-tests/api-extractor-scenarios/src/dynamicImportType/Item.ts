613880aaa70d13853ac075ce060f7cfddef8405c
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-07-29 00:12:34 -0700

    Add dynamicImportType2 test that illustrates how dotted namespaces are handled

--- build-tests/api-extractor-scenarios/src/dynamicImportType/Item.ts
+++ build-tests/api-extractor-scenarios/src/dynamicImportType/Item.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 /** @public */
 export class Item {
   options: import('./Options').Options;
