7d050936f9ddff89e8fd90f5001006a6af4a6a8f
Sachin Joseph <sachinjoseph@users.noreply.github.com>
2020-08-01 15:09:07 -0700

    Remove trace logs

--- apps/rush-lib/src/start.ts
+++ apps/rush-lib/src/start.ts
@@ -1,7 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
-console.log('start.ts: 1: ' + (new Date().getTime() % 20000) / 1000.0);
 import { Rush } from './api/Rush';
 
 Rush.launch(Rush.version, { isManaged: false });
-console.log('start.ts: 2: ' + (new Date().getTime() % 20000) / 1000.0);
