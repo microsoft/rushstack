b54d59b87f34a547d3b90ff0158ce1235933d87e
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-08-02 17:20:16 -0700

    Update TypeScriptPlugin.ts to write jest-typescript-data.json

---
+++ apps/heft/src/plugins/JestPlugin/JestTypeScriptDataFile.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import * as path from 'path';
+import { JsonFile } from '@rushstack/node-core-library';
+import { ITypeScriptConfiguration } from '../../stages/BuildStage';
+
+/**
+ * Schema for jest-typescript-data.json
+ */
+export interface IJestTypeScriptDataFileJson {
+  /**
+   * The "emitFolderPathForJest" from .heft/typescript.json
+   */
+  emitFolderPathForJest: string;
+}
+
+/**
+ * Manages loading/saving the "jest-typescript-data.json" data file.  This file communicates
+ * configuration information from Heft to jest-build-transform.js.  The jest-build-transform.js script gets
+ * loaded dynamically by the Jest engine, so it does not have access to the normal HeftConfiguration objects.
+ */
+export class JestTypeScriptDataFile {
+  /**
+   * Called by TypeScriptPlugin to write the file.
+   */
+  public static saveForProject(
+    projectFolder: string,
+    typeScriptConfiguration: ITypeScriptConfiguration
+  ): void {
+    const jsonFilePath: string = JestTypeScriptDataFile._getConfigFilePath(projectFolder);
+
+    const json: IJestTypeScriptDataFileJson = {
+      emitFolderPathForJest: typeScriptConfiguration.emitFolderPathForJest || 'lib'
+    };
+    JsonFile.save(json, jsonFilePath);
+  }
+
+  /**
+   * Called by jest-build-transform.js to read the file.
+   */
+  public static loadForProject(projectFolder: string): IJestTypeScriptDataFileJson {
+    const jsonFilePath: string = JestTypeScriptDataFile._getConfigFilePath(projectFolder);
+    return JsonFile.load(jsonFilePath);
+  }
+
+  private static _getConfigFilePath(projectFolder: string): string {
+    return path.join(projectFolder, '.heft', 'build-cache', 'jest-typescript-data.json');
+  }
+}
