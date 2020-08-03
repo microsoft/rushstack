e381e806162b3a3afad5362e699b85c9475b0c31
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-08-02 17:20:41 -0700

    Update jest-build-transform to read jest-typescript-data.json

--- apps/heft/src/plugins/JestPlugin/jest-build-transform.ts
+++ apps/heft/src/plugins/JestPlugin/jest-build-transform.ts
@@ -4,30 +4,48 @@
 import * as path from 'path';
 import { Path, FileSystem } from '@rushstack/node-core-library';
 import { InitialOptionsWithRootDir } from '@jest/types/build/Config';
+import { JestTypeScriptDataFile, IJestTypeScriptDataFileJson } from './JestTypeScriptDataFile';
 
 /**
  * This Jest transformer maps TS files under a 'src' folder to their compiled equivalent under 'lib'
  */
 export function process(src: string, filename: string, jestOptions: InitialOptionsWithRootDir): string {
+  // Read typescript-jest-config.json, which is created by Heft's TypeScript plugin.  It tells us
+  // which emitted output folder to use for Jest.
+  const jestTypeScriptConfig: IJestTypeScriptDataFileJson = JestTypeScriptDataFile.loadForProject(
+    jestOptions.rootDir
+  );
+
+  // Is the input file under the "src" folder?
   const srcFolder: string = path.join(jestOptions.rootDir, 'src');
+
   if (Path.isUnder(filename, srcFolder)) {
-    const fileBasename: string = path.basename(filename, path.extname(filename));
-    const srcRelativeFolderPath: string = path.dirname(path.relative(srcFolder, filename));
+    // Example: /path/to/project/src/folder1/folder2/Example.ts
+    const parsedFilename: path.ParsedPath = path.parse(filename);
+
+    // Example: folder1/folder2
+    const srcRelativeFolderPath: string = path.relative(srcFolder, parsedFilename.dir);
+
+    // Example: /path/to/project/lib/folder1/folder2/Example.js
     const libFilename: string = path.join(
       jestOptions.rootDir,
-      'lib',
+      jestTypeScriptConfig.emitFolderPathForJest,
       srcRelativeFolderPath,
-      `${fileBasename}.js`
+      `${parsedFilename.name}.js`
     );
 
     try {
       return FileSystem.readFile(libFilename);
     } catch (error) {
-      if (!FileSystem.isNotExistError(error)) {
+      if (FileSystem.isNotExistError(error)) {
+        throw new Error(
+          'jest-build-transform: The expected transpiler output file does not exist:\n' + libFilename
+        );
+      } else {
         throw error;
       }
     }
+  } else {
+    throw new Error('jest-build-transform: The input path is not under the "src" folder:\n' + filename);
   }
-
-  return src;
 }
