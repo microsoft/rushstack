37a8d5c9c2d167a8f792ff71ef9d17fc668cca07
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-08-17 00:41:09 -0700

    Improve startup performance by minimizing unnecessary imports

--- apps/heft/src/startWithVersionSelector.ts
+++ apps/heft/src/startWithVersionSelector.ts
@@ -1,19 +1,49 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-// NOTE: Since the version-selected entry point is loaded in the same process, we should minimize
-// any dependencies loaded by startWithVersionSelector.ts, since they may be loaded as
-// side-by-side versions.
+// NOTE: Since startWithVersionSelector.ts is loaded in the same process as start.ts, any dependencies that
+// we import here may become side-by-side versions.  We want to minimize any dependencies.
 import * as path from 'path';
-import * as resolve from 'resolve';
-import { PackageJsonLookup, IPackageJson, INodePackageJson, FileSystem } from '@rushstack/node-core-library';
+import * as fs from 'fs';
+import { /* type*/ IPackageJson } from '@rushstack/node-core-library';
 
 const HEFT_PACKAGE_NAME: string = '@rushstack/heft';
 
+// Excerpted from PackageJsonLookup.tryGetPackageFolderFor()
+function tryGetPackageFolderFor(resolvedFileOrFolderPath: string): string | undefined {
+  // Two lookups are required, because get() cannot distinguish the undefined value
+  // versus a missing key.
+  // if (this._packageFolderCache.has(resolvedFileOrFolderPath)) {
+  //   return this._packageFolderCache.get(resolvedFileOrFolderPath);
+  // }
+
+  // Is resolvedFileOrFolderPath itself a folder with a package.json file?  If so, return it.
+  if (fs.existsSync(path.join(resolvedFileOrFolderPath, 'package.json'))) {
+    // this._packageFolderCache.set(resolvedFileOrFolderPath, resolvedFileOrFolderPath);
+    return resolvedFileOrFolderPath;
+  }
+
+  // Otherwise go up one level
+  const parentFolder: string | undefined = path.dirname(resolvedFileOrFolderPath);
+  if (!parentFolder || parentFolder === resolvedFileOrFolderPath) {
+    // We reached the root directory without finding a package.json file,
+    // so cache the negative result
+    // this._packageFolderCache.set(resolvedFileOrFolderPath, undefined);
+    return undefined; // no match
+  }
+
+  // Recurse upwards, caching every step along the way
+  const parentResult: string | undefined = tryGetPackageFolderFor(parentFolder);
+  // Cache the parent's answer as well
+  // this._packageFolderCache.set(resolvedFileOrFolderPath, parentResult);
+
+  return parentResult;
+}
+
 /**
- * If Heft is installed globally or in some other system path, it will look in the project's package.json
- * file for a local dependency.  If found, Heft will launch that version instead.  This avoids accidentally
- * building using the wrong version of Heft.  Use "heft --unmanaged" to bypass this feature.
+ * When Heft is invoked via the shell path, we examine the project's package.json dependencies and try to load
+ * the locally installed version of Heft. This avoids accidentally building using the wrong version of Heft.
+ * Use "heft --unmanaged" to bypass this feature.
  */
 function tryStartLocalHeft(): boolean {
   if (process.argv.indexOf('--unmanaged') >= 0) {
@@ -22,13 +52,21 @@ function tryStartLocalHeft(): boolean {
     return false;
   }
 
-  const lookup: PackageJsonLookup = new PackageJsonLookup();
-  const packageFolder: string | undefined = lookup.tryGetPackageFolderFor(process.cwd());
-  if (packageFolder) {
+  // Find the package.json file that governs the current folder location
+  const projectFolder: string | undefined = tryGetPackageFolderFor(process.cwd());
+  if (projectFolder) {
     let heftEntryPoint: string;
     try {
-      const packageJson: IPackageJson = lookup.loadPackageJson(path.join(packageFolder, 'package.json'));
+      const packageJsonPath: string = path.join(projectFolder, 'package.json');
+      const packageJsonContent: string = fs.readFileSync(packageJsonPath).toString();
+      let packageJson: IPackageJson;
+      try {
+        packageJson = JSON.parse(packageJsonContent);
+      } catch (error) {
+        throw new Error(`Error parsing ${packageJsonPath}:` + error.message);
+      }
 
+      // Does package.json have a dependency on Heft?
       if (
         !(packageJson.dependencies && packageJson.dependencies[HEFT_PACKAGE_NAME]) &&
         !(packageJson.devDependencies && packageJson.devDependencies[HEFT_PACKAGE_NAME])
@@ -37,20 +75,12 @@ function tryStartLocalHeft(): boolean {
         return false;
       }
 
-      const heftPackageJsonPath: string = resolve.sync(HEFT_PACKAGE_NAME, {
-        basedir: packageFolder,
-        preserveSymlinks: false,
-        packageFilter: (packageJson: INodePackageJson) => {
-          return {
-            ...packageJson,
-            // ensure "main" points to a file in the package root folder
-            main: 'package.json'
-          };
-        }
-      });
-      const heftFolder: string = path.dirname(heftPackageJsonPath);
+      // To avoid a loading the "resolve" NPM package, let's assume that the Heft dependency must be
+      // installed as "<projectFolder>/node_modules/@rushstack/heft".
+      const heftFolder: string = path.join(projectFolder, 'node_modules', HEFT_PACKAGE_NAME);
+
       heftEntryPoint = path.join(heftFolder, 'lib', 'start.js');
-      if (!FileSystem.exists(heftEntryPoint)) {
+      if (!fs.existsSync(heftEntryPoint)) {
         throw new Error('Unable to find Heft entry point: ' + heftEntryPoint);
       }
       console.log(`Found local Heft in: ${heftFolder}`);
@@ -64,6 +94,7 @@ function tryStartLocalHeft(): boolean {
     // We found and successfully invoked the local Heft
     return true;
   }
+
   // We couldn't find the package folder
   return false;
 }
