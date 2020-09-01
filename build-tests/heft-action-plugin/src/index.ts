98ab400bcefaa42b60f62401e40151b93b472422
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2020-08-31 22:57:27 -0700

    Include --production flag in heft-action-plugin.

--- build-tests/heft-action-plugin/src/index.ts
+++ build-tests/heft-action-plugin/src/index.ts
@@ -10,9 +10,19 @@ class HeftActionPlugin implements IHeftPlugin {
     heftSession.registerAction({
       actionName: 'my-custom-action',
       documentation: 'An example custom action',
-      callback: () => {
+      parameters: [
+        {
+          kind: 'flag',
+          paramterLongName: '--production',
+          description: 'Run in production mode',
+          callbackValueName: 'production'
+        }
+      ],
+      callback: ({ production }) => {
         const logger: ScopedLogger = heftSession.requestScopedLogger('custom-action');
-        logger.terminal.writeLine('!!!!!!!!!!!!!! Custom action executing !!!!!!!!!!!!!!');
+        logger.terminal.writeLine(
+          `!!!!!!!!!!!!!! Custom action executing (production: ${production}) !!!!!!!!!!!!!!`
+        );
       }
     });
   }
