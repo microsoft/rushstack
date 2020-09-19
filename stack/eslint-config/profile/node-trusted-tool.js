6f53fff5e060f030832ff258c9cdb515861f5122
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-09-18 21:57:21 -0700

    Separate eslint-config/index.js into separate profiles so that we can enable different security rules for Node.js tools vs services

---
+++ stack/eslint-config/profile/node-trusted-tool.js
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+// This profile enables lint rules intended for a Node.js project whose inputs will always
+// come from a developer or other trusted source.  Most build system tasks are like this,
+// since they operate exclusively files prepared by a developer.
+//
+// This profile disables certain security rules that would otherwise prohibit APIs that could
+// cause a denial-of-service by consuming too many resources, or which might interact with
+// the filesystem in unsafe ways.  Such activities are safe and commonplace for a trusted tool.
+
+const common = require('./_common');
+
+module.exports = {
+  ...common
+};
---
+++ stack/eslint-config/profile/web-app.js
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+// This profile enables lint rules intended for a web application.  It enables security rules
+// that are relevant to web browser APIs such as DOM.
+
+const common = require('./_common');
+
+module.exports = {
+  ...common
+};
