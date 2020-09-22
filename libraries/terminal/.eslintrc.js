307a496b9a1928cf4e5b42a7e829e660df66c0cf
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-09-21 18:01:17 -0700

    Enable friendly-locals for Rush Stack projects

--- libraries/terminal/.eslintrc.js
+++ libraries/terminal/.eslintrc.js
@@ -2,6 +2,10 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/node', '@rushstack/eslint-config/mixins/tsdoc'],
+  extends: [
+    '@rushstack/eslint-config/profile/node',
+    '@rushstack/eslint-config/mixins/friendly-locals',
+    '@rushstack/eslint-config/mixins/tsdoc'
+  ],
   parserOptions: { tsconfigRootDir: __dirname }
 };
--- stack/rush-stack-compiler-3.6/.eslintrc.js
+++ stack/rush-stack-compiler-3.6/.eslintrc.js
@@ -2,6 +2,9 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/node-trusted-tool'],
+  extends: [
+    '@rushstack/eslint-config/profile/node-trusted-tool',
+    '@rushstack/eslint-config/mixins/friendly-locals'
+  ],
   parserOptions: { tsconfigRootDir: __dirname }
 };
