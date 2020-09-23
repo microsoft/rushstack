89f83073b191d4bb20c9c65deb345c6801f60c5f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-09-23 15:50:28 -0700

    Add new project "@rushstack/rig-package"

---
+++ libraries/rig-package/README.md
@@ -0,0 +1,2 @@
+# @rushstack/rig-package
+
---
+++ libraries/rig-package/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/**
+ * A standard for sharing project configuration in a monorepo without duplicating config files.
+ *
+ * @packageDocumentation
+ */
+
+/** @public */
+export class X {}
