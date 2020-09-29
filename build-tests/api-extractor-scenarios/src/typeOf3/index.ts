6a62ac5868e5ab1f67aacc027bc1b79afca624cf
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-09-28 18:52:51 -0700

    Add test case for typeOf bug

---
+++ build-tests/api-extractor-scenarios/src/typeOf3/index.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+/**
+ * A function that references its own parameter type.
+ */
+export function f1(x: number): typeof x {
+  return x;
+}
+
+/**
+ * A function that indirectly references its own  parameter type.
+ */
+export function f2(x: number): keyof typeof x {
+  return 'valueOf';
+}
+
+/**
+ * A function that  references its own  type.
+ */
+export function f3(): typeof f3 | undefined {
+  return undefined;
+}
