c8c6e1ee3813d96b0a502c8afa21a97ceb89b0f3
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-11-17 23:58:47 -0800

    Update api-extractor and api-documenter to use the new ApiOptionalMixin API

--- apps/api-extractor/src/generators/ApiModelGenerator.ts
+++ apps/api-extractor/src/generators/ApiModelGenerator.ts
@@ -636,12 +636,15 @@ export class ApiModelGenerator {
       if (releaseTag === ReleaseTag.Internal || releaseTag === ReleaseTag.Alpha) {
         return; // trim out items marked as "@internal" or "@alpha"
       }
+      const isOptional: boolean =
+        (astDeclaration.astSymbol.followedSymbol.flags & ts.SymbolFlags.Optional) !== 0;
 
       apiMethod = new ApiMethod({
         name,
         docComment,
         releaseTag,
         isStatic,
+        isOptional,
         typeParameters,
         parameters,
         overloadIndex,
@@ -689,11 +692,14 @@ export class ApiModelGenerator {
       const apiItemMetadata: ApiItemMetadata = this._collector.fetchApiItemMetadata(astDeclaration);
       const docComment: tsdoc.DocComment | undefined = apiItemMetadata.tsdocComment;
       const releaseTag: ReleaseTag = apiItemMetadata.effectiveReleaseTag;
+      const isOptional: boolean =
+        (astDeclaration.astSymbol.followedSymbol.flags & ts.SymbolFlags.Optional) !== 0;
 
       apiMethodSignature = new ApiMethodSignature({
         name,
         docComment,
         releaseTag,
+        isOptional,
         typeParameters,
         parameters,
         overloadIndex,
@@ -738,8 +744,6 @@ export class ApiModelGenerator {
     const name: string = exportedName ? exportedName : astDeclaration.astSymbol.localName;
 
     const isStatic: boolean = (astDeclaration.modifierFlags & ts.ModifierFlags.Static) !== 0;
-    const isOptional: boolean =
-      (astDeclaration.astSymbol.followedSymbol.flags & ts.SymbolFlags.Optional) !== 0;
 
     const containerKey: string = ApiProperty.getContainerKey(name, isStatic);
 
@@ -757,6 +761,8 @@ export class ApiModelGenerator {
       const apiItemMetadata: ApiItemMetadata = this._collector.fetchApiItemMetadata(astDeclaration);
       const docComment: tsdoc.DocComment | undefined = apiItemMetadata.tsdocComment;
       const releaseTag: ReleaseTag = apiItemMetadata.effectiveReleaseTag;
+      const isOptional: boolean =
+        (astDeclaration.astSymbol.followedSymbol.flags & ts.SymbolFlags.Optional) !== 0;
 
       apiProperty = new ApiProperty({
         name,
@@ -798,11 +804,14 @@ export class ApiModelGenerator {
       const apiItemMetadata: ApiItemMetadata = this._collector.fetchApiItemMetadata(astDeclaration);
       const docComment: tsdoc.DocComment | undefined = apiItemMetadata.tsdocComment;
       const releaseTag: ReleaseTag = apiItemMetadata.effectiveReleaseTag;
+      const isOptional: boolean =
+        (astDeclaration.astSymbol.followedSymbol.flags & ts.SymbolFlags.Optional) !== 0;
 
       apiPropertySignature = new ApiPropertySignature({
         name,
         docComment,
         releaseTag,
+        isOptional,
         excerptTokens,
         propertyTypeTokenRange
       });
