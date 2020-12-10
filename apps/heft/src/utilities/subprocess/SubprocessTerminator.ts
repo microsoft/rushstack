eb9c363019b8c0eb410228dabc06cbf736c05c3f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2020-12-09 22:49:54 -0800

    Add docs and disable for Windows

--- apps/heft/src/utilities/subprocess/SubprocessTerminator.ts
+++ apps/heft/src/utilities/subprocess/SubprocessTerminator.ts
@@ -4,16 +4,54 @@
 import * as childProcess from 'child_process';
 import process from 'process';
 
+/**
+ * When a child process is created, registering it with the SubprocessTerminator will ensure
+ * that the child gets terminated when the current process terminates.
+ *
+ * @remarks
+ * This works by hooking the current process's events for SIGTERM/SIGINT/exit, and ensuring the
+ * child process gets terminated in those cases.
+ *
+ * SubprocessTerminator doesn't do any thing on Windows, since by default Windows automatically
+ * terminates child processes when their parent is terminated.
+ */
 export class SubprocessTerminator {
+  // Whether the hooks are installed
   private static _initialized: boolean = false;
+
+  // The list of registered child processes.  Processes are removed from this set if they
+  // terminate on their own.
   private static _childPids: Set<number> = new Set();
 
-  private static _logDebug(message: string): void {
-    // const logLine: string = `SubprocessTerminator: [${process.pid}] ${message}`;
-    // fs.writeFileSync('trace.log', logLine + '\n', { flag: 'a' });
-    // console.log(logLine);
+  /**
+   * Registers a child process so that it will be terminated automatically if the current process
+   * is terminated.
+   */
+  public static registerChildProcess(subprocess: childProcess.ChildProcess): void {
+    if (process.platform === 'win32') {
+      // Windows works differently from other OS's:
+      // - Bad news: Calls to "process.kill(childPid, 'SIGTERM')" fail with ESRCH because the OS doesn't
+      //   really support POSIX signals
+      // - Good news: By default, child processes are terminated if their parent terminates, so we don't
+      //   really need SubprocessTerminator on Windows
+      return;
+    }
+
+    SubprocessTerminator._ensureInitialized();
+
+    // Avoid capturing subprocess in the closure
+    const childPid: number = subprocess.pid;
+    SubprocessTerminator._childPids.add(childPid);
+
+    SubprocessTerminator._logDebug(`tracking #${childPid}`);
+
+    subprocess.on('close', (code: number, signal: string): void => {
+      SubprocessTerminator._logDebug(`untracking #${childPid}`);
+      SubprocessTerminator._childPids.delete(subprocess.pid);
+    });
   }
 
+  // Install the hooks
   private static _ensureInitialized(): void {
     if (!SubprocessTerminator._initialized) {
       SubprocessTerminator._initialized = true;
@@ -27,6 +65,7 @@ export class SubprocessTerminator {
     }
   }
 
+  // Uninstall the hooks and perform cleanup
   private static _cleanupChildProcesses(): void {
     if (SubprocessTerminator._initialized) {
       SubprocessTerminator._initialized = false;
@@ -63,18 +102,10 @@ export class SubprocessTerminator {
     process.kill(process.pid, signal);
   }
 
-  public static terminateWithCurrentProcess(subprocess: childProcess.ChildProcess): void {
-    SubprocessTerminator._ensureInitialized();
-
-    // Avoid capturing subprocess in the closure
-    const childPid: number = subprocess.pid;
-    SubprocessTerminator._childPids.add(childPid);
-
-    SubprocessTerminator._logDebug(`tracking #${childPid}`);
-
-    subprocess.on('close', (code: number, signal: string): void => {
-      SubprocessTerminator._logDebug(`untracking #${childPid}`);
-      SubprocessTerminator._childPids.delete(subprocess.pid);
-    });
+  // For debugging
+  private static _logDebug(message: string): void {
+    // const logLine: string = `SubprocessTerminator: [${process.pid}] ${message}`;
+    // fs.writeFileSync('trace.log', logLine + '\n', { flag: 'a' });
+    // console.log(logLine);
   }
 }
