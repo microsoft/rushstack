42d047434aa95dd9aa14fc0b3ec716c29f7b4102
David Michon <dmichon-msft@users.noreply.github.com>
2021-02-11 13:02:53 -0800

    Revise for readability

--- apps/rush-lib/src/cli/scriptActions/BulkScriptAction.ts
+++ apps/rush-lib/src/cli/scriptActions/BulkScriptAction.ts
@@ -220,7 +220,10 @@ export class BulkScriptAction extends BaseScriptAction {
    */
   private async _runWatch(options: IExecuteInternalOptions): Promise<void> {
     const {
-      taskSelectorOptions: { selection: projectsToWatch },
+      taskSelectorOptions: {
+        buildCacheConfiguration: initialBuildCacheConfiguration,
+        selection: projectsToWatch
+      },
       stopwatch,
       terminal
     } = options;
@@ -234,13 +237,12 @@ export class BulkScriptAction extends BaseScriptAction {
       projectsToWatch
     });
 
-    let buildCacheConfiguration: BuildCacheConfiguration | undefined =
-      options.taskSelectorOptions.buildCacheConfiguration;
+    let isInitialPass: boolean = true;
 
     // Loop until Ctrl+C
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      // Report so that the developer can always see that it is in watch mode.
+      // Report so that the developer can always see that it is in watch mode as the latest console line.
       terminal.writeLine(
         `Watching for changes to ${projectsToWatch.size} ${
           projectsToWatch.size === 1 ? 'project' : 'projects'
@@ -273,7 +275,10 @@ export class BulkScriptAction extends BaseScriptAction {
       const executeOptions: IExecuteInternalOptions = {
         taskSelectorOptions: {
           ...options.taskSelectorOptions,
-          buildCacheConfiguration,
+          // Current implementation of the build cache deletes output folders before repopulating them;
+          // this tends to break `webpack --watch`, etc.
+          // Also, skipping writes to the local cache reduces CPU overhead and saves disk usage.
+          buildCacheConfiguration: isInitialPass ? initialBuildCacheConfiguration : undefined,
           // Revise down the set of projects to execute the command on
           selection,
           // Pass the PackageChangeAnalyzer from the state differ to save a bit of overhead
@@ -295,10 +300,8 @@ export class BulkScriptAction extends BaseScriptAction {
           throw err;
         }
       }
-      // Current implementation of the build cache deletes output folders before repopulating them;
-      // this tends to break `webpack --watch`, etc.
-      // Also, skipping writes to the local cache improves inner loop performance and saves disk usage.
-      buildCacheConfiguration = undefined;
+
+      isInitialPass = false;
     }
   }
 
