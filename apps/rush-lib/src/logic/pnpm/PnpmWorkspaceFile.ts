3fc9fe1f108ac0bf8a12bc0f23e9272977612c17
OneDrive Build <dmichon-msft@users.noreply.github.com>
2021-04-21 10:54:16 -0700

    Remove unused imports

--- apps/rush-lib/src/logic/pnpm/PnpmWorkspaceFile.ts
+++ apps/rush-lib/src/logic/pnpm/PnpmWorkspaceFile.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import * as os from 'os';
 import * as path from 'path';
-import { FileSystem, Sort, Text, Import } from '@rushstack/node-core-library';
+import { Sort, Text, Import } from '@rushstack/node-core-library';
 
 import { BaseWorkspaceFile } from '../base/BaseWorkspaceFile';
 import { PNPM_SHRINKWRAP_YAML_FORMAT } from './PnpmYamlCommon';
