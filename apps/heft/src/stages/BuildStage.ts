aa6d2e0e775b8879332ac7c5af47e4d994f16889
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2021-06-15 18:22:12 -0700

    Update documentation.
    Co-authored-by: Pete Gonzalez <4673363+octogonz@users.noreply.github.com>

--- apps/heft/src/stages/BuildStage.ts
+++ apps/heft/src/stages/BuildStage.ts
@@ -41,6 +41,11 @@ export type CopyFromCacheMode = 'hardlink' | 'copy';
  * @public
  */
 export class CompileSubstageHooks extends BuildSubstageHooksBase {
+  /**
+   * The `afterCompile` event is fired exactly once, after the "compile" stage completes its first operation.
+   * The "bundle" stage will not begin until all event handlers have resolved their promises.  The behavior
+   * of this event is the same in watch mode and non-watch mode.
+   */
   public readonly afterCompile: AsyncParallelHook = new AsyncParallelHook();
   public readonly afterRecompile: AsyncParallelHook = new AsyncParallelHook();
 }
