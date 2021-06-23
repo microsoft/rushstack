4385d2e20d4283154465a82bf41b178ee594e58c
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2021-06-23 13:13:43 -0700

    Run scripts in heft-jest-plugin to run tests using built plugin

---
+++ heft-plugins/heft-jest-plugin/src/scripts/setupJestPlugin.ts
@@ -0,0 +1,13 @@
+import { JestPlugin } from '../JestPlugin';
+
+import type { IRunScriptOptions, IBuildStageProperties } from '@rushstack/heft';
+
+export async function runAsync(options: IRunScriptOptions<IBuildStageProperties>): Promise<void> {
+  await JestPlugin._setupJestAsync(
+    options.scopedLogger,
+    options.heftConfiguration,
+    options.debugMode,
+    options.properties,
+    options.scriptOptions
+  );
+}
