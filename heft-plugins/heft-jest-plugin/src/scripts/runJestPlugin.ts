1f2e3d0d1854020eb0f9663f2f07180c60e11b06
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2021-06-23 19:46:24 -0700

    Update jestPluginOptions to match exactly how we want it to run

--- heft-plugins/heft-jest-plugin/src/scripts/runJestPlugin.ts
+++ heft-plugins/heft-jest-plugin/src/scripts/runJestPlugin.ts
@@ -3,9 +3,10 @@ import { IJestPluginOptions, JestPlugin } from '../JestPlugin';
 import type { IRunScriptOptions, ITestStageProperties } from '@rushstack/heft';
 
 export async function runAsync(options: IRunScriptOptions<ITestStageProperties>): Promise<void> {
-  // Use the shared config file directly to run tests
+  // Use the shared config file directly
   const jestPluginOptions: IJestPluginOptions = {
-    configurationPath: './includes/jest-shared.config.json'
+    configurationPath: './includes/jest-shared.config.json',
+    disableConfigurationModuleResolution: false
   };
   await JestPlugin._runJestAsync(
     options.scopedLogger,
--- heft-plugins/heft-jest-plugin/src/scripts/setupJestPlugin.ts
+++ heft-plugins/heft-jest-plugin/src/scripts/setupJestPlugin.ts
@@ -1,13 +1,18 @@
-import { JestPlugin } from '../JestPlugin';
+import { IJestPluginOptions, JestPlugin } from '../JestPlugin';
 
 import type { IRunScriptOptions, IBuildStageProperties } from '@rushstack/heft';
 
 export async function runAsync(options: IRunScriptOptions<IBuildStageProperties>): Promise<void> {
+  // Use the shared config file directly
+  const jestPluginOptions: IJestPluginOptions = {
+    configurationPath: './includes/jest-shared.config.json',
+    disableConfigurationModuleResolution: false
+  };
   await JestPlugin._setupJestAsync(
     options.scopedLogger,
     options.heftConfiguration,
     options.debugMode,
     options.properties,
-    options.scriptOptions
+    jestPluginOptions
   );
 }
