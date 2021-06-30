51acb5490c90ef7f83c748221d4332883ad41118
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-06-29 22:00:33 -0700

    Add a second test case

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/exportImportStarAs2/rollup.d.ts
@@ -0,0 +1,26 @@
+
+/**
+ * @public
+ */
+declare function exportedApi(): forgottenNs.ForgottenClass;
+
+/**
+ * @public
+ */
+declare class ForgottenClass {
+}
+
+declare namespace forgottenNs {
+  export {
+    ForgottenClass
+  }
+}
+
+declare namespace ns {
+  export {
+    exportedApi
+  }
+}
+export { ns }
+
+export { }
