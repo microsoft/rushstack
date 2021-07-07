406c49b7f1de7a4400fc5b87ae6561f191a7fa80
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-07-06 17:16:06 -0700

    Add a test that reproduces https://github.com/microsoft/rushstack/issues/2792

---
+++ build-tests/api-extractor-scenarios/etc/test-outputs/spanSorting/rollup.d.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Doc comment
+ * @public
+ */
+export declare class ExampleA {
+    private _member3;
+    member2(): Promise<void>;
+    member1: string;
+}
+
+export { }
---
+++ build-tests/api-extractor-scenarios/src/spanSorting/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Doc comment
+ * @public
+ */
+export class ExampleA {
+  private _member3: string = '';
+  public member2(): Promise<void> {
+    return Promise.resolve();
+  }
+  public member1: string = '';
+}
