cb5acc0735922b66e9576e20f2ad3434c0bbf9cc
Emmanuel Oluyomi <emmanueloluyomi@users.noreply.github.com>
2021-07-13 17:34:05 -0400

    Changes made to the initialization of any EnvironmentConfiguration property

--- apps/rush-lib/src/api/EnvironmentConfiguration.ts
+++ apps/rush-lib/src/api/EnvironmentConfiguration.ts
@@ -1,4 +1,3 @@
-import { InternalError } from '@rushstack/node-core-library';
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
