f4752d7e9245b397fe597283e212bc59ed944a8d
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-08-16 14:55:05 -0700

    Initial working prototype of rush-pnpm logic

--- apps/rush-lib/src/cli/RushPnpmCommandLine.ts
+++ apps/rush-lib/src/cli/RushPnpmCommandLine.ts
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import * as path from 'path';
 import colors from 'colors/safe';
+import { FileSystem, AlreadyReportedError, Executable, EnvironmentMap } from '@rushstack/node-core-library';
+import { PrintUtilities } from '@rushstack/terminal';
 
 import { RushConfiguration } from '../api/RushConfiguration';
 import { NodeJsCompatibility } from '../logic/NodeJsCompatibility';
+import { SpawnSyncReturns } from 'child_process';
 
 export interface ILaunchRushPnpmInternalOptions {
   isManaged: boolean;
@@ -31,11 +35,78 @@ export class RushPnpmCommandLine {
         rushConfiguration
       });
 
-      console.log('Success');
-      process.exitCode = 0;
+      if (!rushConfiguration) {
+        throw new Error(
+          'The "rush-pnpm" command must be executed in a folder that is under a Rush workspace folder'
+        );
+      }
 
+      if (rushConfiguration.packageManager !== 'pnpm') {
+        throw new Error(
+          'The "rush-pnpm" command requires your rush.json to be configured to use the PNPM package manager'
+        );
+      }
+
+      if (!rushConfiguration.pnpmOptions.useWorkspaces) {
+        throw new Error(
+          'The "rush-pnpm" command requires the "useWorkspaces" setting to be enabled in rush.json'
+        );
+      }
+
+      const workspaceFolder: string = rushConfiguration.commonTempFolder;
+      const workspaceFilePath: string = path.join(workspaceFolder, 'pnpm-workspace.yaml');
+
+      if (!FileSystem.exists(workspaceFilePath)) {
+        console.error(colors.red('Error: The PNPM workspace file has not been generated:'));
+        console.error(`  ${colors.red(workspaceFilePath)}\n`);
+        console.error(colors.cyan(`Do you need to run "rush install" or "rush update"?`));
+        throw new AlreadyReportedError();
+      }
+
+      if (!FileSystem.exists(rushConfiguration.packageManagerToolFilename)) {
+        console.error(colors.red('Error: The PNPM local binary has not been installed yet.'));
+        console.error('\n' + colors.cyan(`Do you need to run "rush install" or "rush update"?`));
+        throw new AlreadyReportedError();
+      }
+
+      // 0 = node.exe
+      // 1 = rush-pnpm
+      const originalArgs: string[] = process.argv.slice(2);
+
+      const pnpmArgs: string[] = [];
+
+      if (rushConfiguration.pnpmOptions.pnpmStorePath) {
+        pnpmArgs.push('--store');
+        pnpmArgs.push(rushConfiguration.pnpmOptions.pnpmStorePath);
+      }
+
+      pnpmArgs.push(...originalArgs);
+
+      const pnpmEnvironmentMap: EnvironmentMap = new EnvironmentMap(process.env);
+      pnpmEnvironmentMap.set('NPM_CONFIG_WORKSPACE_DIR', workspaceFolder);
+
+      const result: SpawnSyncReturns<string> = Executable.spawnSync(
+        rushConfiguration.packageManagerToolFilename,
+        pnpmArgs,
+        {
+          environmentMap: pnpmEnvironmentMap,
+          stdio: 'inherit'
+        }
+      );
+      if (result.error) {
+        throw new Error('Failed to invoke PNPM: ' + result.error);
+      }
+      if (result.status === null) {
+        throw new Error('Failed to invoke PNPM: Spawn completed without an exit code');
+      }
+      process.exitCode = result.status;
+
+      console.log('\nFinished rush-pnpm');
     } catch (error) {
-      console.log(colors.red('Error: ' + error.message));
+      if (!(error instanceof AlreadyReportedError)) {
+        const prefix: string = 'ERROR: ';
+        console.error('\n' + colors.red(PrintUtilities.wrapWords(prefix + error.message)));
+      }
     }
   }
 }
