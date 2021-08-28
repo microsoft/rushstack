df509355d781575060db9c41fdbc5b1c648b67d1
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2021-08-28 13:22:33 -1000

    CommonVersionsConfiguration tests

--- apps/rush-lib/src/api/test/CommonVersionsConfiguration.test.ts
+++ apps/rush-lib/src/api/test/CommonVersionsConfiguration.test.ts
@@ -14,4 +14,29 @@ describe('CommonVersionsConfiguration', () => {
     expect(configuration.xstitchPreferredVersions.get('library-2')).toEqual('1.2.3');
     expect(configuration.allowedAlternativeVersions.get('library-3')).toEqual(['^1.2.3']);
   });
+
+  it('can determine default semver style', () => {
+    const filename: string = path.resolve(__dirname, 'jsonFiles', 'common-versions.json');
+    const configuration: CommonVersionsConfiguration = CommonVersionsConfiguration.loadFromFile(filename);
+
+    const exactStyle = {
+      semverStyle: 'exact',
+      semverSymbol: ''
+    };
+    const tildeStyle = {
+      semverStyle: 'tilde',
+      semverSymbol: '~'
+    };
+    const caretStyle = {
+      semverStyle: 'caret',
+      semverSymbol: '^'
+    };
+
+    expect(configuration.getDefaultSemverForPackage('@types/node')).toEqual(exactStyle);
+    expect(configuration.getDefaultSemverForPackage('tilde-library')).toEqual(tildeStyle);
+
+    expect(configuration.getDefaultSemverForPackage('foo-package')).toEqual(caretStyle);
+    expect(configuration.getDefaultSemverForPackage('@type/typo')).toEqual(caretStyle);
+    expect(configuration.getDefaultSemverForPackage('tilde-library-2')).toEqual(caretStyle);
+  });
 });
