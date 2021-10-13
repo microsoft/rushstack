b3caa4f909a746db1d6e9ab9920418c9397ca5b6
Elliot Nelson <nelson.work@gmail.com>
2021-10-12 23:41:01 -0400

    Update apps/rush-lib/src/cli/RushStartupBanner.ts
    Co-authored-by: Ian Clanton-Thuon <iclanton@users.noreply.github.com>

--- apps/rush-lib/src/cli/RushStartupBanner.ts
+++ apps/rush-lib/src/cli/RushStartupBanner.ts
@@ -2,7 +2,6 @@
 // See LICENSE in the project root for license information.
 
 import { EOL } from 'os';
-
 import colors from 'colors/safe';
 
 import { RushConstants } from '../logic/RushConstants';
