017ba06f46238dbc2fe4b5d542964e732558ca43
Cheng Liu <chengcyber@users.noreply.github.com>
2021-10-25 10:54:54 +0800

    feat(rush-lib): clean life cycle

--- apps/rush-lib/src/pluginFramework/RushLifeCycle.ts
+++ apps/rush-lib/src/pluginFramework/RushLifeCycle.ts
@@ -1,8 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { AsyncSeriesHook, SyncBailHook, SyncHook } from 'tapable';
-import { ILogger, ILoggerOptions } from './logging/Logger';
+import { AsyncSeriesHook } from 'tapable';
 
 /**
  * @public
@@ -19,13 +18,4 @@ export class RushLifecycleHooks {
    * The hook to run when all rush plugins is initialized.
    */
   public initialize: AsyncSeriesHook<void> = new AsyncSeriesHook<void>();
-  /**
-   * A hook to mutate options passed to logger
-   */
-  public loggerOptions: SyncHook<ILoggerOptions> = new SyncHook<ILoggerOptions>(['loggerOptions']);
-  /**
-   * A hook to specify a customize logger, which implements ILogger
-   * NOTE: only the first tap which returns a non-null value will be used
-   */
-  public logger: SyncBailHook<ILoggerOptions, ILogger> = new SyncBailHook(['loggerOptions']);
 }
