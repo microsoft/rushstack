0b9163d9518634f7205de7464dfb6fecbb0f2b4c
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-11-27 19:27:05 -0800

    PR feedback: properly declare "global.___rush___rushLibModule"

--- apps/rush-lib/src/pluginFramework/PluginLoader/RushSdk.ts
+++ apps/rush-lib/src/pluginFramework/PluginLoader/RushSdk.ts
@@ -1,17 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+type RushLibModuleType = Record<string, unknown>;
+declare const global: NodeJS.Global &
+  typeof globalThis & {
+    ___rush___rushLibModule?: RushLibModuleType;
+  };
+
 export class RushSdk {
   private static _initialized: boolean = false;
 
   public static ensureInitialized(): void {
     if (!RushSdk._initialized) {
-      const rushLibModule: unknown = require('../../index');
+      const rushLibModule: RushLibModuleType = require('../../index');
 
       // The "@rushstack/rush-sdk" shim will look for this global variable to obtain
       // Rush's instance of "@microsoft/rush-lib".
-      // eslint-disable-next-line
-      (global as any).___rush_rushLibModule = rushLibModule;
+      global.___rush___rushLibModule = rushLibModule;
 
       RushSdk._initialized = true;
     }
--- libraries/rush-sdk/src/index.ts
+++ libraries/rush-sdk/src/index.ts
@@ -4,13 +4,18 @@
 import * as path from 'path';
 import { Import, IPackageJson, PackageJsonLookup } from '@rushstack/node-core-library';
 
-// SCENARIO 1:  Rush's PluginManager initializes "rush-sdk" with Rush's own instance of rush-lib.
-// eslint-disable-next-line
-let rushLibModule: any = (global as any).___rush___rushLibModule;
+type RushLibModuleType = Record<string, unknown>;
+declare const global: NodeJS.Global &
+  typeof globalThis & {
+    ___rush___rushLibModule?: RushLibModuleType;
+  };
 
+// SCENARIO 1:  Rush's PluginManager has initialized "rush-sdk" with Rush's own instance of rush-lib.
+let rushLibModule: RushLibModuleType | undefined = global.___rush___rushLibModule;
+
+// SCENARIO 2:  The project importing "rush-sdk" has installed its own instance of "rush-lib"
+// as a package.json dependency.  For example, this is used by the Jest tests for Rush plugins.
 if (rushLibModule === undefined) {
-  // SCENARIO 2:  The project importing "rush-sdk" has installed its own instance of "rush-lib"
-  // as a package.json dependency.  For example, this is used by the Jest tests for Rush plugins.
   const importingPath: string | undefined = module?.parent?.filename;
   if (importingPath !== undefined) {
     const callerPackageFolder: string | undefined =
@@ -40,28 +45,36 @@ if (rushLibModule === undefined) {
         } catch (error) {
           // If we fail to resolve it, ignore the error
         }
+
+        if (rushLibModule !== undefined) {
+          global.___rush___rushLibModule = rushLibModule;
+        }
       }
     }
   }
-
-  // SCENARIO 3:  A tool or script depends on "rush-sdk", and is meant to be used inside a monorepo folder.
-  // In this case, we can use install-run-rush.js to obtain the appropriate rush-lib version for the monorepo.
-  //
-  // NOT IMPLEMENTED YET
 }
 
+// SCENARIO 3:  A tool or script depends on "rush-sdk", and is meant to be used inside a monorepo folder.
+// In this case, we can use install-run-rush.js to obtain the appropriate rush-lib version for the monorepo.
+//
+// NOT IMPLEMENTED YET
 if (rushLibModule === undefined) {
+  // This error indicates that a project is trying to import "@rushstack/rush-sdk", but the Rush engine
+  // instance cannot be found.  If you are writing Jest tests for a Rush plugin, add "@microsoft/rush-lib"
+  // to the devDependencies for your project.
   throw new Error('The "@rushstack/rush-sdk" package context has not been initialized.');
 }
 
 // Based on TypeScript's __exportStar()
 for (const property in rushLibModule) {
   if (property !== 'default' && !exports.hasOwnProperty(property)) {
+    const rushLibModuleForClosure: RushLibModuleType = rushLibModule;
+
     // Based on TypeScript's __createBinding()
     Object.defineProperty(exports, property, {
       enumerable: true,
       get: function () {
-        return rushLibModule[property];
+        return rushLibModuleForClosure[property];
       }
     });
   }
