7fdf7b7c7b39f4350f5bbdb822a24b38ed3ea988
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2021-12-02 15:14:08 -0800

    Create the @rushstack/hashed-folder-copy-plugin project.

---
+++ webpack/hashed-folder-copy-plugin/src/HashedFolderCopyPlugin.ts
@@ -0,0 +1,352 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import * as path from 'path';
+import * as glob from 'glob';
+import crypto from 'crypto';
+import type * as webpack from 'webpack';
+import { FileSystem } from '@rushstack/node-core-library';
+import type * as EnhancedResolve from 'enhanced-resolve';
+
+interface IParserHelpers {
+  evaluateToString: (type: string) => () => void;
+  toConstantDependency: (parser: webpack.compilation.normalModuleFactory.Parser, type: string) => () => void;
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+interface ConstDependency {
+  loc: unknown;
+}
+
+interface IConstDependencyType {
+  new (expression: string, range: unknown, requireWebpackRequire: boolean): ConstDependency;
+}
+
+const ParserHelpers: IParserHelpers = require('webpack/lib/ParserHelpers');
+const ConstDependency: IConstDependencyType = require('webpack/lib/dependencies/ConstDependency');
+
+const PLUGIN_NAME: string = 'hashed-folder-copy-plugin';
+
+const EXPRESSION_NAME: string = 'requireFolder';
+
+/**
+ * Describes a source folder from which assets should be copied.
+ *
+ * @public
+ */
+export interface IRequireFolderSource {
+  /**
+   * The root under which glob patterns should be evaluated
+   */
+  globsBase: string;
+
+  /**
+   * Glob patterns matching assets to be copied
+   */
+  globPaths: string[];
+}
+
+/**
+ * @public
+ */
+export interface IRequireFolderOptions {
+  /**
+   * A set of sources to copy to the specified output folder name.
+   */
+  sources: IRequireFolderSource[];
+
+  /**
+   * The name of the folder to which assets should be copied. May contain a "[hash]" token.
+   */
+  outputFolder: string;
+}
+
+interface IAcornNode<TExpression> {
+  computed: boolean | undefined;
+  elements: IAcornNode<unknown>[];
+  key: IAcornNode<unknown> | undefined;
+  name: string | undefined;
+  properties: IAcornNode<unknown>[] | undefined;
+  type: 'Literal' | 'ObjectExpression' | 'Identifier' | 'ArrayExpression' | unknown;
+  value: TExpression;
+}
+
+interface IExpression {
+  arguments: IAcornNode<unknown>[];
+  range: unknown;
+  loc: unknown;
+}
+
+interface IExtendedModule extends webpack.Module {
+  context: string;
+  buildInfo: {
+    fileDependencies: Set<string>;
+    assets: { [assetPath: string]: IAsset };
+  };
+  addDependency(dependency: ConstDependency): void;
+}
+
+interface IExtendedParser extends webpack.compilation.normalModuleFactory.Parser {
+  state: {
+    current: IExtendedModule;
+    compilation: webpack.compilation.Compilation;
+  };
+}
+
+interface IAsset {
+  size(): number;
+  source(): Buffer;
+}
+
+interface IResolver {
+  resolveSync: typeof EnhancedResolve.sync;
+}
+
+interface ICollectAssetsOptions {
+  module: IExtendedModule;
+  compilation: webpack.compilation.Compilation;
+  requireFolderOptions: IRequireFolderOptions;
+  resolver: IResolver;
+}
+
+interface IExtendedNormalModuleFactory extends webpack.compilation.NormalModuleFactory {
+  getResolver(type: 'normal', options: { useSyncFileSystemCalls: boolean }): IResolver;
+}
+
+/**
+ * @public
+ */
+export class HashedFolderCopyPlugin implements webpack.Plugin {
+  public apply(compiler: webpack.Compiler): void {
+    compiler.hooks.normalModuleFactory.tap(PLUGIN_NAME, (normalModuleFactory) => {
+      const normalResolver: IResolver = (normalModuleFactory as IExtendedNormalModuleFactory).getResolver(
+        'normal',
+        { useSyncFileSystemCalls: true }
+      );
+      const handler: (parser: webpack.compilation.normalModuleFactory.Parser) => void = (
+        baseParser: webpack.compilation.normalModuleFactory.Parser
+      ) => {
+        const parser: IExtendedParser = baseParser as IExtendedParser;
+        parser.hooks.call.for(EXPRESSION_NAME).tap(PLUGIN_NAME, (expression: IExpression) => {
+          let errorMessage: string | undefined;
+          let requireFolderOptions: IRequireFolderOptions | undefined = undefined;
+          if (expression.arguments.length !== 1) {
+            errorMessage = `Exactly one argument is required to be passed to "${EXPRESSION_NAME}"`;
+          } else {
+            const argument: IAcornNode<IRequireFolderOptions> = expression
+              .arguments[0] as IAcornNode<IRequireFolderOptions>;
+            try {
+              requireFolderOptions = this._evaluateAcornNode(argument) as IRequireFolderOptions;
+            } catch (e) {
+              errorMessage = (e as Error).message;
+            }
+
+            if (requireFolderOptions) {
+              if (
+                !requireFolderOptions.outputFolder ||
+                typeof requireFolderOptions.outputFolder !== 'string'
+              ) {
+                errorMessage = 'The options object must have a "outputFolder" property that is a string';
+              } else if (!requireFolderOptions.sources || !Array.isArray(requireFolderOptions.sources)) {
+                errorMessage = 'The options object must have a "sources" property that is an array';
+              } else {
+                for (const source of requireFolderOptions.sources) {
+                  if (!source.globsBase || typeof source.globsBase !== 'string') {
+                    errorMessage = 'Each "sources" element must have a string "globsBase" property';
+                  } else if (
+                    !source.globPaths ||
+                    !Array.isArray(source.globPaths) ||
+                    source.globPaths.some((globPath) => !globPath || typeof globPath !== 'string')
+                  ) {
+                    errorMessage =
+                      'Each "sources" element must have a "globPaths" property that is an array of glob strings';
+                  }
+                }
+              }
+            }
+          }
+
+          const currentModule: IExtendedModule = parser.state.current;
+          const compilation: webpack.compilation.Compilation = parser.state.compilation;
+          let dependencyText: string;
+          if (!requireFolderOptions) {
+            dependencyText = this._renderError(errorMessage!);
+            compilation.errors.push(new Error(errorMessage));
+          } else {
+            dependencyText = this._collectAssets({
+              module: currentModule,
+              compilation,
+              requireFolderOptions,
+              resolver: normalResolver
+            });
+          }
+
+          const errorDependency: ConstDependency = new ConstDependency(
+            `/* ${EXPRESSION_NAME} */ ${dependencyText}`,
+            expression.range,
+            false
+          );
+          errorDependency.loc = expression.loc;
+          currentModule.addDependency(errorDependency);
+        });
+
+        parser.hooks.evaluateTypeof
+          .for(EXPRESSION_NAME)
+          .tap(PLUGIN_NAME, ParserHelpers.evaluateToString('function'));
+
+        parser.hooks.typeof
+          .for(EXPRESSION_NAME)
+          .tap(PLUGIN_NAME, ParserHelpers.toConstantDependency(parser, JSON.stringify('function')));
+      };
+
+      normalModuleFactory.hooks.parser.for('javascript/auto').tap(PLUGIN_NAME, handler);
+      normalModuleFactory.hooks.parser.for('javascript/dynamic').tap(PLUGIN_NAME, handler);
+    });
+  }
+
+  private _collectAssets(options: ICollectAssetsOptions): string {
+    const { module, compilation, requireFolderOptions, resolver } = options;
+
+    // Map of asset names (to be prepended by the outputFolder) to asset objects
+    const assetsToAdd: Map<string, IAsset> = new Map<string, IAsset>();
+
+    for (const source of requireFolderOptions.sources) {
+      const { globsBase, globPaths } = source;
+
+      let resolvedGlobsBase: string;
+      if (globsBase.startsWith('.')) {
+        // Does this look like a relative path?
+        resolvedGlobsBase = path.resolve(module.context, globsBase);
+      } else if (path.isAbsolute(globsBase)) {
+        // This is an absolute path
+        resolvedGlobsBase = globsBase;
+      } else {
+        // This looks like a NodeJS module path
+        let slashAfterPackageNameIndex: number;
+        if (globsBase.startsWith('@')) {
+          // The package name has a scope
+          slashAfterPackageNameIndex = globsBase.indexOf('/', globsBase.indexOf('/') + 1);
+        } else {
+          slashAfterPackageNameIndex = globsBase.indexOf('/');
+        }
+
+        let packageName: string;
+        let pathInsidePackage: string;
+        if (slashAfterPackageNameIndex === -1) {
+          packageName = globsBase;
+          pathInsidePackage = '';
+        } else {
+          packageName = globsBase.substr(0, slashAfterPackageNameIndex);
+          pathInsidePackage = globsBase.substr(slashAfterPackageNameIndex + 1);
+        }
+
+        let packagePath: string;
+        try {
+          const resolveResult: string = resolver.resolveSync(
+            {},
+            module.context,
+            `${packageName}/package.json`
+          );
+          packagePath = path.dirname(resolveResult);
+        } catch (e) {
+          const errorMessage: string = `Unable to resolve package "${packageName}"`;
+          compilation.errors.push(new Error(errorMessage));
+          return this._renderError(errorMessage);
+        }
+
+        resolvedGlobsBase = path.join(packagePath, pathInsidePackage);
+      }
+
+      for (const globPath of globPaths) {
+        const globResults: string[] = glob.sync(globPath, { cwd: resolvedGlobsBase });
+        for (const globResult of globResults) {
+          const globResultFullPath: string = path.resolve(resolvedGlobsBase, globResult);
+
+          if (assetsToAdd.has(globResult)) {
+            const errorMessage: string = `Two files resolve to the same output path "${globResult}"`;
+            compilation.errors.push(new Error(errorMessage));
+            return this._renderError(errorMessage);
+          }
+
+          const assetContents: Buffer = FileSystem.readFileToBuffer(globResultFullPath);
+          assetsToAdd.set(globResult, { size: () => assetContents.byteLength, source: () => assetContents });
+          module.buildInfo.fileDependencies.add(globResultFullPath);
+        }
+      }
+    }
+
+    const hash: crypto.Hash = crypto.createHash('md5');
+    // Sort the paths to maximize hash stability
+    for (const assetPath of Array.from(assetsToAdd.keys()).sort()) {
+      hash.update(assetPath);
+      hash.update(assetsToAdd.get(assetPath)!.source());
+    }
+
+    const hashTokenRegex: RegExp = /\[hash:?(\d+)?\]/g;
+    const hashDigest: string = hash.digest('hex');
+    let pathPrefix: string = requireFolderOptions.outputFolder.replace(hashTokenRegex, (match, length) => {
+      const hashLength: number | undefined = length ? Number.parseInt(length, 10) : undefined;
+      if (hashLength) {
+        return hashDigest.substr(0, hashLength);
+      } else {
+        return hashDigest;
+      }
+    });
+    pathPrefix = path.posix.join(pathPrefix, '/'); // Ensure trailing slash
+
+    if (!module.buildInfo.assets) {
+      module.buildInfo.assets = {};
+    }
+    const existingAssetNames: Set<string> = new Set<string>(Object.keys(compilation.assets));
+    for (const [assetPath, asset] of assetsToAdd.entries()) {
+      const fullAssetPath: string = path.posix.join(pathPrefix, assetPath);
+      if (existingAssetNames.has(fullAssetPath)) {
+        const errorMessage: string = `An asset with path "${fullAssetPath}" already exists`;
+        compilation.errors.push(new Error(errorMessage));
+        return this._renderError(errorMessage);
+      }
+
+      compilation.assets[fullAssetPath] = asset;
+      module.buildInfo.assets[fullAssetPath] = asset;
+    }
+
+    return `__webpack_require__.p + ${JSON.stringify(pathPrefix)}`;
+  }
+
+  private _renderError(errorMessage: string): string {
+    return `(function () { throw new Error(${JSON.stringify(errorMessage)}); })()`;
+  }
+
+  private _evaluateAcornNode(node: IAcornNode<unknown>): unknown {
+    switch (node.type) {
+      case 'Literal': {
+        return node.value;
+      }
+
+      case 'ObjectExpression': {
+        const result: Record<string, unknown> = {};
+
+        for (const property of node.properties!) {
+          const keyNode: IAcornNode<unknown> = property.key!;
+          if (keyNode.type !== 'Identifier' || keyNode.computed) {
+            throw new Error('Property keys must be non-computed identifiers');
+          }
+
+          const key: string = keyNode.name!;
+          const value: unknown = this._evaluateAcornNode(property.value as IAcornNode<unknown>);
+          result[key] = value;
+        }
+
+        return result;
+      }
+
+      case 'ArrayExpression': {
+        return node.elements.map((element) => this._evaluateAcornNode(element));
+      }
+
+      default: {
+        throw new Error(`Unsupported node type: "${node.type}"`);
+      }
+    }
+  }
+}
