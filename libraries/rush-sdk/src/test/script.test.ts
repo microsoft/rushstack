1a1d25d37fd4855aeb7ca4a549a81f202ef51373
Cheng Liu <chengcyber@users.noreply.github.com>
2021-12-25 19:10:31 +0800

    chore: add mock of rush-lib to rush-sdk test case

--- libraries/rush-sdk/src/test/script.test.ts
+++ libraries/rush-sdk/src/test/script.test.ts
@@ -4,53 +4,34 @@ import { Executable } from '@rushstack/node-core-library';
 const rushSdkPath: string = path.join(__dirname, '../../lib/index.js');
 const sandboxRepoPath: string = path.join(__dirname, './sandbox');
 const mockPackageFolder: string = path.join(sandboxRepoPath, 'mock-package');
+const mockRushLibPath: string = path.join(__dirname, './fixture/mock-rush-lib.js');
+
+const coreLibPath: string = require.resolve('@rushstack/node-core-library');
 
 describe('used in script', () => {
   it('should work when used in script', () => {
-    const result = Executable.spawnSync('node', ['-e', `console.log(require(\"${rushSdkPath}\"))`], {
-      currentWorkingDirectory: mockPackageFolder
-    });
+    const result = Executable.spawnSync(
+      'node',
+      [
+        '-e',
+        `
+const { Import } = require("${coreLibPath}");
+const originalResolveModule = Import.resolveModule;
+const mockResolveModule = (options) => {
+  if (options.baseFolderPath.includes('install-run') && options.modulePath === '@microsoft/rush-lib') {
+    return "${mockRushLibPath}";
+  }
+  return originalResolveModule(options);
+}
+Import.resolveModule = mockResolveModule;
+console.log(require("${rushSdkPath}"));
+`
+      ],
+      {
+        currentWorkingDirectory: mockPackageFolder
+      }
+    );
     expect(result.status).toBe(0);
-    expect(result.stdout.trim()).toMatchInlineSnapshot(`
-      "{
-        ApprovedPackagesPolicy: [Getter],
-        RushConfiguration: [Getter],
-        PackageManagerOptionsConfigurationBase: [Getter],
-        PnpmOptionsConfiguration: [Getter],
-        NpmOptionsConfiguration: [Getter],
-        YarnOptionsConfiguration: [Getter],
-        EnvironmentConfiguration: [Getter],
-        EnvironmentVariableNames: [Getter],
-        RushConstants: [Getter],
-        PackageManager: [Getter],
-        RushConfigurationProject: [Getter],
-        RushUserConfiguration: [Getter],
-        _RushGlobalFolder: [Getter],
-        ApprovedPackagesItem: [Getter],
-        ApprovedPackagesConfiguration: [Getter],
-        CommonVersionsConfiguration: [Getter],
-        PackageJsonEditor: [Getter],
-        PackageJsonDependency: [Getter],
-        DependencyType: [Getter],
-        RepoStateFile: [Getter],
-        LookupByPath: [Getter],
-        EventHooks: [Getter],
-        Event: [Getter],
-        ChangeManager: [Getter],
-        _LastInstallFlag: [Getter],
-        VersionPolicyDefinitionName: [Getter],
-        BumpType: [Getter],
-        LockStepVersionPolicy: [Getter],
-        IndividualVersionPolicy: [Getter],
-        VersionPolicy: [Getter],
-        VersionPolicyConfiguration: [Getter],
-        Rush: [Getter],
-        ExperimentsConfiguration: [Getter],
-        ProjectChangeAnalyzer: [Getter],
-        RushSession: [Getter],
-        RushLifecycleHooks: [Getter],
-        CredentialCache: [Getter]
-      }"
-    `);
+    expect(result.stdout.trim()).toMatchInlineSnapshot(`"{ foo: [Getter] }"`);
   });
 });
