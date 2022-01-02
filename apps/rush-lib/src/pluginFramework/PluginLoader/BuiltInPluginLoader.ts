645b5a349c65f5831e37a65d1420a96f56caa16c
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-01-01 20:54:51 -0800

    Refactors to plugin loaders.

--- apps/rush-lib/src/pluginFramework/PluginLoader/BuiltInPluginLoader.ts
+++ apps/rush-lib/src/pluginFramework/PluginLoader/BuiltInPluginLoader.ts
@@ -1,20 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { Import } from '@rushstack/node-core-library';
+import { IRushPluginConfigurationBase } from '../../api/RushPluginsConfiguration';
+import { IPluginLoaderOptions, PluginLoaderBase } from './PluginLoaderBase';
 
-import { PluginLoaderBase } from './PluginLoaderBase';
+export interface IBuiltInPluginConfiguration extends IRushPluginConfigurationBase {
+  pluginPackageFolder: string;
+}
 
 /**
- * Built-in plugin loader.
- * Loading those plugins are directly installed by Rush.
+ * @remarks
+ * Used to load plugins that are dependencies of Rush.
  */
-export class BuiltInPluginLoader extends PluginLoaderBase {
-  protected override onGetPackageFolder(): string {
-    const packageFolder: string = Import.resolvePackage({
-      baseFolderPath: __dirname,
-      packageName: this._packageName
-    });
-    return packageFolder;
+export class BuiltInPluginLoader extends PluginLoaderBase<IBuiltInPluginConfiguration> {
+  public readonly packageFolder: string;
+
+  public constructor(options: IPluginLoaderOptions<IBuiltInPluginConfiguration>) {
+    super(options);
+    this.packageFolder = options.pluginConfiguration.pluginPackageFolder;
   }
 }
