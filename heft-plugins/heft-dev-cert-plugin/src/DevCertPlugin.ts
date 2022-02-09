198b59509dc8b6fec675cff478b2b85f26a6330f
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-02-08 20:02:07 -0800

    PR feedback - cosmetic issues

--- heft-plugins/heft-dev-cert-plugin/src/DevCertPlugin.ts
+++ heft-plugins/heft-dev-cert-plugin/src/DevCertPlugin.ts
@@ -11,7 +11,8 @@ import type {
 } from '@rushstack/heft';
 import { CertificateManager, ICertificate } from '@rushstack/debug-certificate-manager';
 
-// IMPORTANT: To simplify versioning, 'webpack-dev-server' is a devDependency, not a regular dependency
+// IMPORTANT: To simplify versioning, 'webpack-dev-server' is a devDependency, not a regular dependency.
+// Thus we must always use "import type" instead of "import" in this project.
 import type { Configuration as WebpackDevServerConfig } from 'webpack-dev-server';
 
 export interface IWebpackConfigPartial {
