49404022d8a71af90be88efc5af5b3c9075c5670
Ethan Burrell <ethan.burrell@doordash.com>
2022-02-09 16:01:17 -0800

    add test case for no tag arg

--- apps/rush-lib/src/logic/test/PublishGit.test.ts
+++ apps/rush-lib/src/logic/test/PublishGit.test.ts
@@ -5,7 +5,7 @@ import { PublishGit } from "../PublishGit";
 import { PublishUtilities } from '../PublishUtilities';
 
 describe('PublishGit Test', () => {
-  it('Test Git Tag', () => {
+  it('Test git with no command line arg tag', () => {
     const execCommand = jest.fn()
     PublishUtilities.execCommand = execCommand
 
@@ -19,7 +19,36 @@ describe('PublishGit Test', () => {
       'project1',
       '2',
       undefined,
-      'new_version_tag'
+      undefined // This is undefined to simulate `rush publish ...` without --tag
+    )
+    expect(execCommand).toBeCalledTimes(1)
+    expect(execCommand).toBeCalledWith(
+      false,
+      '/usr/local/bin/git',
+      [
+        'tag',
+        '-a',
+        `project1_v2`,
+        '-m',
+        'project1 v2',
+    ])
+  })
+
+  it('Test git with command line arg tag', () => {
+    const execCommand = jest.fn()
+    PublishUtilities.execCommand = execCommand
+
+    const rushFilename: string = path.resolve(__dirname, '../../api/test/repo', 'rush-npm.json');
+    const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(rushFilename);
+    const git: Git = new Git(rushConfiguration);
+    const publishGit: PublishGit = new PublishGit(git, 'test');
+
+    publishGit.addTag(
+      false,
+      'project1',
+      '2',
+      undefined,
+      'new_version_tag' // Simulates `rush publish ... --tag new_version_tag`
     )
     expect(execCommand).toBeCalledTimes(1)
     expect(execCommand).toBeCalledWith(
