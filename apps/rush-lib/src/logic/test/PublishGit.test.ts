2978cebc2d6bec0085320c662b9c7e6e80512c23
Ethan Burrell <ethan.burrell@doordash.com>
2022-02-10 11:30:57 -0800

    respond to reviews

--- apps/rush-lib/src/logic/test/PublishGit.test.ts
+++ apps/rush-lib/src/logic/test/PublishGit.test.ts
@@ -1,25 +1,34 @@
 import * as path from 'path';
-import { RushConfiguration } from "../..";
+
+import { RushConfiguration } from "../../api/RushConfiguration";
 import { Git } from "../Git";
 import { PublishGit } from "../PublishGit";
 import { PublishUtilities } from '../PublishUtilities';
 
 describe('PublishGit Test', () => {
   let gitPath: string
+  let execCommand: jest.SpyInstance
+  let publishGit: PublishGit
+
   beforeAll(() => {
     gitPath = '/usr/bin/git'
     process.env.RUSH_GIT_BINARY_PATH = gitPath
   })
 
-  it('Test git with no command line arg tag', () => {
-    const execCommand = jest.fn()
-    PublishUtilities.execCommand = execCommand
+  beforeEach(() => {
+    execCommand = jest.spyOn(PublishUtilities, 'execCommand').mockImplementation(() => { /* no-op */ });
 
-    const rushFilename: string = path.resolve(__dirname, '../../api/test/repo', 'rush-npm.json');
+    const rushFilename: string = path.resolve(__dirname, '../../api/test/repo/rush-npm.json');
     const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(rushFilename);
     const git: Git = new Git(rushConfiguration);
-    const publishGit: PublishGit = new PublishGit(git, 'test');
+    publishGit = new PublishGit(git, 'test');
+  })
 
+  afterEach(() => {
+    execCommand.mockClear()
+  })
+
+  it('Test git with no command line arg tag', () => {
     publishGit.addTag(
       false,
       'project1',
@@ -41,20 +50,12 @@ describe('PublishGit Test', () => {
   })
 
   it('Test git with command line arg tag', () => {
-    const execCommand = jest.fn()
-    PublishUtilities.execCommand = execCommand
-
-    const rushFilename: string = path.resolve(__dirname, '../../api/test/repo', 'rush-npm.json');
-    const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(rushFilename);
-    const git: Git = new Git(rushConfiguration);
-    const publishGit: PublishGit = new PublishGit(git, 'test');
-
     publishGit.addTag(
       false,
       'project1',
       '2',
       undefined,
-      'new_version_prerelease' // Simulates `rush publish ... --prerelease-name new_version_tag`
+      'new_version_prerelease' // Simulates `rush publish ... --prerelease-name new_version_prerelease`
     )
     expect(execCommand).toBeCalledTimes(1)
     expect(execCommand).toBeCalledWith(
@@ -63,9 +64,9 @@ describe('PublishGit Test', () => {
       [
         'tag',
         '-a',
-        `project1_v2-new_version_tag`,
+        `project1_v2-new_version_prerelease`,
         '-m',
-        'project1 v2-new_version_tag',
+        'project1 v2-new_version_prerelease',
     ])
   })
 })
