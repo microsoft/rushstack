5097acade7c8e515e20e33ca8e39398aa3a41c68
Claudia Sun <zhas@microsoft.com>
2022-02-14 11:48:48 -0800

    update

--- apps/notifications/sendNotifications.js
+++ apps/notifications/sendNotifications.js
@@ -1,3 +1,11 @@
 const execSync = require('child_process').execSync;
 
-execSync('git cat-file blob refs/remotes/origin/main:common/config/notifications/notifications.json');
+const notificationResponse = execSync(
+  'git cat-file blob refs/remotes/origin/zhas/cli-notification:common/config/notifications/notifications.json'
+);
+
+const notificationJson = JSON.parse(notificationResponse.toString('utf-8'));
+notificationJson.notifications.forEach((annoucement) => {
+  annoucement.timeStamp = new Date().toISOString();
+});
+console.log(notificationJson.notifications);
