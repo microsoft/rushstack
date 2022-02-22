faab0dc7a5f753d64bf3b08a0fcd126f6f49190d
Claudia Sun <zhas@microsoft.com>
2022-02-22 14:41:03 -0800

    convert js file to ts

---
+++ apps/notifications/src/configurations.ts
@@ -0,0 +1,4 @@
+export interface IAnswers {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
---
+++ apps/notifications/src/start.ts
@@ -0,0 +1,5 @@
+import { AddNotifications } from './AddNotifications';
+
+const parser: AddNotifications = new AddNotifications();
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+parser.addNotification();
