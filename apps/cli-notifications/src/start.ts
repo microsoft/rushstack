894c50bdf72126ac3d866266302c1f4b4d03d034
Claudia Sun <zhas@microsoft.com>
2022-02-28 09:54:47 -0800

    update notifications.json fot testing

--- apps/cli-notifications/src/start.ts
+++ apps/cli-notifications/src/start.ts
@@ -1,6 +1,7 @@
 import { displayNotifications } from './DisplayNotifications';
 
 displayNotifications({
-  sourceBranch: 'refs/remotes/origin/master',
+  sourceBranch: 'refs/remotes/origin/zhas/cli-notification',
   configFilePath: 'common/config/notifications/notifications.json'
 });
+// 'git cat-file blob refs/remotes/origin/zhas/cli-notification:common/config/notifications/notifications.json'
