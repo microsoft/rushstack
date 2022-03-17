21f4993131adbf25ebb8dd39e425efe4a4954863
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-03-16 17:57:34 -0700

    npx create-serverless-stack@latest heft-serverless-stack-tutorial --language typescript

---
+++ build-tests-samples/heft-serverless-stack-tutorial/stacks/MyStack.ts
@@ -0,0 +1,19 @@
+import * as sst from '@serverless-stack/resources';
+
+export default class MyStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props);
+
+    // Create a HTTP API
+    const api = new sst.Api(this, 'Api', {
+      routes: {
+        'GET /': 'src/lambda.handler'
+      }
+    });
+
+    // Show the endpoint in the output
+    this.addOutputs({
+      ApiEndpoint: api.url
+    });
+  }
+}
