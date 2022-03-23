ca3f370377a62a3549aec9df0431bfaac79833f7
Zack Elliott <4220717+zelliott@users.noreply.github.com>
2022-03-22 18:27:53 -0700

    Trying to get path mappings picked up in tsconfig.

---
+++ build-tests/api-extractor-scenarios/src/pathMappings/index.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+// Does not work!
+// export { AbstractClass } from 'apiItemKinds';
+
+// Works!
+// export { AbstractClass } from 'lib/apiItemKinds';
