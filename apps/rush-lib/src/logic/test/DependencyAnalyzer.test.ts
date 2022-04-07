a5cd0907cc6cc3fed97e87b74be2f8c0d51b44d2
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-04-07 16:38:06 -0700

    Include test for DependencyAnalyzer.

---
+++ apps/rush-lib/src/logic/test/DependencyAnalyzer.test.ts
@@ -0,0 +1,91 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import { RushConfiguration } from '../../api/RushConfiguration';
+import { DependencyAnalyzer, IDependencyAnalysis } from '../DependencyAnalyzer';
+
+describe(DependencyAnalyzer.name, () => {
+  function getAnalysisForRepoByName(repoName: string): IDependencyAnalysis {
+    const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(
+      `${__dirname}/DependencyAnalyzerTestRepos/${repoName}/rush.json`
+    );
+    const dependencyAnalyzer: DependencyAnalyzer = DependencyAnalyzer.forRushConfiguration(rushConfiguration);
+    const analysis: IDependencyAnalysis = dependencyAnalyzer.getAnalysis();
+    return analysis;
+  }
+
+  it('correctly gets the list of dependencies in a repo without allowed alternative versions', () => {
+    const analysis: IDependencyAnalysis = getAnalysisForRepoByName('no-allowed-alternatives');
+    expect(analysis.allVersionsByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => Set {
+          "1.0.0",
+        },
+        "dep-2" => Set {
+          "1.0.0",
+        },
+        "dep-3" => Set {
+          "1.0.0",
+        },
+      }
+    `);
+    expect(analysis.implicitlyPreferredVersionByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => "1.0.0",
+        "dep-2" => "1.0.0",
+        "dep-3" => "1.0.0",
+      }
+    `);
+  });
+
+  it('correctly gets the list of dependencies in a repo with allowed alternative versions', () => {
+    const analysis: IDependencyAnalysis = getAnalysisForRepoByName('allowed-alternatives');
+    expect(analysis.allVersionsByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => Set {
+          "1.0.0",
+        },
+        "dep-2" => Set {
+          "1.0.0",
+          "2.0.0",
+        },
+        "dep-3" => Set {
+          "1.0.0",
+        },
+      }
+    `);
+    expect(analysis.implicitlyPreferredVersionByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => "1.0.0",
+        "dep-2" => "1.0.0",
+        "dep-3" => "1.0.0",
+      }
+    `);
+  });
+
+  it('correctly gets the list of dependencies in a repo with non-allowed inconsistent versions', () => {
+    const analysis: IDependencyAnalysis = getAnalysisForRepoByName(
+      'no-allowed-alternatives-with-inconsistent-versions'
+    );
+    expect(analysis.allVersionsByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => Set {
+          "1.0.0",
+        },
+        "dep-2" => Set {
+          "1.0.0",
+          "2.0.0",
+        },
+        "dep-3" => Set {
+          "1.0.0",
+        },
+      }
+    `);
+    expect(analysis.implicitlyPreferredVersionByPackageName).toMatchInlineSnapshot(`
+      Map {
+        "dep-1" => "1.0.0",
+        "dep-3" => "1.0.0",
+      }
+    `);
+  });
+});
