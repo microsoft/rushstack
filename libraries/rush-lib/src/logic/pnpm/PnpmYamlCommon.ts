684e3aca85c333f140bffcab9b2917df769f0729
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-04-08 19:00:53 -0700

    Fix a broken link.
    Co-authored-by: Pete Gonzalez <4673363+octogonz@users.noreply.github.com>

--- libraries/rush-lib/src/logic/pnpm/PnpmYamlCommon.ts
+++ libraries/rush-lib/src/logic/pnpm/PnpmYamlCommon.ts
@@ -9,7 +9,7 @@ export interface IYamlDumpOptions {
 }
 
 // This is based on PNPM's own configuration:
-// https://github.com/pnpm/pnpm-shrinkwrap/blob/main/src/write.ts
+// https://github.com/pnpm/pnpm-shrinkwrap/blob/master/src/write.ts
 export const PNPM_SHRINKWRAP_YAML_FORMAT: IYamlDumpOptions = {
   lineWidth: 1000,
   noCompatMode: true,
