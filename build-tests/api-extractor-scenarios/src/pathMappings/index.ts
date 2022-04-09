390dfeb77b8dc3cddff6df085228bbf8bb4e87cd
Zack Elliott <4220717+zelliott@users.noreply.github.com>
2022-04-09 08:22:31 -0700

    Remove pathMappings test case

--- build-tests/api-extractor-scenarios/src/pathMappings/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-// See LICENSE in the project root for license information.
-
-// Relies upon tsconfig `baseUrl` & `paths`.
-export { AbstractClass } from 'path-mapping';
-export { SimpleClass } from 'wildcard-path-mapping/apiItemKinds';
-
-// Relies upon only tsconfig `baseUrl`.
-export { RegularEnum } from 'src/apiItemKinds';
