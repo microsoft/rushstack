7121679ecdc274f53935ef9584b1f1f46c2b711e
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2022-07-18 17:49:33 -0700

    PR feedback

--- heft-plugins/heft-dev-cert-plugin/src/TrustDevCertificatePlugin.ts
+++ heft-plugins/heft-dev-cert-plugin/src/TrustDevCertificatePlugin.ts
@@ -9,39 +9,22 @@ import type {
   IHeftTaskRunHookOptions
 } from '@rushstack/heft';
 
-const PLUGIN_NAME: string = 'TrustDevCertPlugin';
+const PLUGIN_NAME: 'TrustDevCertPlugin' = 'TrustDevCertPlugin';
 
 export default class TrustDevCertificatePlugin implements IHeftTaskPlugin {
-  private _pluginName: string;
-  private _trustDevCert: boolean;
-
-  public constructor(pluginName: string = PLUGIN_NAME, trustDevCert: boolean = true) {
-    this._pluginName = pluginName;
-    this._trustDevCert = trustDevCert;
-  }
-
   public apply(taskSession: IHeftTaskSession, heftConfiguration: HeftConfiguration): void {
-    taskSession.hooks.run.tapPromise(this._pluginName, async (runOptions: IHeftTaskRunHookOptions) => {
+    taskSession.hooks.run.tapPromise(PLUGIN_NAME, async (runOptions: IHeftTaskRunHookOptions) => {
       const { logger } = taskSession;
       const certificateManager: CertificateManager = new CertificateManager();
 
-      if (this._trustDevCert) {
-        try {
-          await certificateManager.ensureCertificateAsync(
-            /* canGenerateNewCertificate: */ true,
-            logger.terminal
-          );
-          logger.terminal.writeLine('Done.');
-        } catch (err) {
-          logger.emitError(new Error(`Unable to generate or trust development certificate. Error: ${err}`));
-        }
-      } else {
-        try {
-          await certificateManager.untrustCertificateAsync(logger.terminal);
-          logger.terminal.writeLine('Done.');
-        } catch (err) {
-          logger.emitError(new Error(`Unable to untrust development certificate. Error: ${err}`));
-        }
+      try {
+        await certificateManager.ensureCertificateAsync(
+          /* canGenerateNewCertificate: */ true,
+          logger.terminal
+        );
+        logger.terminal.writeLine('Certificate successfully trusted.');
+      } catch (err) {
+        logger.emitError(new Error(`Unable to generate or trust development certificate. Error: ${err}`));
       }
     });
   }
