78ece37d94beb44d9717699a85570e35631c5450
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-07-20 10:38:55 -0700

    Move doc comment to the publicly visible signature

--- libraries/api-extractor-model/src/mixins/ApiItemContainerMixin.ts
+++ libraries/api-extractor-model/src/mixins/ApiItemContainerMixin.ts
@@ -104,6 +104,59 @@ export interface ApiItemContainerMixin extends ApiItem {
 
   /**
    * Finds all of the ApiItem's immediate and inherited members by walking up the inheritance tree.
+   *
+   * @remarks
+   *
+   * Given the following class heritage:
+   *
+   * ```
+   * export class A {
+   *   public a: number|boolean;
+   * }
+   *
+   * export class B extends A {
+   *   public a: number;
+   *   public b: string;
+   * }
+   *
+   * export class C extends B {
+   *   public c: boolean;
+   * }
+   * ```
+   *
+   * Calling `findMembersWithInheritance` on `C` will return `B.a`, `B.b`, and `C.c`. Calling the
+   * method on `B` will return `B.a` and `B.b`. And calling the method on `A` will return just
+   * `A.a`.
+   *
+   * The inherited members returned by this method may be incomplete. If so, there will be a flag
+   * on the result object indicating this as well as messages explaining the errors in more detail.
+   * Some scenarios include:
+   *
+   * - Interface extending from a type alias.
+   *
+   * - Class extending from a variable.
+   *
+   * - Extending from a declaration not present in the model (e.g. external package).
+   *
+   * - Extending from an unexported declaration (e.g. ae-forgotten-export). Common in mixin
+   *   patterns.
+   *
+   * - Unexpected runtime errors...
+   *
+   * Lastly, be aware that the types of inherited members are returned with respect to their
+   * defining class as opposed to with respect to the inheriting class. For example, consider
+   * the following:
+   *
+   * ```
+   * export class A<T> {
+   *   public a: T;
+   * }
+   *
+   * export class B extends A<number> {}
+   * ```
+   *
+   * When called on `B`, this method will return `B.a` with type `T` as opposed to type
+   * `number`, although the latter is more accurate.
    */
   findMembersWithInheritance(): IFindApiItemsResult;
 
@@ -221,58 +274,6 @@ export function ApiItemContainerMixin<TBaseClass extends IApiItemConstructor>(
       return this[_membersByName]!.get(name) || [];
     }
 
-    /**
-     * Finds all of the ApiItem's immediate and inherited members by walking up the inheritance tree.
-     *
-     * @remarks
-     *
-     * Given the following class heritage:
-     *
-     * ```
-     * export class A {
-     *   public a: number|boolean;
-     * }
-     *
-     * export class B extends A {
-     *   public a: number;
-     *   public b: string;
-     * }
-     *
-     * export class C extends B {
-     *   public c: boolean;
-     * }
-     * ```
-     *
-     * Calling `findMembersWithInheritance` on `C` will return `B.a`, `B.b`, and `C.c`. Calling the
-     * method on `B` will return `B.a` and `B.b`. And calling the method on `A` will return just
-     * `A.a`.
-     *
-     * The inherited members returned by this method may be incomplete. If so, there will be a flag
-     * on the result object indicating this as well as messages explaining the errors in more detail.
-     * Some scenarios include:
-     *
-     * - Interface extending from a type alias.
-     * - Class extending from a variable.
-     * - Extending from a declaration not present in the model (e.g. external package).
-     * - Extending from an unexported declaration (e.g. ae-forgotten-export). Common in mixin
-     *   patterns.
-     * - Unexpected runtime errors...
-     *
-     * Lastly, be aware that the types of inherited members are returned with respect to their
-     * defining class as opposed to with respect to the inheriting class. For example, consider
-     * the following:
-     *
-     * ```
-     * export class A<T> {
-     *   public a: T;
-     * }
-     *
-     * export class B extends A<number> {}
-     * ```
-     *
-     * When called on `B`, this method will return `B.a` with type `T` as opposed to type
-     * `number`, although the latter is more accurate.
-     */
     public findMembersWithInheritance(): IFindApiItemsResult {
       const messages: IFindApiItemsMessage[] = [];
       let maybeIncompleteResult: boolean = false;
