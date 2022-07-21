f1598af688881d3a765f20acaf9af6545e415985
Zack Elliott <zelliott@users.noreply.github.com>
2022-07-20 17:13:06 -0700

    Improve how diagnostic messages are printed

--- apps/api-documenter/src/documenters/MarkdownDocumenter.ts
+++ apps/api-documenter/src/documenters/MarkdownDocumenter.ts
@@ -1186,8 +1186,11 @@ export class MarkdownDocumenter {
     }
 
     // Log the messages for diagnostic purposes.
+    if (result.messages.length > 0) {
+      console.log('Diagnostic messages for findMembersWithInheritance()');
+    }
     for (const message of result.messages) {
-      console.log(`findMembersWithInheritance() diagnostic: ${message.text}`);
+      console.log(`-  ${message.text}`);
     }
 
     return result.items;
