4cd6474215b9ac6ded8a34f4422483f56fb62a2f
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2022-07-26 16:24:32 -0700

    Fix packages to build correctly using new Heft

--- build-tests/heft-parameter-plugin/src/index.ts
+++ build-tests/heft-parameter-plugin/src/index.ts
@@ -1,91 +1,52 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import {
-  HeftConfiguration,
-  HeftSession,
-  IHeftPlugin,
-  IHeftFlagParameter,
-  IHeftStringParameter,
-  IHeftIntegerParameter,
-  IHeftStringListParameter,
-  IHeftChoiceParameter,
-  IHeftChoiceListParameter,
-  IBuildStageContext,
-  ICompileSubstage
-} from '@rushstack/heft';
 import { FileSystem } from '@rushstack/node-core-library';
+import type {
+  HeftConfiguration,
+  IHeftTaskSession,
+  IHeftTaskPlugin,
+  IHeftTaskRunHookOptions,
+  CommandLineFlagParameter,
+  CommandLineStringParameter,
+  CommandLineChoiceParameter,
+  CommandLineStringListParameter,
+  CommandLineChoiceListParameter,
+  CommandLineIntegerParameter,
+  CommandLineIntegerListParameter
+} from '@rushstack/heft';
 
-class HeftParameterPlugin implements IHeftPlugin {
-  public readonly pluginName: string = 'heft-action-plugin';
+const PLUGIN_NAME: string = 'HeftParameterPlugin';
 
-  public apply(heftSession: HeftSession, heftConfiguration: HeftConfiguration): void {
-    const customParameter: IHeftFlagParameter = heftSession.commandLine.registerFlagParameter({
-      associatedActionNames: ['build', 'test', 'start'],
-      parameterLongName: '--custom-parameter',
-      description: 'Test running a custom parameter'
-    });
+export default class HeftParameterPlugin implements IHeftTaskPlugin {
+  public apply(taskSession: IHeftTaskSession, heftConfiguration: HeftConfiguration): void {
+    const customParameter: CommandLineFlagParameter = taskSession.getFlagParameter('--custom-parameter');
+    const customIntegerParameter: CommandLineIntegerParameter =
+      taskSession.getIntegerParameter('--custom-integer-parameter');
+    const customIntegerListParameter: CommandLineIntegerListParameter =
+      taskSession.getIntegerListParameter('--custom-integer-list-parameter');
+    const customStringParameter: CommandLineStringParameter =
+      taskSession.getStringParameter('--custom-string-parameter');
+    const customStringListParameter: CommandLineStringListParameter =
+      taskSession.getStringListParameter('--custom-string-list-parameter');
+    const customChoiceParameter: CommandLineChoiceParameter =
+      taskSession.getChoiceParameter('--custom-choice-parameter');
+    const customChoiceListParameter: CommandLineChoiceListParameter =
+      taskSession.getChoiceListParameter('--custom-choice-list-parameter');
 
-    const customStringParameter: IHeftStringParameter = heftSession.commandLine.registerStringParameter({
-      associatedActionNames: ['build', 'test', 'start'],
-      parameterLongName: '--custom-string-parameter',
-      description: 'Test running a custom string parameter',
-      argumentName: 'TEXT',
-      required: true
-    });
-
-    const customNumberParameter: IHeftIntegerParameter = heftSession.commandLine.registerIntegerParameter({
-      associatedActionNames: ['build', 'test', 'start'],
-      parameterLongName: '--custom-number-parameter',
-      description: 'Test running a custom number parameter',
-      argumentName: 'NUMBER'
-    });
-
-    const customStringListParameter: IHeftStringListParameter =
-      heftSession.commandLine.registerStringListParameter({
-        associatedActionNames: ['build', 'test', 'start'],
-        parameterShortName: '-x',
-        parameterLongName: '--custom-string-list-parameter',
-        description: 'Test running a custom string list parameter',
-        argumentName: 'LIST_ITEM'
-      });
-
-    const customChoiceParameter: IHeftChoiceParameter = heftSession.commandLine.registerChoiceParameter({
-      associatedActionNames: ['build', 'test', 'start'],
-      parameterLongName: '--custom-choice-parameter',
-      alternatives: ['red', 'blue'],
-      description: 'Test running a custom choice parameter'
-    });
-
-    const customChoiceListParameter: IHeftChoiceListParameter =
-      heftSession.commandLine.registerChoiceListParameter({
-        associatedActionNames: ['build', 'test', 'start'],
-        parameterShortName: '-y',
-        parameterLongName: '--custom-choice-list-parameter',
-        alternatives: ['totodile', 'jynx', 'gudetama', 'impidimp', 'wobbuffet'],
-        description: 'Test running a custom choice list parameter'
-      });
-
-    const { buildFolder } = heftConfiguration;
-
-    heftSession.hooks.build.tap(this.pluginName, (build: IBuildStageContext) => {
-      build.hooks.compile.tap(this.pluginName, (compile: ICompileSubstage) => {
-        compile.hooks.run.tapPromise(this.pluginName, async () => {
-          if (customParameter.actionAssociated && customParameter.value) {
-            const customContent: string =
-              `customIntegerParameter: ${customNumberParameter.value}\n` +
-              `customStringParameter: ${customStringParameter.value}\n` +
-              `customStringListParameter: ${customStringListParameter.value?.join(', ')}\n` +
-              `customChoiceParameter: ${customChoiceParameter.value}\n` +
-              `customChoiceListParameter: ${customChoiceListParameter.value?.join(', ')}`;
-            await FileSystem.writeFileAsync(`${buildFolder}/lib/custom_output.txt`, customContent, {
-              ensureFolderExists: true
-            });
-          }
+    taskSession.hooks.run.tapPromise(PLUGIN_NAME, async (runOptions: IHeftTaskRunHookOptions) => {
+      if (customParameter.value) {
+        const customContent: string =
+          `customIntegerParameter: ${customIntegerParameter.value}\n` +
+          `customIntegerListParameter: ${customIntegerListParameter.values?.join(', ')}\n` +
+          `customStringParameter: ${customStringParameter.value}\n` +
+          `customStringListParameter: ${customStringListParameter.values?.join(', ')}\n` +
+          `customChoiceParameter: ${customChoiceParameter.value}\n` +
+          `customChoiceListParameter: ${customChoiceListParameter.values?.join(', ')}`;
+        await FileSystem.writeFileAsync(`${taskSession.tempFolder}/custom_output.txt`, customContent, {
+          ensureFolderExists: true
         });
-      });
+      }
     });
   }
 }
-
-export default new HeftParameterPlugin();
