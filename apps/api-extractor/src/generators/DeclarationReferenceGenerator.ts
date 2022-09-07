5f969766b8881b85e1d185e41d6712a18f50efff
Zack Elliott <zelliott@users.noreply.github.com>
2022-09-07 13:46:40 -0700

    Nit fix

--- apps/api-extractor/src/generators/DeclarationReferenceGenerator.ts
+++ apps/api-extractor/src/generators/DeclarationReferenceGenerator.ts
@@ -111,15 +111,14 @@ export class DeclarationReferenceGenerator {
 
     // Otherwise, this symbol is from the current package.
     if (parent) {
-      // If we've found an exported CollectorEntity, then it's exported from the package entry point, so
-      // use Exports.
+      // If we've found a CollectorEntity that is exported from the package entry point, use Exports.
       const namedDeclaration: ts.DeclarationName | undefined = (
         declaration as ts.NamedDeclaration | undefined
       )?.name;
       if (namedDeclaration && ts.isIdentifier(namedDeclaration)) {
         const collectorEntity: CollectorEntity | undefined =
           this._collector.tryGetEntityForNode(namedDeclaration);
-        if (collectorEntity && collectorEntity.exported) {
+        if (collectorEntity && collectorEntity.exportedFromEntryPoint) {
           return Navigation.Exports;
         }
       }
