2f3136f74f82c2d34a0c574f7476b346c5205f86
Will Huang <william2958@users.noreply.github.com>
2022-10-11 12:02:16 -0700

    chore: PR comments

--- apps/lockfile-explorer-server/.eslintrc.js
+++ apps/lockfile-explorer-server/.eslintrc.js
@@ -3,8 +3,5 @@ require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
   extends: ['@rushstack/eslint-config/profile/node'],
-  parserOptions: { tsconfigRootDir: __dirname },
-  rules: {
-    'max-lines': 'off'
-  }
+  parserOptions: { tsconfigRootDir: __dirname }
 };
--- apps/lockfile-explorer/src/App.tsx
+++ apps/lockfile-explorer/src/App.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { readLockfile } from './parsing/readLockfile';
 import { LockfileEntry, LockfileEntryKind } from './parsing/LockfileEntry';
 import { LockfileViewer } from './containers/LockfileViewer';
