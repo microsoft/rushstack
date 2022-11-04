dcdb8b1c9c4a9806eeaf3c81869516f9dc615a44
Will Huang <william2958@users.noreply.github.com>
2022-11-03 18:54:01 -0700

    chore: add comment regarding dev messages

--- apps/lockfile-explorer-web/src/helpers/logDiagnosticInfo.ts
+++ apps/lockfile-explorer-web/src/helpers/logDiagnosticInfo.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+// Used to log diagnostics that may be useful when troubleshooting
+// problems with the algorithm.
+// TODO: Only print these messages in a production release
 export const logDiagnosticInfo = (...args: string[]): void => {
   console.log('Diagnostic: ', ...args);
 };
