e70a266d6319b9239dde0137502219676cb9a215
William Huang <william2958@users.noreply.github.com>
2022-11-10 11:27:14 -0800

    chore: ms copyright

--- apps/lockfile-explorer-web/src/store/slices/entrySlice.ts
+++ apps/lockfile-explorer-web/src/store/slices/entrySlice.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { createSlice, current, PayloadAction, Reducer } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, Reducer } from '@reduxjs/toolkit';
 import { LockfileEntry, LockfileEntryFilter } from '../../parsing/LockfileEntry';
 import { RootState } from '../index';
 import {
