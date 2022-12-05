411ae4e2edb4fd800b40595ef10cf602d65b5993
Sean Larkin <thelarkinn@users.noreply.github.com>
2022-12-05 03:55:33 +0000

    remove hello world command

--- apps/rushstack-vscode-extension/src/extension.ts
+++ apps/rushstack-vscode-extension/src/extension.ts
@@ -1,11 +1,9 @@
 import * as vscode from 'vscode';
-import helloWorldCommand from './commands/HelloWorldCommand';
 import rushCDCommand from './commands/RushCDCommand';
 
 export function activate(context: vscode.ExtensionContext): void {
   console.log('Congratulations, your extension "rushstack-vscode-extension" is now active!');
 
-  context.subscriptions.push(helloWorldCommand);
   context.subscriptions.push(rushCDCommand);
 }
 
