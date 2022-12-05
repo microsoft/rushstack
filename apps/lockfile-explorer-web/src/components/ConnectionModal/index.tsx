d3bcde01ac135cda29febb1aea6050cdfdfcbc83
Will Huang <william2958@users.noreply.github.com>
2022-12-05 15:28:18 -0500

    chore: add copyright

--- apps/lockfile-explorer-web/src/components/ConnectionModal/index.tsx
+++ apps/lockfile-explorer-web/src/components/ConnectionModal/index.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 import React, { useCallback, useEffect, useState } from 'react';
 import styles from './styles.scss';
 import appStyles from '../../App.scss';
