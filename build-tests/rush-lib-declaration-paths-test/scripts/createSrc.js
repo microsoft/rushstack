e81cc1cbab335526e55eb48bcf8ed9d7379f3fc2
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-12-20 15:48:18 -0800

    Add a test that ensures that all lib/**/*.d.ts files have valid imports.

---
+++ build-tests/rush-lib-declaration-paths-test/scripts/createSrc.js
@@ -0,0 +1,38 @@
+'ues strict';
+
+const { FileSystem, Import } = require('@rushstack/node-core-library');
+
+const DTS_EXTENSION = '.d.ts';
+
+module.exports = {
+  runAsync: async ({ heftConfiguration: { buildFolder } }) => {
+    const rushLibPath = Import.resolvePackage({
+      packageName: '@microsoft/rush-lib',
+      baseFolderPath: __dirname
+    });
+
+    async function* collectDtsPaths(absoluteFolderPath, relativeFolderPath) {
+      const folderItems = FileSystem.readFolderItems(absoluteFolderPath);
+      for (const folderItem of folderItems) {
+        const folderItemName = folderItem.name;
+        if (folderItem.isDirectory()) {
+          yield* collectDtsPaths(
+            `${absoluteFolderPath}/${folderItemName}`,
+            `${relativeFolderPath}/${folderItemName}`
+          );
+        } else if (folderItemName.endsWith(DTS_EXTENSION)) {
+          yield `${relativeFolderPath}/${folderItemName.slice(0, -DTS_EXTENSION.length)}`;
+        }
+      }
+    }
+
+    const indexFileLines = [];
+    for await (const dtsPath of collectDtsPaths(`${rushLibPath}/lib`, '@microsoft/rush-lib/lib')) {
+      indexFileLines.push(`import '${dtsPath}';`);
+    }
+
+    const srcFolderPath = `${buildFolder}/src`;
+    await FileSystem.ensureEmptyFolderAsync(srcFolderPath);
+    await FileSystem.writeFileAsync(`${srcFolderPath}/index.ts`, indexFileLines.join('\n'));
+  }
+};
