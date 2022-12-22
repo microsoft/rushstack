bb8a58881def2183f216fc47bb7a2c54837c2efb
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2022-12-21 16:28:13 -0800

    Expand on the comment describing what the copyEmptyModules script does.

--- libraries/rush-lib/scripts/copyEmptyModules.js
+++ libraries/rush-lib/scripts/copyEmptyModules.js
@@ -7,8 +7,21 @@ const DTS_FILE_EXTENSION = '.d.ts';
 
 module.exports = {
   runAsync: async ({ scopedLogger: { terminal }, heftConfiguration: { buildFolder } }) => {
-    // This script looks through the `lib-esnext` folder for modules that only
-    // contain types, and copies them to the `lib` folder
+    // We're using a Webpack plugin called `@rushstack/webpack-preserve-dynamic-require-plugin` to
+    // examine all of the modules that are imported by the entrypoints (index, and the start* scripts)
+    // to `rush-lib` and generate stub JS files in the `lib` folder that reference the original modules
+    // in the webpack bundle. The plugin also copies the `.d.ts` files for those modules to the `lib` folder.
+    //
+    // A limitation of this approach is that only modules that contain runtime code end up in the Webpack
+    // bundle, so only modules that contain runtime code get stubs and have their `.d.ts` files copied. This
+    // creates a problem when a `.d.ts` file references a module that doesn't have runtime code (i.e. -
+    // a `.d.ts` file that only contains types).
+    //
+    // This script looks through the `lib-esnext` folder for `.js` files that were produced by the TypeScript
+    // compiler from `.ts` files that contain no runtime code and generates stub `.js` files for them in the
+    // `lib` folder and copies the corresponding `.d.ts` files to the `lib`. This ensures that the `.d.ts`
+    // files that end up in the `lib` folder don't have any unresolved imports. This is tested by the
+    // `rush-lib-declaration-paths-test` project in the `build-tests`
 
     function stripCommentsFromJsFile(jsFileText) {
       const lines = jsFileText.split('\n');
