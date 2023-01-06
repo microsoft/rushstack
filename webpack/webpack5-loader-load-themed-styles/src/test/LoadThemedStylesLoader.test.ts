86cca133b40c306a6669eacc0115dc9e7d7ea9b4
Sean Larkin <thelarkinn@users.noreply.github.com>
2023-01-06 00:13:43 +0000

    introduce webpack5-loader-load-themed-styles

---
+++ webpack/webpack5-loader-load-themed-styles/src/test/LoadThemedStylesLoader.test.ts
@@ -0,0 +1,90 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import { LoadThemedStylesLoader } from '../LoadThemedStylesLoader';
+import LoadThemedStylesMock = require('./testData/LoadThemedStylesMock');
+import getCompiler from './testData/getCompiler';
+
+const MATCH_GENERATED_LOADER_STRING_REGEXP: RegExp = /var\sloader\s\=\srequire\(["'](.+?)["']\)/;
+const MATCH_LOADER_DOT_LOADSTYLES_FUNCTION_ASYNC_VALUE_REGEXP: RegExp = /loader\.loadStyles\(.+?,\s(.+?)\)/;
+
+describe(LoadThemedStylesLoader.name, () => {
+  beforeEach(() => {
+    LoadThemedStylesMock.loadedData = [];
+    LoadThemedStylesMock.calledWithAsync = [];
+  });
+
+  it('follows the Webpack loader interface', () => {
+    expect(LoadThemedStylesLoader).toBeDefined();
+    expect(LoadThemedStylesLoader.pitch).toBeDefined();
+
+    expect(() => new LoadThemedStylesLoader()).toThrow();
+  });
+
+  it('it inserts the resolved load-themed-styles path', async () => {
+    const stats = await getCompiler('./MockStyle1.css');
+    if (stats !== undefined) {
+      const content = stats.toJson({ source: true }).modules?.[0].source;
+      const match = MATCH_GENERATED_LOADER_STRING_REGEXP.exec(content as string);
+      const loadThemedStylesLibPath = match?.[1];
+      const expectedPath: string = require.resolve('@microsoft/load-themed-styles');
+
+      expect(loadThemedStylesLibPath).toEqual(expectedPath);
+    }
+  });
+
+  it('it allows for and inserts override of load-themed-styles path', async () => {
+    // It would error when I attempt to use the .ts mock in src/test/testData
+    // beacuse I'm not setting up default support for webpack to load .ts files.
+    const expectedPath: string = '../../../lib/test/testData/LoadThemedStylesMock';
+    const stats = await getCompiler('./MockStyle1.css', { loadedThemedStylesPath: expectedPath });
+    if (stats !== undefined) {
+      const content = stats.toJson({ source: true }).modules?.[0].source;
+      const match = MATCH_GENERATED_LOADER_STRING_REGEXP.exec(content as string);
+      const loadThemedStylesLibPath = match?.[1];
+
+      expect(loadThemedStylesLibPath).toEqual(expectedPath);
+    }
+  });
+
+  it('correctly calls loadStyles in load-themed-styles with a module reference', () => {
+    // TODO: Can we still mock this?
+  });
+
+  it('correctly calls loadStyles in load-themed-styles with a string reference', async () => {
+    // TODO: Can we still mock this?
+  });
+
+  it('correctly handles the async option set to "false"', async () => {
+    const stats = await getCompiler('./MockStyle1.css', { async: false });
+    if (stats !== undefined) {
+      const content = stats.toJson({ source: true }).modules?.[0].source;
+      const match = MATCH_LOADER_DOT_LOADSTYLES_FUNCTION_ASYNC_VALUE_REGEXP.exec(content as string);
+      const asyncValue = match?.[1];
+
+      expect(asyncValue).toEqual('false');
+    }
+  });
+
+  it('correctly handles and detects the async option not being set', async () => {
+    const stats = await getCompiler('./MockStyle1.css');
+    if (stats !== undefined) {
+      const content = stats.toJson({ source: true }).modules?.[0].source;
+      const match = MATCH_LOADER_DOT_LOADSTYLES_FUNCTION_ASYNC_VALUE_REGEXP.exec(content as string);
+      const asyncValue = match?.[1];
+
+      expect(asyncValue).toEqual('false');
+    }
+  });
+
+  it('correctly handles the async option set to "true"', async () => {
+    const stats = await getCompiler('./MockStyle1.css', { async: true });
+    if (stats !== undefined) {
+      const content = stats.toJson({ source: true }).modules?.[0].source;
+      const match = MATCH_LOADER_DOT_LOADSTYLES_FUNCTION_ASYNC_VALUE_REGEXP.exec(content as string);
+      const asyncValue = match?.[1];
+
+      expect(asyncValue).toEqual('true');
+    }
+  });
+});
