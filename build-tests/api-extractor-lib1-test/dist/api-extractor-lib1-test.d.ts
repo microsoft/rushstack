fc0f754c66b0eb3236c3068f9ff833c28d6d7696
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2023-01-10 16:50:55 -0800

    Ensure API Extract test project outputs are tracked.

---
+++ build-tests/api-extractor-lib1-test/dist/api-extractor-lib1-test.d.ts
@@ -0,0 +1,39 @@
+/**
+ * api-extractor-lib1-test
+ *
+ * @remarks
+ * This library is consumed by api-extractor-scenarios.
+ *
+ * @packageDocumentation
+ */
+
+/** @public */
+export declare class Lib1Class extends Lib1ForgottenExport {
+    readonly readonlyProperty: string;
+    writeableProperty: string;
+}
+
+declare class Lib1ForgottenExport {
+}
+
+/** @public */
+export declare type Lib1GenericType<T1, T2> = {
+    one: T1;
+    two: T2;
+};
+
+/** @alpha */
+export declare interface Lib1Interface {
+}
+
+/** @public */
+export declare namespace Lib1Namespace {
+    export namespace Inner {
+        export class X {
+        }
+    }
+    export class Y {
+    }
+}
+
+export { }
