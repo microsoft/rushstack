abb2a24a9d7a12f5739e82f260ea505cf1770fe4
William Huang <william2958@users.noreply.github.com>
2023-01-12 14:50:32 -0800

    chore: add copyright

--- apps/rush-components/src/components/ScrollArea/index.tsx
+++ apps/rush-components/src/components/ScrollArea/index.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 import React from 'react';
 import * as RadixScrollArea from '@radix-ui/react-scroll-area';
 import styles from './styles.scss';
