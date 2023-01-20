0388bd471345b96e4309eeb07fff62300d6cc307
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2023-01-19 22:36:01 -0800

    Convert rush-themed-ui to use the "library" profile from "@rushstack/heft-web-rig" and enable API Extractor

--- libraries/rush-themed-ui/.eslintrc.js
+++ libraries/rush-themed-ui/.eslintrc.js
@@ -2,6 +2,6 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/node'],
+  extends: ['@rushstack/eslint-config/profile/web-app', '@rushstack/eslint-config/mixins/react'],
   parserOptions: { tsconfigRootDir: __dirname }
 };
