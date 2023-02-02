80a03069a66f5ec1ef9c71c45311b77208de81b4
David Michon <dmichon-msft@users.noreply.github.com>
2023-02-02 15:52:19 -0800

    [rush-serve-plugin] Add workspace-level routing

--- rush-plugins/rush-serve-plugin/src/RushServePlugin.ts
+++ rush-plugins/rush-serve-plugin/src/RushServePlugin.ts
@@ -1,8 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import * as path from 'path';
+
 import type { IRushPlugin, RushSession, RushConfiguration, IPhasedCommand } from '@rushstack/rush-sdk';
 import { PLUGIN_NAME } from './constants';
+
+import type { IBaseRoutingRuleJson, IRoutingRule } from './RushProjectServeConfigFile';
+
+export interface IGlobalRoutingFolderRuleJson extends IBaseRoutingRuleJson {
+  workspaceRelativeFile: undefined;
+  workspaceRelativeFolder: string;
+}
+
+export interface IGlobalRoutingFileRuleJson extends IBaseRoutingRuleJson {
+  workspaceRelativeFile: string;
+  workspaceRelativeFolder: undefined;
+}
+
+export type IGlobalRoutingRuleJson = IGlobalRoutingFileRuleJson | IGlobalRoutingFolderRuleJson;
 
 export interface IRushServePluginOptions {
   /**
@@ -14,6 +30,8 @@ export interface IRushServePluginOptions {
    * If specified, the plugin will ensure the value is synchronized with the port used for its server.
    */
   portParameterLongName?: string | undefined;
+
+  globalRouting?: IGlobalRoutingRuleJson[];
 }
 
 /**
@@ -24,14 +42,29 @@ export class RushServePlugin implements IRushPlugin {
 
   private readonly _phasedCommands: Set<string>;
   private readonly _portParameterLongName: string | undefined;
+  private readonly _globalRoutingRules: IGlobalRoutingRuleJson[];
 
   public constructor(options: IRushServePluginOptions) {
     this._phasedCommands = new Set(options.phasedCommands);
     this._portParameterLongName = options.portParameterLongName;
+    this._globalRoutingRules = options.globalRouting ?? [];
   }
 
   public apply(rushSession: RushSession, rushConfiguration: RushConfiguration): void {
     const handler: (command: IPhasedCommand) => Promise<void> = async (command: IPhasedCommand) => {
+      const globalRoutingRules: IRoutingRule[] = this._globalRoutingRules.map(
+        (rule: IGlobalRoutingRuleJson) => {
+          const { workspaceRelativeFile, workspaceRelativeFolder } = rule;
+          const diskPath: string = workspaceRelativeFolder ?? workspaceRelativeFile;
+          return {
+            type: workspaceRelativeFile ? 'file' : 'folder',
+            diskPath: path.resolve(rushConfiguration.rushJsonFolder, diskPath),
+            servePath: rule.servePath,
+            immutable: !!rule.immutable
+          };
+        }
+      );
+
       // Defer importing the implementation until this plugin is actually invoked.
       await (
         await import('./phasedCommandHandler')
@@ -39,7 +72,8 @@ export class RushServePlugin implements IRushPlugin {
         rushSession,
         rushConfiguration,
         command,
-        portParameterLongName: this._portParameterLongName
+        portParameterLongName: this._portParameterLongName,
+        globalRoutingRules
       });
     };
 
