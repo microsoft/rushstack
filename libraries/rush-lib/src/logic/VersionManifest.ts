82acd041483f25d1caeee2dbe244f264b798d6c4
Elliot Nelson <elliot-nelson@users.noreply.github.com>
2023-04-03 21:20:21 -0400

    linting

--- libraries/rush-lib/src/logic/VersionManifest.ts
+++ libraries/rush-lib/src/logic/VersionManifest.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { FileSystem, JsonFile, JsonSchema, IPackageJson } from '@rushstack/node-core-library';
+import { JsonFile, JsonSchema, IPackageJson } from '@rushstack/node-core-library';
 
 import { RushConfiguration } from '../api/RushConfiguration';
 import { RushConfigurationProject } from '../api/RushConfigurationProject';
