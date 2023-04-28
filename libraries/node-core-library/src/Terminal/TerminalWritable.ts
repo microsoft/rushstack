d8d1fac3bca07454efc178012a869cb8f5d01a57
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2023-04-28 14:18:47 -0700

    Add missing file header

--- libraries/node-core-library/src/Terminal/TerminalWritable.ts
+++ libraries/node-core-library/src/Terminal/TerminalWritable.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 import type { ITerminal } from './ITerminal';
 import { TerminalProviderSeverity } from './ITerminalProvider';
 import { Writable, type WritableOptions } from 'stream';
