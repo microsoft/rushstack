b942fe266399d17d71ba040127841c673d30cb2a
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2023-06-08 20:32:08 -0700

    Fix broken import of dev dependency

--- apps/lockfile-explorer/src/lint.ts
+++ apps/lockfile-explorer/src/lint.ts
@@ -1,5 +1,4 @@
 import { FileSystem, JsonFile, PackageJsonLookup } from '@rushstack/node-core-library';
-import { RushConfiguration } from '@microsoft/rush-lib';
 import yaml from 'js-yaml';
 import path from 'path';
 import colors from 'colors';
@@ -8,6 +7,30 @@ import { init } from './init';
 import { IAppState } from './state';
 
 import { generateLockfileGraph, linter } from './shared';
+
+// This is based on RushConfiguration.tryFindRushJsonLocation(), but since we only need this
+// one small function, it was easier to make a copy of the code.
+function tryFindRushJsonLocation(): string | undefined {
+  let currentFolder: string = process.cwd();
+
+  // Look upwards at parent folders until we find a folder containing rush.json
+  for (let i: number = 0; i < 10; ++i) {
+    const rushJsonFilename: string = path.join(currentFolder, 'rush.json');
+
+    if (FileSystem.exists(rushJsonFilename)) {
+      return rushJsonFilename;
+    }
+
+    const parentFolder: string = path.dirname(currentFolder);
+    if (parentFolder === currentFolder) {
+      break;
+    }
+
+    currentFolder = parentFolder;
+  }
+
+  return undefined;
+}
 
 async function lintLockfile(): Promise<void> {
   const lockfileExplorerProjectRoot: string = PackageJsonLookup.instance.tryGetPackageFolderFor(__dirname)!;
@@ -20,7 +43,14 @@ async function lintLockfile(): Promise<void> {
 
   const lockfileGraph = generateLockfileGraph(doc);
   const output = linter(lockfileGraph, false) as string;
-  const workspaceRoot = path.dirname(RushConfiguration.tryFindRushJsonLocation() as string);
+  const rushJsonLocation: string | undefined = tryFindRushJsonLocation();
+  if (!rushJsonLocation) {
+    throw new Error(
+      'This command must be run in a Rush workspace. ' +
+        'A rush.json file was not found in this folder or any of its parent folders.'
+    );
+  }
+  const workspaceRoot = path.dirname(rushJsonLocation);
 
   const lintingFile = `${workspaceRoot}/lockfileLint.json`;
   await FileSystem.writeFileAsync(lintingFile, output);
