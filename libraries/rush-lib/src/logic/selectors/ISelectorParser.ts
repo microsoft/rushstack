a247e2f4c0a245b1f2f625c833613ae824d68c6e
Elliot Nelson <elliot-nelson@users.noreply.github.com>
2023-08-07 09:43:06 -0400

    Additional comments

--- libraries/rush-lib/src/logic/selectors/ISelectorParser.ts
+++ libraries/rush-lib/src/logic/selectors/ISelectorParser.ts
@@ -4,7 +4,19 @@
 import type { ITerminal } from '@rushstack/node-core-library';
 
 export interface IEvaluateSelectorOptions {
+  /**
+   * The "value" of a selector (i.e., the part after the colon ":", or the entire string
+   * if there is no scope).
+   */
   unscopedSelector: string;
+
+  /**
+   * The interface for selector scopes requires a terminal, so that selector implementations
+   * can interact with other Rush subsystems (which often require a terminal instance).
+   *
+   * If your selector produces an error, instead of writing it to terminal, consider throwing
+   * a SelectorError, so your select is usable both within Rush and in user scripts.
+   */
   terminal: ITerminal;
 
   /**
