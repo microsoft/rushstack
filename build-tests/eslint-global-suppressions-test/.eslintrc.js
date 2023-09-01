2a30c2cbabdb504f37e4d0af7526e80f56587c2a
Kevin Yang <61671317+kevin-y-ang@users.noreply.github.com>
2023-09-01 15:32:13 -0700

    fix(eslint-global-suppressions-test): remove spurious dependencies

--- build-tests/eslint-global-suppressions-test/.eslintrc.js
+++ build-tests/eslint-global-suppressions-test/.eslintrc.js
@@ -4,24 +4,5 @@ require('@rushstack/eslint-config/patch/eslint-global-suppressions');
 
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
-  parserOptions: { tsconfigRootDir: __dirname },
-  ignorePatterns: ['lib'],
-  overrides: [
-    /**
-     * Override the parser from @rushstack/eslint-config. Since the config is coming
-     * from the workspace instead of the external NPM package, the versions of ESLint
-     * and TypeScript that the config consumes will be resolved from the devDependencies
-     * of the config instead of from the eslint-7-test package. Overriding the parser
-     * ensures that the these dependencies come from the eslint-7-test package. See:
-     * https://github.com/microsoft/rushstack/issues/3021
-     */
-    {
-      files: ['**/*.ts', '*/*.tsx'],
-      parser: '@typescript-eslint/parser'
-      // parserOptions: {
-      //   tsconfigRootDir: __dirname,
-      //   project: './tsconfig.json'
-      // }
-    }
-  ]
+  parserOptions: { tsconfigRootDir: __dirname }
 };
