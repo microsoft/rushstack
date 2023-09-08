874d5edd3274d9a2748b5e2dfea26a1c633c7c89
Elliot Nelson <nelson.work@gmail.com>
2023-09-08 14:39:26 -0400

    Update libraries/rush-lib/src/api/test/RushProjectSelector.test.ts
    Co-authored-by: David Michon <dmichon@microsoft.com>

--- libraries/rush-lib/src/api/test/RushProjectSelector.test.ts
+++ libraries/rush-lib/src/api/test/RushProjectSelector.test.ts
@@ -24,7 +24,7 @@ describe(RushProjectSelector.name, () => {
       const projects: ReadonlySet<RushConfigurationProject> = await projectSelector.selectExpression(
         'project2'
       );
-      expect([...projects].map((project) => project.packageName)).toEqual(['project2']);
+      expect(Array.from(projects, (project) => project.packageName)).toEqual(['project2']);
     });
 
     it('selects a project using a detailed scope', async () => {
