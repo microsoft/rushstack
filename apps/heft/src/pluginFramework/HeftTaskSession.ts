f38bdb6350842d199c411b797441867f928f531b
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2023-09-21 10:16:27 -0700

    Remove an unused import.

--- apps/heft/src/pluginFramework/HeftTaskSession.ts
+++ apps/heft/src/pluginFramework/HeftTaskSession.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import * as path from 'path';
 import { AsyncParallelHook, AsyncSeriesWaterfallHook } from 'tapable';
 
 import type { MetricsCollector } from '../metrics/MetricsCollector';
