78777f8c78aaa5a7292c57008582698486f34e9d
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2023-09-25 15:27:48 -0700

    Add eslint profiles, mixins, and patches to rigs

---
+++ rigs/local-web-rig/profiles/app/includes/eslint/profile/web-app.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+module.exports = {
+  extends: ['@rushstack/heft-web-rig/profiles/app/includes/eslint/profile/web-app']
+};
