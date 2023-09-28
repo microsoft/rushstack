4b14d9043cb5e2a341697a1f286db362596059b7
Kevin Yang <61671317+kevin-y-ang@users.noreply.github.com>
2023-09-27 18:10:35 -0700

    fix: rename from global suppressions to bulk suppressions

--- build-tests/eslint-bulk-suppressions-test/.eslintrc.js
+++ build-tests/eslint-bulk-suppressions-test/.eslintrc.js
@@ -1,6 +1,6 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
 require('@rushstack/eslint-config/patch/modern-module-resolution');
-require('@rushstack/eslint-config/patch/eslint-global-suppressions');
+require('@rushstack/eslint-config/patch/eslint-bulk-suppressions');
 
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
