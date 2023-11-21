ffb79175538516fa7c1b850cefa90901b13b73c4
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2023-11-20 19:58:10 -0800

    TypeScript code now compiles without errors

--- eslint/eslint-bulk/src/start.ts
+++ eslint/eslint-bulk/src/start.ts
@@ -1,13 +1,13 @@
-#!/usr/bin/env node
-
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
-function findPatchPath() {
-  let eslintrcPath;
+import { execSync } from 'child_process';
+import * as process from 'process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function findPatchPath(): string {
+  let eslintrcPath: string;
   if (fs.existsSync(path.join(process.cwd(), '.eslintrc.js'))) {
     eslintrcPath = '.eslintrc.js';
   } else if (fs.existsSync(path.join(process.cwd(), '.eslintrc.cjs'))) {
@@ -19,9 +19,9 @@ function findPatchPath() {
     process.exit(1);
   }
 
-  const env = { ...process.env, ESLINT_BULK_FIND: 'true' };
+  const env: { [key: string]: string } = { ...process.env, ESLINT_BULK_FIND: 'true' };
 
-  let stdout;
+  let stdout: Buffer;
   try {
     stdout = execSync(`echo "" | eslint --stdin --config ${eslintrcPath}`, { env, stdio: 'pipe' });
   } catch (e) {
@@ -29,14 +29,14 @@ function findPatchPath() {
     process.exit(1);
   }
 
-  const startDelimiter = 'ESLINT_BULK_STDOUT_START';
-  const endDelimiter = 'ESLINT_BULK_STDOUT_END';
+  const startDelimiter: string = 'ESLINT_BULK_STDOUT_START';
+  const endDelimiter: string = 'ESLINT_BULK_STDOUT_END';
 
-  const regex = new RegExp(`${startDelimiter}(.*?)${endDelimiter}`);
-  const match = stdout.toString().match(regex);
+  const regex: RegExp = new RegExp(`${startDelimiter}(.*?)${endDelimiter}`);
+  const match: RegExpMatchArray | null = stdout.toString().match(regex);
 
   if (match) {
-    const filePath = match[1].trim();
+    const filePath: string = match[1].trim();
     return filePath;
   }
 
@@ -46,10 +46,10 @@ function findPatchPath() {
   process.exit(1);
 }
 
-const patchPath = findPatchPath();
+const patchPath: string = findPatchPath();
 try {
-  const args = process.argv.slice(2).join(' ');
-  const command = `node ${patchPath} ${args}`;
+  const args: string = process.argv.slice(2).join(' ');
+  const command: string = `node ${patchPath} ${args}`;
   execSync(command, { stdio: 'inherit' });
 } catch (e) {
   console.error(`@rushstack/eslint-bulk: Error running patch at ${patchPath}:\n` + e.message);
