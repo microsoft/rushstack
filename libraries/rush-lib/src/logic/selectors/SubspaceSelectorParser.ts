a00e6ec143b9ebe9c50d3f6eed02d5ea2788e9a2
Will Huang <william2958@users.noreply.github.com>
2023-12-29 18:49:55 -0500

    Clean up SubspaceSelectorParser

--- libraries/rush-lib/src/logic/selectors/SubspaceSelectorParser.ts
+++ libraries/rush-lib/src/logic/selectors/SubspaceSelectorParser.ts
@@ -1,6 +1,9 @@
-import { RushConfiguration } from '../../api/RushConfiguration';
-import { RushConfigurationProject } from '../../api/RushConfigurationProject';
-import { IEvaluateSelectorOptions, ISelectorParser } from './ISelectorParser';
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import type { RushConfiguration } from '../../api/RushConfiguration';
+import type { RushConfigurationProject } from '../../api/RushConfigurationProject';
+import type { IEvaluateSelectorOptions, ISelectorParser } from './ISelectorParser';
 
 export class SubspaceSelectorParser implements ISelectorParser<RushConfigurationProject> {
   private readonly _rushConfiguration: RushConfiguration;
@@ -10,9 +13,7 @@ export class SubspaceSelectorParser implements ISelectorParser<RushConfiguration
   }
 
   public async evaluateSelectorAsync({
-    unscopedSelector,
-    terminal,
-    parameterName
+    unscopedSelector
   }: IEvaluateSelectorOptions): Promise<Iterable<RushConfigurationProject>> {
     this._rushConfiguration.validateSubspaceName(unscopedSelector);
 
