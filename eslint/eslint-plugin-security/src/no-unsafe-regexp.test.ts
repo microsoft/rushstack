812f5c8140fd289fb25f6e79389d24d4d751f31d
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2024-01-15 20:57:31 -0800

    RuleTester now requires a tsconfig.json on disk for semantic rules

--- eslint/eslint-plugin-security/src/no-unsafe-regexp.test.ts
+++ eslint/eslint-plugin-security/src/no-unsafe-regexp.test.ts
@@ -5,13 +5,7 @@ import { RuleTester } from '@typescript-eslint/rule-tester';
 import { noUnsafeRegExp } from './no-unsafe-regexp';
 
 const ruleTester = new RuleTester({
-  /*
-   * The underlying API requires an absolute path. `@typescript-eslint/utils` calls `require.resolve()` on the input
-   * and forces it to be of type '@typescript-eslint/parser' but does not have a dependency on `@typescript-eslint/parser`
-   * This means that it will always fail to resolve in a strict environment.
-   * Fortunately `require.resolve(absolutePath)` returns `absolutePath`, so we can resolve it first and cast.
-   */
-  parser: require.resolve('@typescript-eslint/parser') as '@typescript-eslint/parser'
+  parser: require.resolve('@typescript-eslint/parser')
 });
 
 ruleTester.run('no-unsafe-regexp', noUnsafeRegExp, {
