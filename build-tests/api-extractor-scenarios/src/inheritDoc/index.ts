179b6acda0f7ce2e33f2834a9ea256e193d9a04b
Joshua Smithrud <Josmithr@users.noreply.github.com>
2024-01-30 20:01:01 +0000

    docs: Add release tags to test scenario

--- build-tests/api-extractor-scenarios/src/inheritDoc/index.ts
+++ build-tests/api-extractor-scenarios/src/inheritDoc/index.ts
@@ -3,20 +3,24 @@
 
 /**
  * An API item with its own documentation.
+ * @public
  */
 export const withOwnDocs = 0;
 
 /**
  * {@inheritDoc withOwnDocs}
+ * @public
  */
 export const inheritsFromInternal = 1;
 
 /**
  * {@inheritDoc nonExistentTarget}
+ * @public
  */
 export const inheritsFromInvalidInternal = 2;
 
 /**
  * {@inheritDoc some-external-library#foo}
+ * @public
  */
 export const inheritsFromExternal = 3;
