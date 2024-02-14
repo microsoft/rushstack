1e387bc0ecd55fd9ce7340bcad4dba4edaf5f384
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2024-02-13 21:48:00 -0500

    Fix an issue with tests on Windows.

--- libraries/rush-lib/src/logic/test/ProjectImpactGraphGenerator.test.ts
+++ libraries/rush-lib/src/logic/test/ProjectImpactGraphGenerator.test.ts
@@ -1,39 +1,33 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import {
-  FileSystem,
-  PackageJsonLookup,
-  StringBufferTerminalProvider,
-  Terminal
-} from '@rushstack/node-core-library';
+import { FileSystem, Path, StringBufferTerminalProvider, Terminal } from '@rushstack/node-core-library';
 import { ProjectImpactGraphGenerator } from '../ProjectImpactGraphGenerator';
 import { RushConfiguration } from '../../api/RushConfiguration';
 import { Stopwatch } from '../../utilities/Stopwatch';
 
-const PROJECT_FOLDER_PATH: string = PackageJsonLookup.instance.tryGetPackageFolderFor(__dirname)!;
-const RELATIVE_DIRNAME: string = __dirname.replace(`${PROJECT_FOLDER_PATH}/`, '');
-const SRC_DIRNAME: string = `${PROJECT_FOLDER_PATH}/src/${RELATIVE_DIRNAME.substring(
-  RELATIVE_DIRNAME.indexOf('/') + 1
-)}`;
+const NORMALIZED_DIRNAME: string = Path.convertToSlashes(__dirname);
 
 async function runTestForExampleRepoAsync(
   repoName: string,
-  testFn: (
-    repoBasePath: string,
-    generator: ProjectImpactGraphGenerator,
-    terminalProvider: StringBufferTerminalProvider
-  ) => Promise<void>
+  testFn: (generator: ProjectImpactGraphGenerator) => Promise<void>
 ): Promise<void> {
-  const repoBasePath: string = `${SRC_DIRNAME}/${repoName}`;
   const terminalProvider: StringBufferTerminalProvider = new StringBufferTerminalProvider(true);
   const terminal: Terminal = new Terminal(terminalProvider);
   const rushConfiguration: RushConfiguration = RushConfiguration.loadFromConfigurationFile(
-    `${repoBasePath}/rush.json`
+    `${NORMALIZED_DIRNAME}/${repoName}/rush.json`
   );
 
   const generator: ProjectImpactGraphGenerator = new ProjectImpactGraphGenerator(terminal, rushConfiguration);
-  await testFn(repoBasePath, generator, terminalProvider);
+  await testFn(generator);
+
+  expect({
+    output: terminalProvider.getOutput({ normalizeSpecialCharacters: true }),
+    verbose: terminalProvider.getVerbose({ normalizeSpecialCharacters: true }),
+    error: terminalProvider.getDebugOutput({ normalizeSpecialCharacters: true }),
+    warning: terminalProvider.getWarningOutput({ normalizeSpecialCharacters: true }),
+    debug: terminalProvider.getDebugOutput({ normalizeSpecialCharacters: true })
+  }).toMatchSnapshot('Terminal Output');
 }
 
 describe(ProjectImpactGraphGenerator.name, () => {
@@ -45,7 +39,7 @@ describe(ProjectImpactGraphGenerator.name, () => {
     it.each(['workspacePackages', 'packages', 'repo'])(
       'Correctly generates a project impact graph (repo: "%p")',
       async (repoName) =>
-        await runTestForExampleRepoAsync(repoName, async (repoBasePath, generator, terminalProvider) => {
+        await runTestForExampleRepoAsync(repoName, async (generator) => {
           const writeFileAsyncSpy: jest.SpyInstance = jest
             .spyOn(FileSystem, 'writeFileAsync')
             .mockImplementation();
@@ -53,18 +47,13 @@ describe(ProjectImpactGraphGenerator.name, () => {
           await generator.generateAsync();
 
           expect(writeFileAsyncSpy).toHaveBeenCalledTimes(1);
-          expect(writeFileAsyncSpy.mock.calls[0][0].replace(repoBasePath, '<REPO_ROOT>')).toMatchSnapshot(
-            'Output file path'
-          );
+          expect(
+            Path.convertToSlashes(writeFileAsyncSpy.mock.calls[0][0]).replace(
+              `${NORMALIZED_DIRNAME}/${repoName}`,
+              '<REPO_ROOT>'
+            )
+          ).toMatchSnapshot('Output file path');
           expect(writeFileAsyncSpy.mock.calls[0][1]).toMatchSnapshot('Output file data');
-
-          expect({
-            output: terminalProvider.getOutput({ normalizeSpecialCharacters: true }),
-            verbose: terminalProvider.getVerbose({ normalizeSpecialCharacters: true }),
-            error: terminalProvider.getDebugOutput({ normalizeSpecialCharacters: true }),
-            warning: terminalProvider.getWarningOutput({ normalizeSpecialCharacters: true }),
-            debug: terminalProvider.getDebugOutput({ normalizeSpecialCharacters: true })
-          }).toMatchSnapshot('Terminal Output');
         })
     );
   });
@@ -73,7 +62,7 @@ describe(ProjectImpactGraphGenerator.name, () => {
     it.each(['workspacePackages'])(
       'Reports if the project-impact-graph.yaml file is missing (repo: "%p")',
       async (repoName) =>
-        await runTestForExampleRepoAsync(repoName, async (repoBasePath, generator, terminalProvider) => {
+        await runTestForExampleRepoAsync(repoName, async (generator) => {
           await expect(generator.validateAsync()).resolves.toBe(false);
         })
     );
