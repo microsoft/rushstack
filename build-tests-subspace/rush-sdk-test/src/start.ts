1ad5b9b596c55d58de58cb075fae27188f49e058
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2024-02-27 18:38:47 -0800

    Some follow-ups to #4530.

--- build-tests-subspace/rush-sdk-test/src/start.ts
+++ build-tests-subspace/rush-sdk-test/src/start.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+/* eslint-disable no-console */
+
 console.log('rush-sdk-test loading Rush configuration...');
 
 // Important: Since we're calling an internal API, we need to use the unbundled .d.ts files
