ebb1d90654061bf958a10c19cc0e16daa3d36b47
Ben Papp <bpapp-ms@users.noreply.github.com>
2024-03-14 14:04:04 -0400

    Remove parsing of minifier terserOptions.sourceMap

--- libraries/module-minifier/src/MinifySingleFile.ts
+++ libraries/module-minifier/src/MinifySingleFile.ts
@@ -26,7 +26,6 @@ export async function minifySingleFileAsync(
       format: rawFormat,
       output: rawOutput,
       mangle: originalMangle,
-      sourceMap: sourceMapOptions,
       ...remainingOptions
     } = terserOptions;
 
@@ -50,18 +49,12 @@ export async function minifySingleFileAsync(
       mangle.reserved = mangle.reserved ? externals.concat(mangle.reserved) : externals;
     }
 
-    // SourceMap is only generated if nameForMap is provided- ModuleMinifierPlugin will override terserOptions.sourceMap's boolean value
+    // SourceMap is only generated if nameForMap is provided- overrides terserOptions.sourceMap
     if (nameForMap) {
       finalOptions.sourceMap = {
-        includeSources: true
+        includeSources: true,
+        asObject: true
       };
-      if (typeof sourceMapOptions !== 'boolean' && sourceMapOptions !== undefined) {
-        // Include any provided sourceMap options from minifier
-        // Note that any provided terserOptions.sourceMap options must be aligned with devtool or SourceMapDevToolPlugin configuration
-        finalOptions.sourceMap = { ...finalOptions.sourceMap, ...sourceMapOptions };
-      }
-      // Always generate source maps as an object rather than a string
-      finalOptions.sourceMap.asObject = true;
     } else {
       finalOptions.sourceMap = false;
     }
