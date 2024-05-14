35fb928fc6912bc9f74d91b62090a6b8b5949aea
qun <L-Qun@users.noreply.github.com>
2024-05-14 11:15:02 +0000

    chore: remove useless logic

--- apps/lockfile-explorer/src/utils.ts
+++ apps/lockfile-explorer/src/utils.ts
@@ -3,17 +3,6 @@
 
 import * as dp from '@pnpm/dependency-path';
 
-/**
- * Example: "" --> ""
- * Example: "" --> ""
- */
-export function convertLockfileV5DepPathToV6DepPath(newDepPath: string): string {
-  if (newDepPath.startsWith('file:')) return newDepPath;
-  const index = newDepPath.indexOf('/', 2);
-  if (newDepPath.includes('(') && index > dp.indexOfPeersSuffix(newDepPath)) return newDepPath;
-  return `${newDepPath.substring(0, index)}@${newDepPath.substring(index + 1)}`;
-}
-
 export function convertLockfileV6DepPathToV5DepPath(newDepPath: string): string {
   if (!newDepPath.includes('@', 2) || newDepPath.startsWith('file:')) return newDepPath;
   const index = newDepPath.indexOf('@', newDepPath.indexOf('/@') + 2);
