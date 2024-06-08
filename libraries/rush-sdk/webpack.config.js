a9d4568dffeda1fd64a2f1cd9763d96fdd054f4c
Cheng Liu <chengcyber@users.noreply.github.com>
2024-06-08 01:29:00 -0700

    feat: load rush-lib from rush global folder

---
+++ libraries/rush-sdk/webpack.config.js
@@ -0,0 +1,76 @@
+/* eslint-env es6 */
+'use strict';
+
+const webpack = require('webpack');
+const { PackageJsonLookup } = require('@rushstack/node-core-library');
+const { PreserveDynamicRequireWebpackPlugin } = require('@rushstack/webpack-preserve-dynamic-require-plugin');
+
+module.exports = () => {
+  const packageJson = PackageJsonLookup.loadOwnPackageJson(__dirname);
+
+  const externalDependencyNames = new Set([...Object.keys(packageJson.dependencies || {})]);
+
+  // Explicitly exclude @microsoft/rush-lib
+  externalDependencyNames.delete('@microsoft/rush-lib');
+
+  return {
+    context: __dirname,
+    mode: 'development', // So the output isn't minified
+    devtool: 'source-map',
+    entry: {
+      index: `${__dirname}/lib-commonjs/index.js`,
+      loader: `${__dirname}/lib-commonjs/loader.js`
+    },
+    output: {
+      path: `${__dirname}/lib-shim`,
+      filename: '[name].js',
+      chunkFilename: 'chunks/[name].js',
+      library: {
+        type: 'commonjs2'
+      }
+    },
+    target: 'node',
+    plugins: [
+      new PreserveDynamicRequireWebpackPlugin(),
+      new webpack.ids.DeterministicModuleIdsPlugin({
+        maxLength: 6
+      })
+    ],
+    externals: [
+      ({ request }, callback) => {
+        let packageName;
+        let firstSlashIndex = request.indexOf('/');
+        if (firstSlashIndex === -1) {
+          packageName = request;
+        } else if (request.startsWith('@')) {
+          let secondSlash = request.indexOf('/', firstSlashIndex + 1);
+          if (secondSlash === -1) {
+            packageName = request;
+          } else {
+            packageName = request.substring(0, secondSlash);
+          }
+        } else {
+          packageName = request.substring(0, firstSlashIndex);
+        }
+
+        if (externalDependencyNames.has(packageName)) {
+          callback(null, `commonjs ${request}`);
+        } else {
+          callback();
+        }
+      }
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        minChunks: 1,
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'initial'
+          }
+        }
+      }
+    }
+  };
+};
