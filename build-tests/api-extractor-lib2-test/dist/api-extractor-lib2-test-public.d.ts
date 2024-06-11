f93d2d730abd5d5a8696251374bd1b122934f7a6
Joshua Smithrud <Josmithr@users.noreply.github.com>
2024-06-11 00:54:58 +0000

    test: Update collateral with corresponding type-rollups

---
+++ build-tests/api-extractor-lib2-test/dist/api-extractor-lib2-test-public.d.ts
@@ -0,0 +1,19 @@
+/**
+ * api-extractor-lib2-test
+ *
+ * @remarks
+ * This library is consumed by api-extractor-scenarios.
+ *
+ * @packageDocumentation
+ */
+
+/* Excluded from this release type: DefaultClass */
+
+/** @public */
+export declare class Lib2Class {
+    prop: number;
+}
+
+/* Excluded from this release type: Lib2Interface */
+
+export { }
