afd250e70c4190e31a886bc76c0dfcb9d90a4d37
Lincoln <68707557+L-Qun@users.noreply.github.com>
2024-06-13 11:03:21 +0800

    Update apps/lockfile-explorer/src/constants/common.ts
    Co-authored-by: Ian Clanton-Thuon <iclanton@users.noreply.github.com>

--- apps/lockfile-explorer/src/constants/common.ts
+++ apps/lockfile-explorer/src/constants/common.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-export const enum LockfileExplorerConfig {
-  FileName = 'lockfile-lint.json'
-}
+export const LOCKFILE_LINT_JSON_FILENAME: 'lockfile-lint.json' = 'lockfile-lint.json';
+
