e11978a2e327325f44728235a7430f83723197c8
qun <L-Qun@users.noreply.github.com>
2024-06-13 07:46:39 +0000

    migrate init to action

---
+++ apps/lockfile-explorer/src/cli/lint/LintCommandLineParser.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import { ConsoleTerminalProvider, type ITerminal, Terminal } from '@rushstack/terminal';
+import { CommandLineParser } from '@rushstack/ts-command-line';
+import { InitAction } from './actions/InitAction';
+
+const LINT_TOOL_FILENAME: 'lockfile-lint' = 'lockfile-lint';
+
+export class LintCommandLineParser extends CommandLineParser {
+  public readonly globalTerminal: ITerminal;
+  private readonly _terminalProvider: ConsoleTerminalProvider;
+
+  public constructor() {
+    super({
+      toolFilename: LINT_TOOL_FILENAME,
+      toolDescription: 'lockfile-lint is a tool for linting lockfiles.'
+    });
+
+    this._terminalProvider = new ConsoleTerminalProvider();
+    this.globalTerminal = new Terminal(this._terminalProvider);
+
+    this._populateActions();
+  }
+
+  private _populateActions(): void {
+    this.addAction(new InitAction(this));
+  }
+}
--- apps/lockfile-explorer/src/start-lint.ts
+++ apps/lockfile-explorer/src/start-lint.ts
@@ -1,21 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import yargs from 'yargs';
-import { Colorize } from '@rushstack/terminal';
+import { LintCommandLineParser } from './cli/lint/LintCommandLineParser';
 
-import { lintCommand } from './commands/lint';
-import { initCommand } from './commands/init';
+const parser: LintCommandLineParser = new LintCommandLineParser();
 
-yargs
-  .scriptName('lockfile-lint')
-  .command(lintCommand)
-  .command(initCommand)
-  // --debug
-  .boolean('debug')
-  .alias('help', 'h')
-  .parseAsync()
-  .catch((error) => {
-    console.log(Colorize.red('ERROR: ' + error.message));
-    process.exit(1);
-  });
+parser.executeAsync().catch(console.error); // CommandLineParser.executeAsync() should never reject the promise
