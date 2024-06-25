86ca9e2675ddceaf45028845d89af06a46604058
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2024-06-24 17:59:59 -0700

    Avoid double-printing errors when AlreadyReportedError is thrown

---
+++ libraries/ts-command-line/src/TypeUuidLite.ts
@@ -0,0 +1,29 @@
+const classPrototypeUuidSymbol: symbol = Symbol.for('TypeUuid.classPrototypeUuid');
+
+export const uuidAlreadyReportedError: string = 'f26b0640-a49b-49d1-9ead-1a516d5920c7';
+
+// Avoid a dependency on node-core-library to access just this one API:
+export class TypeUuid {
+  /**
+   * Returns true if the `targetObject` is an instance of a JavaScript class that was previously
+   * registered using the specified `typeUuid`.  Base classes are also considered.
+   */
+  public static isInstanceOf(targetObject: unknown, typeUuid: string): boolean {
+    if (targetObject === undefined || targetObject === null) {
+      return false;
+    }
+
+    let objectPrototype: {} = Object.getPrototypeOf(targetObject);
+    while (objectPrototype !== undefined && objectPrototype !== null) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const registeredUuid: string = (objectPrototype as any)[classPrototypeUuidSymbol];
+      if (registeredUuid === typeUuid) {
+        return true;
+      }
+      // Walk upwards an examine base class prototypes
+      objectPrototype = Object.getPrototypeOf(objectPrototype);
+    }
+
+    return false;
+  }
+}
