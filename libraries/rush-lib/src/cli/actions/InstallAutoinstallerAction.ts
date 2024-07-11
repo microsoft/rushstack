d4cff21d5746a1fb0bf9a3c4b846279ddf75bf96
qun <L-Qun@users.noreply.github.com>
2024-07-11 04:44:42 +0000

    feat: abstract the BaseAutoinstallerAction class

--- libraries/rush-lib/src/cli/actions/InstallAutoinstallerAction.ts
+++ libraries/rush-lib/src/cli/actions/InstallAutoinstallerAction.ts
@@ -1,34 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import type { IRequiredCommandLineStringParameter } from '@rushstack/ts-command-line';
-import type { ITerminal } from '@rushstack/terminal';
-
-import { BaseRushAction } from './BaseRushAction';
 import type { RushCommandLineParser } from '../RushCommandLineParser';
 import { Autoinstaller } from '../../logic/Autoinstaller';
+import { BaseAutoinstallerAction } from './BaseAutoinstallerAction';
 
-export class InstallAutoinstallerAction extends BaseRushAction {
-  private readonly _name: IRequiredCommandLineStringParameter;
-  private readonly _terminal: ITerminal;
-
+export class InstallAutoinstallerAction extends BaseAutoinstallerAction {
   public constructor(parser: RushCommandLineParser) {
-    super({
+    super(parser, {
       actionName: 'install-autoinstaller',
       summary: 'Install autoinstaller package dependencies',
       documentation: 'Use this command to install dependencies for an autoinstaller folder.',
       parser
     });
-
-    this._name = this.defineStringParameter({
-      parameterLongName: '--name',
-      argumentName: 'AUTOINSTALLER_NAME',
-      required: true,
-      description:
-        'The name of the autoinstaller, which must be one of the folders under common/autoinstallers.'
-    });
-
-    this._terminal = parser.terminal;
   }
 
   protected async runAsync(): Promise<void> {
