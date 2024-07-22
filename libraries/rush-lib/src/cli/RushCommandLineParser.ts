b41de43c6e4ec98efca604625c6c4475e49877ec
Chao Guo <10736839+g-chao@users.noreply.github.com>
2024-07-21 21:42:42 -0700

    feat: add rush alert cli draft

--- libraries/rush-lib/src/cli/RushCommandLineParser.ts
+++ libraries/rush-lib/src/cli/RushCommandLineParser.ts
@@ -60,6 +60,7 @@ import { PhasedScriptAction } from './scriptActions/PhasedScriptAction';
 import type { IBuiltInPluginConfiguration } from '../pluginFramework/PluginLoader/BuiltInPluginLoader';
 import { InitSubspaceAction } from './actions/InitSubspaceAction';
 import { RushAlerts } from '../utilities/RushAlerts';
+import { AlertAction } from './actions/AlertAction';
 
 /**
  * Options for `RushCommandLineParser`.
@@ -289,6 +290,7 @@ export class RushCommandLineParser extends CommandLineParser {
     try {
       // Alphabetical order
       this.addAction(new AddAction(this));
+      this.addAction(new AlertAction(this));
       this.addAction(new ChangeAction(this));
       this.addAction(new CheckAction(this));
       this.addAction(new DeployAction(this));
