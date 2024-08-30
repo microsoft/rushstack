961e18cf66c0eef0d0a7e669435c263be36b850d
qun <L-Qun@users.noreply.github.com>
2024-08-30 07:21:28 +0000

    fix ci

--- libraries/rush-lib/src/logic/pnpm/test/PnpmShrinkwrapFile.test.ts
+++ libraries/rush-lib/src/logic/pnpm/test/PnpmShrinkwrapFile.test.ts
@@ -218,7 +218,7 @@ describe(PnpmShrinkwrapFile.name, () => {
             project,
             project.rushConfiguration.defaultSubspace
           )
-        ).resolves.toBe(false);
+        ).resolves.toBe(true);
       });
     });
   });
