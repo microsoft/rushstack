31860438d4e49c5f41a83a44b78f5f4a4c7d795a
Bart van den Ende <bartvandenende-wm@users.noreply.github.com>
2024-10-01 09:20:11 +0200

    feat: Add support for new d.ts extension format when using TS moduleresolution 'bundler' or 'nodenext'

---
+++ apps/api-extractor/src/api/test/ExtractorConfig.test.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import { ExtractorConfig } from '../ExtractorConfig';
+
+describe('ExtractorConfig', () => {
+  describe('hasDtsFileExtension', () => {
+    it.each([
+      ['test.ts', false],
+      ['test.cts', false],
+      ['test.mts', false],
+      ['test.d.ts', true],
+      ['test.d.mts', true],
+      ['test.d.cts', true],
+      ['test.css', false],
+      ['test.css.ts', false],
+      ['test.css.d.ts', true],
+      ['test.json', false],
+      ['test.json.ts', false],
+      ['test.d.css.ts', true],
+      ['test.json.d.ts', true],
+      ['test.d.json.ts', true]
+    ])('file "%s" has dts file extension equals "%s"', (file, expected) => {
+      const result = ExtractorConfig.hasDtsFileExtension(file);
+      expect(result).toEqual(expected);
+    });
+  });
+});
