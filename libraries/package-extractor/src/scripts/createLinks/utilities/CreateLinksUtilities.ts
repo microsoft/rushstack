0038932a5e23704418ab3cf8ec6b47778c407854
Daniel <D4N14L@users.noreply.github.com>
2024-10-23 05:25:34 +0000

    Change to use path.join in specific place that gets logged

--- libraries/package-extractor/src/scripts/createLinks/utilities/CreateLinksUtilities.ts
+++ libraries/package-extractor/src/scripts/createLinks/utilities/CreateLinksUtilities.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import path from 'node:path';
 import { FileSystem } from '@rushstack/node-core-library';
 import type { IExtractorMetadataJson } from '../../../PackageExtractor';
 import { EXTRACTOR_METADATA_FILENAME } from '../../../PathConstants';
@@ -10,7 +11,7 @@ export function getTargetRootFolder(): string {
   // This script is bundled and dropped into the extraction target folder, so we can use the __dirname
   // of this script combined with the relative path to determine the target root folder. The relative
   // path is updated in the script when the script is included in the extraction
-  return `${__dirname}/${TARGET_ROOT_SCRIPT_RELATIVE_PATH}`;
+  return path.join(__dirname, TARGET_ROOT_SCRIPT_RELATIVE_PATH);
 }
 
 export async function getExtractorMetadataAsync(): Promise<IExtractorMetadataJson> {
