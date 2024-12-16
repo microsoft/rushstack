bab00974b0d11a9794e45c511db5a94cda027103
He Yunfeng <11866967+sherlockfeng@users.noreply.github.com>
2024-12-16 11:49:46 +0800

    Update libraries/rush-lib/src/cli/actions/ScanAction.ts
    Co-authored-by: Ian Clanton-Thuon <iclanton@users.noreply.github.com>

--- libraries/rush-lib/src/cli/actions/ScanAction.ts
+++ libraries/rush-lib/src/cli/actions/ScanAction.ts
@@ -45,7 +45,7 @@ export class ScanAction extends BaseConfiglessRushAction {
         ` declaring them as dependencies in the package.json file.  Such "phantom dependencies"` +
         ` can cause problems.  Rush and PNPM use symlinks specifically to protect against phantom dependencies.` +
         ` These protections may cause runtime errors for existing projects when they are first migrated into` +
-        ` a Rush monorepo.  The "rush scan" command is a handy tool for fixing these errors. It default scans the "./src"` +
+        ` a Rush monorepo.  The "rush scan" command is a handy tool for fixing these errors. By default, it scans the "./src"` +
         ` and "./lib" folders for import syntaxes such as "import __ from '__'", "require('__')",` +
         ` and "System.import('__').  It prints a report of the referenced packages.  This heuristic is` +
         ` not perfect, but it can save a lot of time when migrating projects.`,
