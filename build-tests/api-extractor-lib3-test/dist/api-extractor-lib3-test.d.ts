05bae997da442e63b58d006b83225bc8e2812d35
Joshua Smithrud <Josmithr@users.noreply.github.com>
2025-01-14 00:50:40 +0000

    test: Update test case to leverage new functionality

--- build-tests/api-extractor-lib3-test/dist/api-extractor-lib3-test.d.ts
+++ build-tests/api-extractor-lib3-test/dist/api-extractor-lib3-test.d.ts
@@ -11,8 +11,15 @@ import { Lib1Class } from 'api-extractor-lib1-test';
 
 export { Lib1Class }
 
-/** @public */
+/**
+ * @customModifierTag
+ * @public
+ */
 export declare class Lib3Class {
+    /**
+     * I am a documented property!
+     * @customBlockTag My docs include a custom block tag!
+     */
     prop: boolean;
 }
 
