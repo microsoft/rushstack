3bc5c961a61971220e6645dadc532daf97fbbcf7
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-02-18 17:16:22 -0500

    Add a test for doc model trimming.

---
+++ build-tests/api-extractor-test-05/dist/api-extractor-test-05-beta.d.ts
@@ -0,0 +1,23 @@
+/**
+ * api-extractor-test-05
+ *
+ * Test trimming of @internal and @alpha from doc model.
+ *
+ * @packageDocumentation
+ */
+
+/* Excluded from this release type: alpha */
+
+/**
+ * @beta
+ */
+export declare const beta: string;
+
+/* Excluded from this release type: _internal */
+
+/**
+ * @public
+ */
+export declare const publick: string;
+
+export { }
