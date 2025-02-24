9cfe80c395a85d3c22c97752c8236a757442577d
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-02-24 16:58:14 -0500

    Clean up build for localization-plugin-test-* projects.

--- build-tests/localization-plugin-test-03/src/indexB.ts
+++ build-tests/localization-plugin-test-03/src/indexB.ts
@@ -1,11 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 import strings3 from './strings3.resx.json';
 import strings6 from './strings7.resjson';
 
 const strings4: string = require('./strings4.loc.json');
 
+// eslint-disable-next-line no-console
 console.log(strings3.string1);
+// eslint-disable-next-line no-console
 console.log(strings3.string2);
 
+// eslint-disable-next-line no-console
 console.log(strings4);
 
+// eslint-disable-next-line no-console
 console.log(strings6.string);
