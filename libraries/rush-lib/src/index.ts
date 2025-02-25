01ca318b2eacd362740daaf1941a703eea34bab9
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-02-24 23:10:24 -0500

    Fix up API reports and rollups.

--- libraries/rush-lib/src/index.ts
+++ libraries/rush-lib/src/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+/// <reference types="node" preserve="true" />
+
 /**
  * A library for writing scripts that interact with the {@link https://rushjs.io/ | Rush} tool.
  * @packageDocumentation
