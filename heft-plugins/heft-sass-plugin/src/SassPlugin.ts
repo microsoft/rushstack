2cf7e7ee67ac4ab5f98ace4dde3ceb5c2f721d71
David Michon <dmichon-msft@users.noreply.github.com>
2025-03-01 00:32:13 +0000

    [heft-sass] (BREAKING CHANGE) Emit .js shims
    Remove `preserveSCSSExtension` option.

--- heft-plugins/heft-sass-plugin/src/SassPlugin.ts
+++ heft-plugins/heft-sass-plugin/src/SassPlugin.ts
@@ -1,21 +1,30 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
+import path from 'node:path';
 
 import type {
   HeftConfiguration,
   IHeftTaskSession,
   IHeftPlugin,
-  IScopedLogger,
   IHeftTaskRunHookOptions,
   IHeftTaskRunIncrementalHookOptions,
   IWatchedFileState,
   ConfigurationFile
 } from '@rushstack/heft';
 
-import { type ISassConfiguration, SassProcessor } from './SassProcessor';
+import { type ICssOutputFolder, type ISassProcessorOptions, SassProcessor } from './SassProcessor';
 import sassConfigSchema from './schemas/heft-sass-plugin.schema.json';
 
-export interface ISassConfigurationJson extends Partial<ISassConfiguration> {}
+export interface ISassConfigurationJson {
+  srcFolder?: string;
+  generatedTsFolder?: string;
+  cssOutputFolders?: (string | ICssOutputFolder)[];
+  secondaryGeneratedTsFolders?: string[];
+  exportAsDefault?: boolean;
+  fileExtensions?: string[];
+  nonModuleFileExtensions?: string[];
+  silenceDeprecations?: string[];
+}
 
 const PLUGIN_NAME: 'sass-plugin' = 'sass-plugin';
 const SASS_CONFIGURATION_LOCATION: string = 'config/sass.json';
@@ -27,130 +36,120 @@ const SASS_CONFIGURATION_FILE_SPECIFICATION: ConfigurationFile.IProjectConfigura
   };
 
 export default class SassPlugin implements IHeftPlugin {
-  private _sassConfiguration: ISassConfiguration | undefined;
-  private _sassProcessor: SassProcessor | undefined;
-
   /**
    * Generate typings for Sass files before TypeScript compilation.
    */
   public apply(taskSession: IHeftTaskSession, heftConfiguration: HeftConfiguration): void {
+    const { numberOfCores, slashNormalizedBuildFolderPath, rigConfig } = heftConfiguration;
+    const { logger, tempFolderPath } = taskSession;
+
+    const { terminal } = logger;
+
+    let sassProcessorPromise: Promise<SassProcessor> | undefined;
+    function initializeSassProcessor(): Promise<SassProcessor> {
+      if (sassProcessorPromise) {
+        return sassProcessorPromise;
+      }
+
+      return (sassProcessorPromise = (async (): Promise<SassProcessor> => {
+        const sassConfigurationJson: ISassConfigurationJson | undefined =
+          await heftConfiguration.tryLoadProjectConfigurationFileAsync(
+            SASS_CONFIGURATION_FILE_SPECIFICATION,
+            terminal
+          );
+
+        const {
+          generatedTsFolder = 'temp/sass-ts',
+          srcFolder = 'src',
+          cssOutputFolders,
+          secondaryGeneratedTsFolders,
+          exportAsDefault = true,
+          fileExtensions,
+          nonModuleFileExtensions,
+          silenceDeprecations
+        } = sassConfigurationJson || {};
+
+        function resolveFolder(folder: string): string {
+          return path.resolve(slashNormalizedBuildFolderPath, folder);
+        }
+
+        const sassProcessorOptions: ISassProcessorOptions = {
+          buildFolder: slashNormalizedBuildFolderPath,
+          concurrency: numberOfCores,
+          dtsOutputFolders: [generatedTsFolder, ...(secondaryGeneratedTsFolders || [])].map(resolveFolder),
+          logger,
+          exportAsDefault,
+          srcFolder: resolveFolder(srcFolder),
+          fileExtensions,
+          nonModuleFileExtensions,
+          cssOutputFolders: cssOutputFolders?.map((folder: string | ICssOutputFolder) => {
+            const folderPath: string = typeof folder === 'string' ? folder : folder.folder;
+            const shimType: 'commonjs' | 'esm' | undefined =
+              typeof folder === 'string' ? undefined : folder.shimType;
+            return {
+              folder: resolveFolder(folderPath),
+              shimType
+            };
+          }),
+          silenceDeprecations
+        };
+
+        const sassProcessor: SassProcessor = new SassProcessor(sassProcessorOptions);
+
+        await sassProcessor.loadCacheAsync(tempFolderPath);
+
+        return sassProcessor;
+      })());
+    }
+
     taskSession.hooks.run.tapPromise(PLUGIN_NAME, async (runOptions: IHeftTaskRunHookOptions) => {
-      await this._runSassTypingsGeneratorAsync(taskSession, heftConfiguration);
+      terminal.writeLine(`Initializing SASS compiler...`);
+      const sassProcessor: SassProcessor = await initializeSassProcessor();
+
+      terminal.writeLine(`Scanning for SCSS files...`);
+      const files: string[] = await runOptions.globAsync(sassProcessor.inputFileGlob, {
+        absolute: true,
+        ignore: sassProcessor.ignoredFileGlobs,
+        cwd: sassProcessor.sourceFolderPath
+      });
+
+      const fileSet: Set<string> = new Set();
+      for (const file of files) {
+        // Using path.resolve to normalize slashes
+        fileSet.add(path.resolve(file));
+      }
+
+      await sassProcessor.compileFilesAsync(fileSet);
+      terminal.writeLine(`Finished compiling.`);
     });
 
     taskSession.hooks.runIncremental.tapPromise(
       PLUGIN_NAME,
-      async (runIncrementalOptions: IHeftTaskRunIncrementalHookOptions) => {
-        await this._runSassTypingsGeneratorAsync(taskSession, heftConfiguration, runIncrementalOptions);
-      }
-    );
-  }
+      async (runOptions: IHeftTaskRunIncrementalHookOptions) => {
+        terminal.writeLine(`Initializing SASS compiler...`);
+        const sassProcessor: SassProcessor = await initializeSassProcessor();
 
-  private async _runSassTypingsGeneratorAsync(
-    taskSession: IHeftTaskSession,
-    heftConfiguration: HeftConfiguration,
-    runIncrementalOptions?: IHeftTaskRunIncrementalHookOptions
-  ): Promise<void> {
-    taskSession.logger.terminal.writeVerboseLine('Starting sass typings generation...');
-    const sassProcessor: SassProcessor = await this._loadSassProcessorAsync(
-      heftConfiguration,
-      taskSession.logger
-    );
-    // If we have the incremental options, use them to determine which files to process.
-    // Otherwise, process all files. The typings generator also provides the file paths
-    // as relative paths from the sourceFolderPath.
-    let changedRelativeFilePaths: string[] | undefined;
-    if (runIncrementalOptions) {
-      changedRelativeFilePaths = [];
-      const relativeFilePaths: Map<string, IWatchedFileState> = await runIncrementalOptions.watchGlobAsync(
-        sassProcessor.inputFileGlob,
-        {
-          cwd: sassProcessor.sourceFolderPath,
-          ignore: Array.from(sassProcessor.ignoredFileGlobs),
-          absolute: false
-        }
-      );
-
-      for (const [relativePath, { changed }] of relativeFilePaths) {
-        if (changed) {
-          changedRelativeFilePaths.push(relativePath);
-        }
-      }
-
-      if (changedRelativeFilePaths.length === 0) {
-        return;
-      }
-    }
-
-    taskSession.logger.terminal.writeLine('Generating sass typings...');
-    await sassProcessor.generateTypingsAsync(changedRelativeFilePaths);
-    taskSession.logger.terminal.writeLine('Generated sass typings');
-  }
-
-  private async _loadSassProcessorAsync(
-    heftConfiguration: HeftConfiguration,
-    logger: IScopedLogger
-  ): Promise<SassProcessor> {
-    if (!this._sassProcessor) {
-      const sassConfiguration: ISassConfiguration = await this._loadSassConfigurationAsync(
-        heftConfiguration,
-        logger
-      );
-      this._sassProcessor = new SassProcessor({
-        sassConfiguration,
-        buildFolder: heftConfiguration.slashNormalizedBuildFolderPath
-      });
-    }
-    return this._sassProcessor;
-  }
-
-  private async _loadSassConfigurationAsync(
-    heftConfiguration: HeftConfiguration,
-    logger: IScopedLogger
-  ): Promise<ISassConfiguration> {
-    if (!this._sassConfiguration) {
-      const sassConfigurationJson: ISassConfigurationJson | undefined =
-        await heftConfiguration.tryLoadProjectConfigurationFileAsync(
-          SASS_CONFIGURATION_FILE_SPECIFICATION,
-          logger.terminal
+        terminal.writeLine(`Scanning for SCSS files...`);
+        const changedFiles: Map<string, IWatchedFileState> = await runOptions.watchGlobAsync(
+          sassProcessor.inputFileGlob,
+          {
+            absolute: true,
+            cwd: sassProcessor.sourceFolderPath,
+            ignore: sassProcessor.ignoredFileGlobs
+          }
         );
 
-      const { slashNormalizedBuildFolderPath } = heftConfiguration;
-      if (sassConfigurationJson) {
-        if (sassConfigurationJson.srcFolder) {
-          sassConfigurationJson.srcFolder = `${slashNormalizedBuildFolderPath}/${sassConfigurationJson.srcFolder}`;
-        }
-
-        if (sassConfigurationJson.generatedTsFolder) {
-          sassConfigurationJson.generatedTsFolder = `${slashNormalizedBuildFolderPath}/${sassConfigurationJson.generatedTsFolder}`;
-        }
-
-        function resolveFolderArray(folders: string[] | undefined): void {
-          if (folders) {
-            for (let i: number = 0; i < folders.length; i++) {
-              folders[i] = `${slashNormalizedBuildFolderPath}/${folders[i]}`;
-            }
+        const modifiedFiles: Set<string> = new Set();
+        for (const [file, { changed }] of changedFiles) {
+          if (changed) {
+            modifiedFiles.add(file);
           }
         }
+        terminal.writeLine(`Compiling ${modifiedFiles.size} changed SCSS files...`);
 
-        resolveFolderArray(sassConfigurationJson.cssOutputFolders);
-        resolveFolderArray(sassConfigurationJson.secondaryGeneratedTsFolders);
+        await sassProcessor.compileFilesAsync(modifiedFiles);
+        terminal.writeLine(`Finished compiling.`);
       }
-
-      // Set defaults if no configuration file or option was found
-      this._sassConfiguration = {
-        srcFolder: `${slashNormalizedBuildFolderPath}/src`,
-        generatedTsFolder: `${slashNormalizedBuildFolderPath}/temp/sass-ts`,
-        exportAsDefault: true,
-        fileExtensions: ['.sass', '.scss', '.css'],
-        importIncludePaths: [
-          `${slashNormalizedBuildFolderPath}/node_modules`,
-          `${slashNormalizedBuildFolderPath}/src`
-        ],
-        ...sassConfigurationJson
-      };
-    }
-
-    return this._sassConfiguration;
+    );
   }
 }
