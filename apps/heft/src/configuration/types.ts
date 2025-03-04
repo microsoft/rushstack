d6b26578112265a620a0709714dd7d04fc665438
David Michon <dmichon-msft@users.noreply.github.com>
2025-03-04 21:11:34 +0000

    [heft] Add `tryLoadProjectConfigurationFileAsync` API

---
+++ apps/heft/src/configuration/types.ts
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+export type {
+  CustomValidationFunction,
+  ICustomJsonPathMetadata,
+  ICustomPropertyInheritance,
+  IJsonPathMetadata,
+  IJsonPathMetadataResolverOptions,
+  IJsonPathsMetadata,
+  INonCustomJsonPathMetadata,
+  IOriginalValueOptions,
+  IProjectConfigurationFileSpecification,
+  IPropertiesInheritance,
+  IPropertyInheritance,
+  IPropertyInheritanceDefaults,
+  InheritanceType,
+  PathResolutionMethod,
+  PropertyInheritanceCustomFunction
+} from '@rushstack/heft-config-file';
