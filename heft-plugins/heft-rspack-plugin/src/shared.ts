f3c1511f5f85e3018691774349f0a65b8c50818f
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-03-24 01:43:50 -0700

    Update to use the new Heft API.

--- heft-plugins/heft-rspack-plugin/src/shared.ts
+++ heft-plugins/heft-rspack-plugin/src/shared.ts
@@ -1,36 +1,121 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import type * as rspack from '@rspack/core';
-import type { IBuildStageProperties, IBundleSubstageProperties } from '@rushstack/heft';
+import type * as TRspack from '@rspack/core';
+import type { HeftConfiguration, IHeftTaskSession } from '@rushstack/heft';
+import { AsyncParallelHook, AsyncSeriesBailHook, AsyncSeriesHook, AsyncSeriesWaterfallHook } from 'tapable';
 
 /**
+ * The environment passed into the Webpack configuration function. Loosely based
+ * on the default Webpack environment options, specified here:
+ * https://webpack.js.org/api/cli/#environment-options
+ *
  * @public
  */
-export type IRspackConfiguration = rspack.Configuration | rspack.Configuration[] | undefined;
-
-/**
- * @public
- */
-export interface IRspackBundleSubstageProperties extends IBundleSubstageProperties {
+export interface IRspackConfigurationFnEnvironment {
   /**
-   * The configuration used by the rspack plugin. This must be populated
-   * for rspack to run. If rspackConfigFilePath is specified,
-   * this will be populated automatically with the exports of the
-   * config file referenced in that property.
+   * Whether or not the run is in production mode. Synonym of
+   * {@link IRspackConfigurationFnEnvironment.production}.
+   */
+  prod: boolean;
+  /**
+   * Whether or not the run is in production mode. Synonym of
+   * {@link IRspackConfigurationFnEnvironment.prod}.
+   */
+  production: boolean;
+
+  // Non-standard environment options
+  /**
+   * The task session provided to the plugin.
+   */
+  taskSession: IHeftTaskSession;
+  /**
+   * The Heft configuration provided to the plugin.
+   */
+  heftConfiguration: HeftConfiguration;
+  /**
+   * The resolved Rspack package.
+   */
+  rspack: typeof TRspack;
+}
+
+/**
+ * @public
+ */
+export type IRspackConfiguration = TRspack.Configuration | TRspack.Configuration[];
+
+/**
+ * @public
+ */
+export interface IRspackPluginAccessorHooks {
+  /**
+   * A hook that allows for loading custom configurations used by the Rspack
+   * plugin. If a tap returns a value other than `undefined` before stage {@link STAGE_LOAD_LOCAL_CONFIG},
+   * it will suppress loading from the Rspack config file. To provide a fallback behavior in the
+   * absence of a local config file, tap this hook with a `stage` value greater than {@link STAGE_LOAD_LOCAL_CONFIG}.
    *
    * @remarks
-   * Tapable event handlers can return `null` instead of `undefined` to suppress
-   * other handlers from creating a configuration object.
+   * Tapable event handlers can return `false` instead of `undefined` to suppress
+   * other handlers from creating a configuration object, and prevent Rspack from running.
    */
-  // We are inheriting this problem from Tapable's API
-  // eslint-disable-next-line @rushstack/no-new-null
-  rspackConfiguration?: rspack.Configuration | rspack.Configuration[] | null;
+  readonly onLoadConfiguration: AsyncSeriesBailHook<never, never, never, IRspackConfiguration | false>;
+  /**
+   * A hook that allows for modification of the loaded configuration used by the Rspack
+   * plugin. If no configuration was loaded, this hook will not be called.
+   */
+  readonly onConfigure: AsyncSeriesHook<IRspackConfiguration, never, never>;
+  /**
+   * A hook that provides the finalized configuration that will be used by Rspack.
+   * If no configuration was loaded, this hook will not be called.
+   */
+  readonly onAfterConfigure: AsyncParallelHook<IRspackConfiguration, never, never>;
+  /**
+   * A hook that provides the stats output from Rspack. If no configuration is loaded,
+   * this hook will not be called.
+   */
+  readonly onEmitStats: AsyncParallelHook<TRspack.Stats | TRspack.MultiStats, never, never>;
+  /**
+   * A hook that allows for customization of the file watcher options. If not running in watch mode, this hook will not be called.
+   */
+  readonly onGetWatchOptions: AsyncSeriesWaterfallHook<
+    Parameters<TRspack.Compiler['watch']>[0],
+    Readonly<IRspackConfiguration>,
+    never
+  >;
 }
 
 /**
  * @public
  */
-export interface IRspackBuildStageProperties extends IBuildStageProperties {
-  rspackStats?: rspack.Stats;
+export interface IRspackPluginAccessorParameters {
+  /**
+   * Whether or not serve mode was enabled by passing the `--serve` flag.
+   */
+  readonly isServeMode: boolean;
 }
+
+/**
+ * @public
+ */
+export interface IRspackPluginAccessor {
+  /**
+   * Hooks that are called at various points in the Webpack plugin lifecycle.
+   */
+  readonly hooks: IRspackPluginAccessorHooks;
+  /**
+   * Parameters that are provided by the Webpack plugin.
+   */
+  readonly parameters: IRspackPluginAccessorParameters;
+}
+
+/**
+ * The stage in the `onLoadConfiguration` hook at which the config will be loaded from the local
+ * webpack config file.
+ * @public
+ */
+export const STAGE_LOAD_LOCAL_CONFIG: 1000 = 1000;
+
+/**
+ * @public
+ */
+export const PLUGIN_NAME: 'webpack5-plugin' = 'webpack5-plugin';
