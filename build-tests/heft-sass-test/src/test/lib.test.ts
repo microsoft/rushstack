139d85c5abc679cbc2b3a978e12ce0d0a9499df7
David Michon <dmichon-msft@users.noreply.github.com>
2025-04-10 22:11:51 +0000

    [sass-test] Add snapshot tests

---
+++ build-tests/heft-sass-test/src/test/lib.test.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import * as path from 'node:path';
+import { validateSnapshots, getScssFiles } from './validateSnapshots';
+
+describe('SASS ESM Shims', () => {
+  const libFolder: string = path.join(__dirname, '../../lib');
+  getScssFiles().forEach((fileName: string) => {
+    it(fileName, () => {
+      validateSnapshots(libFolder, fileName);
+    });
+  });
+});
