71033b93aa4a75f31d3b0d8946b5266a8330091d
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-04-20 17:19:32 -0400

    Fix launching Heft from lib-commonjs.

---
+++ apps/heft/src/root-lib/start.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+// For compatibility globally-installed copies of Heft have the `startWithVersionSelector.js` script that points
+// at 'lib/start.js'
+
+require('../lib-commonjs/start');
