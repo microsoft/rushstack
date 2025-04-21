8a680505273c768d48f5ac549c9c6cf58765a260
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-04-20 20:32:58 -0400

    Fix build in rush-vscode-extension.

--- vscode-extensions/rush-vscode-extension/webpack.config.js
+++ vscode-extensions/rush-vscode-extension/webpack.config.js
@@ -16,7 +16,7 @@ function createExtensionConfig({ production, webpack }) {
     target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
     mode: production ? 'production' : 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-    entry: './lib/extension.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: './lib-esm/extension.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
       // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
       path: path.resolve(__dirname, 'dist'),
