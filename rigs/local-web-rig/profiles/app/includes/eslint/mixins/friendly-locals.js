55af9c1bdc54e757812549844ee68051ddcfd87e
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2025-05-06 12:55:44 -0700

    Undo changes to web rig

--- rigs/local-web-rig/profiles/app/includes/eslint/mixins/friendly-locals.js
+++ rigs/local-web-rig/profiles/app/includes/eslint/mixins/friendly-locals.js
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-const { defineConfig } = require('eslint/config');
-const friendlyLocalsConfig = require('local-eslint-config/mixins/friendly-locals');
-
-module.exports = defineConfig([...friendlyLocalsConfig]);
+module.exports = {
+  extends: ['local-eslint-config/mixins/friendly-locals']
+};
--- rigs/local-web-rig/profiles/library/includes/eslint/profile/web-app.js
+++ rigs/local-web-rig/profiles/library/includes/eslint/profile/web-app.js
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-const { defineConfig } = require('eslint/config');
-const webAppConfig = require('local-eslint-config/profile/web-app');
-
-module.exports = defineConfig([...webAppConfig]);
+module.exports = {
+  extends: ['local-eslint-config/profile/web-app']
+};
