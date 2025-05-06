3ec636a0b6a55fd434df44950b1e0dc0ad6f271a
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2025-05-06 12:56:13 -0700

    Add flat config versions to the web rig

---
+++ rigs/local-web-rig/profiles/app/includes/eslint/flat/profile/web-app.js
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+const { defineConfig } = require('eslint/config');
+const webAppProfile = require('local-eslint-config/flat/profile/web-app');
+
+module.exports = defineConfig([...webAppProfile]);
