b069e39a13736345751aa7a91e729dd7eec1f037
Daniel Nadeau <3473356+D4N14L@users.noreply.github.com>
2025-05-06 18:02:12 -0700

    Update more projects

---
+++ build-tests/rush-lib-declaration-paths-test/eslint.config.js
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+const { defineConfig } = require('eslint/config');
+const nodeTrustedToolProfile = require('local-node-rig/profiles/default/includes/eslint/flat/profile/node-trusted-tool');
+const friendlyLocalsMixin = require('local-node-rig/profiles/default/includes/eslint/flat/mixins/friendly-locals');
+
+module.exports = defineConfig([
+  ...nodeTrustedToolProfile,
+  ...friendlyLocalsMixin,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname
+      }
+    },
+    rules: {
+      // This project contains only unshipped generated TS code which doesn't contain the copyright header.
+      'header/header': 'off'
+    }
+  }
+]);
