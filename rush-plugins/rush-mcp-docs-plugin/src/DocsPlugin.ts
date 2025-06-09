d032f0c33b74ba5ce0a193eaee8a94029cc19ec2
Lincoln <778157949@qq.com>
2025-06-09 13:00:50 +0000

    feat: rush docs plugin

---
+++ rush-plugins/rush-mcp-docs-plugin/src/DocsPlugin.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+import type { IRushMcpPlugin, RushMcpPluginSession } from '@rushstack/mcp-server';
+import { DocsTool } from './DocsTool';
+
+export interface IDocsPluginConfigFile {}
+
+export class DocsPlugin implements IRushMcpPlugin {
+  public session: RushMcpPluginSession;
+  public configFile: IDocsPluginConfigFile | undefined = undefined;
+
+  public constructor(session: RushMcpPluginSession, configFile: IDocsPluginConfigFile | undefined) {
+    this.session = session;
+    this.configFile = configFile;
+  }
+
+  public async onInitializeAsync(): Promise<void> {
+    this.session.registerTool(
+      {
+        toolName: 'rush_docs',
+        description:
+          'Search and retrieve relevant sections from the official Rush documentation based on user queries.'
+      },
+      new DocsTool(this)
+    );
+  }
+}
--- rush-plugins/rush-mcp-docs-plugin/src/DocsTool.ts
+++ rush-plugins/rush-mcp-docs-plugin/src/DocsTool.ts
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import { z } from 'zod';
+import type { IRushMcpTool, RushMcpPluginSession, CallToolResult, zodModule } from '@rushstack/mcp-server';
 import { JsonFile } from '@rushstack/node-core-library';
 import path from 'path';
 
-import { BaseTool, type CallToolResult } from './base.tool';
+import type { DocsPlugin } from './DocsPlugin';
 
 interface IDocsResult {
   query: string;
@@ -17,15 +17,22 @@ interface IDocsResult {
   searchTimeMs: number;
 }
 
-export class RushDocsTool extends BaseTool {
-  public constructor() {
-    super({
-      name: 'rush_docs',
-      description:
-        'Search and retrieve relevant sections from the official Rush documentation based on user queries.',
-      schema: {
-        userQuery: z.string().describe('The user query to search for relevant documentation sections.')
-      }
+export class DocsTool implements IRushMcpTool<DocsTool['schema']> {
+  public readonly plugin: DocsPlugin;
+  public readonly session: RushMcpPluginSession;
+
+  public constructor(plugin: DocsPlugin) {
+    this.plugin = plugin;
+    this.session = plugin.session;
+  }
+
+  // ZOD relies on type inference generate a messy expression in the .d.ts file
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  public get schema() {
+    const zod: typeof zodModule = this.session.zod;
+
+    return zod.object({
+      userQuery: zod.string().describe('The user query to search for relevant documentation sections.')
     });
   }
 
@@ -34,7 +41,7 @@ export class RushDocsTool extends BaseTool {
     const startTime: number = Date.now();
 
     const results: IDocsResult['results'] = JsonFile.load(
-      path.join(__dirname, '../rush-doc-fragment.mock.json')
+      path.join(__dirname, './rush-doc-fragment.mock.json')
     );
 
     return {
@@ -45,7 +52,7 @@ export class RushDocsTool extends BaseTool {
     };
   }
 
-  public async executeAsync({ userQuery }: { userQuery: string }): Promise<CallToolResult> {
+  public async executeAsync({ userQuery }: zodModule.infer<DocsTool['schema']>): Promise<CallToolResult> {
     const docSearchResult: IDocsResult = this._searchDocs(userQuery);
 
     return {
