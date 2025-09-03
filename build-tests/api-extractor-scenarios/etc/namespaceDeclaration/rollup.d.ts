488cc95ea5428d4cc59abc4efe564b9a8165eb47
adventure-yunfei <adventure-yunfei@users.noreply.github.com>
2025-09-03 13:55:14 +0800

    add namespace test case

---
+++ build-tests/api-extractor-scenarios/etc/namespaceDeclaration/rollup.d.ts
@@ -0,0 +1,26 @@
+export declare namespace AllExportedNS {
+    class ExportedClass {
+    }
+    interface ExportedInterface {
+    }
+}
+
+export declare interface FinalRenamedClass {
+}
+
+export declare namespace PartalExportedNS {
+    export const var1 = 1, var2 = 2;
+    interface UnexportedClass {
+    }
+    export interface ExportedInterface {
+        prop: UnexportedClass;
+    }
+    export {};
+}
+
+export declare namespace ReexportNS {
+    export { FinalRenamedClass as RenamedClass };
+    export { FinalRenamedClass as RenamedClass3 };
+}
+
+export { }
---
+++ build-tests/api-extractor-scenarios/src/namespaceDeclaration/index.ts
@@ -0,0 +1,22 @@
+export namespace PartalExportedNS {
+  export const var1 = 1,
+    var2 = 2;
+  interface UnexportedClass {}
+  export interface ExportedInterface {
+    prop: UnexportedClass;
+  }
+}
+
+export namespace AllExportedNS {
+  export class ExportedClass {}
+  export interface ExportedInterface {}
+}
+
+export declare namespace ReexportNS {
+  export { RenamedClass };
+  export { RenamedClass as RenamedClass3 };
+}
+
+interface RenamedClass {}
+
+export { RenamedClass as FinalRenamedClass };
