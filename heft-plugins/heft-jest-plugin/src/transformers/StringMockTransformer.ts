ed3f1c26dd87347d49f3b3d4c02066c35e0a45ce
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-10-02 13:45:33 -0700

    Fix header issues.

--- heft-plugins/heft-jest-plugin/src/transformers/StringMockTransformer.ts
+++ heft-plugins/heft-jest-plugin/src/transformers/StringMockTransformer.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
+
 import { relative } from 'node:path';
 import type { SyncTransformer, TransformedSource, TransformOptions } from '@jest/transform';
 
--- heft-plugins/heft-lint-plugin/src/SarifFormatter.ts
+++ heft-plugins/heft-lint-plugin/src/SarifFormatter.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
+
 import type * as TEslint from 'eslint';
 import type * as TEslintLegacy from 'eslint-8';
 import path from 'node:path';
