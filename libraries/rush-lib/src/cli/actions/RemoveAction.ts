0c79862a3f63cfd8616831b02afcbc1b93ba1e6c
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-10-07 18:04:50 +0000

    Clean up the add and remove actions.

--- libraries/rush-lib/src/cli/actions/RemoveAction.ts
+++ libraries/rush-lib/src/cli/actions/RemoveAction.ts
@@ -1,53 +1,35 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import type {
-  CommandLineFlagParameter,
-  CommandLineStringListParameter,
-  CommandLineStringParameter
-} from '@rushstack/ts-command-line';
-
-import { BaseAddAndRemoveAction } from './BaseAddAndRemoveAction';
+import { BaseAddAndRemoveAction, PACKAGE_PARAMETER_NAME } from './BaseAddAndRemoveAction';
 import type { RushCommandLineParser } from '../RushCommandLineParser';
 import type { RushConfigurationProject } from '../../api/RushConfigurationProject';
 import type {
   IPackageForRushRemove,
   IPackageJsonUpdaterRushRemoveOptions
 } from '../../logic/PackageJsonUpdaterTypes';
-import { getVariantAsync, VARIANT_PARAMETER } from '../../api/Variants';
+import { getVariantAsync } from '../../api/Variants';
+
+const REMOVE_ACTION_NAME: 'remove' = 'remove';
 
 export class RemoveAction extends BaseAddAndRemoveAction {
-  protected readonly _allFlag: CommandLineFlagParameter;
-  protected readonly _packageNameList: CommandLineStringListParameter;
-  private readonly _variantParameter: CommandLineStringParameter;
-
   public constructor(parser: RushCommandLineParser) {
     const documentation: string = [
       'Removes specified package(s) from the dependencies of the current project (as determined by the current working directory)' +
         ' and then runs "rush update".'
     ].join('\n');
     super({
-      actionName: 'remove',
+      actionName: REMOVE_ACTION_NAME,
       summary: 'Removes one or more dependencies from the package.json and runs rush update.',
       documentation,
       safeForSimultaneousRushProcesses: false,
-      parser
-    });
+      parser,
 
-    this._packageNameList = this.defineStringListParameter({
-      parameterLongName: '--package',
-      parameterShortName: '-p',
-      required: true,
-      argumentName: 'PACKAGE',
-      description:
+      packageNameListParameterDescription:
         'The name of the package which should be removed.' +
-        ' To remove multiple packages, run "rush remove --package foo --package bar".'
+        ` To remove multiple packages, run "rush ${REMOVE_ACTION_NAME} ${PACKAGE_PARAMETER_NAME} foo ${PACKAGE_PARAMETER_NAME} bar".`,
+      allFlagDescription: 'If specified, the dependency will be removed from all projects that declare it.'
     });
-    this._allFlag = this.defineFlagParameter({
-      parameterLongName: '--all',
-      description: 'If specified, the dependency will be removed from all projects that declare it.'
-    });
-    this._variantParameter = this.defineStringParameter(VARIANT_PARAMETER);
   }
 
   public async getUpdateOptionsAsync(): Promise<IPackageJsonUpdaterRushRemoveOptions> {
