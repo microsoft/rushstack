7c45a7ab4e75e1a2ca5c741ebb92da6614b47e86
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2025-12-11 01:03:03 -0800

    Fix lint issues.

--- libraries/rush-pnpm-kit-v10/src/index.ts
+++ libraries/rush-pnpm-kit-v10/src/index.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 export * as dependencyPath from './dependencyPath';
 export * as lockfileFs from './lockfileFs';
 export * as logger from './logger';
--- libraries/rush-pnpm-kit-v9/src/dependencyPath.ts
+++ libraries/rush-pnpm-kit-v9/src/dependencyPath.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 import { depPathToFilename, indexOfPeersSuffix, parse, removeSuffix } from '@pnpm/dependency-path-pnpm-v9';
 import type { DependencyPath } from '@pnpm/dependency-path-pnpm-v9';
 
