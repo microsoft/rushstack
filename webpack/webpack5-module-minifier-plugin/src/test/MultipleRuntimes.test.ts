30e32b2d9ed9fdc12eaf8b162b9d63aa78dae723
Yashwanth Reddy Verupaka <yashwanth195@users.noreply.github.com>
2025-12-17 22:26:50 +0000

    updated snapshots

--- webpack/webpack5-module-minifier-plugin/src/test/MultipleRuntimes.test.ts
+++ webpack/webpack5-module-minifier-plugin/src/test/MultipleRuntimes.test.ts
@@ -19,8 +19,9 @@ async function multipleRuntimesTest(minifier: IModuleMinifier): Promise<void> {
   memoryFileSystem.fromJSON(
     {
       '/package.json': '{}',
-      '/entry-1.js': `// A comment\nconsole.log("Do stuff");\nimport(/* webpackChunkName: 'async-1' */ /* webpackExports: ["async1"] */ './async.js').then(mod => mod.async1());\nimport(/* webpackChunkName: 'async-1' */ './async-1.js').then(mod => mod.async1());`,
-      '/entry-2.js': `// A comment\nconsole.log("Do stuff");\nimport(/* webpackChunkName: 'async-2' */ /* webpackExports: ["a2"] */ './async.js').then(mod => mod.a2());\nimport(/* webpackChunkName: 'async-2' */ './async-2.js').then(mod => mod.a2());`,
+      // Removed unnecessary webpackExports magic comments from dynamic imports
+      '/entry-1.js': `// A comment\nconsole.log("Do stuff");\nimport(/* webpackChunkName: 'async-1' */ './async.js').then(mod => mod.async1());\nimport(/* webpackChunkName: 'async-1' */ './async-1.js').then(mod => mod.async1());`,
+      '/entry-2.js': `// A comment\nconsole.log("Do stuff");\nimport(/* webpackChunkName: 'async-2' */ './async.js').then(mod => mod.a2());\nimport(/* webpackChunkName: 'async-2' */ './async-2.js').then(mod => mod.a2());`,
       '/async.js': `// @license MIT\nexport { async1 } from './async-1';\nexport { a2 } from './async-2';`,
       '/async-1.js': `// @license BAR\nexport function async1() { console.log('async-1'); }`,
       '/async-2.js': `// @license BAZ\nexport function a2() { console.log('async-2'); }`
