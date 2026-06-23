1047d9fe26efda18a6f51c6045e7f589144d0f4c
chenzhelong.sirius <EscapeB@users.noreply.github.com>
2026-06-23 13:32:25 +0800

    chore: split package manager install lock fix

--- libraries/rush-lib/src/logic/test/InstallHelpers.test.ts
+++ libraries/rush-lib/src/logic/test/InstallHelpers.test.ts
@@ -1,17 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-import * as path from 'node:path';
-
-import { FileSystem, type IPackageJson, JsonFile, LockFile } from '@rushstack/node-core-library';
+import { type IPackageJson, JsonFile } from '@rushstack/node-core-library';
 import { StringBufferTerminalProvider, Terminal } from '@rushstack/terminal';
 import { TestUtilities } from '@rushstack/heft-config-file';
 
 import { InstallHelpers } from '../installManager/InstallHelpers';
 import { RushConfiguration } from '../../api/RushConfiguration';
-import { LastInstallFlag } from '../../api/LastInstallFlag';
-import type { RushGlobalFolder } from '../../api/RushGlobalFolder';
-import { Utilities } from '../../utilities/Utilities';
 
 describe('InstallHelpers', () => {
   describe('generateCommonPackageJson', () => {
@@ -76,73 +71,6 @@ describe('InstallHelpers', () => {
           }
         })
       );
-    });
-  });
-
-  describe(InstallHelpers.ensureLocalPackageManagerAsync.name, () => {
-    const tempFolderPath: string = `${__dirname}/temp/${InstallHelpers.name}`;
-
-    beforeEach(() => {
-      FileSystem.ensureEmptyFolder(tempFolderPath);
-    });
-
-    afterEach(() => {
-      FileSystem.deleteFolder(tempFolderPath);
-      jest.restoreAllMocks();
-    });
-
-    it('does not acquire the global lock when the package manager is already installed', async () => {
-      const rushGlobalFolder: RushGlobalFolder = {
-        path: `${tempFolderPath}/rush-global`,
-        nodeSpecificPath: `${tempFolderPath}/rush-global/node-${process.version}`
-      } as RushGlobalFolder;
-      const packageManagerToolFolder: string = path.join(rushGlobalFolder.nodeSpecificPath, 'pnpm-10.27.0');
-      await new LastInstallFlag(packageManagerToolFolder, { node: process.versions.node }).createAsync();
-
-      const lockAcquireSpy: jest.SpyInstance = jest.spyOn(LockFile, 'acquireAsync');
-      const rushConfiguration: RushConfiguration = {
-        commonRushConfigFolder: `${tempFolderPath}/common/config/rush`,
-        commonTempFolder: `${tempFolderPath}/common/temp`,
-        packageManager: 'pnpm',
-        packageManagerToolVersion: '10.27.0'
-      } as RushConfiguration;
-
-      await InstallHelpers.ensureLocalPackageManagerAsync(rushConfiguration, rushGlobalFolder, 1, true);
-
-      expect(lockAcquireSpy).not.toHaveBeenCalled();
-      expect(FileSystem.exists(`${rushConfiguration.commonTempFolder}/pnpm-local`)).toEqual(true);
-    });
-
-    it('acquires the global lock if an install lock file is present', async () => {
-      const rushGlobalFolder: RushGlobalFolder = {
-        path: `${tempFolderPath}/rush-global`,
-        nodeSpecificPath: `${tempFolderPath}/rush-global/node-${process.version}`
-      } as RushGlobalFolder;
-      const packageManagerToolFolder: string = path.join(rushGlobalFolder.nodeSpecificPath, 'pnpm-10.27.0');
-      await new LastInstallFlag(packageManagerToolFolder, { node: process.versions.node }).createAsync();
-      FileSystem.writeFile(`${rushGlobalFolder.nodeSpecificPath}/pnpm-10.27.0#123.lock`, '');
-
-      const releaseAsync: jest.Mock = jest.fn();
-      const lockAcquireSpy: jest.SpyInstance = jest.spyOn(LockFile, 'acquireAsync').mockResolvedValue({
-        dirtyWhenAcquired: true,
-        release: releaseAsync
-      } as unknown as LockFile);
-      const installSpy: jest.SpyInstance = jest
-        .spyOn(Utilities, 'installPackageInDirectoryAsync')
-        .mockResolvedValue();
-      const rushConfiguration: RushConfiguration = {
-        commonRushConfigFolder: `${tempFolderPath}/common/config/rush`,
-        commonTempFolder: `${tempFolderPath}/common/temp`,
-        packageManager: 'pnpm',
-        packageManagerToolVersion: '10.27.0'
-      } as RushConfiguration;
-
-      await InstallHelpers.ensureLocalPackageManagerAsync(rushConfiguration, rushGlobalFolder, 1, true);
-
-      expect(lockAcquireSpy).toHaveBeenCalledTimes(1);
-      expect(installSpy).toHaveBeenCalledTimes(1);
-      expect(releaseAsync).toHaveBeenCalledTimes(1);
-      expect(FileSystem.exists(`${rushConfiguration.commonTempFolder}/pnpm-local`)).toEqual(true);
     });
   });
 });
