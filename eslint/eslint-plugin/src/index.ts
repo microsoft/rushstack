fa4b86031c3be6a1317b69d2aa03bad4570fd8ba
Ian Clanton-Thuon <iclanton@users.noreply.github.com>
2026-09-14 12:57:06 -0400

    fixup! Clean up types.

--- eslint/eslint-plugin/src/index.ts
+++ eslint/eslint-plugin/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import type { ESLint } from 'eslint';
 import type { TSESLint } from '@typescript-eslint/utils';
 
 import { hoistJestMock } from './hoist-jest-mock';
@@ -60,4 +61,4 @@ const plugin: IPlugin = {
 // structurally assignable to ESLint's `RuleDefinition`. Widen through `object` (rather than `unknown`) to
 // present the plugin as an `ESLint.Plugin` so that consumers (such as flat-config `plugins` maps) can
 // reference it without a cast of their own.
-export = plugin as unknown as IPlugin;
+export = plugin as unknown as ESLint.Plugin;
